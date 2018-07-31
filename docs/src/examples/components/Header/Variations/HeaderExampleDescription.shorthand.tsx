8f398deb7309d8f9072e4a36b38132bb21727b9f
manajdov <manajdov@microsoft.com>
2018-07-31 10:20:03 +0200

    -changed header example description

--- docs/src/examples/components/Header/Variations/HeaderExampleDescription.shorthand.tsx
+++ docs/src/examples/components/Header/Variations/HeaderExampleDescription.shorthand.tsx
@@ -5,7 +5,7 @@ const HeaderExampleDescriptionShorthand = () => (
   <Header
     as="h2"
     content="Account Settings"
-    description={{ content: 'Manage your account settings and set email preferences' }}
+    description="Manage your account settings and set email preferences"
   />
 )
 
