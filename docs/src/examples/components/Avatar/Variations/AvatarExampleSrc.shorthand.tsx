0fc3605eea7d3593f9bfb0df8ba7ea16406b3945
manajdov <manajdov@microsoft.com>
2018-08-01 14:38:24 +0200

    -reverted changes in the avatar examples

--- docs/src/examples/components/Avatar/Variations/AvatarExampleSrc.shorthand.tsx
+++ docs/src/examples/components/Avatar/Variations/AvatarExampleSrc.shorthand.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Avatar } from '@stardust-ui/react'
 
-const AvatarExampleSrcShorthand = () => <Avatar src="public/images/avatar/small/matt.jpg" />
+const AvatarExampleSrcShorthand = () => <Avatar src="/public/images/avatar/small/matt.jpg" />
 
 export default AvatarExampleSrcShorthand
