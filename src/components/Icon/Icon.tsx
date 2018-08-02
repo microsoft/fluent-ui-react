941ea1fac4975a237c5bdecbb2ff6a9a2ecc06a6
manajdov <manajdov@microsoft.com>
2018-08-02 13:14:48 +0200

    -removed interfaces for the props in the components

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,46 +1,13 @@
-import React, { CSSProperties } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { customPropTypes, UIComponent, SUI } from '../../lib'
 
 import iconRules from './iconRules'
 import iconVariables from './iconVariables'
 
-export type IconColor =
-  | 'white'
-  | 'red'
-  | 'orange'
-  | 'yellow'
-  | 'olive'
-  | 'green'
-  | 'teal'
-  | 'blue'
-  | 'violet'
-  | 'purple'
-  | 'pink'
-  | 'brown'
-  | 'grey'
-  | 'black'
-
-export type IconSize = 'mini' | 'tiny' | 'small' | 'large' | 'big' | 'huge' | 'massive'
-
 export type IconXSpacing = 'none' | 'before' | 'after' | 'both'
 
-export interface IconProps {
-  as?: string
-  bordered?: boolean
-  circular?: boolean
-  className?: string
-  color?: IconColor
-  disabled?: boolean
-  kind?: string
-  name?: string
-  size?: IconSize
-  xSpacing?: IconXSpacing
-  style?: CSSProperties
-  title?: string
-}
-
-class Icon extends UIComponent<IconProps, {}> {
+class Icon extends UIComponent<any, any> {
   static className = 'ui-icon'
 
   static displayName = 'Icon'
