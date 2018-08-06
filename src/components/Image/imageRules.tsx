d24fed656ca56e397589312e2377a767aa610159
kuzhelov <kuroman@microsoft.com>
2018-08-07 01:47:31 +0200

    introduce support for resizing Image

--- src/components/Image/imageRules.tsx
+++ src/components/Image/imageRules.tsx
@@ -4,6 +4,8 @@ export default {
   root: ({ props, variables }) => ({
     display: 'inline-block',
     verticalAlign: 'middle',
+    width: variables.width,
+    height: variables.height || 'auto',
     ...(props.circular && { borderRadius: pxToRem(9999) }),
     ...(props.avatar && {
       width: variables.avatarSize,
