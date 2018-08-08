ed788c0e9a48f672d8d703bf907dfd94ea87e999
manajdov <manajdov@microsoft.com>
2018-08-08 14:21:20 +0200

    -added changelog and fixed examples

--- docs/src/examples/components/ItemLayout/Content/ItemLayoutExampleHeaderMedia.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { ItemLayout } from '@stardust-ui/react'
-
-const ListExample = () => <ItemLayout header="Irving Kuhic" headerMedia="7:26:56 AM" />
-
-export default ListExample
