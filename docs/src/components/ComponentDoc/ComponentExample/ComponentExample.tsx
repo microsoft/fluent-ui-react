b5e92cf1a9c29374ca4ec9e447e180c74fa83c50
Levi Thomason <me@levithomason.com>
2018-08-09 00:03:17 -0700

    docs: cleanup examples (#69)
    * fix: copy-to-clipboard imports
    
    * docs: fix Label example name and UX
    
    * docs: add missing description periods
    
    * docs: remove non-existent Menu fitted prop
    
    * refactor: improve ComponentExample method name
    
    * docs: fix Sidebar active link state
    
    * docs: fix Header example name
    
    * docs: fix List example name
    
    * docs: cleanup Label icon shorthand examples

--- docs/src/components/ComponentDoc/ComponentExample/ComponentExample.tsx
+++ docs/src/components/ComponentDoc/ComponentExample/ComponentExample.tsx
@@ -4,7 +4,7 @@ import * as React from 'react'
 import { withRouter, RouteComponentProps } from 'react-router'
 import { renderToStaticMarkup } from 'react-dom/server'
 import { html } from 'js-beautify'
-import copyToClipboard from 'copy-to-clipboard'
+import * as copyToClipboard from 'copy-to-clipboard'
 import { Divider, Form, Grid, Menu, Segment, Visibility, SemanticCOLORS } from 'semantic-ui-react'
 import { Provider } from '@stardust-ui/react'
 
@@ -346,7 +346,7 @@ class ComponentExample extends React.PureComponent<IComponentExampleProps, IComp
     return Knobs ? <Knobs {...this.getKnobsValue()} onKnobChange={this.handleKnobChange} /> : null
   }
 
-  private getComponentName = () => this.props.examplePath.split('/')[1]
+  private getDisplayName = () => this.props.examplePath.split('/')[1]
 
   private renderWithProvider(ExampleComponent) {
     return (
@@ -526,7 +526,7 @@ class ComponentExample extends React.PureComponent<IComponentExampleProps, IComp
     const { showVariables } = this.state
     if (!showVariables) return
 
-    const name = this.getComponentName()
+    const displayName = this.getDisplayName()
 
     return (
       <div>
@@ -535,7 +535,7 @@ class ComponentExample extends React.PureComponent<IComponentExampleProps, IComp
         </Divider>
         <Provider.Consumer
           render={({ siteVariables }) => {
-            const variablesFilename = `./${name}/${_.camelCase(name)}Variables.ts`
+            const variablesFilename = `./${displayName}/${_.camelCase(displayName)}Variables.ts`
             const hasVariablesFile = _.includes(variablesContext.keys(), variablesFilename)
 
             if (!hasVariablesFile) {
@@ -558,7 +558,7 @@ class ComponentExample extends React.PureComponent<IComponentExampleProps, IComp
                       key={key}
                       label={key}
                       defaultValue={val}
-                      onChange={this.handleVariableChange(name, key)}
+                      onChange={this.handleVariableChange(displayName, key)}
                     />
                   ))}
                 </Form.Group>
