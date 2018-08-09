1f80c2f9b24746c55472915d465807642deb2f85
Levi Thomason <me@levithomason.com>
2018-08-08 23:29:49 -0700

    docs: add missing description periods

--- docs/src/examples/components/Label/Variations/index.tsx
+++ docs/src/examples/components/Label/Variations/index.tsx
@@ -6,27 +6,27 @@ const Variations = () => (
   <ExampleSection title="Variations">
     <ComponentExample
       title="Circular"
-      description="A label can be circular"
+      description="A label can be circular."
       examplePath="components/Label/Variations/LabelExampleCircular"
     />
     <ComponentExample
       title="Icon"
-      description="The label can contain an icon"
+      description="The label can contain an icon."
       examplePath="components/Label/Variations/LabelExampleIcon"
     />
     <ComponentExample
       title="Icon position"
-      description="The icon inside the label can be aligned before or after the content"
+      description="The icon inside the label can be aligned before or after the content."
       examplePath="components/Label/Variations/LabelExampleIconPosition"
     />
     <ComponentExample
       title="Clickable icon"
-      description="The icon inside the label can be clickable"
+      description="The icon inside the label can be clickable."
       examplePath="components/Label/Variations/LabelExampleOnIconClick"
     />
     <ComponentExample
       title="The Icon inside Label can be customized"
-      description="The Icon component inside the Label can be defined with customizing it's prop"
+      description="The Icon component inside the Label can be defined with customizing it's prop."
       examplePath="components/Label/Variations/LabelExampleIconAsShorthand"
     />
   </ExampleSection>
