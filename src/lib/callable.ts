48c5b96e2dbed24c757828e5e9cd67839943d854
Levi Thomason <me@levithomason.com>
2018-08-09 11:00:47 -0700

    refactor: add perf link to callable

--- src/lib/callable.ts
+++ src/lib/callable.ts
@@ -1,3 +1,4 @@
+// https://jsperf.com/startdust-callable
 const callable = (possibleFunction: any) => (...args: any[]) => {
   return typeof possibleFunction === 'function' ? possibleFunction(...args) : possibleFunction
 }
