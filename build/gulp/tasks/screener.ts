9e5b8177d09fcc7b69f02d97f9b76a61ba2cd5d4
Levi Thomason <me@levithomason.com>
2018-08-09 23:38:25 -0700

    chore: setup visual testing

---
+++ build/gulp/tasks/screener.ts
@@ -0,0 +1,27 @@
+import { task, parallel, series } from 'gulp'
+
+import sh from '../sh'
+import config from '../../../config'
+
+const { paths } = config
+
+// ----------------------------------------
+// Visual
+// ----------------------------------------
+
+task('screener:runner', cb => {
+  sh(`screener-runner --conf ${paths.base('screener.config.js')}`, err => {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    } else {
+      process.exit(0)
+    }
+  })
+})
+
+// ----------------------------------------
+// Default
+// ----------------------------------------
+
+task('screener', series('serve:docs', 'screener:runner'))
---
+++ screener.config.js
@@ -0,0 +1,38 @@
+const _ = require('lodash')
+const glob = require('glob')
+const path = require('path')
+
+module.exports = {
+  projectRepo: 'stardust-ui/react',
+
+  apiKey: process.env.SCREENER_API_KEY,
+
+  tunnel: {
+    host: 'localhost:8080',
+    gzip: true, // gzip compress all content being served from tunnel host
+    cache: true, // sets cache-control header for all content being served from tunnel host. Must be used with gzip option
+  },
+
+  diffOptions: {
+    structure: true,
+    layout: true,
+    style: true,
+    content: true,
+    minLayoutPosition: 1, // Optional threshold for Layout changes. Defaults to 4 pixels.
+    minLayoutDimension: 1, // Optional threshold for Layout changes. Defaults to 10 pixels.
+    minShiftGraphic: 1, // Optional threshold for pixel shifts in graphics.
+    compareSVGDOM: false, // Pass if SVG DOM is the same. Defaults to false.
+  },
+
+  // screenshot every example in maximized mode
+  states: glob
+    .sync('docs/src/examples/**/*.tsx', { ignore: ['**/index.tsx', '**/*.knobs.tsx'] })
+    .map(examplePath => {
+      const { name: nameWithoutExtension, base: nameWithExtension } = path.parse(examplePath)
+
+      return {
+        url: `http://localhost:8080/maximize/${_.kebabCase(nameWithoutExtension)}`,
+        name: nameWithExtension,
+      }
+    }),
+}
