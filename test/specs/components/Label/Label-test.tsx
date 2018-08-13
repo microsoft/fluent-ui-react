4ca94e66c28f48a5b4662d798e946044360a40aa
manajdov <manajdov@microsoft.com>
2018-08-13 12:16:36 +0200

    -added test for handling the tabIndex property set to the icon with onClick handler

--- test/specs/components/Label/Label-test.tsx
+++ test/specs/components/Label/Label-test.tsx
@@ -2,7 +2,18 @@ import * as React from 'react'
 import { isConformant } from 'test/specs/commonTests'
 
 import Label from 'src/components/Label/Label'
+import { mountWithProvider } from '../../../utils'
 
 describe('Label', () => {
   isConformant(Label)
+
+  describe('icon', () => {
+    it('sets tabIndex="0" if the icon has onClick prop', () => {
+      const icon = mountWithProvider(
+        <Label icon={{ name: 'close', key: 'icon-key', onClick: () => {} }} />,
+      ).find('Icon[name="close"]')
+
+      expect(icon.prop('tabIndex')).toEqual('0')
+    })
+  })
 })
