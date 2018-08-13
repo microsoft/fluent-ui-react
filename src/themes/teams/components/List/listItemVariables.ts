243e8e2623132c58b509b56002765b76d006f47b
manajdov <manajdov@microsoft.com>
2018-08-13 13:46:21 +0200

    -removed unused variables from the list item variables

--- src/themes/teams/components/List/listItemVariables.ts
+++ src/themes/teams/components/List/listItemVariables.ts
@@ -1,12 +1,4 @@
-import { pxToRem } from '../../../../lib'
-
 export default siteVariables => ({
-  paddingLeft: pxToRem(20),
-  paddingRight: pxToRem(18),
-  columnGap: pxToRem(8),
-
-  height: pxToRem(48),
-
   // Header
   // TODO: prod app uses 17.5px here, it should be 16px per the design guide!
   headerLineHeight: siteVariables.lineHeightSmall,
