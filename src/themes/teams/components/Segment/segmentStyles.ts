f5b35af933c4c125ea6ff3add403cd044e5c588d
kuzhelov <deckster@mail.ru>
2018-08-21 12:33:48 +0200

    feat(Segment): base implementation (#103)
    * introduce base Segment component
    
    * changelog
    
    * remove unnecessary tyoe definition

---
+++ src/themes/teams/components/Segment/segmentStyles.ts
@@ -0,0 +1,13 @@
+import { pxToRem } from '../../../../lib'
+import { ICSSInJSStyle } from '../../../../../types/theme'
+
+export default {
+  root: (): ICSSInJSStyle => {
+    return {
+      padding: '1em',
+      boxShadow: '0 1px 2px 0 rgba(34,36,38,.15)',
+      border: '1px solid rgba(34,36,38,.15)',
+      borderRadius: pxToRem(5),
+    }
+  },
+}
