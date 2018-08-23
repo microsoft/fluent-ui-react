afa6847d81be1a7c19ef0e4505163e77a9fc058d
Alexandru Buliga <bugaaa92@gmail.com>
2018-08-23 12:28:48 +0200

    feat(portal): simple base implementation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -29,6 +29,7 @@ This project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.htm
 - Add `descriptionColor` to Header @kuzhelov ([#78](https://github.com/stardust-ui/react/pull/78))
 - Add accessibility behavior description @kolaps33 ([#74](https://github.com/stardust-ui/react/pull/74))
 - Add strict null checks for generated TS types @smykhailov ([#108](https://github.com/stardust-ui/react/pull/108))
+- Add `Portal`, `PortalInner` and `Ref` components base implementation @Bugaa92 ([#144](https://github.com/stardust-ui/react/pull/144))
 
 <!--------------------------------[ v0.3.0 ]------------------------------- -->
 ## [v0.3.0](https://github.com/stardust-ui/react/tree/v0.3.0) (2018-08-22)
---
+++ src/components/Ref/Ref.tsx
@@ -0,0 +1,35 @@
+import * as PropTypes from 'prop-types'
+import { invoke } from 'lodash'
+import { Children, Component } from 'react'
+import { findDOMNode } from 'react-dom'
+import { Extendable } from '../../../types/utils'
+
+export interface IRefProps {
+  innerRef?: (ref: HTMLElement) => void
+}
+
+/**
+ * This component exposes a callback prop that always returns the DOM node of both functional and class component
+ * children.
+ */
+export default class Ref extends Component<Extendable<IRefProps>, any> {
+  static propTypes = {
+    /** Primary content. */
+    children: PropTypes.element,
+
+    /**
+     * Called when componentDidMount.
+     *
+     * @param {HTMLElement} node - Referred node.
+     */
+    innerRef: PropTypes.func,
+  }
+
+  componentDidMount() {
+    invoke(this.props, 'innerRef', findDOMNode(this))
+  }
+
+  render() {
+    return Children.only(this.props.children)
+  }
+}
