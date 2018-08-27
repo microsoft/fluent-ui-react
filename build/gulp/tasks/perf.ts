bae680e39083608634bdfb8f99a202583f1d19d4
Miroslav Stastny <mistastn@microsoft.com>
2018-08-27 14:26:31 +0200

    chore: Component performance tests

---
+++ build/gulp/tasks/perf.ts
@@ -0,0 +1,159 @@
+import { task, series, shell } from 'gulp'
+import { argv } from 'yargs'
+
+const _ = require('lodash')
+const glob = require('glob')
+const path = require('path')
+const chalk = require('chalk')
+
+const lighthouse = require('lighthouse')
+const chromeLauncher = require('chrome-launcher')
+
+interface Example {
+  url: string
+  name: string
+  group: string
+}
+
+interface ExampleResult extends Example {
+  lighthouse: any
+}
+
+function getExampleFiles(filter = ''): Example[] {
+  return glob
+    .sync(`docs/src/examples/**/${filter}*.tsx`, { ignore: ['**/index.tsx', '**/*.knobs.tsx'] })
+    .map(examplePath => {
+      const { name: nameWithoutExtension, base: nameWithExtension, dir } = path.parse(examplePath)
+
+      const group = /^docs\/src\/examples\/([^/]+\/[^/]+)/.exec(examplePath)
+
+      return {
+        url: `http://localhost:8080/maximize/${_.kebabCase(nameWithoutExtension)}`,
+        name: nameWithExtension,
+        group: (group && group[1]) || 'general',
+      }
+    })
+}
+
+async function launchChromeAndRunLighthouse(flags, config) {
+  const { filter, headless } = argv
+  const chromeConfig = {
+    ...(headless && { chromeFlags: ['--headless'] }),
+  }
+
+  const chrome = await chromeLauncher.launch(chromeConfig)
+  flags.port = chrome.port
+
+  const results: (Example & { lighthouse?: any })[] = []
+  const measure = getExampleFiles(filter)
+
+  for (let i = 0; i < measure.length; i++) {
+    process.stdout.write(chalk.dim(measure[i].url))
+    const lighthouseResult = await lighthouse(measure[i].url, flags, config).then(
+      lighthouseReport =>
+        _.find(
+          lighthouseReport.lhr.audits['user-timings'].details.items,
+          i => i.name === 'stardust.perf.measureMount',
+        ),
+    )
+
+    const result: ExampleResult = {
+      ...measure[i],
+      lighthouse: lighthouseResult,
+    }
+    results.push(result)
+    if (lighthouseResult) {
+      console.log(` ${lighthouseResult.duration} ms`)
+    } else {
+      console.log(chalk.red(' error'))
+    }
+  }
+
+  await chrome.kill()
+  return _.groupBy(results, 'group')
+}
+
+function printResults(results) {
+  console.log()
+  console.log(
+    chalk.bold('========================== Performance tests results ==========================='),
+  )
+  _.forEach(results, (groupResults, groupName) => {
+    const stats = _.reduce(
+      groupResults,
+      (sum, testResult) => {
+        let aggregate
+        if (testResult.lighthouse) {
+          const duration = testResult.lighthouse.duration
+          aggregate = {
+            ...sum,
+            count: sum.count + 1,
+            min: sum.min ? Math.min(duration, sum.min) : duration,
+            max: sum.max ? Math.max(duration, sum.max) : duration,
+            total: sum.total + duration,
+          }
+        } else {
+          aggregate = {
+            ...sum,
+            count: sum.count + 1,
+            errors: sum.errors ? sum.errors + 1 : 1,
+          }
+        }
+        return aggregate
+      },
+      {
+        count: 0,
+        total: 0,
+      },
+    )
+
+    let details = ''
+    if (stats.count > 0) {
+      let errorMsg = ''
+      if (stats.errors) {
+        errorMsg = `, ${stats.errors} ERROR(S)!!!`
+      }
+      details = `${errorMsg}, avg = ${stats.total / stats.count} ms, max = ${stats.max} ms, min = ${
+        stats.min
+      } ms`
+    }
+
+    console.log(`${groupName} ${stats.count} test(s)${details}`)
+
+    if (argv.details) {
+      _.forEach(groupResults, testResult => {
+        const duration = testResult.lighthouse ? `${testResult.lighthouse.duration} ms` : 'ERROR'
+        console.log(`\t${testResult.name}  ${duration}`)
+      })
+    }
+  })
+  console.log(
+    chalk.bold('================================================================================'),
+  )
+  console.log()
+}
+
+task('lighthouse', cb => {
+  const config = {
+    extends: 'lighthouse:default',
+    settings: {
+      onlyCategories: ['performance'],
+      useThrottling: false,
+      disableDeviceEmulation: true,
+    },
+  }
+
+  return launchChromeAndRunLighthouse({}, config)
+    .then(results => {
+      printResults(results)
+      cb()
+      process.exit(0)
+    })
+    .catch(err => {
+      // console.error(err)
+      cb(err)
+      process.exit(1)
+    })
+})
+
+task('perf', series('dll', 'build:docs', 'serve:docs', 'lighthouse'))
--- docs/src/components/ExternalExampleLayout.tsx
+++ docs/src/components/ExternalExampleLayout.tsx
@@ -7,32 +7,43 @@ import PageNotFound from '../views/PageNotFound'
 
 const examplePaths = exampleContext.keys()
 
-const ExternalExampleLayout: any = props => {
-  const { exampleName } = props.match.params
-  const exampleFilename = exampleKebabNameToFilename(exampleName)
-
-  const examplePath = _.find(path => {
-    const { exampleName } = parseExamplePath(path)
-    return exampleFilename === exampleName
-  }, examplePaths)
-
-  if (!examplePath) return <PageNotFound />
-
-  const ExampleComponent = exampleContext(examplePath).default
-  if (!ExampleComponent) return <PageNotFound />
-
-  return <ExampleComponent />
-}
-
-ExternalExampleLayout.propTypes = {
-  children: PropTypes.node,
-  history: PropTypes.object.isRequired,
-  location: PropTypes.object.isRequired,
-  match: PropTypes.shape({
-    params: PropTypes.shape({
-      exampleName: PropTypes.string.isRequired,
+class ExternalExampleLayout extends React.Component<any, any> {
+  static propTypes = {
+    children: PropTypes.node,
+    history: PropTypes.object.isRequired,
+    location: PropTypes.object.isRequired,
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        exampleName: PropTypes.string.isRequired,
+      }).isRequired,
     }).isRequired,
-  }).isRequired,
+  }
+
+  componentWillMount() {
+    performance.mark('startMount')
+  }
+
+  componentDidMount() {
+    performance.mark('endMount')
+    performance.measure('stardust.perf.measureMount', 'startMount', 'endMount')
+  }
+
+  render() {
+    const { exampleName } = this.props.match.params
+    const exampleFilename = exampleKebabNameToFilename(exampleName)
+
+    const examplePath = _.find(path => {
+      const { exampleName } = parseExamplePath(path)
+      return exampleFilename === exampleName
+    }, examplePaths)
+
+    if (!examplePath) return <PageNotFound />
+
+    const ExampleComponent = exampleContext(examplePath).default
+    if (!ExampleComponent) return <PageNotFound />
+
+    return <ExampleComponent />
+  }
 }
 
 export default ExternalExampleLayout
