fd1078bea46c0b975af64a63f0105edfe2ccdbef
Silviu Avram <siavram@microsoft.com>
2018-08-28 11:58:01 +0200

    added aria label and controls to the tab behavior

--- src/lib/accessibility/Behaviors/Tab/TabBehavior.ts
+++ src/lib/accessibility/Behaviors/Tab/TabBehavior.ts
@@ -9,8 +9,12 @@ const TabBehavior: Accessibility = (props: any) => ({
       role: 'tab',
       'aria-selected': props['active'],
       tabIndex: '0',
+      'aria-label': props['aria-label'],
+      'aria-labelledby': props['aria-labelledby'],
+      'aria-controls': props['aria-controls'],
     },
   },
+  handledProps: ['aria-label', 'aria-labelledby', 'aria-controls'],
 })
 
 export default TabBehavior
