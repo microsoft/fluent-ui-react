e33a4e58aefc03af68fa94278bf5e067f32e6f10
Levi Thomason <me@levithomason.com>
2018-08-28 08:55:09 -0700

    address code snippet feedback

--- docs/src/components/CodeSnippet.tsx
+++ docs/src/components/CodeSnippet.tsx
@@ -1,54 +1,50 @@
 import * as React from 'react'
 import Editor, { EDITOR_BACKGROUND_COLOR } from './Editor'
 
-export interface ICodeSnippetProps {
+export interface CodeSnippetProps {
   label?: string
   mode?: 'jsx' | 'html' | 'sh'
   value: string
   style?: React.CSSProperties
 }
 
-const CodeSnippet = ({ label = '', value, mode, style, ...rest }: ICodeSnippetProps) => {
-  const editorMode: any = mode || label.replace(/.*\./, '') || 'jsx'
-
-  return (
+const CodeSnippet = ({ label = '', value, mode, style, ...rest }: CodeSnippetProps) => (
+  <div
+    style={{
+      position: 'relative',
+      padding: '1rem',
+      marginBottom: '2rem',
+      background: EDITOR_BACKGROUND_COLOR,
+      ...style,
+    }}
+  >
     <div
       style={{
-        position: 'relative',
-        padding: '1rem',
-        marginBottom: '2rem',
-        background: EDITOR_BACKGROUND_COLOR,
-        ...style,
+        position: 'absolute',
+        padding: '0.2rem 0.35rem',
+        top: '1rem',
+        right: '1rem',
+        lineHeight: 1,
+        color: '#899',
+        fontFamily: 'monospace',
+        fontSize: '0.8rem',
+        border: '1px solid #566',
+        zIndex: 100,
       }}
     >
-      <div
-        style={{
-          position: 'absolute',
-          padding: '0.2rem 0.35rem',
-          top: '1rem',
-          right: '1rem',
-          lineHeight: 1,
-          color: '#899',
-          fontFamily: 'monospace',
-          fontSize: '0.8rem',
-          border: '1px solid #566',
-          zIndex: 100,
-        }}
-      >
-        {label || editorMode}
-      </div>
-      <Editor
-        id={btoa(value)}
-        highlightActiveLine={false}
-        highlightGutterLine={false}
-        mode={editorMode}
-        readOnly
-        showGutter={false}
-        showCursor={false}
-        value={editorMode === 'sh' ? value.replace(/^/g, '$  ') : value}
-        {...rest}
-      />
+      {label || mode}
     </div>
-  )
-}
+    <Editor
+      id={btoa(value)}
+      highlightActiveLine={false}
+      highlightGutterLine={false}
+      mode={mode}
+      readOnly
+      showGutter={false}
+      showCursor={false}
+      value={mode === 'sh' ? value.replace(/^/g, '$  ') : value}
+      {...rest}
+    />
+  </div>
+)
 export default CodeSnippet
