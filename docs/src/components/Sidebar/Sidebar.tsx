a4dc14e7d2fb08558751195b4e7fb82349b4a0c9
EUROPE\hueland <hueland@microsoft.com>
2018-08-31 15:16:55 +0200

    adding links

--- docs/src/components/Sidebar/Sidebar.tsx
+++ docs/src/components/Sidebar/Sidebar.tsx
@@ -202,6 +202,9 @@ class Sidebar extends React.Component<any, any> {
             <Menu.Item as={NavLink} exact to="/theming" activeClassName="active">
               Theming
             </Menu.Item>
+            <Menu.Item as={NavLink} exact to="/accessibility" activeClassName="active">
+              Accessibility
+            </Menu.Item>
           </Menu.Menu>
         </Menu.Item>
         <Menu.Item active>
