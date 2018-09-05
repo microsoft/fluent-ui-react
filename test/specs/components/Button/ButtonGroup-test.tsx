e27e3c9e492c292192ca38c2de1c5e81eba78963
manajdov <manajdov@microsoft.com>
2018-09-05 12:52:36 +0200

    -added common test for collection shorthand -updated ButtonGroup-test.tsx to confirm to the collection shorthand test

--- test/specs/components/Button/ButtonGroup-test.tsx
+++ test/specs/components/Button/ButtonGroup-test.tsx
@@ -2,7 +2,12 @@ import * as React from 'react'
 
 import { isConformant } from 'test/specs/commonTests'
 import ButtonGroup from 'src/components/Button/ButtonGroup'
+import implementsCollectionShorthandProp from '../../commonTests/implementsCollectionShorthandProp'
+import Button from 'src/components/Button'
+
+const buttonGroupImplementsShorthandProp = implementsCollectionShorthandProp(ButtonGroup)
 
 describe('ButtonGroup', () => {
   isConformant(ButtonGroup)
+  buttonGroupImplementsShorthandProp('buttons', Button)
 })
