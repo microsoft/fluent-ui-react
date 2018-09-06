46979754dbf48676c5e5ea70c2ab43aed52b5165
Marija Najdova <mnajdova@gmail.com>
2018-09-06 14:02:27 +0200

    feat(Button): added ButtonGroup component (#179)
    * -added ButtonGroup component
    
    * -updated CHANGELOG
    
    * -added iconOnly in the buttons group example
    
    * -added common test for collection shorthand
    -updated ButtonGroup-test.tsx to confirm to the collection shorthand test
    
    * -added collection shorthand tests for the List and Menu components
    -addressed comments on the PR
    
    * -resolved merge conflicts
    
    * -fixed generation of the className in the groups component
    -changed defaultBorderRadius to borderRadius in the button's variables

--- docs/src/examples/components/Button/index.tsx
+++ docs/src/examples/components/Button/index.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Types from './Types'
 import Variations from './Variations'
 import States from './States'
+import Groups from './Groups'
 
 const ButtonExamples = () => (
   <div>
     <Types />
     <States />
     <Variations />
+    <Groups />
   </div>
 )
 
