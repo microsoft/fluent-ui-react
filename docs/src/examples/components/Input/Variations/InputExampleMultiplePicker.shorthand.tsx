d0fe411a8d3563f14e0fbdc46cc15a5c3e185034
Silviu Avram <siavram@microsoft.com>
2018-09-06 14:54:18 +0200

    fixed an error from merge

--- docs/src/examples/components/Input/Variations/InputExampleMultiplePicker.shorthand.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react'
-import { Input } from '@stardust-ui/react'
-
-const InputExampleIconClearableShorthand = () => (
-  <Input icon="search" clearable placeholder="Search..." />
-)
-
-export default InputExampleIconClearableShorthand
