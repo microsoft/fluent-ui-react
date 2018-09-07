5044cdaef72358f4ed087194ca2942628b526df7
Silviu Avram <siavram@microsoft.com>
2018-09-07 10:07:16 +0200

    added the downshift library and an example

---
+++ docs/src/examples/components/Input/Variations/InputExampleChatPeoplePicker.shorthand.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import { ChatPeoplePicker } from '@stardust-ui/react'
+
+const InputExampleChatPeoplePickerShorthand = () => <ChatPeoplePicker />
+
+export default InputExampleChatPeoplePickerShorthand
---
+++ src/components/ChatPeoplePicker/ChatPeoplePicker.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react'
+import Downshift from 'downshift'
+import { UIComponent } from '../../lib'
+
+const items = [
+  { value: 'apple' },
+  { value: 'pear' },
+  { value: 'orange' },
+  { value: 'grape' },
+  { value: 'banana' },
+]
+
+export class ChatPeoplePicker extends UIComponent<any, any> {
+  render() {
+    return (
+      <Downshift
+        onChange={selection => alert(`You selected ${selection.value}`)}
+        itemToString={item => (item ? item.value : '')}
+      >
+        {({
+          getInputProps,
+          getItemProps,
+          getLabelProps,
+          getMenuProps,
+          isOpen,
+          inputValue,
+          highlightedIndex,
+          selectedItem,
+        }) => (
+          <div>
+            <label {...getLabelProps()}>Enter a fruit</label>
+            <input {...getInputProps()} />
+            <ul {...getMenuProps()}>
+              {isOpen
+                ? items
+                    .filter(item => !inputValue || item.value.includes(inputValue))
+                    .map((item, index) => (
+                      <li
+                        {...getItemProps({
+                          key: item.value,
+                          index,
+                          item,
+                          style: {
+                            backgroundColor: highlightedIndex === index ? 'lightgray' : 'white',
+                            fontWeight: selectedItem === item ? 'bold' : 'normal',
+                          },
+                        })}
+                      >
+                        {item.value}
+                      </li>
+                    ))
+                : null}
+            </ul>
+          </div>
+        )}
+      </Downshift>
+    )
+  }
+}
+
+export default ChatPeoplePicker
