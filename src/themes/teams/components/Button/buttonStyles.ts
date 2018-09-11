0fb83e96b1fabb21e3a763878c8c8f1b9174872f
Andrew Martin <andmarti@microsoft.com>
2018-09-11 16:22:50 -0700

    Ensuring focus border is only applied on keyboard focus

--- src/themes/teams/components/Button/buttonStyles.ts
+++ src/themes/teams/components/Button/buttonStyles.ts
@@ -131,18 +131,20 @@ const buttonStyles: IComponentPartStylesInput = {
             borderColor: typeSecondaryBorderColorHover,
           },
           ':focus': {
-            backgroundColor: typeSecondaryBackgroundColorFocus,
-            borderColor: typeSecondaryBorderColorFocus,
-            '::before': {
-              content: '""',
-              position: 'absolute',
-              top: '0',
-              left: '0',
-              right: '0',
-              bottom: '0',
-              border: `${pxToRem(1)} solid ${typeSecondaryBorderColorInsetFocus}`,
-              borderRadius: `${pxToRem(2)}`,
-            },
+            ...(isFromKeyboard && {
+              backgroundColor: typeSecondaryBackgroundColorFocus,
+              borderColor: typeSecondaryBorderColorFocus,
+              '::before': {
+                content: '""',
+                position: 'absolute',
+                top: '0',
+                left: '0',
+                right: '0',
+                bottom: '0',
+                border: `${pxToRem(1)} solid ${typeSecondaryBorderColorInsetFocus}`,
+                borderRadius: `${pxToRem(2)}`,
+              },
+            }),
           },
         }),
 
