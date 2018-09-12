9b4314b46772d200523d11aa65dd7382ba44d655
Milan Turon <milanturon@centrum.cz>
2018-09-12 10:31:08 +0200

    feat(docs): adding accessibility description for behaviors (#181)
    * adding behavior description, and link which point to particular behavior from component
    
    * moving description from component to behavior, modify and crate new description
    
    * removing emtpy string
    
    * changes after PR
    
    * refactoring get default behavior
    
    * changes after PR, adding description on the top of page
    
    * adding changelog
    
    * moving tickets under documentation section
    
    * changes in descriptions and desc. title
    
    * renaming changes after PR
    
    * renaming after PR
    
    * renaming 2 after PR
    
    * refactor logic of rendering behavior info
    
    * changing styles based on PR comment

--- src/lib/accessibility/Behaviors/Menu/MenuBehavior.ts
+++ src/lib/accessibility/Behaviors/Menu/MenuBehavior.ts
@@ -1,5 +1,11 @@
 import { Accessibility, FocusZoneMode } from '../../interfaces'
 
+/**
+ * @description
+ * Adds role='menu'.
+ * The 'menu' role is used to identify an element that creates a list of common actions or functions that a user can invoke.
+ */
+
 const MenuBehavior: Accessibility = {
   attributes: {
     root: {
--- src/lib/accessibility/Behaviors/Menu/MenuItemBehavior.ts
+++ src/lib/accessibility/Behaviors/Menu/MenuItemBehavior.ts
@@ -2,6 +2,14 @@ import { Accessibility } from '../../interfaces'
 import { IS_FOCUSABLE_ATTRIBUTE } from '../../FocusZone/focusUtilities'
 import * as keyboardKey from 'keyboard-key'
 
+/**
+ * @description
+ *  Adds role 'presentation' on the root element
+ *  Adds role 'menuitem' on anchor element
+ *  Adds attribute 'aria-expanded=true' on anchor element based on "submenuOpened" property. Based on this screen readers will recognize the expanded state of the item.
+ *  The behavior is designed for particular structure of menu item. The item consists of root element and anchor inside the root element.
+ */
+
 const MenuItemBehavior: Accessibility = (props: any) => ({
   attributes: {
     root: {
