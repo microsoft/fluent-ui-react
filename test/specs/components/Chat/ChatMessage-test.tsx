84f0180d4bf0bbecd8a0cad7743924f304b51508
Tomas Iser <t-tois@microsoft.com>
2018-09-14 16:04:56 +0200

    Introduced handlesAccessibility tests to chat and fixed them for embed

--- test/specs/components/Chat/ChatMessage-test.tsx
+++ test/specs/components/Chat/ChatMessage-test.tsx
@@ -1,13 +1,22 @@
 import * as React from 'react'
-import { isConformant, implementsShorthandProp } from 'test/specs/commonTests'
+import { isConformant, implementsShorthandProp, handlesAccessibility } from 'test/specs/commonTests'
 import { mountWithProvider } from '../../../utils'
 
 import ChatMessage from 'src/components/Chat/ChatMessage'
 import Avatar from 'src/components/Avatar'
+import ChatMessageBehavior from 'src/lib/accessibility/Behaviors/Chat/ChatMessageBehavior'
+import { IAccessibilityDefinition } from 'src/lib/accessibility/interfaces'
 
 describe('ChatMessage', () => {
   isConformant(ChatMessage)
   implementsShorthandProp(ChatMessage)('avatar', Avatar, { mapsValueToProp: 'name' })
+
+  describe('accessibility', () => {
+    handlesAccessibility(ChatMessage, {
+      defaultRootRole: 'presentation',
+      focusZoneDefinition: (ChatMessageBehavior as IAccessibilityDefinition).focusZone,
+    })
+  })
 
   describe('avatar', () => {
     it('creates an Avatar component when the avatar shorthand is provided', () => {
