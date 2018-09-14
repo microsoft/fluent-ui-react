d6ecfe530c17dfaa340f83b29f281f07ad2ff25b
Silviu Avram <siavram@microsoft.com>
2018-09-14 15:36:01 +0200

    removed startdust properties and improved functionality

--- src/components/PeoplePicker/PeoplePicker.tsx
+++ src/components/PeoplePicker/PeoplePicker.tsx
@@ -1,32 +1,38 @@
 import * as React from 'react'
 import Downshift from 'downshift'
-import { UIComponent, pxToRem } from '../../lib'
 import Label from '../Label'
 import Input from '../Input'
 import Menu from '../Menu'
-import * as PropTypes from 'prop-types'
 import * as _ from 'lodash'
+import keyboardKey from 'keyboard-key'
+import Button from '../Button'
+import { pxToRem } from '../../lib'
 
-const styles: any = {
-  div: {
+const peoplePickerStyles: any = {
+  containerDiv: {
     display: 'flex',
     flexWrap: 'wrap',
-    alignItems: 'center',
     outline: 0,
     border: 0,
     borderRadius: `0.2143rem`,
     borderBottom: `0.1429rem solid transparent`,
     color: '#252423',
     backgroundColor: '#F3F2F1',
+    borderColor: 'transparent',
   },
-  onFocus: {
+  personContainerLabel: {
+    root: {
+      margin: '.4rem 0 0 .4rem',
+    },
+  },
+  containerDivOnFocus: {
     borderColor: '#6264A7',
     borderRadius: '0.2143rem 0.2143rem 0.1429rem 0.1429rem',
   },
-  labelAdd: {
+  addLabel: {
     root: { backgroundColor: 'white' },
   },
-  input: {
+  textInput: {
     input: {
       width: '100%',
       ':focus': {
@@ -46,27 +52,19 @@ interface IPeoplePickerState {
   focused: boolean
 }
 
-export class PeoplePicker extends UIComponent<IPeoplePickerProps, IPeoplePickerState> {
-  static displayName = 'PeoplePicker'
+export class PeoplePicker extends React.Component<IPeoplePickerProps, IPeoplePickerState> {
+  private input: any
+  private labelId: string = 'picker-label-id'
 
-  static create: Function
+  constructor(props) {
+    super(props, {})
 
-  static className = 'ui-people-picker'
+    this.state = {
+      selected: [],
+      focused: false,
+    }
 
-  static propTypes = {
-    /** Custom styles to be applied for component. */
-    styles: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
-    /** Callback function that takes as input the content of the text field and returns name suggestions. */
-    source: PropTypes.PropTypes.func,
-  }
-
-  static handledProps = ['styles', 'source']
-
-  static defaultProps = {}
-
-  state = {
-    selected: [],
-    focused: false,
+    this.input = React.createRef()
   }
 
   private handleSelection = element => {
@@ -79,21 +77,20 @@ export class PeoplePicker extends UIComponent<IPeoplePickerProps, IPeoplePickerS
     }))
   }
 
-  private remove(element) {
-    this.setState(({ selected }) => ({
-      selected: selected.filter(currentElement => currentElement !== element),
-    }))
+  private remove(element?) {
+    let { selected } = this.state
+    if (element) {
+      selected = selected.filter(currentElement => currentElement !== element)
+    } else {
+      selected.pop()
+    }
+    this.setState({ selected })
   }
 
-  public renderComponent({
-    ElementType,
-    classes,
-    accessibility,
-    variables,
-    rest,
-  }): React.ReactNode {
+  public render(): React.ReactNode {
     return (
-      <ElementType>
+      <React.Fragment>
+        <Label content="Add" styles={peoplePickerStyles.addLabel} id={this.labelId} />
         <Downshift
           onChange={this.handleSelection}
           selectedItem={null}
@@ -102,76 +99,108 @@ export class PeoplePicker extends UIComponent<IPeoplePickerProps, IPeoplePickerS
           {({
             getInputProps,
             getItemProps,
-            getLabelProps,
             getMenuProps,
             isOpen,
             inputValue,
             highlightedIndex,
-            selectedItem,
-          }) => (
-            <div>
-              <Label content="Add" {...getLabelProps()} styles={styles.labelAdd} />
-              <div
-                style={
-                  this.state.focused ? { ...styles.div, ...styles.onFocus } : { ...styles.div }
-                }
-              >
-                {this.state.selected.length > 0
-                  ? this.state.selected.map((element, index) => (
-                      <Label
-                        circular
-                        key={`peoplePickerItem-${index}`}
-                        content={element.name}
-                        image={{
-                          src: element.image,
-                          avatar: true,
-                        }}
-                        icon={{
-                          name: 'close',
-                          onClick: () => {
-                            this.remove(element)
+            toggleMenu,
+          }) => {
+            return (
+              <div id="people-picker">
+                <div
+                  id="people-picker-container"
+                  style={{
+                    ...peoplePickerStyles.containerDiv,
+                    ...(this.state.focused ? peoplePickerStyles.containerDivOnFocus : {}),
+                  }}
+                  onClick={() => {
+                    toggleMenu()
+                    !isOpen && this.input.current.inputRef.focus()
+                  }}
+                >
+                  {this.state.selected.length === 0
+                    ? null
+                    : this.state.selected.map((element, index) => (
+                        <Label
+                          styles={peoplePickerStyles.personContainerLabel}
+                          circular
+                          key={`peoplePickerItem-${index}`}
+                          content={element.name}
+                          image={{
+                            src: element.image,
+                            avatar: true,
+                          }}
+                          icon={{
+                            name: 'close',
+                            onClick: event => {
+                              this.remove(element)
+                              this.input.current.inputRef.focus()
+                              event.stopPropagation()
+                            },
+                            onKeyDown: event => {
+                              if (keyboardKey.getCode(event) === keyboardKey.Enter) {
+                                this.remove(element)
+                                this.input.current.inputRef.focus()
+                                event.stopPropagation()
+                              }
+                            },
+                          }}
+                        />
+                      ))}
+                  <Input
+                    styles={peoplePickerStyles.textInput}
+                    ref={this.input}
+                    placeholder={this.state.selected.length > 0 ? '' : 'Start typing a name'}
+                    onFocus={() => {
+                      this.setState({ focused: true })
+                    }}
+                    onKeyUp={event => {
+                      if (keyboardKey.getCode(event) === keyboardKey.Backspace && !inputValue) {
+                        this.state.selected.length > 0 && this.remove()
+                      }
+                    }}
+                    {...getInputProps()}
+                    aria-labelledby={this.labelId}
+                    onBlur={() => {
+                      this.setState({ focused: false })
+                    }}
+                  />
+                </div>
+                {isOpen && (
+                  <Menu
+                    vertical
+                    fluid
+                    {...getMenuProps()}
+                    defaultActiveIndex={0}
+                    items={this.props.source(inputValue, this.state.selected).map((item, index) => {
+                      return {
+                        key: `peoplePickerItem-${index}`,
+                        content: item.name,
+                        ...getItemProps({
+                          index,
+                          item,
+                          style: {
+                            backgroundColor: highlightedIndex === index ? 'lightgray' : 'white',
                           },
-                        }}
-                      />
-                    ))
-                  : null}
-                <Input
-                  styles={styles.input}
-                  placeholder={this.state.selected.length > 0 ? '' : 'Start typing a name'}
-                  onFocus={() => {
-                    this.setState({ focused: true })
-                  }}
-                  onBlur={() => {
-                    this.setState({ focused: false })
-                  }}
-                  {...getInputProps()}
-                />
+                        }),
+                      }
+                    })}
+                  />
+                )}
               </div>
-              {isOpen && (
-                <Menu
-                  vertical
-                  fluid
-                  {...getMenuProps()}
-                  defaultActiveIndex={0}
-                  items={this.props.source(inputValue, this.state.selected).map((item, index) => {
-                    return {
-                      key: `peoplePickerItem-${index}`,
-                      content: item.name,
-                      ...getItemProps({
-                        index,
-                        item,
-                        style: {
-                          backgroundColor: highlightedIndex === index ? 'lightgray' : 'white',
-                        },
-                      }),
-                    }
-                  })}
-                />
-              )}
-            </div>
-          )}
+            )
+          }}
         </Downshift>
-      </ElementType>
+        <div
+          style={{
+            marginTop: `${pxToRem(8)}`,
+            textAlign: 'right',
+          }}
+        >
+          <Button type="secondary" content="Cancel" style={{ margin: '0' }} />
+          <Button type="primary" content="Add" style={{ margin: `0 0 0 ${pxToRem(8)}` }} />
+        </div>
+      </React.Fragment>
     )
   }
 }
