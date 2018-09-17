fdd04ba123606001239e924594c75c37af187fb5
Sofiya Huts <sohuts@microsoft.com>
2018-09-17 17:33:32 +0200

    Improvements after CR

--- src/lib/accessibility/Behaviors/Popup/PopupBehavior.ts
+++ src/lib/accessibility/Behaviors/Popup/PopupBehavior.ts
@@ -2,6 +2,36 @@ import { Accessibility } from '../../interfaces'
 import * as keyboardKey from 'keyboard-key'
 import _ from 'lodash'
 
+/**
+ * @description
+ *  Adds role='button' to 'trigger' component's part, if it is not focusable element and no role attribute provided.
+ *  Adds tabIndex='0' to 'trigger' component's part, if it is not tabbable element and no tabIndex attribute provided.
+ *  Adds attribute 'aria-disabled=true' to 'trigger' component's part based on the property 'disabled'.
+ *  Adds attribute 'aria-haspopup=true' to 'trigger' component's part.
+ */
+const PopupBehavior: Accessibility = (props: any) => ({
+  attributes: {
+    trigger: {
+      role: getAriaAttribute('role', props, 'button'),
+      tabIndex: getAriaAttribute('tabIndex', props, '0'),
+      'aria-haspopup': 'true',
+      'aria-disabled': !!props['disabled'],
+    },
+  },
+  keyActions: {
+    trigger: {
+      performClick: {
+        keyCombinations: [{ keyCode: keyboardKey.Enter }, { keyCode: keyboardKey.Spacebar }],
+      },
+    },
+    popup: {
+      closeAndFocusTrigger: {
+        keyCombinations: [{ keyCode: keyboardKey.Escape }],
+      },
+    },
+  },
+})
+
 const isFocusable = props => {
   try {
     const { as, href } = props.trigger.props
@@ -30,28 +60,5 @@ const getAriaAttribute = (attribute, props, defaultValue) => {
     return defaultValue
   }
 }
-
-const PopupBehavior: Accessibility = (props: any) => ({
-  attributes: {
-    trigger: {
-      role: getAriaAttribute('role', props, 'button'),
-      tabIndex: getAriaAttribute('tabIndex', props, '0'),
-      'aria-haspopup': 'true',
-      'aria-disabled': !!props['disabled'],
-    },
-  },
-  keyActions: {
-    trigger: {
-      performClick: {
-        keyCombinations: [{ keyCode: keyboardKey.Enter }, { keyCode: keyboardKey.Spacebar }],
-      },
-    },
-    popup: {
-      closeAndFocusTrigger: {
-        keyCombinations: [{ keyCode: keyboardKey.Escape }],
-      },
-    },
-  },
-})
 
 export default PopupBehavior
