d6bd809a251b174144c360d19a37dfd75958c4cb
kuzhelov <kuroman@microsoft.com>
2018-09-19 01:52:21 +0200

    introduce full script

--- build/gulp/tasks/test-projects.tsx
+++ build/gulp/tasks/test-projects.tsx
@@ -3,6 +3,7 @@ import { task, series } from 'gulp'
 import * as rimraf from 'rimraf'
 import sh from '../sh'
 import * as mkdirp from 'mkdirp'
+import * as parseJson from 'parse-json'
 
 import config from '../../../config'
 import { tmpdir } from 'os'
@@ -31,7 +32,7 @@ task('clean:test:projects:cra-ts', cb => {
 //  - Update the App.tsx to include some stardust imports
 //  - Link our package
 //  - Try and run a build
-task('build:test:projects:cra-ts', () => {
+task('build:test:projects:cra-ts', async () => {
   const appTSX = `import {
   Avatar,
   Button,
@@ -64,49 +65,68 @@ class App extends React.Component {
 export default App;
 `
 
-  const log = (msg: string) => arg => {
-    console.log()
-    console.log('='.repeat(80))
-    console.log('CRA TS:', msg)
-    console.log('='.repeat(80))
-    return arg
-  }
-
-  const logSimple = msg => {
+  const log = msg => {
     console.log()
     console.log('='.repeat(80))
     console.log('CRA TS:', msg)
     console.log('='.repeat(80))
   }
 
-  const runInProjects = cmd => () => sh(`cd ${projectsPath()} && ${cmd}`)
-  const runInTSApp = cmd => () => sh(`cd ${tsAppPath()} && ${cmd}`)
+  log('STEP 0. Preparing Stardust package..')
+  await sh('yarn build:dist')
 
-  let testAppDir = ''
-  const runInTestApp = cmd => () => sh(`cd ${testAppDir} && ${cmd}`)
+  const stardustPackageFilename = (await sh(`npm pack`, true)).trim()
+  log(`Stardust package is published: ${paths.base(stardustPackageFilename)}`)
 
-  return Promise.resolve()
-    .then(() => mkdirp.sync(projectsPath()))
-    .then(() => sh('yarn build:dist'))
-    .then(log('Testing temp dir scenario'))
-    .then(() => sh(`mktemp -d`, true))
-    .then(tmpDir => {
-      testAppDir = `${tmpDir.trim()}/test`
-      logSimple(testAppDir)
-    })
-    .then(() => sh(`mkdir ${testAppDir}`))
-    .then(runInTestApp(`yarn create react-app . --scripts-version=react-scripts-ts`))
-    .then(runInTestApp(`yarn add ${paths.base()}`))
-    .then(runInTestApp(`rm -rf node_modules/@stardust-ui/react/node_modules`))
-    .then(() => {
-      fs.writeFileSync(`${testAppDir}/src/App.tsx`, appTSX)
-    })
-    .then(runInTestApp(`yarn build`))
-  // .then(() => sh(`pwd`))
-  // .then((res) => log('Result is: ' + res))
+  ////////
+
+  try {
+    log('STEP 1. Create test React project with TSX scripts..')
+    const testAppDir = `${(await sh(`mktemp -d`, true)).trim()}/test`
+    const runInTestApp = cmd => sh(`cd ${testAppDir} && ${cmd}`)
+
+    await sh(`mkdir ${testAppDir}`)
+
+    await runInTestApp(`yarn create react-app . --scripts-version=react-scripts-ts`)
+
+    log(`Test React project is successfully created: ${testAppDir}`)
+
+    ///////////
+
+    log('STEP 2. Add Stardust dependency to test project..')
+    await runInTestApp(`yarn add ${paths.base(stardustPackageFilename)}`)
+    log("Stardust is successfully added as test project's dependency.")
+
+    //////////////
+
+    log("STEP 3. Enable 'skipLibCheck' flag for test project's TS compiler")
+    const tsconfigPath = `${testAppDir}/tsconfig.json`
+
+    const tsConfigAsJson = JSON.parse(`${fs.readFileSync(tsconfigPath)}`)
+    if (!tsConfigAsJson.compilerOptions) {
+      tsConfigAsJson.compilerOptions = {}
+    }
+
+    tsConfigAsJson.compilerOptions.skipLibCheck = true
+
+    fs.writeFileSync(tsconfigPath, JSON.stringify(tsConfigAsJson))
+
+    ///////////////
+
+    log("STEP 4. Reference Stardust components in test project's App.tsx")
+    fs.writeFileSync(`${testAppDir}/src/App.tsx`, appTSX)
+
+    ////////////
+
+    log('STEP 5. Build test project..')
+    await runInTestApp(`yarn build`)
+    log('Test project is built successfully!')
+  } finally {
+    await sh(`rm ${stardustPackageFilename}`)
+  }
 })
 
 // ----------------------------------------
 // Test
 // ----------------------------------------
-task('test:projects:cra-ts', series('clean:test:projects:cra-ts', 'build:test:projects:cra-ts'))
+task('test:projects:cra-ts', series('build:test:projects:cra-ts'))
