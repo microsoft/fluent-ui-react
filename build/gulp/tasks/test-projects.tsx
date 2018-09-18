2688dbda1d13beb78227b54ad17ec278fa469328
kuzhelov <kuroman@microsoft.com>
2018-09-18 21:10:09 +0200

    add App.tsx change

--- build/gulp/tasks/test-projects.tsx
+++ build/gulp/tasks/test-projects.tsx
@@ -97,6 +97,9 @@ export default App;
     .then(runInTestApp(`create-react-app . --scripts-version=react-scripts-ts`))
     .then(runInTestApp(`yarn add ${paths.base()}`))
     .then(runInTestApp(`rm -rf node_modules/@stardust-ui/react/node_modules`))
+    .then(() => {
+      fs.writeFileSync(`${testAppDir}/src/App.tsx`, appTSX)
+    })
     .then(runInTestApp(`yarn build`))
   // .then(() => sh(`pwd`))
   // .then((res) => log('Result is: ' + res))
