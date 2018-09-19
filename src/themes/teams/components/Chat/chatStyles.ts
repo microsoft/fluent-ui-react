4aae38b1ec4fbea022d44dec20e9f37669b57bc3
manajdov <manajdov@microsoft.com>
2018-09-19 18:20:16 +0200

    -removed padding variable

--- src/themes/teams/components/Chat/chatStyles.ts
+++ src/themes/teams/components/Chat/chatStyles.ts
@@ -1,5 +1,6 @@
 import { ICSSInJSStyle } from '../../../../../types/theme'
 import { IChatVariables } from './chatVariables'
+import { pxToRem } from '../../../../lib'
 
 const chatStyles = {
   root: ({ variables: v }: { variables: IChatVariables }): ICSSInJSStyle => ({
@@ -8,7 +9,7 @@ const chatStyles = {
     display: 'flex',
     flexDirection: 'column',
     listStyle: 'none',
-    padding: v.padding,
+    padding: `0 ${pxToRem(10)} 0 ${pxToRem(10)}`,
     margin: 0,
   }),
 }
