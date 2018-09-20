ba945d99a99c5a5776fbfd6b68fb861184465bab
kuzhelov <kuroman@microsoft.com>
2018-09-20 12:21:46 +0200

    introduce test for 'icon' shorthand prop

--- test/specs/components/Attachment/Attachment-test.tsx
+++ test/specs/components/Attachment/Attachment-test.tsx
@@ -2,6 +2,7 @@ import { isConformant, implementsShorthandProp } from 'test/specs/commonTests'
 
 import Attachment from 'src/components/Attachment'
 import Text from 'src/components/Text'
+import Icon from 'src/components/Icon'
 
 const attachmentImplementsShorthandProp = implementsShorthandProp(Attachment)
 
@@ -9,4 +10,5 @@ describe('Attachment', () => {
   isConformant(Attachment)
   attachmentImplementsShorthandProp('header', Text)
   attachmentImplementsShorthandProp('description', Text)
+  attachmentImplementsShorthandProp('icon', Icon, { mapsValueToProp: 'name' })
 })
