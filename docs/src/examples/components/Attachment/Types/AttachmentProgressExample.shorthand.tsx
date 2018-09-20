ac1621481148cadeda0d6c47bddac8b5d1ed6e56
kuzhelov <kuroman@microsoft.com>
2018-09-20 12:36:13 +0200

    make examples consistent: add alerting on 'close' icon click

--- docs/src/examples/components/Attachment/Types/AttachmentProgressExample.shorthand.tsx
+++ docs/src/examples/components/Attachment/Types/AttachmentProgressExample.shorthand.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import { Attachment } from '@stardust-ui/react'
 
 const AttachmentProgressExampleShorthand = () => (
-  <Attachment header="Photo.jpg" action={{ icon: 'x' }} progress={33} />
+  <Attachment
+    header="Photo.jpg"
+    action={{ icon: 'x', onClick: () => alert("'X' is clicked!") }}
+    progress={33}
+  />
 )
 
 export default AttachmentProgressExampleShorthand
