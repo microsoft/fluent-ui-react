27ad11266f1e2e4bd598b853f5269c43ab7a3716
kuzhelov <deckster@mail.ru>
2018-09-21 19:17:01 +0200

    feat: themable icons (#260)
    * add SVG icons to teams theme
    
    * udpate theme icons api, add example enumerating icons
    
    * generalize API for svg and font-based icons
    
    * feat(theme): add font icons support for themes (#243)
    
    * generalize API for svg and font-based icons
    
    * remove 'svg' prop in IconSet example
    
    * add mechanism for providing font-based icons in theme
    
    * fix UTs
    
    * remove getExtraProps method
    
    * fix theme icons example in docs
    
    * generalize object and function types as icon descriptors
    
    * fix unit tests
    
    * address review comments
    
    * fix lint issues
    
    * fix import statement
    
    * replace helper methods
    
    * remove 'derivedProps' from renderer
    
    * remove unused styles for SVG paths
    
    * address review comments
    
    * fix lint issues
    
    * changelog

---
+++ src/themes/teams/components/Icon/svg/index.ts
@@ -0,0 +1,19 @@
+import svgIconsAndStyles from './icons'
+import { SvgIconSpecWithStyles, TeamsSvgIconSpec } from './types'
+
+export const getStyle = partName => {
+  return args => {
+    const { props } = args
+
+    const maybeIconSpec = svgIconsAndStyles[props.name]
+    const maybeIconStyles = maybeIconSpec && (maybeIconSpec as SvgIconSpecWithStyles).styles
+
+    if (maybeIconStyles && maybeIconStyles[partName]) {
+      return maybeIconStyles[partName](args)
+    }
+
+    return undefined
+  }
+}
+
+export default svgIconsAndStyles as { [iconName: string]: TeamsSvgIconSpec }
