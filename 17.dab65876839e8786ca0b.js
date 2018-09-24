a138fc6b089b307ca542c249184a396bf75f2d99
Miroslav Stastny <mistastn@microsoft.com>
2018-09-24 13:21:15 +0200

    deploy docs [ci skip]

--- 17.dab65876839e8786ca0b.js
+++
@@ -1,6 +0,0 @@
-webpackJsonp([17],{1969:/*!**************************************************!*\
-  !*** ./docs/src/exampleMenus/Chat.examples.json ***!
-  \**************************************************/
-/*! dynamic exports provided */
-/*! all exports used */
-function(a,e){a.exports=[{sectionName:"Types",examples:[{title:"Default",examplePath:"components/Chat/Types/ChatExample"}]},{sectionName:"Variations",examples:[{title:"Avatar",examplePath:"components/Chat/Variations/ChatExampleAvatar"}]}]}});
---
+++ 5.c8cd8517e030b72e7f21.js
@@ -0,0 +1,6 @@
+webpackJsonp([5],{2011:/*!***************************************************!*\
+  !*** ./docs/src/exampleMenus/Popup.examples.json ***!
+  \***************************************************/
+/*! dynamic exports provided */
+/*! all exports used */
+function(e,p){e.exports=[{sectionName:"Types",examples:[{title:"Default",examplePath:"components/Popup/Types/PopupExample"}]},{sectionName:"Variations",examples:[{title:"Basic",examplePath:"components/Popup/Variations/PopupExampleBasic"},{title:"Alignment and Position",examplePath:"components/Popup/Variations/PopupExamplePosition"}]}]}});
