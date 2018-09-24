1ff9900261031a33126024f973761ba34329e1cf
Hugh Eland <hughreeling@users.noreply.github.com>
2018-09-24 10:17:28 +0200

    docs(comments): Improved docsite comments for 'variables' (#262)
    * Docs(comments): Improved docsite comments for 'variables'
    
    * Fixing accidental file change

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -80,7 +80,7 @@ class Text extends UIComponent<Extendable<ITextProps>, any> {
     /** Custom styles to be applied for component. */
     styles: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
 
-    /** Custom variables to be applied for component. */
+    /** Override for theme site variables to allow modifications of component styling via themes. */
     variables: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
   }
 
