2d2f484cebc396f9b8c00633941bf21218572f39
Levi Thomason <me@levithomason.com>
2018-09-25 12:29:06 -0700

    deploy docs [ci skip]

---
+++ 17.b368cdd56d782506882f.js
@@ -0,0 +1,6 @@
+webpackJsonp([17],{2031:/*!**************************************************!*\
+  !*** ./docs/src/exampleMenus/Chat.examples.json ***!
+  \**************************************************/
+/*! dynamic exports provided */
+/*! all exports used */
+function(a,e){a.exports=[{sectionName:"Types",examples:[{title:"Default",examplePath:"components/Chat/Types/ChatExample"}]},{sectionName:"Variations",examples:[{title:"Avatar",examplePath:"components/Chat/Variations/ChatExampleAvatar"}]}]}});
