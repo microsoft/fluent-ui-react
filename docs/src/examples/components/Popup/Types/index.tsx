2a3b3e6caf4d32a30f0a0d21c8e045369303889e
kuzhelov <deckster@mail.ru>
2018-10-01 18:11:41 +0300

    feat(Popup): refactor and add controlled mode (#282)
    * first round of changes
    
    * introduce auto controlled 'open' prop to Popup
    
    * adjust examples code
    
    * introduce defaultOpen prop
    
    * fix unit tests
    
    * address lint issues
    
    * add keys for example components array
    
    * use 'open' from destructured props
    
    * fix prop types for Popup
    
    * inline button styles in docs
    
    * rename example class
    
    * fix onOpenChange handler's logic
    
    * fix PopupPosition example
    
    * add handling 'open' prop change event to the default example
    
    * remove data-placement attribute
    
    * update changelog

--- docs/src/examples/components/Popup/Types/index.tsx
+++ docs/src/examples/components/Popup/Types/index.tsx
@@ -7,7 +7,7 @@ const Types = () => (
   <ExampleSection title="Types">
     <ComponentExample
       title="Default"
-      description="A default popup."
+      description="A default popup. Note that Popup is a controlled component, and its 'open' prop value could be changed either by parent component, or by user actions (keypress) - thus it is necessary to handle 'onOpenChanged' event. Try to press space key while button is focused to see the effect."
       examplePath="components/Popup/Types/PopupExample"
     />
   </ExampleSection>
