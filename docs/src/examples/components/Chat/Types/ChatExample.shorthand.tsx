e5b6e51628c8c17f8b2c67fb9d930532bd17e4d3
manajdov <manajdov@microsoft.com>
2018-10-01 15:45:24 +0200

    -providede the content API for the ChatItems in the shorthand example for the Chat

--- docs/src/examples/components/Chat/Types/ChatExample.shorthand.tsx
+++ docs/src/examples/components/Chat/Types/ChatExample.shorthand.tsx
@@ -22,28 +22,39 @@ const items = [
       />
     ),
   },
-  <Chat.Item>
-    <Chat.Message
-      content="Would you like to grab a lunch?"
-      author="John Doe"
-      timestamp="Yesterday, 10:16 PM"
-      mine
-    />
-  </Chat.Item>,
-  <Chat.Item>
-    <Chat.Message
-      content="Sure! Let's try the new place downtown"
-      author="Jane Doe"
-      timestamp="Yesterday, 10:15 PM"
-      avatar={janeAvatar}
-    />
-  </Chat.Item>,
-  <Chat.Item>
-    <Divider content="Today" type="primary" important />
-  </Chat.Item>,
-  <Chat.Item>
-    <Chat.Message content="Let's have a call" author="John Doe" timestamp="Today, 11:15 PM" mine />
-  </Chat.Item>,
+  {
+    content: (
+      <Chat.Message
+        content="Would you like to grab a lunch?"
+        author="John Doe"
+        timestamp="Yesterday, 10:16 PM"
+        mine
+      />
+    ),
+  },
+  {
+    content: (
+      <Chat.Message
+        content="Sure! Let's try the new place downtown"
+        author="Jane Doe"
+        timestamp="Yesterday, 10:15 PM"
+        avatar={janeAvatar}
+      />
+    ),
+  },
+  {
+    content: <Divider content="Today" type="primary" important />,
+  },
+  {
+    content: (
+      <Chat.Message
+        content="Let's have a call"
+        author="John Doe"
+        timestamp="Today, 11:15 PM"
+        mine
+      />
+    ),
+  },
 ]
 
 const ChatExample = () => <Chat items={items} />
