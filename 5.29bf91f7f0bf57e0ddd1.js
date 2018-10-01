f1f77083b9b268234e1a7c8c517804e22cdd54c4
Levi Thomason <me@levithomason.com>
2018-10-01 11:18:16 -0700

    deploy docs [ci skip]

---
+++ 5.29bf91f7f0bf57e0ddd1.js
@@ -0,0 +1,6 @@
+webpackJsonp([5],{2052:/*!***************************************************!*\
+  !*** ./docs/src/exampleMenus/Popup.examples.json ***!
+  \***************************************************/
+/*! dynamic exports provided */
+/*! all exports used */
+function(e,p){e.exports=[{sectionName:"Types",examples:[{title:"Default",examplePath:"components/Popup/Types/PopupExample"}]},{sectionName:"Variations",examples:[{title:"Alignment and Position",examplePath:"components/Popup/Variations/PopupExamplePosition"}]}]}});
