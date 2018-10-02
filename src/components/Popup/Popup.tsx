2b450b5a1215a4a38ed1786f3864936b656f0df4
kuzhelov <kuroman@microsoft.com>
2018-10-03 01:51:54 +0300

    render popup as body children

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react'
+import { createPortal } from 'react-dom'
 import * as PropTypes from 'prop-types'
 import _ from 'lodash'
 import { Popper, PopperChildrenProps } from 'react-popper'
 
-import { childrenExist, AutoControlledComponent, IRenderResultConfig } from '../../lib'
+import { childrenExist, AutoControlledComponent, IRenderResultConfig, isBrowser } from '../../lib'
 import {
   ComponentEventHandler,
   ItemShorthand,
@@ -104,6 +105,8 @@ export default class Popup extends AutoControlledComponent<Extendable<IPopupProp
 
   public static autoControlledProps = ['open']
 
+  private static isBrowserContext = isBrowser()
+
   protected actionHandlers: AccessibilityActionHandlers = {
     toggle: e =>
       _.invoke(this.props, 'onOpenChange', e, { ...this.props, ...{ open: !this.props.open } }),
@@ -135,7 +138,9 @@ export default class Popup extends AutoControlledComponent<Extendable<IPopupProp
             ...accessibility.keyHandlers.trigger,
           })}
         </Ref>
-        {open && this.renderPopupContent(rtl, accessibility)}
+        {open &&
+          Popup.isBrowserContext &&
+          createPortal(this.renderPopupContent(rtl, accessibility), document.body)}
       </>
     )
   }
