d011db2756286dffc4ec8c4b27b94b37371a826b
Brigette Lundeen <brlundee@microsoft.com>
2018-10-04 10:12:40 -0700

    adding in theme background aware doc examples

--- docs/src/components/ComponentDoc/ComponentExample/ComponentExample.tsx
+++ docs/src/components/ComponentDoc/ComponentExample/ComponentExample.tsx
@@ -687,13 +687,23 @@ class ComponentExample extends React.Component<IComponentExampleProps, IComponen
             </Grid.Column>
           )}
 
-          <Grid.Column
-            width={16}
-            className={`rendered-example ${this.getKebabExamplePath()}`}
-            style={{ padding: '2rem' }}
-          >
-            <div dir={showRtl ? 'rtl' : undefined}>{exampleElement}</div>
-          </Grid.Column>
+          <Provider.Consumer
+            render={({ siteVariables }) => {
+              return (
+                <Grid.Column
+                  width={16}
+                  className={`rendered-example ${this.getKebabExamplePath()}`}
+                  style={{
+                    padding: '2rem',
+                    backgroundColor: siteVariables.bodyBackground,
+                    color: siteVariables.bodyColor,
+                  }}
+                >
+                  <div dir={showRtl ? 'rtl' : undefined}>{exampleElement}</div>
+                </Grid.Column>
+              )
+            }}
+          />
           <Grid.Column width={16} style={{ padding: 0, background: EDITOR_BACKGROUND_COLOR }}>
             {this.renderJSX()}
             {this.renderError()}
--- src/themes/teams-high-contrast/siteVariables.ts
+++ src/themes/teams-high-contrast/siteVariables.ts
@@ -1,6 +1,12 @@
 import { white } from '../teams/siteVariables'
 
 //
+// COLORS
+//
+export const black = '#000'
+
+//
 // SEMANTIC ASSIGNMENTS
 //
+export const bodyBackground = black
 export const bodyColor = white
