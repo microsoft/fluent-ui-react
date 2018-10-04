a603e98992a92afe27ca450c035b296d6040fef8
Brigette Lundeen <brlundee@microsoft.com>
2018-10-04 10:30:37 -0700

    redlining the themeing for text component in Teams dark and contrast themes

--- src/themes/teams-high-contrast/siteVariables.ts
+++ src/themes/teams-high-contrast/siteVariables.ts
@@ -1,12 +1,31 @@
-import { white } from '../teams/siteVariables'
+import { white, green } from '../teams/siteVariables'
 
 //
 // COLORS
 //
 export const black = '#000'
 
+export const hyperlinkColor = '#ffff01'
+export const disabledColor = '#3ff23f'
+export const selectionBackgroundColor = '#1aebff'
+
+export const red = '#f00'
+export const green04 = green
+
 //
 // SEMANTIC ASSIGNMENTS
 //
 export const bodyBackground = black
 export const bodyColor = white
+
+//
+// TEXT VARIABLES
+//
+export const importantTextColor = red
+export const atMentionMeTextColor = hyperlinkColor
+export const atMentionOtherTextColor = hyperlinkColor
+export const disabledTextColor = disabledColor
+export const errorTextColor = red
+export const successTextColor = green04
+export const timestampTextColor = white
+export const timestampHoverTextColor = white
