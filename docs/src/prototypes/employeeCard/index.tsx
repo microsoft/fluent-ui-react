f12306e0d74bb4c67e7500a05e85a7086e570cab
manajdov <manajdov@microsoft.com>
2018-10-04 19:11:59 +0200

    -added headers to the examples

--- docs/src/prototypes/employeeCard/index.tsx
+++ docs/src/prototypes/employeeCard/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import EmployeeCard from './EmployeeCard'
-import Divider from '../../../../src/components/Divider/Divider'
+import { Divider, Header } from '@stardust-ui/react'
 import AvatarEmployeeCard from './AvatarEmployeeCard'
 
 class EmployeeCardPrototype extends React.Component<any, { popupOpen: boolean }> {
@@ -20,8 +20,21 @@ class EmployeeCardPrototype extends React.Component<any, { popupOpen: boolean }>
     }
     return (
       <div style={{ margin: '20px' }}>
+        <Header
+          as="h3"
+          content="Employee Card"
+          description={{ content: 'Simple employee card component.', styles: { fontSize: '16px' } }}
+        />
         <EmployeeCard {...employee} />
         <Divider variables={{ dividerColor: 'transparent' }} />
+        <Header
+          as="h3"
+          content="Avatar Employee Card"
+          description={{
+            content: "Avatar component that show's the employee's card on hovering.",
+            styles: { fontSize: '16px' },
+          }}
+        />
         <AvatarEmployeeCard {...employee} />
       </div>
     )
