b390b6429ae6ed194709f37fa5c8b039287c4ed7
Levi Thomason <me@levithomason.com>
2018-10-09 13:09:07 -0700

    wip

---
+++ src/components/Layout3d/Layout3d.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+
+class Layout3d extends React.Component<any, any> {
+  render() {
+    const { media, header, description, endMedia } = this.props
+    return (
+      <div
+        style={
+          {
+            display: 'grid',
+          } as React.CSSProperties
+        }
+      >
+        {children}
+      </div>
+    )
+  }
+}
+
+export default Layout3d
