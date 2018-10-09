6b9b926b90e04cfde20dcc8ac8df9129e939cf93
manajdov <manajdov@microsoft.com>
2018-10-09 11:22:42 +0200

    -added logic for expanding the shorthand css props in the nested pseudo classes too -added test for the new fela plugin

--- src/lib/felaExpandCssShorthandsPlugin.ts
+++ src/lib/felaExpandCssShorthandsPlugin.ts
@@ -5,13 +5,25 @@ export default () => {
   const expandCssShorthands = styles => {
     return _.keys(styles).reduce((acc, cssPropertyName) => {
       const cssPropertyValue = styles[cssPropertyName]
+
+      if (typeof cssPropertyValue === 'object') {
+        return { ...acc, [cssPropertyName]: expandCssShorthands(cssPropertyValue) }
+      }
+
       const expandedProps = expand(_.kebabCase(cssPropertyName), String(cssPropertyValue))
       if (expandedProps) {
-        return { ...acc, ...expandedProps }
+        return { ...acc, ...transformKebabCaseKeysToCamelCase(expandedProps) }
       }
+
       return { ...acc, [cssPropertyName]: cssPropertyValue }
     }, {})
   }
 
   return expandCssShorthands
+}
+
+const transformKebabCaseKeysToCamelCase = obj => {
+  return _.mapKeys(obj, (value, key) => {
+    return _.camelCase(key)
+  })
 }
