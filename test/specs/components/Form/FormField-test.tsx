c5deb09360d51951d743737b78d6ba315717f8e4
manajdov <manajdov@microsoft.com>
2018-10-16 17:23:43 +0200

    -added some tests

--- test/specs/components/Form/FormField-test.tsx
+++ test/specs/components/Form/FormField-test.tsx
@@ -1,8 +1,9 @@
+import * as React from 'react'
 import { isConformant } from 'test/specs/commonTests'
 
-import FormField from 'src/components/Form/FormField'
 import { implementsShorthandProp } from '../../commonTests'
-import Text from '../../../../src/components/Text'
+import { Button, RadioGroup, Input, Text, FormField } from '../../../../src/'
+import { mountWithProvider } from '../../../utils'
 
 const formFieldImplementsShorthandProp = implementsShorthandProp(FormField)
 
@@ -10,4 +11,28 @@ describe('FormField', () => {
   isConformant(FormField)
   formFieldImplementsShorthandProp('label', Text)
   formFieldImplementsShorthandProp('message', Text)
+
+  it('renders the control provided in the controlType prop', () => {
+    const controls = [Button, Input, 'input', RadioGroup]
+    controls.forEach(control => {
+      const formField = mountWithProvider(
+        <FormField controlType={control} name="firstName" />,
+      ).find('FormField')
+
+      const controlElement = formField.find(control)
+      expect(controlElement.length).toEqual(1)
+    })
+  })
+
+  it('uses the control prop as a shorthand together with the controlType', () => {
+    const controls = [Button, Input, 'input', RadioGroup]
+    controls.forEach(control => {
+      const formField = mountWithProvider(
+        <FormField controlType={control} control={{ name: 'firstName' }} />,
+      ).find('FormField')
+
+      const controlElement = formField.find(control)
+      expect(controlElement.first().props()).toMatchObject({ name: 'firstName' })
+    })
+  })
 })
