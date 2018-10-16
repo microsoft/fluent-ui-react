c9ac44c5d2c3dd1cad3fc75f5cccfe8749676371
Andrew Martin <andmarti@microsoft.com>
2018-10-15 18:04:22 -0700

    fix: grays in Teams dark theme siteVariables (#362)

--- src/themes/teams-dark/siteVariables.ts
+++ src/themes/teams-dark/siteVariables.ts
@@ -4,20 +4,21 @@ import { white, green } from '../teams/siteVariables'
 // COLORS
 //
 export const black = '#201f1f'
+
 export const gray02 = '#c8c6c4'
 export const gray03 = '#b3b0ad'
 export const gray04 = '#8a8886'
 export const gray06 = '#605e5c'
 export const gray08 = '#484644'
-export const gray09 = '#323130'
-export const gray10 = '#2d2c2c'
+export const gray09 = '#3b3a39'
+export const gray10 = '#323130'
 export const gray14 = '#292828'
 
 export const brand = '#6264A7'
 export const brand02 = '#e2e2f6'
 export const brand04 = '#bdbde6'
 export const brand06 = '#a6a7dc'
-export const brand08 = '#393942'
+export const brand08 = '#8b8cc7'
 export const brand12 = brand
 export const brand14 = '#464775'
 export const brand16 = '#33344a'
