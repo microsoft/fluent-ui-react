ff49338cf1132d3d3c0c7c174569e445ed7666ad
musingh <musingh@microsoft.com>
2018-10-17 14:51:27 -0700

    Reverting endmedia example change

--- docs/src/examples/components/List/Content/ListExampleEndMedia.tsx
+++ docs/src/examples/components/List/Content/ListExampleEndMedia.tsx
@@ -4,21 +4,18 @@ import { List } from '@stardust-ui/react'
 const ellipsis = <span>&hellip;</span>
 
 const ListExample = () => (
-  <List selection>
+  <List>
     <List.Item
       content="Program the sensor to the SAS alarm through the haptic SQL card!"
       endMedia={ellipsis}
-      selection
     />
     <List.Item
       content="Use the online FTP application to input the multi-byte application!"
       endMedia={ellipsis}
-      selection
     />
     <List.Item
       content="The GB pixel is down, navigate the virtual interface!"
       endMedia={ellipsis}
-      selection
     />
   </List>
 )
