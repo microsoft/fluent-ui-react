a3fdefc7558f59300be74988fb252305ffc8f8eb
Andrew Martin <andmarti@microsoft.com>
2018-10-22 15:35:37 -0700

    border bottom

--- src/themes/teams-high-contrast/components/Input/inputVariables.ts
+++ src/themes/teams-high-contrast/components/Input/inputVariables.ts
@@ -4,7 +4,7 @@ export default (siteVars: any) => {
   return {
     backgroundColor: siteVars.bodyBackground,
     border: `${pxToRem(1)} solid ${siteVars.bodyColor}`,
-    inputFocusBorderColor: siteVars.accessibleYellow,
+    borderBottom: `${pxToRem(2)} solid ${siteVars.accessibleYellow}`,
     fontColor: siteVars.bodyColor,
     iconColor: siteVars.bodyColor,
   }
