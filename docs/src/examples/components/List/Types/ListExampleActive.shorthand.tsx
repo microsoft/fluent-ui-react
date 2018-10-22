187d42dfbbc0de15d3644b657eb50b3fcaa49c12
Gopal Goel <gogoe@microsoft.com>
2018-10-22 18:58:46 +0530

    Make List example change active prop on clicking

--- docs/src/examples/components/List/Types/ListExampleActive.shorthand.tsx
+++ docs/src/examples/components/List/Types/ListExampleActive.shorthand.tsx
@@ -1,32 +1,52 @@
 import React from 'react'
 import { List, Image } from '@stardust-ui/react'
 
-const ListExampleActive = ({ knobs }) => {
-  const items = [
-    {
-      key: 'irving',
-      media: <Image src="public/images/avatar/small/matt.jpg" avatar />,
-      header: 'Irving Kuhic',
-      headerMedia: '7:26:56 AM',
-      content: 'Program the sensor to the SAS alarm through the haptic SQL card!',
-      active: knobs.active,
-    },
-    {
-      key: 'skyler',
-      media: <Image src="public/images/avatar/small/steve.jpg" avatar />,
-      header: 'Skyler Parks',
-      headerMedia: '11:30:17 PM',
-      content: 'Use the online FTP application to input the multi-byte application!',
-    },
-    {
-      key: 'dante',
-      media: <Image src="public/images/avatar/small/nom.jpg" avatar />,
-      header: 'Dante Schneider',
-      headerMedia: '5:22:40 PM',
-      content: 'The GB pixel is down, navigate the virtual interface!',
-    },
-  ]
-  return <List items={items} />
+class ListExampleActive extends React.Component<any, any> {
+  state = {
+    key: 'irving',
+  }
+
+  handleClick = key => {
+    this.setState(() => ({
+      key,
+    }))
+  }
+
+  render() {
+    const items = [
+      {
+        key: 'irving',
+        media: <Image src="public/images/avatar/small/matt.jpg" avatar />,
+        header: 'Irving Kuhic',
+        headerMedia: '7:26:56 AM',
+        content: 'Program the sensor to the SAS alarm through the haptic SQL card!',
+      },
+      {
+        key: 'skyler',
+        media: <Image src="public/images/avatar/small/steve.jpg" avatar />,
+        header: 'Skyler Parks',
+        headerMedia: '11:30:17 PM',
+        content: 'Use the online FTP application to input the multi-byte application!',
+      },
+      {
+        key: 'dante',
+        media: <Image src="public/images/avatar/small/nom.jpg" avatar />,
+        header: 'Dante Schneider',
+        headerMedia: '5:22:40 PM',
+        content: 'The GB pixel is down, navigate the virtual interface!',
+      },
+    ]
+    return (
+      <List
+        items={items.map(item => {
+          if (item.key === this.state.key) item['active'] = this.props.knobs.active
+          else item['active'] = false
+          item['onClick'] = () => this.handleClick(item.key)
+          return item
+        })}
+      />
+    )
+  }
 }
 
 export default ListExampleActive
--- docs/src/examples/components/List/Types/ListExampleActive.tsx
+++ docs/src/examples/components/List/Types/ListExampleActive.tsx
@@ -1,31 +1,50 @@
 import React from 'react'
 import { List, Image } from '@stardust-ui/react'
 
-const ListExampleActive = ({ knobs }) => (
-  <List>
-    <List.Item
-      key="irving"
-      media={<Image src="public/images/avatar/small/matt.jpg" avatar />}
-      header="Irving Kuhic"
-      headerMedia="7:26:56 AM"
-      content="Program the sensor to the SAS alarm through the haptic SQL card!"
-      active={knobs.active}
-    />
-    <List.Item
-      key="skyler"
-      media={<Image src="public/images/avatar/small/steve.jpg" avatar />}
-      header="Skyler Parks"
-      headerMedia="11:30:17 PM"
-      content="Use the online FTP application to input the multi-byte application!"
-    />
-    <List.Item
-      key="dante"
-      media={<Image src="public/images/avatar/small/nom.jpg" avatar />}
-      header="Dante Schneider"
-      headerMedia="5:22:40 PM"
-      content="The GB pixel is down, navigate the virtual interface!"
-    />
-  </List>
-)
+class ListExampleActive extends React.Component<any, any> {
+  state = {
+    key: 'irving',
+  }
+
+  handleClick = key => {
+    this.setState(() => ({
+      key,
+    }))
+  }
+
+  render() {
+    return (
+      <List>
+        <List.Item
+          key="irving"
+          media={<Image src="public/images/avatar/small/matt.jpg" avatar />}
+          header="Irving Kuhic"
+          headerMedia="7:26:56 AM"
+          content="Program the sensor to the SAS alarm through the haptic SQL card!"
+          active={this.state.key === 'irving' ? this.props.knobs.active : false}
+          onClick={() => this.handleClick('irving')}
+        />
+        <List.Item
+          key="skyler"
+          media={<Image src="public/images/avatar/small/steve.jpg" avatar />}
+          header="Skyler Parks"
+          headerMedia="11:30:17 PM"
+          content="Use the online FTP application to input the multi-byte application!"
+          active={this.state.key === 'skyler' ? this.props.knobs.active : false}
+          onClick={() => this.handleClick('skyler')}
+        />
+        <List.Item
+          key="dante"
+          media={<Image src="public/images/avatar/small/nom.jpg" avatar />}
+          header="Dante Schneider"
+          headerMedia="5:22:40 PM"
+          content="The GB pixel is down, navigate the virtual interface!"
+          active={this.state.key === 'dante' ? this.props.knobs.active : false}
+          onClick={() => this.handleClick('dante')}
+        />
+      </List>
+    )
+  }
+}
 
 export default ListExampleActive
