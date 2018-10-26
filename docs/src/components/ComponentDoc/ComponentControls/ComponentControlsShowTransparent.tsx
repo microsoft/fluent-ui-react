2af01f67160d28d70538d6a0b55bfc037a39b8e8
Levi Thomason <me@levithomason.com>
2018-10-26 14:35:36 -0700

    docs: add transparent button to examples

---
+++ docs/src/components/ComponentDoc/ComponentControls/ComponentControlsShowTransparent.tsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types'
+import * as React from 'react'
+import { Icon, Menu } from 'semantic-ui-react'
+
+import { updateForKeys } from 'docs/src/hoc'
+
+const ComponentControlsShowTransparent: React.SFC = ({ active, onClick }: any) => (
+  <Menu.Item active={active} onClick={onClick}>
+    <Icon color={active ? 'green' : 'grey'} size="large" name="adjust" fitted />
+    Transparent
+  </Menu.Item>
+)
+
+ComponentControlsShowTransparent.propTypes = {
+  active: PropTypes.bool,
+  onClick: PropTypes.func,
+}
+
+export default updateForKeys(['active'])(ComponentControlsShowTransparent)
