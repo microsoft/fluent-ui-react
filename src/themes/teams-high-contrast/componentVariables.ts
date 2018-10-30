a26b31994c666cf65cdf031941606e8a92c82a83
Brian Calvery <bcalvery@microsoft.com>
2018-10-30 14:39:31 -0700

    undoing unnecessary file changes

--- src/themes/teams-high-contrast/componentVariables.ts
+++ src/themes/teams-high-contrast/componentVariables.ts
@@ -1,3 +1,2 @@
-export { default as Divider } from './components/Divider/dividerVariables'
-
 export { default as Text } from './components/Text/textVariables'
+export { default as Divider } from './components/Divider/dividerVariables'
