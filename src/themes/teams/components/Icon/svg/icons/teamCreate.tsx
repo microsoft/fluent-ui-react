5d82d22924cbaf5d92f68b39276270c4ead55625
Brigette Lundeen <brlundee@microsoft.com>
2018-10-30 11:24:46 -0700

    adding some new icons

--- src/themes/teams/components/Icon/svg/icons/teamCreate.tsx
+++ src/themes/teams/components/Icon/svg/icons/teamCreate.tsx
@@ -3,7 +3,7 @@ import { TeamsSvgIconSpec } from '../types'
 
 export default {
   icon: ({ classes }) => (
-    <svg className={classes.svg} viewBox="0 0 32 32" role="presentation" focusable="false">
+    <svg className={classes.svg} viewBox="8 8 16 16" role="presentation" focusable="false">
       <g>
         <path
           d="M10,11c-1.1,0-2,0.9-2,2s0.9,2,2,2s2-0.9,2-2S11.1,11,10,11z M10,14c-0.6,0-1-0.4-1-1s0.4-1,1-1s1,0.4,1,1S10.6,14,10,14zM16,14c1.1,0,2-0.9,2-2s-0.9-2-2-2s-2,0.9-2,2S14.9,14,16,14z M16,11c0.6,0,1,0.4,1,1s-0.4,1-1,1s-1-0.4-1-1S15.4,11,16,11zM13,20c0,1.3,1.2,2,3,2c1.9,0,3-0.7,3-2v-5h-6V20z M14,16h4v4c0,1-1.7,1.1-2,1.1S14,21,14,20V16zM11.5,17c0.3,0,0.5-0.2,0.5-0.5c0-0.3-0.2-0.5-0.5-0.5H8v3c0,1.3,1.1,2,3,2h0.5c0.3,0,0.5-0.2,0.5-0.5
