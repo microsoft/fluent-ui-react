8382bb0f5a1d228010ca4680d785a4d9301a132b
Silviu Avram <siavram@microsoft.com>
2018-10-30 14:14:45 +0100

    added cypress for typescript

---
+++ cypress/integration/form.spec.ts
@@ -0,0 +1,9 @@
+describe('combobox', () => {
+  before(() => {
+    cy.visit('/components/form')
+  })
+
+  beforeEach(() => {})
+
+  it('loads', () => {})
+})
--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { createPortal } from 'react-dom'
 import * as PropTypes from 'prop-types'
-import _ from 'lodash'
+import * as _ from 'lodash'
 import { Popper, PopperChildrenProps } from 'react-popper'
 
 import {
