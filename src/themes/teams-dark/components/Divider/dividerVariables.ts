5bb41a4358f525b86ead966cd7f4a5829110fbab
Brian Calvery <capro@capro.org>
2018-10-31 13:47:44 -0700

    fix(Avatar): Adding dark and contrast theme files for Avatar and Status Indicator. (#373)
    * Adding dark and contrast theme files for Avatar and Status Indicator.
    
    * Making Avatar Status Indicator respond to theme change.
    
    * pass through styles variables and classes for icon in status indicator
    
    * reverting changes for avatar example shorthand.
    
    * changing language for usage example.
    giving contrast theme status colors.
    fixing contrast theme status border width.
    
    * changes based on Roman's comments.
    
    * undoing unnecessary file changes
    
    * remove duplication in theme variations
    
    * Added changelog entries

--- src/themes/teams-dark/components/Divider/dividerVariables.ts
+++ src/themes/teams-dark/components/Divider/dividerVariables.ts
@@ -1,6 +1,7 @@
-export default (siteVars: any) => {
-  return {
-    primaryColor: siteVars.brand06,
-    textColor: siteVars.gray02,
-  }
-}
+import { DividerVariables } from '../../../teams/components/Divider/dividerVariables'
+import { Partial } from 'types/utils'
+
+export default (siteVars: any): Partial<DividerVariables> => ({
+  primaryColor: siteVars.brand06,
+  textColor: siteVars.gray02,
+})
--- src/themes/teams-high-contrast/siteVariables.ts
+++ src/themes/teams-high-contrast/siteVariables.ts
@@ -13,6 +13,21 @@ export const red = '#f00'
 export const green04 = green
 
 //
+// STATUS COLORS
+//
+// TODO: bcalvery - color alone is not an adequate means for differentiating in an accessible way.
+export const successStatusBackgroundColor = accessibleGreen
+export const successStatusTextColor = black
+export const infoStatusBackgroundColor = accessibleCyan
+export const infoStatusTextColor = black
+export const warningStatusBackgroundColor = accessibleYellow
+export const warningStatusTextColor = black
+export const errorStatusBackgroundColor = red
+export const errorStatusTextColor = black
+export const unknownStatusBackgroundColor = white
+export const unknownStatusTextColor = black
+
+//
 // SEMANTIC ASSIGNMENTS
 //
 export const bodyBackground = black
