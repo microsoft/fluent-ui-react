8ee8f5df0950faa2c52cf1353e242a399835a531
manajdov <manajdov@microsoft.com>
2018-10-31 17:45:41 +0100

    -refactored FormsyFormField

--- docs/src/prototypes/formValidation/FormsyFormField.tsx
+++ docs/src/prototypes/formValidation/FormsyFormField.tsx
@@ -2,63 +2,61 @@ import * as React from 'react'
 import { FormField } from '@stardust-ui/react'
 import { withFormsy } from 'formsy-react'
 
-class FormFieldWrapper extends React.Component {
-  state = { error: '' }
+const FormFieldWrapper = props => {
+  const {
+    children,
+    className,
+    id,
+    control,
+    inline,
+    label,
+    message,
+    name,
+    renderControl,
+    renderLabel,
+    renderMessage,
+    required,
+    styles,
+    type,
+    variables,
+    showMessage,
+    valueProp,
+    onChangeProp,
+    eventTargetAsValue,
+    getErrorMessage,
+    getValue,
+    setValue,
+  } = props
 
-  render() {
-    const error = (this.props as any).getErrorMessage()
-    const {
-      children,
-      className,
-      id,
-      control,
-      inline,
-      label,
-      message,
-      name,
-      renderControl,
-      renderLabel,
-      renderMessage,
-      required,
-      styles,
-      type,
-      variables,
-      showMessage,
-      valueProp,
-      onChangeProp,
-      eventTargetAsValue,
-    } = this.props as any
+  const error = getErrorMessage()
+  const value = valueProp || 'value'
+  const onChange = onChangeProp || 'onChange'
 
-    const value = valueProp || 'value'
-    const onChange = onChangeProp || 'onChange'
-
-    const formFieldProps = {
-      children,
-      className,
-      id,
-      inline,
-      label,
-      message,
-      name,
-      renderControl,
-      renderLabel,
-      renderMessage,
-      required,
-      styles,
-      type,
-      variables,
-      ...(!children && {
-        control: {
-          ...control,
-          [value]: (this.props as any).getValue(),
-          [onChange]: (e, props) =>
-            (this.props as any).setValue(eventTargetAsValue ? e.target.value : props.value),
-        },
-        ...(showMessage && { message: { content: error, styles: { color: 'red' } } }),
-      }),
-    }
-    return <FormField {...formFieldProps} />
+  const formFieldProps = {
+    children,
+    className,
+    id,
+    inline,
+    label,
+    message,
+    name,
+    renderControl,
+    renderLabel,
+    renderMessage,
+    required,
+    styles,
+    type,
+    variables,
+    ...(!children && {
+      control: {
+        ...control,
+        [value]: getValue(),
+        [onChange]: (e, props) => setValue(eventTargetAsValue ? e.target.value : props.value),
+      },
+      ...(showMessage && { message: { content: error, styles: { color: 'red' } } }),
+    }),
   }
+  return <FormField {...formFieldProps} />
 }
 
 export default withFormsy(FormFieldWrapper)
