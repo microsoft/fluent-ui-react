dee136c9945754d086bdd2ac3257d9e5c2c489b2
Andrew Martin <andmarti@microsoft.com>
2018-11-02 16:15:01 -0700

    removing trailing space

--- src/themes/teams-high-contrast/componentVariables.ts
+++ src/themes/teams-high-contrast/componentVariables.ts
@@ -1,7 +1,7 @@
 export { default as Input } from './components/Input/inputVariables'
 
 export { default as Avatar } from './components/Avatar/avatarVariables'
-  
+
 export { default as Button } from './components/Button/buttonVariables'
 
 export { default as Divider } from './components/Divider/dividerVariables'
