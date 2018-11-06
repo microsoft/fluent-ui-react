7c3b946f2bc21d400fe7f0372aed1035ea21a603
Brigette Lundeen <brlundee@microsoft.com>
2018-11-06 12:36:14 -0800

    fixed a bunch of icons to use correct filled/outline props

--- src/themes/teams/components/Icon/svg/icons/callEnd.tsx
+++ src/themes/teams/components/Icon/svg/icons/callEnd.tsx
@@ -5,7 +5,7 @@ export default {
   icon: ({ classes }) => (
     <svg role="presentation" focusable="false" viewBox="8 8 16 16" className={classes.svg}>
       <path
-        className={classes.outlinePart}
+        className={classes.filledPart}
         d="M23.4,15.6C21.3,13.9,18.7,13,16,13s-5.3,0.9-7.4,2.6C8.1,16,7.9,16.5,8,17.1c0,0.1,0.2,0.5,0.2,0.5
                   c0.4,0.6,0.9,1.4,1.9,1.2l0.7-0.1c1.1-0.2,2.5-0.8,2.7-2.1l0.2-1.1l4.2,0l0.2,1.1c0.3,1.3,1.7,1.9,2.7,2l0.7,0.1c0.1,0,0.2,0,0.2,0
                   c0.7,0,1.1-0.5,1.5-1c0,0,0.4-0.7,0.4-0.7C24.1,16.5,23.9,16,23.4,15.6z"
