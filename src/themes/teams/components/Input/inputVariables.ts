471c0b180b84e13706356bf2d318525e71e6f5c2
Levi Thomason <me@levithomason.com>
2018-11-06 14:34:43 -0800

    cleanup nits

--- src/themes/teams/components/Input/inputVariables.ts
+++ src/themes/teams/components/Input/inputVariables.ts
@@ -14,29 +14,20 @@ export interface InputVariables {
   inputFocusBorderRadius: string
 }
 
-const [_1px_asRem, _2px_asRem, _3px_asRem, _6px_asRem, _12px_asRem, _24px_asRem] = [
-  1,
-  2,
-  3,
-  6,
-  12,
-  24,
-].map(v => pxToRem(v))
-
 export default (siteVars): InputVariables => ({
   backgroundColor: siteVars.gray10,
-  border: `${_1px_asRem} solid transparent`,
-  borderRadius: _3px_asRem,
-  boxShadow: `0 ${_1px_asRem} 0 ${siteVars.brand}`,
+  border: `${pxToRem(1)} solid transparent`,
+  borderRadius: pxToRem(3),
+  boxShadow: `0 ${pxToRem(1)} 0 ${siteVars.brand}`,
 
   fontColor: siteVars.bodyColor,
   fontSize: siteVars.fontSizes.medium,
 
   iconPosition: 'absolute',
-  iconRight: _2px_asRem,
+  iconRight: pxToRem(2),
   iconColor: siteVars.bodyColor,
 
-  inputPadding: `${_6px_asRem} ${_24px_asRem} ${_6px_asRem} ${_12px_asRem}`,
+  inputPadding: `${pxToRem(6)} ${pxToRem(4)} ${pxToRem(6)} ${pxToRem(2)}`,
   inputFocusBorderBottomColor: siteVars.brand,
-  inputFocusBorderRadius: `${_3px_asRem} ${_3px_asRem} ${_2px_asRem} ${_2px_asRem}`,
+  inputFocusBorderRadius: `${pxToRem(3)} ${pxToRem(3)} ${pxToRem(2)} ${pxToRem(2)}`,
 })
