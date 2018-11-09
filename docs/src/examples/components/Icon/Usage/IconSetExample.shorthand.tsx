ba4821419275d7e6ff9ff25a1afe2b2539040d91
Brigette Lundeen <brlundee@microsoft.com>
2018-11-09 15:37:30 -0800

    separating out the filled / outline icon versions in the icon set example

--- docs/src/examples/components/Icon/Usage/IconSetExample.shorthand.tsx
+++ docs/src/examples/components/Icon/Usage/IconSetExample.shorthand.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid, Icon, Provider } from '@stardust-ui/react'
+import { Grid, Divider, Header, Icon, Provider } from '@stardust-ui/react'
 
 const cellStyles = {
   margin: '10px 0',
@@ -8,22 +8,37 @@ const cellStyles = {
 const IconSetExampleShorthand = () => (
   <Provider.Consumer
     render={theme => (
-      <Grid columns={4} style={{ textAlign: 'center' }}>
-        {Object.keys(theme.icons).map(name => (
-          <div key={name} style={cellStyles}>
-            <Icon name={name} />
-            <br />
-            <code>{name}</code>
-          </div>
-        ))}
-        {Object.keys(theme.icons).map(name => (
-          <div key={name + '-outline'} style={cellStyles}>
-            <Icon name={name} variables={{ outline: true }} />
-            <br />
-            <code>{name} outline</code>
-          </div>
-        ))}
-      </Grid>
+      <>
+        <div>
+          <Divider>
+            <Header as="h3" content="Regular" />
+          </Divider>
+          <Grid columns={4} style={{ textAlign: 'center' }}>
+            {Object.keys(theme.icons).map(name => (
+              <div key={name} style={cellStyles}>
+                <Icon name={name} />
+                <br />
+                <code>{name}</code>
+              </div>
+            ))}
+          </Grid>
+        </div>
+
+        <div>
+          <Divider>
+            <Header as="h3" content="Outline" />
+          </Divider>
+          <Grid columns={4} style={{ textAlign: 'center' }}>
+            {Object.keys(theme.icons).map(name => (
+              <div key={name + '-outline'} style={cellStyles}>
+                <Icon name={name} variables={{ outline: true }} />
+                <br />
+                <code>{name} outline</code>
+              </div>
+            ))}
+          </Grid>
+        </div>
+      </>
     )}
   />
 )
