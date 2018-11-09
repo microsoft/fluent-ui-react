ba764d7267cac3f42b99298e89eaeb87abd72567
olfedias <olfedias@microsoft.com>
2018-11-09 16:24:41 +0200

    docs(handleRef): add JSDoc

--- src/lib/handleRef.ts
+++ src/lib/handleRef.ts
@@ -1,13 +1,23 @@
 import * as React from 'react'
 
+/** A type that ensures that passed prop is defined in props and matches the type. */
 type HandleRefProps<TProp extends string, N> = { [K in TProp]?: React.Ref<N> }
 
+/**
+ * The function that correctly handles passing refs.
+ *
+ * @param props All components props
+ * @param propName The name of a ref prop
+ * @param node A node that should be passed by ref
+ */
 const handleRef = <P extends string, N>(props: HandleRefProps<P, N>, propName: P, node: N) => {
   const ref: React.Ref<N> = props[propName]
 
   if (process.env.NODE_ENV !== 'production') {
     if (typeof ref === 'string') {
-      throw new Error("We don't support refs as string")
+      throw new Error(
+        'We do not support refs as string, this is a legacy API and will be likely to be removed in one of the future releases of React.',
+      )
     }
   }
 
@@ -17,7 +27,7 @@ const handleRef = <P extends string, N>(props: HandleRefProps<P, N>, propName: P
   }
 
   if (typeof ref === 'object') {
-    // @ts-ignore
+    // @ts-ignore The `current` property is defined as readonly, however it a valid way
     ref.current = node
   }
 }
