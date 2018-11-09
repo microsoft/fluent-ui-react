8d2a4a2071ea2e57b252887f640543ae5eed6213
Juraj Kapsiar <jukapsia@microsoft.com>
2018-11-09 11:31:23 +0100

    UT for selectable list item handleClick

--- test/specs/components/List/ListItem-test.tsx
+++ test/specs/components/List/ListItem-test.tsx
@@ -3,24 +3,29 @@ import { isConformant, handlesAccessibility } from 'test/specs/commonTests'
 import { mountWithProvider } from 'test/utils'
 
 import ListItem from 'src/components/List/ListItem'
+import { selectableListItemBehavior } from 'src/lib/accessibility'
 
 describe('ListItem', () => {
   isConformant(ListItem)
   handlesAccessibility(ListItem, { defaultRootRole: 'listitem' })
 
-  describe('handleClick', () => {
+  describe('selectable list handleClick', () => {
     test('is executed when Enter is pressed', () => {
       const onClick = jest.fn()
-      const listItem = mountWithProvider(<ListItem onClick={onClick} />).find('ListItem')
+      const listItem = mountWithProvider(
+        <ListItem accessibility={selectableListItemBehavior} onClick={onClick} />,
+      ).find('ListItem')
       listItem.simulate('keydown', { keyCode: 13 })
-      expect(onClick).not.toHaveBeenCalled()
+      expect(onClick).toHaveBeenCalled()
     })
 
     test('is executed when Spacebar is pressed', () => {
       const onClick = jest.fn()
-      const listItem = mountWithProvider(<ListItem onClick={onClick} />).find('ListItem')
+      const listItem = mountWithProvider(
+        <ListItem accessibility={selectableListItemBehavior} onClick={onClick} />,
+      ).find('ListItem')
       listItem.simulate('keydown', { keyCode: 32 })
-      expect(onClick).not.toHaveBeenCalled()
+      expect(onClick).toHaveBeenCalled()
     })
   })
 })
