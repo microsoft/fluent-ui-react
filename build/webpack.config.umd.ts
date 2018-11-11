8387580957e1ab1e74367497b7492e88511013e1
Miroslav Stastny <mistastn@microsoft.com>
2018-11-11 17:31:52 +0100

    chore: Upgrade to Webpack 4

--- build/webpack.config.umd.ts
+++ build/webpack.config.umd.ts
@@ -1,4 +1,4 @@
-import * as webpack from 'webpack'
+// import * as webpack from 'webpack'
 
 import config from '../config'
 import webpackConfig from '../webpack.config'
@@ -8,6 +8,7 @@ const { paths } = config
 
 const webpackUMDConfig = {
   target: 'web',
+  mode: 'production',
   devtool: false,
   entry: {
     [pkg.name]: paths.src('umd.ts'),
@@ -24,19 +25,22 @@ const webpackUMDConfig = {
     publicPath: '/',
     pathinfo: true,
   },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        unused: true,
-        dead_code: true,
-        warnings: false,
-      },
-    }),
-  ],
+  // plugins: [
+  //   new webpack.optimize.UglifyJsPlugin({
+  //     compress: {
+  //       unused: true,
+  //       dead_code: true,
+  //       warnings: false,
+  //     },
+  //   }),
+  // ],
   resolve: webpackConfig.resolve,
   module: {
     noParse: webpackConfig.module.noParse,
     rules: webpackConfig.module.rules,
+  },
+  performance: {
+    hints: false, // to (temporarily) disable "WARNING in entrypoint size limit: The following entrypoint(s) combined asset size exceeds the recommended limit")
   },
 }
 
--- webpack.config.ts
+++ webpack.config.ts
@@ -12,6 +12,7 @@ const { __DEV__, __PROD__ } = config.compiler_globals
 const webpackConfig: any = {
   name: 'client',
   target: 'web',
+  mode: __DEV__ ? 'development' : 'production',
   entry: {
     app: paths.docsSrc('index'),
     vendor: config.compiler_vendor,
@@ -38,7 +39,7 @@ const webpackConfig: any = {
     readline: 'empty',
   },
   module: {
-    noParse: [/\.json$/, /anchor-js/],
+    noParse: [/anchor-js/],
     rules: [
       {
         test: /\.(js|ts|tsx)$/,
@@ -98,6 +99,9 @@ const webpackConfig: any = {
       'package.json': paths.base('package.json'),
     },
   },
+  performance: {
+    hints: false, // to (temporarily) disable "WARNING in entrypoint size limit: The following entrypoint(s) combined asset size exceeds the recommended limit")
+  },
 }
 
 // ------------------------------------
@@ -129,13 +133,13 @@ if (__PROD__) {
     new webpack.LoaderOptionsPlugin({
       minimize: true,
     }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        unused: true,
-        dead_code: true,
-        warnings: false,
-      },
-    }),
+    // new webpack.optimize.UglifyJsPlugin({
+    //   compress: {
+    //     unused: true,
+    //     dead_code: true,
+    //     warnings: false,
+    //   },
+    // }),
   )
 }
 
