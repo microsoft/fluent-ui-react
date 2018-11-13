3194603365d60b812bed13b1b8244d7236e108b0
Brigette Lundeen <brlundee@microsoft.com>
2018-11-12 16:20:00 -0800

    added more icons

--- src/themes/teams/components/Icon/svg/ProcessedIcons/index-new.ts
+++ src/themes/teams/components/Icon/svg/ProcessedIcons/index-new.ts
@@ -1,22 +1,97 @@
 import { TeamsSvgIconSpec } from '../types'
 
 // IMPORTS
+import customvotesmall from './icons-custom-vote-small'
+import customvote from './icons-custom-vote'
+import cortana from './icons-cortana'
+import copy from './icons-copy'
+import contactlist from './icons-contact-list'
+import connectorbadge from './icons-connector-badge'
+import composeextmenuitem from './icons-compose-ext-menu-item'
+import composeextensionunpin from './icons-compose-extension-unpin'
+import composeextensionpin from './icons-compose-extension-pin'
+import composeextensionbadge from './icons-compose-extension-badge'
+import compose from './icons-compose'
+import collapse from './icons-collapse'
+import codesnippet from './icons-codesnippet'
+import code from './icons-code'
+import closeendcall from './icons-close-end-call'
+import closedcaption from './icons-closed-caption'
+import close from './icons-close'
+import chromeunmaximize from './icons-chrome-unmaximize'
+import chromeminimize from './icons-chrome-minimize'
+import chromemaximize from './icons-chrome-maximize'
+import chromeforward from './icons-chrome-forward'
+import chromeback from './icons-chrome-back'
+import chevronsmright from './icons-chevronsm-right'
+import chevronright from './icons-chevron-right'
+import chevronmedright from './icons-chevronmed-right'
+import chevronmedleft from './icons-chevronmed-left'
+import chevronleftsmall from './icons-chevron-left-small'
+import chevronleft from './icons-chevron-left'
+import chevrondown from './icons-chevron-down'
+import checksmall from './icons-check-small'
+import checked from './icons-checked'
+import checkboxunselected from './icons-checkbox-unselected'
+import checkboxselected from './icons-checkbox-selected'
+import checkbox from './icons-checkbox'
+import check from './icons-check'
+import chat from './icons-chat'
+import channelicon from './icons-channel-icon'
+import changename from './icons-changename'
+import canvasaddpage from './icons-canvas-addpage'
+import callvideolineoff from './icons-call-video-line-off'
+import callvideoline from './icons-call-video-line'
+import callvideofilled from './icons-call-video-filled'
+import callvideo from './icons-call-video'
+import calltransfernotification from './icons-call-transfer-notification'
+import calltransfer from './icons-call-transfer'
+import callswitchcamera from './icons-call-switch-camera'
+import callresume from './icons-call-resume'
+import callrecording from './icons-call-recording'
+import callpstnfull from './icons-call-pstn-full'
+import callpstn from './icons-call-pstn'
+import callparticipantonhold from './icons-call-participant-onhold'
+import callparticipantincomingline from './icons-call-participant-incoming-line'
+import callparticipantincoming from './icons-call-participant-incoming'
+import callparticipantfailed from './icons-call-participant-failed'
+import callparticipantending from './icons-call-participant-ending'
+import callparticipantconnectingline from './icons-call-participant-connecting-line'
+import callparticipantconnecting from './icons-call-participant-connecting'
+import callparticipantconnected from './icons-call-participant-connected'
+import callmissedlinefilled from './icons-call-missed-line-filled'
+import callmissedline from './icons-call-missed-line'
+import callmissed from './icons-call-missed'
+import callmicrophoneunmuting from './icons-call-microphone-unmuting'
+import callmicrophoneofffilled from './icons-call-microphone-off-filled'
+import callmeetupline from './icons-call-meetup-line'
+import callmeetupfilled from './icons-call-meetup-filled'
+import callincomingvideo from './icons-call-incoming-video'
+import callhold from './icons-call-hold'
+import callend from './icons-call-end'
+import callemergency from './icons-call-emergency'
+import calldialpad from './icons-call-dialpad'
+import callcontrolstoppresentingnew from './icons-call-control-stop-presenting-new'
+import callcontrolpresentnew from './icons-call-control-present-new'
+import callconsultation from './icons-call-consultation'
+import callchatoff from './icons-call-chat-off'
+import callchat from './icons-call-chat'
+import callblocked from './icons-call-blocked'
+import callaudio from './icons-call-audio'
+import callalertfullbody from './icons-call-alert-full-body'
+import callalert from './icons-call-alert'
+import calladmitall from './icons-call-admit-all'
+import call from './icons-call'
+import calendar from './icons-calendar'
 import bullets from './icons-bullets'
-import browsepc from './icons-browsepc'
 import broadcastviewright from './icons-broadcast-view-right'
 import broadcastviewleft from './icons-broadcast-view-left'
 import broadcastviewfullscreen from './icons-broadcast-view-fullscreen'
-import botprivacymessage from './icons-bot-privacy-message'
-import botbadge from './icons-bot-badge'
 import bot from './icons-bot'
-import bookmarked from './icons-bookmarked'
 import bookmark from './icons-bookmark'
 import bold from './icons-bold'
-import blurbackgroundfilled from './icons-blur-background-filled'
 import blurbackground from './icons-blur-background'
 import block from './icons-block'
-import blank from './icons-blank'
-import bellmuted from './icons-bell-muted'
 import bellmute from './icons-bell-mute'
 import bell from './icons-bell'
 import badgeadd from './icons-badge-add'
@@ -30,7 +105,6 @@ import arrowup from './icons-arrow-up'
 import arrowright from './icons-arrow-right'
 import arrowleft from './icons-arrow-left'
 import arrowdown from './icons-arrow-down'
-import arrowback from './icons-arrow-back'
 import archive from './icons-archive'
 import apps from './icons-apps'
 import announcement from './icons-announcement'
@@ -41,22 +115,97 @@ import accept from './icons-accept'
 
 export default {
   // EXPORTS
+  customvotesmall,
+  customvote,
+  cortana,
+  copy,
+  contactlist,
+  connectorbadge,
+  composeextmenuitem,
+  composeextensionunpin,
+  composeextensionpin,
+  composeextensionbadge,
+  compose,
+  collapse,
+  codesnippet,
+  code,
+  closeendcall,
+  closedcaption,
+  close,
+  chromeunmaximize,
+  chromeminimize,
+  chromemaximize,
+  chromeforward,
+  chromeback,
+  chevronsmright,
+  chevronright,
+  chevronmedright,
+  chevronmedleft,
+  chevronleftsmall,
+  chevronleft,
+  chevrondown,
+  checksmall,
+  checked,
+  checkboxunselected,
+  checkboxselected,
+  checkbox,
+  check,
+  chat,
+  channelicon,
+  changename,
+  canvasaddpage,
+  callvideolineoff,
+  callvideoline,
+  callvideofilled,
+  callvideo,
+  calltransfernotification,
+  calltransfer,
+  callswitchcamera,
+  callresume,
+  callrecording,
+  callpstnfull,
+  callpstn,
+  callparticipantonhold,
+  callparticipantincomingline,
+  callparticipantincoming,
+  callparticipantfailed,
+  callparticipantending,
+  callparticipantconnectingline,
+  callparticipantconnecting,
+  callparticipantconnected,
+  callmissedlinefilled,
+  callmissedline,
+  callmissed,
+  callmicrophoneunmuting,
+  callmicrophoneofffilled,
+  callmeetupline,
+  callmeetupfilled,
+  callincomingvideo,
+  callhold,
+  callend,
+  callemergency,
+  calldialpad,
+  callcontrolstoppresentingnew,
+  callcontrolpresentnew,
+  callconsultation,
+  callchatoff,
+  callchat,
+  callblocked,
+  callaudio,
+  callalertfullbody,
+  callalert,
+  calladmitall,
+  call,
+  calendar,
   bullets,
-  browsepc,
   broadcastviewright,
   broadcastviewleft,
   broadcastviewfullscreen,
-  botprivacymessage,
-  botbadge,
   bot,
-  bookmarked,
   bookmark,
   bold,
-  blurbackgroundfilled,
   blurbackground,
   block,
-  blank,
-  bellmuted,
   bellmute,
   bell,
   badgeadd,
@@ -70,7 +219,6 @@ export default {
   arrowright,
   arrowleft,
   arrowdown,
-  arrowback,
   archive,
   apps,
   announcement,
--- src/themes/teams/components/Icon/svg/ProcessedIcons/index.ts
+++ src/themes/teams/components/Icon/svg/ProcessedIcons/index.ts
@@ -1,6 +1,70 @@
 import { TeamsSvgIconSpec } from '../types'
 
 // IMPORTS
+import cortana from './icons-cortana'
+import copy from './icons-copy'
+import contactlist from './icons-contact-list'
+import connectorbadge from './icons-connector-badge'
+import composeextmenuitem from './icons-compose-ext-menu-item'
+import composeextensionunpin from './icons-compose-extension-unpin'
+import composeextensionpin from './icons-compose-extension-pin'
+import compose from './icons-compose'
+import collapse from './icons-collapse'
+import codesnippet from './icons-codesnippet'
+import code from './icons-code'
+import closedcaption from './icons-closed-caption'
+import close from './icons-close'
+import chromeunmaximize from './icons-chrome-unmaximize'
+import chromeminimize from './icons-chrome-minimize'
+import chromemaximize from './icons-chrome-maximize'
+import chevronright from './icons-chevron-right'
+import chevronmedright from './icons-chevronmed-right'
+import chevronmedleft from './icons-chevronmed-left'
+import chevronleft from './icons-chevron-left'
+import chevrondown from './icons-chevron-down'
+import checkboxunselected from './icons-checkbox-unselected'
+import checkboxselected from './icons-checkbox-selected'
+import chat from './icons-chat'
+import channelicon from './icons-channel-icon'
+import changename from './icons-changename'
+import canvasaddpage from './icons-canvas-addpage'
+import callvideolineoff from './icons-call-video-line-off'
+import callvideoline from './icons-call-video-line'
+import callvideofilled from './icons-call-video-filled'
+import callvideo from './icons-call-video'
+import calltransfernotification from './icons-call-transfer-notification'
+import calltransfer from './icons-call-transfer'
+import callswitchcamera from './icons-call-switch-camera'
+import callrecording from './icons-call-recording'
+import callpstnfull from './icons-call-pstn-full'
+import callpstn from './icons-call-pstn'
+import callparticipantonhold from './icons-call-participant-onhold'
+import callparticipantincomingline from './icons-call-participant-incoming-line'
+import callparticipantincoming from './icons-call-participant-incoming'
+import callparticipantfailed from './icons-call-participant-failed'
+import callparticipantending from './icons-call-participant-ending'
+import callparticipantconnectingline from './icons-call-participant-connecting-line'
+import callparticipantconnecting from './icons-call-participant-connecting'
+import callparticipantconnected from './icons-call-participant-connected'
+import callmissedlinefilled from './icons-call-missed-line-filled'
+import callmissedline from './icons-call-missed-line'
+import callmissed from './icons-call-missed'
+import callmicrophoneunmuting from './icons-call-microphone-unmuting'
+import callmicrophoneofffilled from './icons-call-microphone-off-filled'
+import callmeetupline from './icons-call-meetup-line'
+import callmeetupfilled from './icons-call-meetup-filled'
+import callincomingvideo from './icons-call-incoming-video'
+import callhold from './icons-call-hold'
+import callend from './icons-call-end'
+import calldialpad from './icons-call-dialpad'
+import callcontrolstoppresentingnew from './icons-call-control-stop-presenting-new'
+import callcontrolpresentnew from './icons-call-control-present-new'
+import callblocked from './icons-call-blocked'
+import callaudio from './icons-call-audio'
+import callalert from './icons-call-alert'
+import calladmitall from './icons-call-admit-all'
+import call from './icons-call'
+import calendar from './icons-calendar'
 import bullets from './icons-bullets'
 import broadcastviewright from './icons-broadcast-view-right'
 import broadcastviewleft from './icons-broadcast-view-left'
@@ -33,6 +97,70 @@ import accept from './icons-accept'
 
 export default {
   // EXPORTS
+  cortana,
+  copy,
+  contactlist,
+  connectorbadge,
+  composeextmenuitem,
+  composeextensionunpin,
+  composeextensionpin,
+  compose,
+  collapse,
+  codesnippet,
+  code,
+  closedcaption,
+  close,
+  chromeunmaximize,
+  chromeminimize,
+  chromemaximize,
+  chevronright,
+  chevronmedright,
+  chevronmedleft,
+  chevronleft,
+  chevrondown,
+  checkboxunselected,
+  checkboxselected,
+  chat,
+  channelicon,
+  changename,
+  canvasaddpage,
+  callvideolineoff,
+  callvideoline,
+  callvideofilled,
+  callvideo,
+  calltransfernotification,
+  calltransfer,
+  callswitchcamera,
+  callrecording,
+  callpstnfull,
+  callpstn,
+  callparticipantonhold,
+  callparticipantincomingline,
+  callparticipantincoming,
+  callparticipantfailed,
+  callparticipantending,
+  callparticipantconnectingline,
+  callparticipantconnecting,
+  callparticipantconnected,
+  callmissedlinefilled,
+  callmissedline,
+  callmissed,
+  callmicrophoneunmuting,
+  callmicrophoneofffilled,
+  callmeetupline,
+  callmeetupfilled,
+  callincomingvideo,
+  callhold,
+  callend,
+  calldialpad,
+  callcontrolstoppresentingnew,
+  callcontrolpresentnew,
+  callblocked,
+  callaudio,
+  callalert,
+  calladmitall,
+  call,
+  calendar,
   bullets,
   broadcastviewright,
   broadcastviewleft,
