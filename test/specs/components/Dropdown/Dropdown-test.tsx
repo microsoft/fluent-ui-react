d12f2ad78fd24cd4020d3eb1259ef4d120ff8384
Silviu Avram <siavram@microsoft.com>
2018-11-15 11:41:18 +0100

    added isconformant test for dropdown

--- test/specs/components/Dropdown/Dropdown-test.tsx
+++ test/specs/components/Dropdown/Dropdown-test.tsx
@@ -2,12 +2,6 @@
 import { isConformant } from 'test/specs/commonTests'
 
 import Dropdown from 'src/components/Dropdown/Dropdown'
-// import implementsCollectionShorthandProp from '../../commonTests/implementsCollectionShorthandProp'
-// import { chatBehavior } from 'src/lib/accessibility'
-// import { AccessibilityDefinition } from 'src/lib/accessibility/types'
-
-// const chatImplementsCollectionShorthandProp = implementsCollectionShorthandProp(Dropdown)
-
 describe('Dropdown', () => {
   isConformant(Dropdown)
 
