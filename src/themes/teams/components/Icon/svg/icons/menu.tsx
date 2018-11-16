ed128fd72ef1c68482a2aa7701af24ed450e6490
Vyshnavi Dasari <vydasari@microsoft.com>
2018-11-16 13:13:00 +0530

    Modified classNames

--- src/themes/teams/components/Icon/svg/icons/menu.tsx
+++ src/themes/teams/components/Icon/svg/icons/menu.tsx
@@ -2,10 +2,7 @@ import * as React from 'react'
 import { TeamsSvgIconSpec } from '../types'
 
 export default (
-  <svg viewBox="0 0 32 32" role="presentation" className="app-svg icons-menu">
-    <path
-      className="icons-default-fill"
-      d="M9,11h13c0.6,0,1-0.4,1-1s-0.4-1-1-1H9c-0.6,0-1,0.4-1,1S8.4,11,9,11zM22,19H9c-0.6,0-1,0.4-1,1s0.4,1,1,1h13c0.6,0,1-0.4,1-1S22.6,19,22,19zM22,14H9c-0.6,0-1,0.4-1,1s0.4,1,1,1h13c0.6,0,1-0.4,1-1S22.6,14,22,14z"
-    />
+  <svg viewBox="0 0 32 32" role="presentation">
+    <path d="M9,11h13c0.6,0,1-0.4,1-1s-0.4-1-1-1H9c-0.6,0-1,0.4-1,1S8.4,11,9,11zM22,19H9c-0.6,0-1,0.4-1,1s0.4,1,1,1h13c0.6,0,1-0.4,1-1S22.6,19,22,19zM22,14H9c-0.6,0-1,0.4-1,1s0.4,1,1,1h13c0.6,0,1-0.4,1-1S22.6,14,22,14z" />
   </svg>
 ) as TeamsSvgIconSpec
