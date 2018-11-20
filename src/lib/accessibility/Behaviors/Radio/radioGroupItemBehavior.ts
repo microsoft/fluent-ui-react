28c8307fa24dc8e5adbdb882f1c0c2d8cfbeeec2
Milan Turon <milanturon@centrum.cz>
2018-11-20 17:25:33 +0100

    test(behaviors): make behavior tests and specification mandatory (#455)
    * first raw changes adding specification tag
    
    * make tests mandatory
    
    * make tests mandatory
    
    * small fix in name
    
    * reverting change, which have not been done by purpose
    
    * fix number of ticket
    
    * adding test if description and specification is missing in the file
    
    * adressing comments from PR
    
    * fix2 after review
    
    * adressing changes after PR, another fixes
    
    * adressing changes after PR 2
    
    * rename parameter

--- src/lib/accessibility/Behaviors/Radio/radioGroupItemBehavior.ts
+++ src/lib/accessibility/Behaviors/Radio/radioGroupItemBehavior.ts
@@ -2,7 +2,7 @@ import { Accessibility } from '../../types'
 import * as _ from 'lodash'
 
 /**
- * @description
+ * @specification
  *  Adds role='radio'. This allows screen readers to handle the component as a radio button.
  *  Adds attribute 'aria-checked=true' based on the property 'checked'.
  *  Adds attribute 'aria-disabled=true' based on the property 'disabled'. This can be overriden by providing 'aria-disabled' property directly to the component.
