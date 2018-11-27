124c5642a89f36e24701dc0b5f72a74989cbd1d6
manajdov <manajdov@microsoft.com>
2018-11-27 12:05:55 +0100

    -switched Header to semantic-ui-react -removed unnecessary brackets

--- docs/src/prototypes/formValidation/index.tsx
+++ docs/src/prototypes/formValidation/index.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react'
-import { Header } from '@stardust-ui/react'
+import { Header } from 'semantic-ui-react'
 import FormValidationOnChange from './FormValidationOnChange'
 import FormValidationOnSubmit from './FormValidationOnSubmit'
 
 const FormValidationPrototype: React.SFC<{}> = () => (
   <div style={{ margin: '10px' }}>
-    <Header content={'Form validation on submit'} />
+    <Header content="Form validation on submit" />
     <FormValidationOnSubmit />
-    <Header content={'Form validation on change'} />
+    <Header content="Form validation on change" />
     <FormValidationOnChange />
   </div>
 )
