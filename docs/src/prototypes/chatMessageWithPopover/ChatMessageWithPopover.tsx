7f8bb6dbdd3cc03bbd8373299e92419a161f22fa
Sofiya Huts <sohuts@microsoft.com>
2018-11-27 21:26:41 +0100

    Small improvements

--- docs/src/prototypes/chatMessageWithPopover/ChatMessageWithPopover.tsx
+++ docs/src/prototypes/chatMessageWithPopover/ChatMessageWithPopover.tsx
@@ -28,51 +28,56 @@ class ChatMessageWithPopover extends React.Component {
 
   render() {
     return (
-      <Provider.Consumer
-        render={({ siteVariables }) => (
-          <Chat.Message
-            styles={{
-              position: 'relative',
-
-              '&.focused .actions': {
-                opacity: 1,
-              },
-              ':hover .actions': {
-                opacity: 1,
-              },
-              '& a': {
-                color: siteVariables.brand,
-              },
-            }}
-            author="Jane Doe"
-            timestamp="Yesterday, 10:15 PM"
-            content={{
-              content: (
-                <div>
-                  <Popover className="actions" />
-                  <a href="/">Link</a> Hover me to see the actions <a href="/">Some Link</a>
-                </div>
-              ),
-            }}
-            avatar={janeAvatar}
-            onFocus={this.handleFocus}
-            onBlur={this.handleBlur}
-            className={this.state.focused ? 'focused' : undefined}
-          />
-        )}
+      <Chat.Message
+        author="Jane Doe"
+        timestamp="Yesterday, 10:15 PM"
+        content={{
+          content: (
+            <div>
+              <Popover className="actions" />
+              <a href="/">Link</a> Hover me to see the actions <a href="/">Some Link</a>
+            </div>
+          ),
+        }}
+        avatar={janeAvatar}
+        onFocus={this.handleFocus}
+        onBlur={this.handleBlur}
+        className={this.state.focused ? 'focused' : undefined}
       />
     )
   }
 }
 
 const ChatWithPopover = () => (
-  <Chat
-    items={[
-      { key: 'a', content: <ChatMessageWithPopover /> },
-      { key: 'b', content: <ChatMessageWithPopover /> },
-      { key: 'c', content: <ChatMessageWithPopover /> },
-    ]}
-  />
+  <Provider
+    theme={{
+      componentStyles: {
+        ChatMessage: {
+          root: {
+            position: 'relative',
+
+            '&.focused .actions': {
+              opacity: 1,
+            },
+            ':hover .actions': {
+              opacity: 1,
+            },
+            '& a': {
+              color: '#6264A7',
+            },
+          },
+        },
+      },
+    }}
+  >
+    <Chat
+      items={[
+        { key: 'a', content: <ChatMessageWithPopover /> },
+        { key: 'b', content: <ChatMessageWithPopover /> },
+        { key: 'c', content: <ChatMessageWithPopover /> },
+      ]}
+    />
+  </Provider>
 )
 
 export default ChatWithPopover
