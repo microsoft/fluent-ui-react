836dc0d519626f1673591df6701c02eb1118d185
manajdov <manajdov@microsoft.com>
2018-12-03 20:59:41 +0100

    -added react-dom in the imports for the editor

--- docs/src/components/Playground/renderConfig.ts
+++ docs/src/components/Playground/renderConfig.ts
@@ -1,6 +1,7 @@
 import * as _ from 'lodash'
 import * as React from 'react'
 import * as Stardust from '@stardust-ui/react'
+import * as ReactDOM from 'react-dom'
 
 export const babelConfig = {
   plugins: ['proposal-class-properties', ['transform-typescript', { isTSX: true }]],
@@ -20,6 +21,7 @@ const imports = {
   '@stardust-ui/react': Stardust,
   lodash: _,
   react: React,
+  'react-dom': ReactDOM,
 }
 
 export const importResolver = importName => imports[importName]
