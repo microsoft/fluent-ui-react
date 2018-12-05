5fc1bb8aa22b4bead425c9c1d8cebb62f1bece20
Miroslav Stastny <mistastn@microsoft.com>
2018-12-05 15:34:10 +0100

    deploy docs [ci skip]

--- 12.11d459d14ee637b60512.js
+++
@@ -1,7 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{1342:
-/*!**************************************************!*\
-  !*** ./docs/src/exampleMenus/Icon.examples.json ***!
-  \**************************************************/
-/*! exports provided: 0, 1, 2, 3, default */
-/*! all exports used */
-/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e){e.exports=[{sectionName:"Types",examples:[{title:"Default",examplePath:"components/Icon/Types/IconExample"}]},{sectionName:"States",examples:[{title:"Disabled",examplePath:"components/Icon/States/IconExampleDisabled"}]},{sectionName:"Variations",examples:[{title:"Space",examplePath:"components/Icon/Variations/IconExampleSpace"},{title:"Color",examplePath:"components/Icon/Variations/IconExampleColor"},{title:"Size",examplePath:"components/Icon/Variations/IconExampleSize"},{title:"Bordered",examplePath:"components/Icon/Variations/IconExampleBordered"},{title:"Circular",examplePath:"components/Icon/Variations/IconExampleCircular"}]},{sectionName:"Usage",examples:[{title:"Theme Icons",examplePath:"components/Icon/Usage/IconSetExample"}]}]}}]);
--- 22.7d0c94de197a11590e17.js
+++
@@ -1,7 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[22],{1352:
-/*!******************************************************!*\
-  !*** ./docs/src/exampleMenus/Provider.examples.json ***!
-  \******************************************************/
-/*! exports provided: 0, default */
-/*! all exports used */
-/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e){e.exports=[{sectionName:"Types",examples:[{title:"Theme",examplePath:"components/Provider/Types/ProviderExample"}]}]}}]);
