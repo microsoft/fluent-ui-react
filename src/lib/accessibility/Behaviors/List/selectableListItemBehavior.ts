94882026e289da097b059cf9c3253f21eee35f40
Sofiya Huts <sohuts@microsoft.com>
2018-12-05 14:01:41 -0800

    small fix

--- src/lib/accessibility/Behaviors/List/selectableListItemBehavior.ts
+++ src/lib/accessibility/Behaviors/List/selectableListItemBehavior.ts
@@ -4,7 +4,7 @@ import * as keyboardKey from 'keyboard-key'
 /**
  * @specification
  * Adds role='option'. This role is used for a selectable item in a list.
- * Adds attribute 'aria-selected=true' based on the property 'active'. Based on this screen readers will recognize the selected state of the item.
+ * Adds attribute 'aria-selected=true' based on the property 'selected'. Based on this screen readers will recognize the selected state of the item.
  */
 
 const selectableListItemBehavior: Accessibility = (props: any) => ({
