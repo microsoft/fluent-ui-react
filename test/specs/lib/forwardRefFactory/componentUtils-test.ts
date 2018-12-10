6d9f3ba95f36e7944378b802009185a880c7d839
olfedias <olfedias@microsoft.com>
2018-12-10 14:41:10 +0200

    fix lint

--- test/specs/lib/forwardRefFactory/componentUtils-test.ts
+++ test/specs/lib/forwardRefFactory/componentUtils-test.ts
@@ -4,8 +4,10 @@ import { DOMClass, DOMFunction, ForwardedRef } from '../../components/Ref/fixtur
 describe('componentUtils', () => {
   test('isForwardRefComponent', () => {
     it('returns "false" when passed component do not support ref forwarding', () => {
-       [DOMClass, DOMFunction, 'div'].map(Component => {
-        expect(isForwardRefComponent(Component)).toBe(false)
+      const components = [DOMClass, DOMFunction, 'div']
+
+      components.forEach(component => {
+        expect(isForwardRefComponent(component)).toBe(false)
       })
     })
 
@@ -16,14 +18,18 @@ describe('componentUtils', () => {
 
   describe('supportsRef', () => {
     it('returns "false" when passed component do not support "ref" prop', () => {
-       [DOMClass, DOMFunction, 'div'].map(Component => {
-        expect(supportsRef(Component)).toBe(false)
+      const components = [DOMClass, DOMFunction, 'div']
+
+      components.forEach(component => {
+        expect(supportsRef(component)).toBe(false)
       })
     })
 
     it('returns "true" when passed component supports "ref" prop', () => {
-       ['div', ForwardedRef].map(Component => {
-        expect(supportsRef(Component)).toBe(true)
+      const components = ['div', ForwardedRef]
+
+      components.forEach(component => {
+        expect(supportsRef(component)).toBe(true)
       })
     })
   })
