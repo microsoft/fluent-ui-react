f8c42cdd0c94eef5ab81e1bffa28ca451c89e3c5
alinais <alina.is@gmail.com>
2018-12-14 17:45:42 +0100

    restoring the teams textStyles

--- src/themes/teams/components/Text/textStyles.ts
+++ src/themes/teams/components/Text/textStyles.ts
@@ -1,14 +1,27 @@
 import { ComponentStyleFunctionParam, ICSSInJSStyle } from '../../../types'
+import { truncateStyle } from '../../../../styles/customCSS'
 import { TextVariables } from '../../../base/components/Text/textVariables'
 import { TextProps } from '../../../../components/Text/Text'
 
 export default {
   root: ({
-    props: { atMention, disabled, error, important, success, timestamp, temporary },
+    props: {
+      atMention,
+      disabled,
+      error,
+      size,
+      weight,
+      important,
+      success,
+      timestamp,
+      truncated,
+      temporary,
+    },
     variables: v,
   }: ComponentStyleFunctionParam<TextProps, TextVariables>): ICSSInJSStyle => {
     return {
       display: 'inline-block',
+      ...(truncated && truncateStyle),
       ...(atMention === true && {
         color: v.atMentionOtherColor,
       }),
@@ -29,6 +42,43 @@ export default {
       ...(important && {
         fontWeight: v.importantWeight,
         color: v.importantColor,
+      }),
+
+      ...(weight === 'light' && {
+        fontWeight: v.fontWeightLight,
+      }),
+      ...(weight === 'semilight' && {
+        fontWeight: v.fontWeightSemilight,
+      }),
+      ...(weight === 'regular' && {
+        fontWeight: v.fontWeightRegular,
+      }),
+      ...(weight === 'semibold' && {
+        fontWeight: v.fontWeightSemibold,
+      }),
+      ...(weight === 'bold' && {
+        fontWeight: v.fontWeightBold,
+      }),
+
+      ...(size === 'smaller' && {
+        fontSize: v.fontSizeExtraSmall,
+        lineHeight: v.fontLineHeightExtraSmall,
+      }),
+      ...(size === 'small' && {
+        fontSize: v.fontSizeSmall,
+        lineHeight: v.fontLineHeightSmall,
+      }),
+      ...(size === 'medium' && {
+        fontSize: v.fontSizeMedium,
+        lineHeight: v.fontLineHeightMedium,
+      }),
+      ...(size === 'large' && {
+        fontSize: v.fontSizeLarge,
+        lineHeight: v.fontLineHeightLarge,
+      }),
+      ...(size === 'larger' && {
+        fontSize: v.fontSizeExtraLarge,
+        lineHeight: v.fontLineHeightExtraLarge,
       }),
     }
   },
