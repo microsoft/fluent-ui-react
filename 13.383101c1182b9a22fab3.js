b4a01bb2c055b6cbf2f2f8039bcd351e7a49226d
Miroslav Stastny <mistastn@microsoft.com>
2018-12-17 14:41:07 +0100

    deploy docs [ci skip]

--- 13.383101c1182b9a22fab3.js
+++
@@ -1,7 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{2403:
-/*!***************************************************!*\
-  !*** ./docs/src/exampleMenus/Image.examples.json ***!
-  \***************************************************/
-/*! exports provided: 0, 1, default */
-/*! all exports used */
-/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e){e.exports=[{sectionName:"Types",examples:[{title:"Default",examplePath:"components/Image/Types/ImageExample"},{title:"Avatar",examplePath:"components/Image/Types/ImageExampleAvatar"}]},{sectionName:"Variations",examples:[{title:"Fluid",examplePath:"components/Image/Variations/ImageExampleFluid"},{title:"Circular",examplePath:"components/Image/Variations/ImageExampleCircular"}]}]}}]);
