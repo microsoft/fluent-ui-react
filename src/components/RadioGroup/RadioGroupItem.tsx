a97493f2d333978e1d433a9eacc5d54644465e3d
Brigette Lundeen <brlundee@microsoft.com>
2018-12-17 17:15:15 -0800

    updating sizing scheme in Icon to be small(er/est), medium, large(r/est)

--- src/components/RadioGroup/RadioGroupItem.tsx
+++ src/components/RadioGroup/RadioGroupItem.tsx
@@ -159,7 +159,7 @@ class RadioGroupItem extends AutoControlledComponent<
           {Icon.create(icon || '', {
             defaultProps: {
               circular: true,
-              size: 'mini',
+              size: 'smaller',
               variables: variables.icon,
               styles: styles.icon,
             },
