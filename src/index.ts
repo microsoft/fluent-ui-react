3c6e75e35ff27148bd721d8739476839b142c645
Sofiya Huts <8460706+sophieH29@users.noreply.github.com>
2018-12-19 10:28:56 +0100

    fix(SelectableList): Items in list should be selectable (#566)
    * Reflect which item is selected in list
    
    * Make list derived from autocontrolled component
    
    * small fix
    
    * Update ListExampleSelection.tsx
    
    * Update ListExampleSelection.shorthand.tsx
    
    * Small improvement
    
    * Rename *ItemIndex -> *Index
    
    * Names refactoring
    
    * Minor improvements
    
    * update changelog
    
    * Add onSelectedIndexChange
    
    * Add some tests
    
    * Small improvements afer CR
    
    * Small improvements afer CR
    
    * Small improvements afer CR
    
    * create focus handler when List is constructed
    
    * fix changelog
    
    * changelog

--- src/index.ts
+++ src/index.ts
@@ -72,7 +72,7 @@ export { default as Label, LabelProps } from './components/Label/Label'
 export { default as Layout, LayoutPropsWithDefaults, LayoutProps } from './components/Layout/Layout'
 
 export { default as List, ListProps } from './components/List/List'
-export { default as ListItem, ListItemState, ListItemProps } from './components/List/ListItem'
+export { default as ListItem, ListItemProps } from './components/List/ListItem'
 
 export { default as Menu, MenuProps, MenuState } from './components/Menu/Menu'
 export { default as MenuItem, MenuItemState, MenuItemProps } from './components/Menu/MenuItem'
