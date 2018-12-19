d3d60124310285a90b0d13d13bf1424134642b56
Oleksandr Fediashov <alexander.mcgarret@gmail.com>
2018-12-19 13:53:12 +0100

    docs(Examples): allow to use TS in examples (#617)
    * docs(Examples): allow to use TS in examples
    
    * add jsdoc
    
    * fix typo
    
    * rename file
    
    * add comment
    
    * `createExample` to `createExampleSourceCode`
    
    * rework with `path.relative()`
    
    * remove JSON files on remove tsx
    
    * create getRelativePathToSource function

--- docs/src/examples/components/Text/States/TextExampleDisabled.tsx
+++ docs/src/examples/components/Text/States/TextExampleDisabled.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import * as React from 'react'
 import { Text } from '@stardust-ui/react'
 
 const TextExampleDisabled = () => <Text disabled>This feature has been disabled.</Text>
