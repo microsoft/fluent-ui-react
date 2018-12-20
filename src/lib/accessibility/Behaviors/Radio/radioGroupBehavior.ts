b9aba231bd99b443e67e437a8bd40371e4d32bf7
Juraj Kapsiar <jukapsia@microsoft.com>
2018-12-20 13:57:27 +0100

    typo and changelog

--- src/lib/accessibility/Behaviors/Radio/radioGroupBehavior.ts
+++ src/lib/accessibility/Behaviors/Radio/radioGroupBehavior.ts
@@ -3,7 +3,7 @@ import * as keyboardKey from 'keyboard-key'
 
 /**
  * @description
- * Implements ARIA Rafio Group design pattern.
+ * Implements ARIA Radio Group design pattern.
  * @specification
  *  Adds role='radiogroup'. This allows screen readers to handle the component as a radio group.
  *  Performs 'nextItem' action on ArrowDown, ArrowRight.
