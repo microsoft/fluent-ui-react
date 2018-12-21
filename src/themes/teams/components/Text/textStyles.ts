b54d059fe8095bf0a840c635dc1b37643e276093
alinais <alina.is@gmail.com>
2018-12-21 15:05:50 +0100

    cleaning teams text styles

--- src/themes/teams/components/Text/textStyles.ts
+++ src/themes/teams/components/Text/textStyles.ts
@@ -1,5 +1,3 @@
-import * as _ from 'lodash'
-
 import { ComponentStyleFunctionParam, ICSSInJSStyle } from '../../../types'
 import { TeamsTextVariables } from './textVariables'
 import { TextProps } from '../../../../components/Text/Text'
@@ -11,20 +9,16 @@ export default {
   }: ComponentStyleFunctionParam<TextProps, TeamsTextVariables>): ICSSInJSStyle => {
     return {
       ...(atMention === 'me' && {
-        color: v.atMentionMeColor,
         fontWeight: v.atMentionMeFontWeight,
       }),
       ...(timestamp && {
-        color: v.timestampColor,
         ':hover': {
           color: v.timestampHoverColor,
         },
       }),
       ...(important && {
         fontWeight: v.importantWeight,
-        color: v.importantColor,
       }),
-      ...(color && { color: _.get(v.colors, color) }),
     }
   },
 }
