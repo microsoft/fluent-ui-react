93e1bc0ce682dabb23ea14b48216456388afc47d
Oleksandr Fediashov <olfedias@microsoft.com>
2019-01-02 12:55:23 +0200

    restore changes

--- docs/src/utils/exampleKebabNameToSourceFilename.ts
+++ docs/src/utils/exampleKebabNameToSourceFilename.ts
@@ -4,14 +4,12 @@ import * as _ from 'lodash'
  * Converts kebab-cased-example-name back into the original filename.
  * @param {string} exampleKebabName
  */
-const exampleKebabNameToSourceFilename = exampleKebabName => {
-  const trimmedKebabName = exampleKebabName.trim()
-  // button-example           => ButtonExample
-  // button-example-shorthand => ButtonExampleShorthand
-  const startCasedName = _.startCase(trimmedKebabName)
-
+const exampleKebabNameToSourceFilename = (exampleKebabName: string) => {
+  // button-example           => ButtonExample.source.json
   // button-example-shorthand => ButtonExample.shorthand.source.json
-  return `${startCasedName.replace(/Shorthand$/, '.shorthand')}.source.json`
+  return `${_.startCase(exampleKebabName)
+    .replace(/ /g, '')
+    .replace(/Shorthand$/, '.shorthand')}.source.json`
 }
 
 export default exampleKebabNameToSourceFilename
