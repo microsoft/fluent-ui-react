a671202c866044ca0842c0f90530fc43c5acf482
manajdov <manajdov@microsoft.com>
2019-01-08 16:43:50 +0100

    wip

--- src/themes/teams/components/Input/inputVariables.ts
+++ src/themes/teams/components/Input/inputVariables.ts
@@ -1,40 +1,14 @@
 import { pxToRem } from '../../utils'
+import { InputVariables } from '../../../base/components/Input/inputVariables'
+import { Partial } from '../../../../../types/utils'
 
-export interface InputVariables {
-  backgroundColor: string
-  border: string
-  borderRadius: string
+export interface TeamsInputVariables extends InputVariables {
   boxShadow: string
-  fontColor: string
-  fontSize: string
-  iconColor: string
-  iconPosition: string
-  iconRight: string
-  iconLeft: string
-  inputPaddingWithIconAtStart: string
-  inputPaddingWithIconAtEnd: string
-  inputPadding: string
   inputFocusBorderBottomColor: string
-  inputFocusBorderRadius: string
 }
 
-export default (siteVars): InputVariables => ({
+export default (siteVars): Partial<TeamsInputVariables> => ({
   backgroundColor: siteVars.gray10,
-  border: `${pxToRem(1)} solid transparent`,
-  borderRadius: pxToRem(3),
   boxShadow: `0 ${pxToRem(1)} 0 ${siteVars.brand}`,
-
-  fontColor: siteVars.bodyColor,
-  fontSize: siteVars.fontSizes.medium,
-
-  iconPosition: 'absolute',
-  iconRight: pxToRem(2),
-  iconColor: siteVars.bodyColor,
-  iconLeft: pxToRem(6),
-  inputPaddingWithIconAtStart: `${pxToRem(6)} ${pxToRem(12)} ${pxToRem(6)} ${pxToRem(24)}`,
-  inputPaddingWithIconAtEnd: `${pxToRem(6)} ${pxToRem(24)} ${pxToRem(6)} ${pxToRem(12)}`,
-
-  inputPadding: `${pxToRem(6)} ${pxToRem(12)} ${pxToRem(6)} ${pxToRem(12)}`,
   inputFocusBorderBottomColor: siteVars.brand,
-  inputFocusBorderRadius: `${pxToRem(3)} ${pxToRem(3)} ${pxToRem(2)} ${pxToRem(2)}`,
 })
