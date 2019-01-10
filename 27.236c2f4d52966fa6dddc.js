b6ab5d7e027f3f4f3020fabfb9af9b9af4d546d8
Miroslav Stastny <mistastn@microsoft.com>
2019-01-10 17:53:17 +0100

    deploy docs [ci skip]

---
+++ 27.236c2f4d52966fa6dddc.js
@@ -0,0 +1,7 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[27],{2078:
+/*!****************************************************!*\
+  !*** ./docs/src/exampleMenus/Status.examples.json ***!
+  \****************************************************/
+/*! exports provided: 0, 1, default */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(t){t.exports=[{sectionName:"Types",examples:[{title:"Default",examplePath:"components/Status/Types/StatusExample"},{title:"State",examplePath:"components/Status/Types/StatusTypeExample"}]},{sectionName:"Variations",examples:[{title:"Color",examplePath:"components/Status/Variations/StatusColorExample"},{title:"Icon",examplePath:"components/Status/Variations/StatusIconExample"},{title:"Custom",examplePath:"components/Status/Variations/StatusCustomExample"}]}]}}]);
