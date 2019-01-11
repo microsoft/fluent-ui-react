6cfc12243a9a6960761b61f1ec48d3cf688916f3
manajdov <manajdov@microsoft.com>
2019-01-11 18:27:16 +0100

    -fixed the generateColorScheme object, and documented it's conditions

--- src/lib/colorUtils.ts
+++ src/lib/colorUtils.ts
@@ -51,10 +51,26 @@ export const generateColorScheme = (
   colorProp: ComplexColorPropType,
   colorScheme: ColorValues<Partial<ColorScheme>>,
 ): Partial<ColorScheme> => {
-  if (!colorProp) {
+  // if both color prop and color scheme are defined, we are merging them
+  if (colorProp && colorScheme) {
+    return typeof colorProp === 'string'
+      ? _.get(colorScheme, colorProp as string, colorScheme.default)
+      : { ...colorScheme.default, ...getColorSchemeFromObject(colorScheme, colorProp) }
+  }
+
+  // if the color prop is not defined, but the the color scheme is defined, then we are returning
+  // the defaults from the color scheme if they exists
+  if (colorScheme) {
     return colorScheme && colorScheme.default ? colorScheme.default : {}
   }
-  return typeof colorProp === 'string'
-    ? _.get(colorScheme, colorProp as string, colorScheme.default)
-    : { ...colorScheme.default, ...getColorSchemeFromObject(colorScheme, colorProp) }
+
+  // if the color scheme is not defined, then if the color prop is a scheme object we are
+  // returning it, otherwise we return an empty object, as it means that the component is
+  // implementing the simple color prop
+  if (colorProp) {
+    return typeof colorProp === 'string' ? {} : colorProp
+  }
+
+  // if neither the color prop, nor the color scheme are defined, we are returning empty object
+  return {}
 }
