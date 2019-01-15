4167da35c542344f057e017d7831a2c0af8478de
Brigette Lundeen <brlundee@microsoft.com>
2019-01-15 13:16:02 -0800

    adding menu variables for teams-dark theme

--- src/themes/teams-dark/components/Menu/menuVariables.ts
+++ src/themes/teams-dark/components/Menu/menuVariables.ts
@@ -1,9 +1,11 @@
 export interface MenuVariables {
   color: string
+  backgroundColor: string
 }
 
 export default (siteVars: any): MenuVariables => {
   return {
     color: siteVars.white,
+    backgroundColor: siteVars.black,
   }
 }
