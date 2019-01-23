f3939e81db16ccffed540f7dd8a03a213094b483
Sofiya Huts <8460706+sophieH29@users.noreply.github.com>
2019-01-23 16:05:37 +0100

    Update accessibilityStyles.ts

--- src/lib/accessibility/Styles/accessibilityStyles.ts
+++ src/lib/accessibility/Styles/accessibilityStyles.ts
@@ -1,6 +1,6 @@
-// Visually hides elements which remain visible for screen reader
 import { ICSSInJSStyle } from '../../../themes/types'
 
+// Visually hides elements which remain visible for screen reader
 export const screenReaderContainerStyles: ICSSInJSStyle = {
   border: '0',
   clip: 'rect(0 0 0 0)',
