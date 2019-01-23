4a4e11c53a900190482fc22cb13af4e0026d5ee3
Alina Medintu <almedint@microsoft.com>
2019-01-23 16:56:13 +0100

    using stardust Header in Shorthand Props doc page

--- docs/src/views/ShorthandProps.tsx
+++ docs/src/views/ShorthandProps.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { NavLink } from 'react-router-dom'
-import { Header } from 'semantic-ui-react'
+import { Header } from '@stardust-ui/react'
 import DocPage from '../components/DocPage/DocPage'
 import CodeSnippet from '../components/CodeSnippet'
 import Button from '../../../src/components/Button/Button'
