5b0c028e53f3e5dce77e33a65adcdc178c481af2
Vadim Varfolomeyev <vadimva@microsoft.com>
2019-01-22 17:09:51 -0800

    message card II

--- docs/src/examples/components/Flex/Types/FlexExampleMessage.tsx
+++ docs/src/examples/components/Flex/Types/FlexExampleMessage.tsx
@@ -1,20 +1,42 @@
 import * as React from 'react'
-import { Avatar, Flex, Icon, Text } from '@stardust-ui/react'
+import { Avatar, Flex, Icon, Text, Divider } from '@stardust-ui/react'
+
+const baseFont = { fontFamily: 'Base' }
+const captionFont = { fontFamily: 'Caption' }
 
 const FlexExampleMessage = () => (
-  <Flex gap="1rem">
-    <Avatar image="public/images/avatar/small/matt.jpg" />
-    <Flex vertical fluid>
-      <Flex baseline gap="1rem">
-        <Text important content="John Smith" />
-        <Flex.Body>
-          <Text content="Yesterday 11:13 AM" size="small" disabled />
-        </Flex.Body>
-        <Icon name="home" />
+  <>
+    <Flex gap="1rem">
+      <Avatar image="public/images/avatar/small/matt.jpg" />
+      <Flex vertical fluid>
+        <Flex baseline gap="1rem">
+          <Text important content="John Smith" />
+          <Flex.Body>
+            <Text content="Yesterday 11:13 AM" size="small" disabled />
+          </Flex.Body>
+          <Icon name="home" />
+        </Flex>
+        <Text content="How are you? Hope you are doing well" />
       </Flex>
-      <Text content="How are you? Hope you are doing well" />
     </Flex>
-  </Flex>
+
+    <Divider />
+
+    <Flex center debug gap="12px" style={{ height: '48px', padding: '0 20px', width: '326px' }}>
+      <Avatar image="public/images/avatar/small/matt.jpg" size={28} />
+      <Flex vertical fluid>
+        <Flex baseline between>
+          <Text weigth="bold" content="Name" style={baseFont} />
+          <Text timestamp content="0:00 PM" size="small" style={captionFont} />
+        </Flex>
+        <Text
+          content="Message Preview"
+          size="small"
+          style={{ fontFamily: 'Caption', lineHeight: 1 }}
+        />
+      </Flex>
+    </Flex>
+  </>
 )
 
 export default FlexExampleMessage
