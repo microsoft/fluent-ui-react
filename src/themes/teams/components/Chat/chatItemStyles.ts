39c6ebc7d89edc6f5b052a96d7169fa235212a4a
manajdov <manajdov@microsoft.com>
2019-01-25 11:22:32 +0100

    -renamed classNames to selectors variables

--- src/themes/teams/components/Chat/chatItemStyles.ts
+++ src/themes/teams/components/Chat/chatItemStyles.ts
@@ -4,73 +4,63 @@ import { ChatItemProps } from '../../../../components/Chat/ChatItem'
 import { pxToRem } from '../../../../lib'
 import ChatMessage from '../../../../components/Chat/ChatMessage'
 
-const chatMessageClassname = `& .${ChatMessage.className}`
-const chatMessageAuthorClassname = `& .${ChatMessage.className}__author`
-const chatMessageTimestampClassname = `& .${ChatMessage.className}__timestamp`
+const chatMessageClassNameSelector = `& .${ChatMessage.className}`
+const chatMessageAuthorClassNameSelector = `& .${ChatMessage.className}__author`
+const chatMessageTimestampClassNameSelector = `& .${ChatMessage.className}__timestamp`
 
 const chatItemStyles: ComponentSlotStylesInput<ChatItemProps, ChatItemVariables> = {
-  root: ({ props: p, variables: v }): ICSSInJSStyle => {
-    const { grouped } = p
-    return {
-      position: 'relative',
-      ...((!grouped || grouped === 'start') && { marginTop: pxToRem(16) }),
-      ...(grouped &&
-        grouped !== 'start' && {
-          marginTop: pxToRem(2),
-          [chatMessageAuthorClassname]: {
-            display: 'none',
-          },
-          [chatMessageTimestampClassname]: {
-            display: 'none',
-          },
-        }),
-      marginBottom: 0,
-    }
-  },
+  root: ({ props: p, variables: v }): ICSSInJSStyle => ({
+    position: 'relative',
+    ...((!p.grouped || p.grouped === 'start') && { marginTop: pxToRem(16) }),
+    ...(p.grouped &&
+      p.grouped !== 'start' && {
+        marginTop: pxToRem(2),
+        [chatMessageAuthorClassNameSelector]: {
+          display: 'none',
+        },
+        [chatMessageTimestampClassNameSelector]: {
+          display: 'none',
+        },
+      }),
+    marginBottom: 0,
+  }),
 
-  gutter: ({ props: p, variables: v }): ICSSInJSStyle => {
-    const { grouped } = p
-    return {
-      position: 'absolute',
-      marginTop: v.gutterMargin,
-      [p.gutterPosition === 'end' ? 'right' : 'left']: 0,
-      ...(grouped &&
-        grouped !== 'start' && {
-          visibility: 'hidden',
-        }),
-    }
-  },
+  gutter: ({ props: p, variables: v }): ICSSInJSStyle => ({
+    position: 'absolute',
+    marginTop: v.gutterMargin,
+    [p.gutterPosition === 'end' ? 'right' : 'left']: 0,
+    ...(p.grouped !== 'start' && {
+      visibility: 'hidden',
+    }),
+  }),
 
-  message: ({ props: p, variables: v }): ICSSInJSStyle => {
-    const { grouped } = p
-    return {
-      position: 'relative',
-      marginLeft: v.messageMargin,
-      marginRight: v.messageMargin,
-      ...(grouped === 'middle' && {
-        [chatMessageClassname]: {
-          borderTopLeftRadius: 0,
-          borderBottomLeftRadius: 0,
-          paddingTop: pxToRem(5),
-          paddingBottom: pxToRem(7),
-        },
-      }),
-      ...(grouped === 'start' && {
-        [chatMessageClassname]: {
-          borderTopLeftRadius: pxToRem(3),
-          borderBottomLeftRadius: 0,
-        },
-      }),
-      ...(grouped === 'end' && {
-        [chatMessageClassname]: {
-          borderTopLeftRadius: 0,
-          borderBottomLeftRadius: pxToRem(3),
-          paddingTop: pxToRem(5),
-          paddingBottom: pxToRem(7),
-        },
-      }),
-    }
-  },
+  message: ({ props: p, variables: v }): ICSSInJSStyle => ({
+    position: 'relative',
+    marginLeft: v.messageMargin,
+    marginRight: v.messageMargin,
+    ...(p.grouped === 'middle' && {
+      [chatMessageClassNameSelector]: {
+        borderTopLeftRadius: 0,
+        borderBottomLeftRadius: 0,
+        paddingTop: pxToRem(5),
+        paddingBottom: pxToRem(7),
+      },
+    }),
+    ...(p.grouped === 'start' && {
+      [chatMessageClassNameSelector]: {
+        borderTopLeftRadius: pxToRem(3),
+        borderBottomLeftRadius: 0,
+      },
+    }),
+    ...(p.grouped === 'end' && {
+      [chatMessageClassNameSelector]: {
+        borderTopLeftRadius: 0,
+        borderBottomLeftRadius: pxToRem(3),
+        paddingTop: pxToRem(5),
+        paddingBottom: pxToRem(7),
+      },
+    }),
+  }),
 }
 
 export default chatItemStyles
