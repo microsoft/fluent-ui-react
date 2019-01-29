a72269b3bcf0a75eaa95704d4aa2f709037fa03f
Oleksandr Fediashov <olfedias@microsoft.com>
2019-01-29 18:04:20 +0200

    remove test

--- test/specs/components/Dialog/Dialog-test.tsx
+++
@@ -1,8 +0,0 @@
-import { isConformant } from 'test/specs/commonTests'
-import Dialog from 'src/components/Dialog/Dialog'
-
-describe('Dialog', () => {
-  isConformant(Dialog, {
-    rendersPortal: true,
-  })
-})
