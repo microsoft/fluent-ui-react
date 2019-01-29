59c47be484909d2d4cae9feb0a007b3a1e62e0a7
Brian Calvery <bcalvery@microsoft.com>
2019-01-29 13:43:23 -0800

    changes to add white border around avatar in contrast theme

--- src/themes/teams-high-contrast/components/Avatar/avatarVariables.ts
+++ src/themes/teams-high-contrast/components/Avatar/avatarVariables.ts
@@ -2,5 +2,7 @@ import { AvatarVariables } from '../../../teams/components/Avatar/avatarVariable
 import { Partial } from 'types/utils'
 
 export default (siteVariables: any): Partial<AvatarVariables> => ({
+  avatarBorderColor: 'white',
+  avatarBorderWidth: 2,
   statusBorderColor: siteVariables.black,
 })
