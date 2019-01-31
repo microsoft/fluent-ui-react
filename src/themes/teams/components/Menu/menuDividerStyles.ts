adc1ead0a81776b19df3b2338b6da34b3760ec5c
manajdov <manajdov@microsoft.com>
2019-01-31 13:45:35 +0100

    -fixed styles for menu divider for horizontal menus by using alignSelf

--- src/themes/teams/components/Menu/menuDividerStyles.ts
+++ src/themes/teams/components/Menu/menuDividerStyles.ts
@@ -1,16 +1,18 @@
 import { ComponentSlotStylesInput, ICSSInJSStyle } from '../../../types'
 import { MenuDividerProps } from '../../../../components/Menu/MenuDivider'
 import { MenuVariables } from './menuVariables'
-import { pxToRem } from '../../../../lib'
 
 const menuDividerStyles: ComponentSlotStylesInput<MenuDividerProps, MenuVariables> = {
   root: ({ props: p, variables: v }): ICSSInJSStyle => {
     const borderColor = p.primary ? v.primaryBorderColor : v.borderColor
     const borderType = p.vertical ? 'borderTop' : 'borderLeft'
 
+    // TODO add margins depending on the type of the Menu
     return {
       [borderType]: `1px solid ${borderColor}`,
-      height: p.vertical ? 0 : pxToRem(32),
+      ...(!p.vertical && {
+        alignSelf: 'stretch',
+      }),
     }
   },
 }
