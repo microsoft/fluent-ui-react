0327dee4754946e7b734d029c37cbdffbb273d9b
Silviu Avram <siavram@microsoft.com>
2019-02-06 12:14:06 +0100

    removed unneeded imports from test file

--- packages/react/test/specs/components/Label/Label-test.tsx
+++ packages/react/test/specs/components/Label/Label-test.tsx
@@ -1,10 +1,8 @@
-import * as React from 'react'
 import { isConformant } from 'test/specs/commonTests'
 
 import Label from 'src/components/Label/Label'
 import Icon from 'src/components/Icon/Icon'
 import Image from 'src/components/Image/Image'
-import { mountWithProvider } from 'test/utils'
 import { implementsShorthandProp } from '../../commonTests'
 
 const labelImplementsShorthandProp = implementsShorthandProp(Label)
