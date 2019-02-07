7797fc18b963c359e381f861b9c5b03ca8321092
kuzhelov <kuroman@microsoft.com>
2019-02-07 19:19:15 +0100

    expose prop types to public interface

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -58,8 +58,8 @@ export {
   DropdownSearchInputProps,
 } from './components/Dropdown/DropdownSearchInput'
 
-export { default as Flex } from './components/Flex/Flex'
-export { default as FlexItem } from './components/Flex/FlexItem'
+export { default as Flex, FlexProps } from './components/Flex/Flex'
+export { default as FlexItem, FlexItemProps } from './components/Flex/FlexItem'
 
 export { default as Form, FormProps } from './components/Form/Form'
 export { default as FormField, FormFieldProps } from './components/Form/FormField'
