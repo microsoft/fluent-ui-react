fc1582e438ea4abfe30507f6d89caa4236ea76bb
kuzhelov <kuroman@microsoft.com>
2019-02-07 18:27:10 +0100

    minor syntax simplification

--- packages/react/src/themes/teams/components/Flex/flexStyles.ts
+++ packages/react/src/themes/teams/components/Flex/flexStyles.ts
@@ -4,35 +4,33 @@ import { FlexProps } from '../../../../components/Flex/Flex'
 import { toFlexAlignment } from './utils'
 
 const flexStyles: ComponentSlotStylesInput<FlexProps, any> = {
-  root: ({ props: p, variables: v }) => {
-    return {
-      display: 'flex',
-      ...(p.debug && { border: '1px dotted grey', background: 'lightgrey' }),
+  root: ({ props: p, variables: v }) => ({
+    display: 'flex',
+    ...(p.debug && { border: '1px dotted grey', background: 'lightgrey' }),
 
-      ...(p.inline && { display: 'inline-flex' }),
+    ...(p.inline && { display: 'inline-flex' }),
 
-      ...(p.column && { flexDirection: 'column' }),
+    ...(p.column && { flexDirection: 'column' }),
 
-      ...(p.hAlign &&
-        (p.column
-          ? { alignItems: toFlexAlignment(p.hAlign) }
-          : { justifyContent: toFlexAlignment(p.hAlign) })),
-      ...(p.vAlign &&
-        (p.column
-          ? { justifyContent: toFlexAlignment(p.vAlign) }
-          : { alignItems: toFlexAlignment(p.vAlign) })),
+    ...(p.hAlign &&
+      (p.column
+        ? { alignItems: toFlexAlignment(p.hAlign) }
+        : { justifyContent: toFlexAlignment(p.hAlign) })),
+    ...(p.vAlign &&
+      (p.column
+        ? { justifyContent: toFlexAlignment(p.vAlign) }
+        : { alignItems: toFlexAlignment(p.vAlign) })),
 
-      ...(p.space && { justifyContent: `space-${p.space}` }),
+    ...(p.space && { justifyContent: `space-${p.space}` }),
 
-      ...(p.wrap && { flexWrap: 'wrap' }),
+    ...(p.wrap && { flexWrap: 'wrap' }),
 
-      ...(p.fill && {
-        height: '100%',
-      }),
+    ...(p.fill && {
+      height: '100%',
+    }),
 
-      ...(p.padding && { padding: v[p.padding] }),
-    }
-  },
+    ...(p.padding && { padding: v[p.padding] }),
+  }),
 
   gap: ({ props: p, variables: v }) => {
     const gapValue = v[p.gap]
