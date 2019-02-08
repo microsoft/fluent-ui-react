742d81e39ec8f28b6c549ab90b6a35a3990152e2
Alexandru Buliga <bugaaa92@gmail.com>
2019-02-08 14:26:36 +0100

    fixed list item styles and removed redundancy

--- packages/react/src/themes/teams/components/Dropdown/dropdownItemStyles.ts
+++ packages/react/src/themes/teams/components/Dropdown/dropdownItemStyles.ts
@@ -1,20 +1,14 @@
 import { ComponentSlotStylesInput, ICSSInJSStyle } from '../../../types'
 import { DropdownVariables } from './dropdownVariables'
 import { DropdownItemProps } from '../../../../components/Dropdown/DropdownItem'
-import ListItem from '../../../../components/List/ListItem'
 
 const dropdownItemStyles: ComponentSlotStylesInput<DropdownItemProps, DropdownVariables> = {
-  root: ({ variables: v, props: { active } }): ICSSInJSStyle => ({
-    [`&.${ListItem.className}`]: {
-      backgroundColor: v.listItemBackgroundColor,
-      whiteSpace: 'nowrap',
-    },
-
-    ...(active && {
-      [`&.${ListItem.className}`]: {
-        backgroundColor: v.listItemBackgroundColorActive,
-        color: v.listItemColorActive,
-      },
+  root: ({ props: p, variables: v }): ICSSInJSStyle => ({
+    whiteSpace: 'nowrap',
+    backgroundColor: v.listItemBackgroundColor,
+    ...(p.active && {
+      color: v.listItemColorActive,
+      backgroundColor: v.listItemBackgroundColorActive,
     }),
   }),
 }
