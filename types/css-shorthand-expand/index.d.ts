ba2541ce6539721f2f7cced0bb5c56eb09e688eb
manajdov <manajdov@microsoft.com>
2019-02-11 13:33:23 +0100

    -added typings

---
+++ types/css-shorthand-expand/index.d.ts
@@ -0,0 +1,6 @@
+// An internal typings for `css-shorthand-expand` package
+declare module 'css-shorthand-expand' {
+  const expand: (property: string, value: string) => any
+
+  export default expand
+}
