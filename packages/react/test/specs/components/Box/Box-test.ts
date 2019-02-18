505e90ac0533aa2a3e67afe2b74d7ab8b1dad968
Oleksandr Fediashov <olfedias@microsoft.com>
2019-02-18 17:16:57 +0200

    refactor(customPropTypes): remove usage of _.chain

--- packages/react/test/specs/components/Box/Box-test.ts
+++
@@ -1,8 +0,0 @@
-import Box from 'src/components/Box/Box'
-import { isConformant } from 'test/specs/commonTests'
-
-describe('Box', () => {
-  xdescribe('is conformant', () => {
-    isConformant(Box)
-  })
-})
