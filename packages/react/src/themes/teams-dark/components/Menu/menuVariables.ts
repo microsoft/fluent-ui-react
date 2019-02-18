f85aad216bc3eea1d683147fbe82be31cb52a554
Jaanus Post <jaanusp@microsoft.com>
2019-02-18 17:01:47 +0200

    More theming

--- packages/react/src/themes/teams-dark/components/Menu/menuVariables.ts
+++ packages/react/src/themes/teams-dark/components/Menu/menuVariables.ts
@@ -1,8 +1,28 @@
 import { MenuVariables } from '../../../teams/components/Menu/menuVariables'
 
 export default (siteVars: any): Partial<MenuVariables> => ({
-    color: siteVars.white,
+    color: siteVars.gray02,
+    iconOnlyActiveColor: siteVars.brand06,
+    activeColor: siteVars.black,
+    activeBackgroundColor: siteVars.gray10,
+    focusedBackgroundColor: siteVars.gray14,
+    borderColor: siteVars.gray08,
 
-    verticalMenuBackgroundColor: 'red',
+    primaryActiveColor: siteVars.colors.white,
+    primaryActiveBackgroundColor: siteVars.brand08,
+    primaryActiveBorderColor: siteVars.colors.primary[500],
+
+    primaryFocusedColor: siteVars.white,
+    primaryFocusedBackgroundColor: siteVars.brand12,
+
+    primaryBorderColor: siteVars.gray10,
+    primaryHoverBorderColor: siteVars.gray08,
+    primaryUnderlinedBorderColor: siteVars.gray08,
+
+    disabledColor: siteVars.gray06,
+
+    lineHeightBase: siteVars.lineHeightMedium,
+
+    verticalMenuBackgroundColor: siteVars.colors.black,
     verticalMenuBoxShadow: siteVars.shadowLevel3,
 })
