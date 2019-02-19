743b85c952f5ae5c33b286be8cbe482c2890bce0
Miroslav Stastny <mistastn@microsoft.com>
2019-02-19 22:18:04 +0100

    chore(dangerjs): check dependency changes

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,6 +1,9 @@
-import { danger, fail, warn } from 'danger'
+import { danger, fail, markdown, warn } from 'danger'
 import * as fs from 'fs'
 import * as path from 'path'
+import * as _ from 'lodash'
+
+/* === CHANGELOG ==================================================================================================== */
 
 const CHANGELOG_FILE = 'CHANGELOG.md'
 
@@ -37,3 +40,53 @@ if (!hasChangelog) {
     }
   })
 }
+
+/* === Package dependencies ========================================================================================= */
+
+let dependenciesChangedLogged = false
+
+function logDependenciesChanged() {
+  if (!dependenciesChangedLogged) {
+    dependenciesChangedLogged = true
+    warn('Package dependencies changed. Make sure you have OSS approval before merging!')
+  }
+}
+
+danger.git.created_files
+  .filter(filepath => filepath.match(/\bpackage\.json$/))
+  .forEach(filepath => {
+    warn(`New package added: ${filepath}. Make sure you have OSS approval before merging!`)
+  })
+
+danger.git.modified_files
+  .filter(filepath => filepath.match(/\bpackage\.json$/))
+  .forEach(async filepath => {
+    const diff = await danger.git.JSONDiffForFile(filepath)
+    if (diff.dependencies) {
+      const before = { ...diff.dependencies.before, ..._.zipObject(diff.dependencies.added) }
+      const after = diff.dependencies.after || {}
+      const changedDependencies = _.reduce(
+        before,
+        (result, value, key) => {
+          return value === after[key] || !after[key]
+            ? result
+            : { ...result, [key]: { before: value, after: after[key] } }
+        },
+        {},
+      )
+      if (!_.isEmpty(changedDependencies)) {
+        const md = [
+          `Changed dependencies in \`${filepath}\``,
+          '',
+          'package | before | after',
+          '--- | --- | ---',
+          ..._.map(
+            changedDependencies,
+            (value, key) => `${key} | ${value['before'] || '-'} | ${value['after']}`,
+          ),
+        ].join('\n')
+        logDependenciesChanged()
+        markdown(md)
+      }
+    }
+  })
