a9212a41f8755bd3ceebfabcefae94cb2e37b81d
Andrew Martin <andmarti@microsoft.com>
2019-02-18 21:50:43 -0800

    fix(button): Update button to use semibold weight (#829)
    * updating button to use semibold weight
    
    * added a changelog entry
    
    * renaming variables
    
    * rename 'buttonFontWeight' to 'contentFontWeight'

--- packages/react/src/themes/teams/components/Button/buttonVariables.ts
+++ packages/react/src/themes/teams/components/Button/buttonVariables.ts
@@ -9,6 +9,7 @@ export interface ButtonVariables {
   borderRadius: string
   circularRadius: string
   paddingLeftRightValue: number
+  contentFontWeight: string
 
   color: string
   colorActive: string
@@ -75,7 +76,7 @@ export default (siteVars: any): ButtonVariables => {
     maxWidth: pxToRem(280),
     borderRadius: pxToRem(2),
     circularRadius: pxToRem(999),
-    fontWeight: siteVars.fontWeightSemibold,
+    contentFontWeight: siteVars.fontWeightSemibold,
     paddingLeftRightValue: 20,
 
     color: siteVars.colors.grey[900],
