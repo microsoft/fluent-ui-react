d9f00a24d0c3fb3df17182ac3acac38c2a64355a
manajdov <manajdov@microsoft.com>
2019-02-20 17:00:03 +0100

    -improved examples

--- docs/src/examples/components/Icon/Variations/IconExampleCircular.shorthand.tsx
+++ docs/src/examples/components/Icon/Variations/IconExampleCircular.shorthand.tsx
@@ -3,54 +3,17 @@ import { Icon } from '@stardust-ui/react'
 
 const IconExampleCircular = () => (
   <div>
-    <Icon
-      name="chess rook"
-      circular
-      variables={siteVars => ({
-        backgroundColor: siteVars.colors.grey[500],
-        color: siteVars.colors.white,
-      })}
-    />
-    <Icon
-      name="book"
-      circular
-      variables={siteVars => ({
-        backgroundColor: siteVars.colors.grey[500],
-        color: siteVars.colors.white,
-      })}
-    />
-    <Icon
-      name="expand"
-      circular
-      variables={siteVars => ({
-        backgroundColor: siteVars.colors.grey[500],
-        color: siteVars.colors.white,
-      })}
-    />
-    <Icon
-      name="calendar alternate outline"
-      circular
-      variables={siteVars => ({
-        backgroundColor: siteVars.colors.grey[500],
-        color: siteVars.colors.white,
-      })}
-    />
-    <Icon
-      name="compass outline"
-      circular
-      variables={siteVars => ({
-        backgroundColor: siteVars.colors.grey[500],
-        color: siteVars.colors.white,
-      })}
-    />
-    <Icon
-      name="area chart"
-      circular
-      variables={siteVars => ({
-        backgroundColor: siteVars.colors.grey[500],
-        color: siteVars.colors.white,
-      })}
-    />
+    <Icon name="chess rook" circular bordered />
+    <Icon name="book" circular bordered />
+    <Icon name="expand" circular bordered />
+    <Icon name="play" circular bordered />
+    <Icon name="call" circular bordered />
+    <Icon name="call-video" circular bordered />
+    <Icon name="stop" circular bordered />
+    <Icon name="calendar alternate outline" circular bordered />
+    <Icon name="coffee" circular bordered />
+    <Icon name="compass outline" circular bordered />
+    <Icon name="area chart" circular bordered />
   </div>
 )
 
