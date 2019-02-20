bc9b4fa692322bd2229e2067ca5b5b2948ab4a0f
Jaanus Post <jaanusp@microsoft.com>
2019-02-20 12:56:12 +0200

    Removing duplicate menu variables

--- packages/react/src/themes/teams-dark/components/Menu/menuVariables.ts
+++ packages/react/src/themes/teams-dark/components/Menu/menuVariables.ts
@@ -3,7 +3,6 @@ import { MenuVariables } from '../../../teams/components/Menu/menuVariables'
 
 export default (siteVars: any): Partial<MenuVariables> => ({
 
-    color: siteVars.gray02,
     borderColor: siteVars.black,
 
     focusedBorder: `solid ${pxToRem(1)} ${siteVars.colors.white}`,
@@ -13,17 +12,8 @@ export default (siteVars: any): Partial<MenuVariables> => ({
 
     activeColor: siteVars.colors.white,
     activeBackgroundColor: siteVars.gray08,
-    iconOnlyActiveColor: siteVars.brand06,
-
-    primaryActiveColor: siteVars.colors.white,
-    primaryActiveBackgroundColor: siteVars.brand08,
-    primaryActiveBorderColor: siteVars.colors.primary[500],
-
-    primaryFocusedColor: siteVars.white,
-    primaryFocusedBackgroundColor: siteVars.brand12,
 
     verticalMenuBackgroundColor: siteVars.gray10,
-    verticalMenuBoxShadow: siteVars.shadowLevel3,
 
     menuDividerHeight: pxToRem(2),
     menuBorderWidth: pxToRem(2),
