b3bfe018a9443ea556651fa14612f512c8f4954a
kuzhelov <kuroman@microsoft.com>
2019-02-21 19:19:05 +0300

    add performance benchmark for List

---
+++ docs/src/examples/components/List/Performance/List.perf.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react'
+import { List, Image } from '@stardust-ui/react'
+
+let items = [
+  {
+    key: 'irving',
+    media: <Image avatar />,
+    header: 'Irving Kuhic',
+    headerMedia: '7:26:56 AM',
+    content: 'Program the sensor to the SAS alarm through the haptic SQL card!',
+  },
+  {
+    key: 'skyler',
+    media: <Image avatar />,
+    header: 'Skyler Parks',
+    headerMedia: '11:30:17 PM',
+    content: 'Use the online FTP application to input the multi-byte application!',
+  },
+  {
+    key: 'dante',
+    media: <Image avatar />,
+    header: 'Dante Schneider',
+    headerMedia: '5:22:40 PM',
+    content: 'The GB pixel is down, navigate the virtual interface!',
+  },
+]
+
+items = [...items, ...items]
+items = [...items, ...items]
+items = [...items, ...items]
+items = [...items, ...items]
+
+class ListExampleMediaShorthand extends React.Component {
+  render() {
+    return (
+      <>
+        <List items={items} />
+      </>
+    )
+  }
+}
+
+export default ListExampleMediaShorthand
