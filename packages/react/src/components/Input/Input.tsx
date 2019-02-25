f701d3cdf8adea3a2f32723cbba2987e8051a9d4
Paras Jindal <pajindal@microsoft.com>
2019-02-25 15:33:52 +0530

    Invoked onChange on clicking clear icon

--- packages/react/src/components/Input/Input.tsx
+++ packages/react/src/components/Input/Input.tsx
@@ -176,7 +176,7 @@ class Input extends AutoControlledComponent<ReactProps<InputProps>, InputState> 
 
   private handleIconOverrides = predefinedProps => ({
     onClick: (e: React.SyntheticEvent) => {
-      this.handleOnClear()
+      this.handleOnClear(e)
       this.inputRef.current.focus()
       _.invoke(predefinedProps, 'onClick', e, this.props)
     },
@@ -191,9 +191,13 @@ class Input extends AutoControlledComponent<ReactProps<InputProps>, InputState> 
     this.trySetState({ value })
   }
 
-  private handleOnClear = () => {
+  private handleOnClear = (e: React.SyntheticEvent) => {
     if (this.props.clearable) {
-      this.trySetState({ value: '' })
+      const value = ''
+
+      _.invoke(this.props, 'onChange', e, { ...this.props, value })
+
+      this.trySetState({ value })
     }
   }
 
