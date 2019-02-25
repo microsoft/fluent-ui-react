53a2dae9d7531c2d901868f2c74171204ac14183
Jaanus Post <jaanuspost@hotmail.com>
2019-02-25 18:06:42 +0200

    Removing duplicate variables

--- packages/react/src/themes/teams-dark/siteVariables.ts
+++ packages/react/src/themes/teams-dark/siteVariables.ts
@@ -31,14 +31,6 @@ export const red08 = '#4f232b' // no mapping color
 export const green04 = naturalColors.lightGreen[900]
 
 //
-// SHADOW LEVELS
-//
-export const shadowLevel1 = '0 .2rem .4rem -.075rem rgba(0, 0, 0, .1)'
-export const shadowLevel2 = '0 .4rem 0.7rem -0.1rem rgba(0, 0, 0, .1)'
-export const shadowLevel3 = '0 .8rem 1rem -0.2rem rgba(0, 0, 0, .1)'
-export const shadowLevel4 = '0 .6rem 1.8rem -0.4rem rgba(0, 0, 0, .1)'
-
-//
 // SEMANTIC ASSIGNMENTS
 //
 export const bodyBackground = black
