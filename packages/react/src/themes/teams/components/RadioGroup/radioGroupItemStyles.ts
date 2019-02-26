c8a39cc819cb68065e35372196d87e8ce66256d5
kuzhelov <kuroman@microsoft.com>
2019-02-26 21:48:10 +0100

    fix styles of radio group

--- packages/react/src/themes/teams/components/RadioGroup/radioGroupItemStyles.ts
+++ packages/react/src/themes/teams/components/RadioGroup/radioGroupItemStyles.ts
@@ -31,6 +31,7 @@ const radioStyles: ComponentSlotStylesInput<
 
   icon: ({ props: p, variables: v }): ICSSInJSStyle => ({
     margin: `0 ${pxToRem(10)} 0 0`,
+    padding: 0,
     color: p.checked ? v.colorChecked : v.color,
     backgroundColor: p.checked ? v.colorBackgroundChecked : v.colorBackground,
     borderColor: p.checked ? v.colorBorderChecked : v.colorBorder,
