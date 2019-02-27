3da01fe04f4ef32a3892edb5baf4ee5f6e359a49
Oleksandr Fediashov <olfedias@microsoft.com>
2019-02-27 15:03:36 +0200

    fix visual regressions

--- docs/src/examples/components/Dropdown/Types/DropdownExample.shorthand.steps.ts
+++ docs/src/examples/components/Dropdown/Types/DropdownExample.shorthand.steps.ts
@@ -8,9 +8,23 @@ const selectors = {
 const steps = [
   steps => steps.click(selectors.triggerButton).snapshot('Shows list'),
   steps => steps.click(selectors.item(3)).snapshot('Selects an item'),
-  steps => steps.click(selectors.triggerButton).snapshot('Opens with selected item highlighted'),
-  steps => steps.hover(selectors.item(2)).snapshot('Highlights another item'),
-  steps => steps.click(selectors.triggerButton).snapshot('Closes the list'),
+  steps =>
+    steps
+      .click(selectors.item(3))
+      .click(selectors.triggerButton)
+      .snapshot('Opens with selected item highlighted'),
+  steps =>
+    steps
+      .click(selectors.item(3))
+      .click(selectors.triggerButton)
+      .hover(selectors.item(2))
+      .snapshot('Highlights another item'),
+  steps =>
+    steps
+      .click(selectors.triggerButton)
+      .click(selectors.item(3))
+      .click(selectors.triggerButton)
+      .snapshot('Closes the list'),
 ]
 
 export default steps
