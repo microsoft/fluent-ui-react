87df5dda60cc97da277592909a16e5876223ceb3
kuzhelov <kuroman@microsoft.com>
2019-02-28 11:52:18 +0100

    honor predefined classnames

--- packages/react/src/components/Box/Box.tsx
+++ packages/react/src/components/Box/Box.tsx
@@ -1,14 +1,23 @@
 import { createShorthandFactory, getClasses, felaRenderer } from '../../lib'
 
 import { BoxClassName, BoxProps, renderBox } from './common'
+import cx from 'classnames'
 
 /**
  * A Box is a basic component (no default styles)
  */
 const BoxLight = (props: BoxProps) => {
-  const { as: ElementType = 'div', children, content, styles, ...unhandledProps } = props
+  const {
+    as: ElementType = 'div',
+    children,
+    className: predefinedClasses,
+    content,
+    styles,
+    ...unhandledProps
+  } = props
 
   const classes = getClasses(felaRenderer, { root: styles }, {} as any)
+  classes.root = cx(predefinedClasses, classes.root)
 
   return renderBox({
     ElementType,
