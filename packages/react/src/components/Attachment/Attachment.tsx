41da62fbdfadb6d9f66dacf5f741fa048f2ba1b9
manajdov <manajdov@microsoft.com>
2019-03-01 15:35:06 +0100

    -added import

--- packages/react/src/components/Attachment/Attachment.tsx
+++ packages/react/src/components/Attachment/Attachment.tsx
@@ -5,7 +5,7 @@ import { ReactProps, ShorthandValue, ComponentEventHandler } from '../../types'
 import {
   UIComponent,
   customPropTypes,
-  // createShorthandFactory,
+  createShorthandFactory,
   commonPropTypes,
   isFromKeyboard,
 } from '../../lib'
