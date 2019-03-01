ed2a93501ac539de41dbfda62923f4654bf4a0ef
kuzhelov <kuroman@microsoft.com>
2019-03-01 15:21:59 +0300

    move getDisplayName to test utils

--- packages/react/test/specs/commonTests/isConformant.tsx
+++ packages/react/test/specs/commonTests/isConformant.tsx
@@ -9,11 +9,11 @@ import { ThemeProvider, FelaTheme } from 'react-fela'
 import Ref from 'src/components/Ref/Ref'
 import RefFindNode from 'src/components/Ref/RefFindNode'
 
-import { getDisplayName } from 'src/lib'
 import isExportedAtTopLevel from './isExportedAtTopLevel'
 import {
   assertBodyContains,
   consoleUtil,
+  getDisplayName,
   mountWithProvider as mount,
   syntheticEvent,
 } from 'test/utils'
