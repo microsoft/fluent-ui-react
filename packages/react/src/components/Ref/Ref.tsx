8af8a7e9094585efd0f30d990f84b19124b832ac
Oleksandr Fediashov <olfedias@microsoft.com>
2019-03-11 15:34:08 +0200

    fix(types): make *Props interface extendable

--- packages/react/src/components/Ref/Ref.tsx
+++ packages/react/src/components/Ref/Ref.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 import { isForwardRef } from 'react-is'
 
 import { ChildrenComponentProps, customPropTypes } from '../../lib'
-import { ReactPropsStrict } from '../../types'
+import { ReactProps } from '../../types'
 import RefFindNode from './RefFindNode'
 import RefForward from './RefForward'
 
@@ -16,7 +16,7 @@ export interface RefProps extends ChildrenComponentProps<React.ReactElement<any>
   innerRef?: React.Ref<any>
 }
 
-const Ref: React.SFC<ReactPropsStrict<RefProps>> = props => {
+const Ref: React.FunctionComponent<ReactProps<RefProps>> = props => {
   const { children, innerRef } = props
 
   const child = React.Children.only(children)
