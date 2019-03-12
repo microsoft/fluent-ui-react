d7c2449b13efabbe99eab8c2c0269ac8fd9eb00e
Oleksandr Fediashov <olfedias@microsoft.com>
2019-03-12 14:37:54 +0200

    TMP!

--- build/screener/screener.steps.ts
+++ build/screener/screener.steps.ts
@@ -3,6 +3,8 @@ import * as fs from 'fs'
 import * as Steps from 'screener-runner/src/steps'
 import * as keys from 'screener-runner/src/keys'
 
+const DEFAULT_THEMES = ['teams']
+
 Steps.prototype.resetExternalLayout = function () {
   return this.executeScript(`window.resetExternalLayout()`)
 }
@@ -11,63 +13,25 @@ Steps.prototype.switchTheme = function (themeName: ScreenerThemeName) {
   return this.executeScript(`window.switchTheme("${themeName}")`)
 }
 
-const getStepsForTheme = (
-  sb: ScreenerStepBuilder,
-  themeName: ScreenerThemeName,
-): ScreenerStepBuilder =>
-  themeName ? sb.switchTheme(themeName).snapshot(`${_.startCase(themeName)} theme`) : sb
-
-const applyThemeBeforeStep = (step: ScreenerStep, themeName: ScreenerThemeName): ScreenerStep => (
-  sb,
-  keys,
-) => step(getStepsForTheme(sb, themeName), keys)
-
-/**
- * Inserts the switchTheme step before [step] steps for each theme in [themes] array
- * returns an array of steps
- * e.g.: applyThemesBeforeStep(steps => steps.hover('sel').snapshot('snapshot hover'), ['teams', 'teamsDark'])
- * returns:
- * [
- *   steps => steps.switchTheme('teams').snapshot('Teams theme').hover('#sel').snapshot('snapshot name'),
- *   steps => steps.switchTheme('teamsDark').snapshot('Teams Dark theme').hover('#sel').snapshot('snapshot name'),
- *   steps => steps.switchTheme('teamsHighContrast').snapshot('Teams High Contrast theme').hover('#sel').snapshot('snapshot name'),
- * ]
- */
-const applyThemesBeforeStep = (step: ScreenerStep, themes: ScreenerThemeName[]): ScreenerStep => {
-  if (!themes || themes.length < 1) return step
-  return (sb, keys) =>
-    themes
-      .map(themeName => applyThemeBeforeStep(step, themeName))
-      .reduce((stepAcc, currentStep) => currentStep(stepAcc, keys), sb)
-}
-
-/**
- * Resets the layout after each element in [steps] and reduces it to one step
- * e.g.: reduceSteps([
- *  steps => steps.click('#sel1'),
- *  steps => steps.click('#sel2'),
- *  steps => steps.click('#sel3'),
- * ]
- * returns:
- * steps =>
- *  steps.click('#sel1').resetExternalLayout(),
- *  steps.click('#sel2').resetExternalLayout(),
- *  steps.click('#sel3').resetExternalLayout(),
- */
-const reduceSteps = (steps: ScreenerSteps): ScreenerStep => (sb, keys) => {
-  if (!steps || steps.length < 1) return sb
-  return steps.reduce((stepsAcc, steps) => steps(stepsAcc, keys).resetExternalLayout(), sb)
-}
-
 const getScreenerSteps = (stepsModulePath: string): any[] => {
   if (!fs.existsSync(`${stepsModulePath}.ts`)) {
     return undefined
   }
 
-  const { steps, themes } = require(stepsModulePath).default as ScreenerTestsConfig
-  const themedStep = applyThemesBeforeStep(reduceSteps(steps), themes)
+  const stepsBuilder = new Steps()
+  const { steps: screenerSteps, themes = DEFAULT_THEMES } = require(stepsModulePath)
+    .default as ScreenerTestsConfig
 
-  return themedStep(new Steps(), keys).end()
+  _.forEach(themes, themeName => {
+    stepsBuilder.switchTheme(themeName).snapshot(`Theme: `, themeName)
+
+    _.forEach(screenerSteps, (screenerStep: ScreenerStep) => {
+      screenerStep(stepsBuilder, keys)
+      stepsBuilder.executeScript(`window.resetExternalLayout()`)
+    })
+  })
+
+  return stepsBuilder.end()
 }
 
 export default getScreenerSteps
