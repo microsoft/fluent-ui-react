b1cee181b82ceca33264b6b31990571657ef5484
Oleksandr Fediashov <olfedias@microsoft.com>
2019-03-13 12:17:05 +0200

    fix typings

--- types/simulant.d.ts
+++ types/simulant.d.ts
@@ -1,3 +1,11 @@
 declare module 'simulant' {
-  export function fire(node: Node, eventType: string)
+  namespace event {
+    function fire(el: HTMLElement, event: Event): void
+    function fire(el: HTMLElement, event: 'string', payload?: { [key: string]: any }): void
+  }
+
+  function event(window: Window, eventName: string, payload?: { [key: string]: any }): Event
+  function event(eventName: string, payload?: { [key: string]: any }): Event
+
+  export = event
 }
