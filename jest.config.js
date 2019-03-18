fe4ad6d56fb559875b4ca7a0372839cc62c3b9c5
Oleksandr Fediashov <olfedias@microsoft.com>
2019-03-18 14:40:41 +0200

    fix typo

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
-const { rollup: lernaAlises } = require('lerna-alias')
+const { rollup: lernaAliases } = require('lerna-alias')
 const commonConfig = require('./build/jest/jest.config.common')
 
 // packages/react/src -> packages/react,
 // as lernaAliases append 'src' by default
-const stardustPackages = lernaAlises({ sourceDirectory: false })
+const stardustPackages = lernaAliases({ sourceDirectory: false })
 const projects = Object.keys(stardustPackages).map(packageName => stardustPackages[packageName])
 
 module.exports = {
