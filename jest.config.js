9a1166fbbe459e9e9e4235b3c47119e9bc2ff1f0
Miroslav Stastny <mistastn@microsoft.com>
2019-03-18 00:29:22 -0700

    fix(unit tests): fix warnings (#1059)
    * Fix tests execution from WebStorm
    
    * Get rid of the esModuleInterop warning
    
    * Add Popper.js mock
    
    * config fixes
    
    * fix memory leak

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
-const commonConfig = require('./build/jest/jest.config.common')
+const { rollup: lernaAlises } = require('lerna-alias')
 
-module.exports = commonConfig
+// packages/react/src -> packages/react,
+// as lernaAliases append 'src' by default
+const stardustPackages = lernaAlises({ sourceDirectory: false })
+const projects = Object.keys(stardustPackages).map(packageName => stardustPackages[packageName])
+
+module.exports = {
+  projects,
+}
