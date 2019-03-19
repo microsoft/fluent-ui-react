69c1ce62e815435c84cc7d08d1a64487ffa221fd
manajdov <manajdov@microsoft.com>
2019-03-19 17:08:26 +0100

    -updated button variables in dark theme

--- packages/react/src/themes/teams-dark/components/Button/buttonVariables.ts
+++ packages/react/src/themes/teams-dark/components/Button/buttonVariables.ts
@@ -6,11 +6,16 @@ export default (siteVars: any): Partial<ButtonVariables> => {
     colorActive: siteVars.colors.white,
     colorHover: siteVars.colors.white,
     colorFocus: siteVars.colors.white,
+    colorDisabled: siteVars.colors.grey.dark06,
     backgroundColor: 'transparent',
     backgroundColorHover: siteVars.colors.grey.dark14,
     backgroundColorFocus: siteVars.colors.grey.dark08,
     backgroundColorActive: siteVars.colors.grey.dark08,
+    backgroundColorDisabled: siteVars.colors.grey.dark09,
+    borderColor: siteVars.colors.grey.dark08,
+    borderColorActive: siteVars.colors.grey.dark06,
     borderColorFocus: siteVars.black,
+    borderColorHover: siteVars.colors.grey.dark06,
     borderColorFocusIndicator: siteVars.colors.white,
 
     primaryBackgroundColorActive: siteVars.brand08,
@@ -19,10 +24,12 @@ export default (siteVars: any): Partial<ButtonVariables> => {
     primaryBorderColorFocus: siteVars.black,
     primaryBorderColorFocusIndicator: siteVars.colors.white,
 
+    circularColor: siteVars.colors.grey.dark02,
     circularColorActive: siteVars.black,
     circularBackgroundColorActive: siteVars.colors.grey.dark02,
     circularBackgroundColorHover: siteVars.colors.grey.dark03,
     circularBackgroundColorFocus: siteVars.colors.grey.dark02,
+    circularBorderColor: siteVars.colors.grey.dark02,
     circularBorderColorFocusIndicator: siteVars.black,
   }
 }
