4b6296e9084cdd604b71cbc8a3a83ab7c504bf67
Oleksandr Fediashov <olfedias@microsoft.com>
2019-03-19 15:38:41 +0200

    fix(jest): avoid test double run

--- build/jest/jest.config.common.js
+++ build/jest/jest.config.common.js
@@ -5,7 +5,6 @@ const setupFilesAfterEnv = [
 ]
 
 module.exports = {
-  rootDir: `${__dirname}/../../`,
   coverageDirectory: './coverage/',
   coverageReporters: ['json', 'lcov'],
   testRegex: '/test/.*-test\\.tsx?$',
@@ -14,17 +13,10 @@ module.exports = {
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
-  moduleNameMapper: {
-    ...lernaAliases(),
-    'docs/(.*)$': `<rootDir>/docs/$1`,
-
-    // Legacy aliases, they should not be used in new tests
-    'src/(.*)$': `<rootDir>/packages/react/src/$1`,
-    'test/(.*)$': `<rootDir>/packages/react/test/$1`,
-  },
+  moduleNameMapper: lernaAliases(),
   globals: {
     'ts-jest': {
-      tsConfig: `<rootDir>/build/tsconfig.test.json`,
+      tsConfig: `${__dirname}/../tsconfig.test.json`,
       diagnostics: {
         ignoreCodes: 151001,
       },
