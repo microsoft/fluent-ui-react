f12c1ef03400297cf321db9cfbcfd481cc577ea5
manajdov <manajdov@microsoft.com>
2019-03-20 17:15:04 +0100

    -fixed grey color -improved menu color example

--- docs/src/examples/components/Menu/Variations/MenuExampleColor.shorthand.tsx
+++ docs/src/examples/components/Menu/Variations/MenuExampleColor.shorthand.tsx
@@ -1,34 +1,43 @@
-import * as React from 'react'
-import { Menu, Provider } from '@stardust-ui/react'
-
+import React from 'react'
+import { Menu, Flex, Provider } from '@stardust-ui/react'
 const items = [
-  { key: 'editorials', content: 'Editorials' },
-  { key: 'review', content: 'Reviews' },
-  { key: 'events', content: 'Upcoming Events' },
+  {
+    key: 'editorials',
+    content: 'Editorials',
+  },
+  {
+    key: 'review',
+    content: 'Reviews',
+  },
+  {
+    key: 'events',
+    content: 'Upcoming Events',
+  },
 ]
 
 const MenuExamplePrimary = () => (
   <Provider.Consumer
-    render={theme =>
-      Object.keys(theme.siteVariables.colorScheme).map(color => (
-        <Menu
-          // variables={{
-          //   // flat
-          //   backgroundColor: 'red',
-          //   backgroundColorHover: 'blue',
-          //   verticalBackgroundColor: 'green',
-          //   verticalBackgroundColorHover: 'orange',
-          //   verticalItemBackgroundColor: 'red',
-          //   verticalItemBackgroundColorHover: 'black',
-          // }}
-          vertical
-          key={color}
-          defaultActiveIndex={0}
-          items={items}
-          color={color}
-        />
-      ))
-    }
+    render={theme => (
+      <Flex column gap="gap.medium">
+        {Object.keys(theme.siteVariables.colorScheme).map(color => (
+          <Menu // variables={{
+            //   // flat
+            //   backgroundColor: 'red',
+            //   backgroundColorHover: 'blue',
+            //   verticalBackgroundColor: 'green',
+            //   verticalBackgroundColorHover: 'orange',
+            //   verticalItemBackgroundColor: 'red',
+            //   verticalItemBackgroundColorHover: 'black',
+            // }}
+            vertical
+            key={color}
+            defaultActiveIndex={0}
+            items={items}
+            color={color}
+          />
+        ))}
+      </Flex>
+    )}
   />
 )
 
