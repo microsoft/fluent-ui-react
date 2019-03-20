fa4c6f9dbfff5ca7c4043fc0ef5c3d963be7cda0
Oleksandr Fediashov <olfedias@microsoft.com>
2019-03-20 16:33:22 +0200

    fix markup

--- docs/src/components/ComponentPlayground.tsx
+++ docs/src/components/ComponentPlayground.tsx
@@ -2,34 +2,28 @@ import { KnobInspector, KnobProvider } from '@stardust-ui/docs-components'
 import { Flex, Segment } from '@stardust-ui/react'
 import * as React from 'react'
 
-const ComponentPlayground: React.FunctionComponent = props => {
-  console.log(props.children)
-  return (
-    <KnobProvider>
-      <Flex gap="gap.medium">
-        <Flex.Item grow>
-          <Flex column>
-            <Segment
-              styles={{
-                alignItems: 'center',
-                display: 'flex',
-                justifyContent: 'center',
-                minHeight: 250,
-              }}
-            >
-              {props.children}
-            </Segment>
-          </Flex>
-        </Flex.Item>
+const ComponentPlayground: React.FunctionComponent = props => (
+  <KnobProvider>
+    <Flex gap="gap.medium">
+      <Flex.Item grow>
+        <Segment
+          styles={{
+            alignItems: 'center',
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          {props.children}
+        </Segment>
+      </Flex.Item>
 
-        <Flex.Item align="start" push>
-          <Segment color="primary">
-            <KnobInspector />
-          </Segment>
-        </Flex.Item>
-      </Flex>
-    </KnobProvider>
-  )
-}
+      <Flex.Item align="start" push>
+        <Segment color="primary">
+          <KnobInspector />
+        </Segment>
+      </Flex.Item>
+    </Flex>
+  </KnobProvider>
+)
 
 export default ComponentPlayground
