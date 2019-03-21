499eae7bc6e0a011708f7b39d141bd2175568793
Oleksandr Fediashov <olfedias@microsoft.com>
2019-03-21 15:10:32 +0200

    move warning

--- docs/src/examples/components/Box/index.tsx
+++ docs/src/examples/components/Box/index.tsx
@@ -1,8 +1,30 @@
+import { Alert } from '@stardust-ui/react'
 import * as React from 'react'
+import { Link } from 'react-router-dom'
+
 import Types from './Types'
 
 const HeaderExamples = () => (
   <>
+    <Alert styles={{ display: 'block' }} warning>
+      <p>
+        <code>Box</code> component should be used carefully, in almost all cases you don't need it.
+      </p>
+      <p>
+        For layouts and positioning please prefer <Link to="/components/flex">Flex</Link> and{' '}
+        <Link to="/components/grid">Grid</Link> components, we also have{' '}
+        <Link to="/components/text">Text</Link> component to wrap text. In other cases please try to
+        use existing components and apply styles via theming features. You also can{' '}
+        <Link to="/integrate-custom-components">create your own custom component</Link> for custom
+        behaviors.
+      </p>
+      <p>
+        Remember that <code>styles</code> that applied directly to any component are not friendly
+        for theme switching, prefer to use <code>variables</code> instead of <code>styles</code> for
+        overrides.
+      </p>
+    </Alert>
+
     <Types />
   </>
 )
