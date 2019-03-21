c965396fcdbcaa3a57f323c4a1983fe4ed1061b2
Oleksandr Fediashov <olfedias@microsoft.com>
2019-03-21 17:55:05 +0200

    styling fixes

--- packages/react-component-event-listener/jest.config.js
+++ packages/react-component-event-listener/jest.config.js
@@ -1,7 +1,4 @@
 module.exports = {
   ...require('@stardust-ui/internal-tooling/jest'),
   name: 'react-component-event-listener',
-  // transform: {
-  //   '^.+\\.tsx?$': `${__dirname}/transform.js`,
-  // },
 }
