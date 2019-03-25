3ae62b706395a53c602fdadef3d2ab9bf1128451
Jaanus Post <jaanusp@microsoft.com>
2019-03-25 17:25:34 +0200

    PR feedback

--- docs/src/examples/components/Button/commonScreenerSteps.ts
+++ docs/src/examples/components/Button/commonScreenerSteps.ts
@@ -3,9 +3,9 @@ import { Button } from '@stardust-ui/react'
 const button = `.${Button.className}`
 
 const getScreenerSteps = (): ScreenerSteps => [
-  builder => builder.hover(button).snapshot('Hover state of the first button'),
-  builder => builder.click(button).snapshot('Active state of the first button (click)'),
-  (builder, keys) => builder.keys(button, keys.tab).snapshot('Focus state of the second button'),
+  builder => builder.hover(button).snapshot('Hovers the first button'),
+  builder => builder.click(button).snapshot('Clicks the first button'),
+  (builder, keys) => builder.keys(button, keys.tab).snapshot('Focuses on the second button'),
 ]
 
 export default getScreenerSteps
--- docs/src/examples/components/Menu/Usages/MenuExampleToolbar.shorthand.steps.ts
+++ docs/src/examples/components/Menu/Usages/MenuExampleToolbar.shorthand.steps.ts
@@ -1,8 +1,5 @@
 import { Menu } from '@stardust-ui/react'
-
-interface StepsOptions {
-  vertical?: boolean
-}
+import getScreenerSteps from '../commonScreenerSteps'
 
 const selectors = {
   menu: `.${Menu.className}`,
@@ -10,31 +7,17 @@ const selectors = {
   lastItem: `.${Menu.className} li:last-child a`,
 }
 
-const getScreenerSteps = ({ vertical }: StepsOptions = {}): ScreenerSteps => [
-  (builder, keys) =>
-    builder
-      .hover(selectors.item(2))
-      .snapshot('Hovers 2nd item (hover state styles)')
-
-      .click(selectors.item(2))
-      .snapshot('Clicks on 2nd item (active state styles)')
-
-      .keys(selectors.item(2), vertical ? keys.downArrow : keys.rightArrow)
-      .snapshot('Navigates to next item (focus state styles)')
-
-      .keys(selectors.item(3), vertical ? keys.upArrow : keys.leftArrow)
-      .snapshot('Navigates to previous item (active and focus state styles)')
-
-      .click(selectors.lastItem)
-      .snapshot('Clicks on the last item and opens submenu')
-
-      .keys(selectors.lastItem, keys.downArrow)
-      .snapshot('Focuses on the first element in the submenu'),
-]
-
 const config: ScreenerTestsConfig = {
   themes: ['teams', 'teamsDark', 'teamsHighContrast'],
-  steps: getScreenerSteps(),
+  steps: [
+    (builder, keys) =>
+      getScreenerSteps()[0](builder, keys)
+        .click(selectors.lastItem)
+        .snapshot('Clicks on the last item and opens submenu')
+
+        .keys(selectors.lastItem, keys.downArrow)
+        .snapshot('Focuses on the first element in the submenu'),
+  ],
 }
 
 export default config
