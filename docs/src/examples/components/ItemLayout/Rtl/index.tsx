915d67a9191bbac695fa3948eef538ffdf3f7da9
Miroslav Stastny <mistastn@microsoft.com>
2019-03-28 13:01:24 +0100

    Add RTL example

---
+++ docs/src/examples/components/ItemLayout/Rtl/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+import ComponentExample from 'docs/src/components/ComponentDoc/ComponentExample'
+import NonPublicSection from 'docs/src/components/ComponentDoc/NonPublicSection'
+
+const Rtl = () => (
+  <NonPublicSection title="Rtl">
+    <ComponentExample examplePath="components/ItemLayout/Rtl/ItemLayoutExample.rtl" />
+  </NonPublicSection>
+)
+
+export default Rtl
