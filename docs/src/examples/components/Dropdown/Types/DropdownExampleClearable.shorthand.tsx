e3291997ab1e2182d4c7e81bc344ab5d003e87e9
Oleksandr Fediashov <olfedias@microsoft.com>
2019-03-29 15:38:24 +0200

    revert changes in example

--- docs/src/examples/components/Dropdown/Types/DropdownExampleClearable.shorthand.tsx
+++ docs/src/examples/components/Dropdown/Types/DropdownExampleClearable.shorthand.tsx
@@ -1,4 +1,4 @@
-import { Dropdown, Flex } from '@stardust-ui/react'
+import { Dropdown } from '@stardust-ui/react'
 import * as React from 'react'
 
 const inputItems = [
@@ -14,16 +14,7 @@ const inputItems = [
 ]
 
 const DropdownClearableExample = () => (
-  <Flex column gap="gap.medium">
-    <Dropdown clearable items={inputItems} placeholder="Select your hero" />
-    <Dropdown
-      clearable
-      items={inputItems}
-      noResultsMessage="We couldn't find any matches."
-      placeholder="Select your hero"
-      search
-    />
-  </Flex>
+  <Dropdown clearable items={inputItems} placeholder="Select your hero" />
 )
 
 export default DropdownClearableExample
