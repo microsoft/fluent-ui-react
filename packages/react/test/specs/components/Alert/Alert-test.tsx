310ea661a8d7fa6845db236dd753d30560d08dfe
Juraj Kapsiar <jurokapsiar@gmail.com>
2019-04-01 17:15:59 +0200

    feat(acc): Alert behavior (#1119)
    * alert behavior
    
    * behavior test
    
    * alert danger and info behaviors
    
    * only target warning
    
    * changelog and PR comments

--- packages/react/test/specs/components/Alert/Alert-test.tsx
+++ packages/react/test/specs/components/Alert/Alert-test.tsx
@@ -1,4 +1,11 @@
-import { isConformant, implementsShorthandProp } from 'test/specs/commonTests'
+import * as React from 'react'
+
+import {
+  isConformant,
+  implementsShorthandProp,
+  handlesAccessibility,
+  htmlIsAccessibilityCompliant,
+} from 'test/specs/commonTests'
 
 import Alert from 'src/components/Alert/Alert'
 import Box from 'src/components/Box/Box'
@@ -8,7 +15,24 @@ const alertImplementsShorthandProp = implementsShorthandProp(Alert)
 
 describe('Alert', () => {
   isConformant(Alert)
+  handlesAccessibility(Alert, { defaultRootRole: undefined, requiredProps: { content: 'test' } })
+  handlesAccessibility(Alert, {
+    defaultRootRole: undefined,
+    partSelector: `.${Alert.slotClassNames.content}`,
+    requiredProps: { content: 'test' },
+  })
+  handlesAccessibility(Alert, {
+    defaultRootRole: 'alert',
+    partSelector: `.${Alert.slotClassNames.content}`,
+    requiredProps: { content: 'test', warning: true },
+  })
 
   alertImplementsShorthandProp('action', Button, { mapsValueToProp: 'content' })
   alertImplementsShorthandProp('content', Box, { mapsValueToProp: 'children' })
+
+  describe('compliance', () => {
+    test('default', async () => await htmlIsAccessibilityCompliant(<Alert content="Test" />))
+
+    test('danger', async () => await htmlIsAccessibilityCompliant(<Alert danger content="Test" />))
+  })
 })
