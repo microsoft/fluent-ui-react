5cf354448e060f7237cf800ab4f8e1bdcff512d0
Silviu Avram <silviu.avram91@gmail.com>
2019-04-01 18:05:53 +0200

    added first round of dropdown tests

--- cypress/integration/components/dropdown.spec.ts
+++ cypress/integration/components/dropdown.spec.ts
@@ -1,19 +1,99 @@
-// import Dropdown from '@stardust-ui/react/src/components/Dropdown/Dropdown'
-import Dropdown from '../../../packages/react/src/components/Dropdown/Dropdown'
-import DropdownItem from '../../../packages/react/src/components/Dropdown/DropdownItem'
-// import DropdownItem from '@stardust-ui/react/src/components/Dropdown/DropdownItem'
+const triggerButton = '.ui-dropdown__trigger-button'
+const item = '.ui-dropdown__item'
 
 describe('Dropdown', () => {
   describe('Selection', () => {
+    const selectionComponentPath = '/dropdown-example-shorthand/false'
     before(() => {
-      cy.visit('/dropdown-example-shorthand/false')
+      cy.visit(selectionComponentPath)
     })
 
-    it('select an option by click', () => {
-      cy.get(Dropdown.slotClassNames.triggerButton).click()
+    it('selects item on click', () => {
+      cy.get(triggerButton).click()
+      cy.get(`${item}:nth-child(1)`).click()
+      cy.get(triggerButton).should('have.text', 'Bruce Wayne')
+    })
 
-      cy.get(`${DropdownItem.className}:nth-child(0)`).click()
-      cy.get(Dropdown.slotClassNames.triggerButton).should('have.value', 'Bruce Wayne')
+    it('selects next item by opening with ArrowDown', () => {
+      cy.get(triggerButton)
+        .focus()
+        .trigger('keydown', { key: 'ArrowDown' })
+        .trigger('keyup', { key: 'ArrowDown' })
+        .trigger('keydown', { key: 'Enter' })
+        .trigger('keyup', { key: 'Enter' })
+        .should('have.text', 'Natasha Romanoff')
+    })
+
+    it('selects next item by ArrowDown navigation', () => {
+      cy.get(triggerButton)
+        .click()
+        .trigger('keydown', { key: 'ArrowDown' })
+        .trigger('keyup', { key: 'ArrowDown' })
+        .trigger('keydown', { key: 'Enter' })
+        .trigger('keyup', { key: 'Enter' })
+        .should('have.text', 'Steven Strange')
+    })
+
+    it('selects previous item by opening with ArrowUp', () => {
+      cy.get(triggerButton)
+        .focus()
+        .trigger('keydown', { key: 'ArrowUp' })
+        .trigger('keyup', { key: 'ArrowUp' })
+        .trigger('keydown', { key: 'Enter' })
+        .trigger('keyup', { key: 'Enter' })
+        .should('have.text', 'Natasha Romanoff')
+    })
+
+    it('selects previous item by ArrowUp navigation', () => {
+      cy.get(triggerButton)
+        .click()
+        .trigger('keydown', { key: 'ArrowUp' })
+        .trigger('keyup', { key: 'ArrowUp' })
+        .trigger('keydown', { key: 'Enter' })
+        .trigger('keyup', { key: 'Enter' })
+        .should('have.text', 'Bruce Wayne')
+    })
+
+    it('selects last item by opening with ArrowUp', () => {
+      cy.get(triggerButton)
+        .focus()
+        .trigger('keydown', { key: 'ArrowUp' })
+        .trigger('keyup', { key: 'ArrowUp' })
+        .trigger('keydown', { key: 'Enter' })
+        .trigger('keyup', { key: 'Enter' })
+        .should('have.text', 'Selina Kyle')
+    })
+
+    it('selects last item by opening with ArrowDown', () => {
+      cy.get(triggerButton)
+        .focus()
+        .trigger('keydown', { key: 'ArrowDown' })
+        .trigger('keyup', { key: 'ArrowDown' })
+        .trigger('keydown', { key: 'Enter' })
+        .trigger('keyup', { key: 'Enter' })
+        .should('have.text', 'Bruce Wayne')
+    })
+
+    it('selects first item by first opening with ArrowDown', () => {
+      cy.visit(selectionComponentPath)
+        .get(triggerButton)
+        .focus()
+        .trigger('keydown', { key: 'ArrowDown' })
+        .trigger('keyup', { key: 'ArrowDown' })
+        .trigger('keydown', { key: 'Enter' })
+        .trigger('keyup', { key: 'Enter' })
+        .should('have.text', 'Bruce Wayne')
+    })
+
+    it('selects first item by first opening with ArrowUp', () => {
+      cy.visit(selectionComponentPath)
+        .get(triggerButton)
+        .focus()
+        .trigger('keydown', { key: 'ArrowUp' })
+        .trigger('keyup', { key: 'ArrowUp' })
+        .trigger('keydown', { key: 'Enter' })
+        .trigger('keyup', { key: 'Enter' })
+        .should('have.text', 'Selina Kyle')
     })
   })
 })
