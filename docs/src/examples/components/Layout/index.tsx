4c85b2f2658a6bd0301ccd34f20a1419b3182c09
Alexandru Buliga <bugaaa92@gmail.com>
2019-04-02 15:51:06 +0200

    feat(docs): links to layout documentation

--- docs/src/examples/components/Layout/index.tsx
+++ docs/src/examples/components/Layout/index.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react'
 import { Alert } from '@stardust-ui/react/'
 
+import { LayoutSuggestions } from 'docs/src/components/ComponentDoc/Suggestions'
 import Types from './Types'
 import Variations from './Variations'
 
 const LayoutExamples = () => (
   <div>
-    <Alert warning>This component is deprecated</Alert>
+    <Alert warning>
+      <LayoutSuggestions />
+    </Alert>
     <Types />
     <Variations />
   </div>
