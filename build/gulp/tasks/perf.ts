f5faa4e0c0803d54471b129ee5436ae5227a6524
Oleksandr Fediashov <alexander.mcgarret@gmail.com>
2019-04-04 11:26:23 +0200

    perf(Theme): performance measures for Context usage (#1106)
    * perf tooling
    
    * add perf tests
    
    * add dependency, silence in CI
    
    * remove examples
    
    * update react-hot-loader
    
    * update condition
    
    * add more types
    
    * dedupe react-is
    
    * update note for Provider
    
    * update comment
    
    * remove sortObjectKeys
    
    * dedupe react-test-renderer
    
    * => `componentFilePath`
    
    * rename container

--- build/gulp/tasks/perf.ts
+++ build/gulp/tasks/perf.ts
@@ -2,11 +2,17 @@ import * as express from 'express'
 import * as fs from 'fs'
 import { task, series } from 'gulp'
 import * as _ from 'lodash'
+import * as ProgressBar from 'progress'
 import * as puppeteer from 'puppeteer'
 import * as rimraf from 'rimraf'
 import { argv } from 'yargs'
 
-import { ProfilerMeasure, ProfilerMeasureCycle } from '../../../perf/types'
+import {
+  NormalizedMeasures,
+  ProfilerMeasure,
+  ProfilerMeasureCycle,
+  ReducedMeasures,
+} from '../../../perf/types'
 import config from '../../../config'
 import webpackPlugin from '../plugins/gulp-webpack'
 
@@ -27,7 +33,7 @@ const computeMeasureMedian = (measures: ProfilerMeasure[], key: string) => {
   return (values[lowMiddle][key] + values[highMiddle][key]) / 2
 }
 
-const reduceMeasures = (measures: ProfilerMeasure[], key: string) => {
+const reduceMeasures = (measures: ProfilerMeasure[], key: string): ReducedMeasures => {
   if (measures.length === 0) throw new Error('`measures` are empty')
 
   let min = measures[0][key]
@@ -53,7 +59,7 @@ const reduceMeasures = (measures: ProfilerMeasure[], key: string) => {
   }
 }
 
-const normalizeMeasures = (measures: ProfilerMeasureCycle[]) => {
+const normalizeMeasures = (measures: ProfilerMeasureCycle[]): NormalizedMeasures => {
   const perExampleMeasures = _.reduce(
     measures,
     (result, cycle: ProfilerMeasureCycle) => {
@@ -72,6 +78,56 @@ const normalizeMeasures = (measures: ProfilerMeasureCycle[]) => {
   }))
 }
 
+const getPercentDiff = (minValue: number, actualValue: number): number =>
+  _.round((actualValue / minValue) * 100 - 100, 2)
+
+const createMarkdownTable = (
+  normalizedMeasures: NormalizedMeasures,
+  metricName: string = 'actualTime',
+  fields: string[] = ['avg', 'median'],
+) => {
+  const exampleMeasures = _.mapValues(
+    normalizedMeasures,
+    exampleMeasure => exampleMeasure[metricName],
+  )
+
+  const fieldLabels: string[] = _.flatMap(fields, field => [
+    _.capitalize(field),
+    `${_.capitalize(field)} diff`,
+  ])
+  const minFieldValues: Record<string, number> = _.zipObject(
+    fields,
+    _.map(fields, fieldName => _.min(_.map(exampleMeasures, fieldName))),
+  )
+  const fieldValues = _.mapValues(exampleMeasures, exampleMeasure =>
+    _.reduce(
+      exampleMeasure,
+      (result, value, field) => {
+        if (_.includes(fields, field)) {
+          const minValue = minFieldValues[field]
+          const percentDiff =
+            minValue === value ? `**${value}**` : `+${getPercentDiff(minValue, value)}%`
+
+          result.push(value, percentDiff)
+        }
+
+        return result
+      },
+      [],
+    ),
+  )
+
+  return [
+    `| Example | ${fieldLabels.join(' | ')} |`,
+    `| --- | ${_.map(fieldLabels, () => ' --- ').join(' | ')} |`,
+    ..._.map(
+      exampleMeasures,
+      (exampleMeasure, exampleName) =>
+        `| ${exampleName} | ${fieldValues[exampleName].join(' | ')} |`,
+    ),
+  ].join('\n')
+}
+
 task('perf:clean', cb => {
   rimraf(paths.perfDist(), cb)
 })
@@ -85,11 +141,18 @@ task('perf:run', async () => {
   const times = (argv.times as string) || DEFAULT_RUN_TIMES
   const filter = argv.filter
 
+  const bar = process.env.CI
+    ? { tick: _.noop }
+    : new ProgressBar(':bar :current/:total', { total: times })
+
   let browser
 
   try {
     browser = await puppeteer.launch({
-      args: ['--single-process'], // Workaround for newPage hang in CircleCI: https://github.com/GoogleChrome/puppeteer/issues/1409#issuecomment-453845568
+      args: [
+        // Workaround for newPage hang in CircleCI: https://github.com/GoogleChrome/puppeteer/issues/1409#issuecomment-453845568
+        process.env.CI && '--single-process',
+      ].filter(Boolean),
     })
 
     for (let i = 0; i < times; i++) {
@@ -98,6 +161,7 @@ task('perf:run', async () => {
 
       const measuresFromStep = await page.evaluate(filter => window.runMeasures(filter), filter)
       measures.push(measuresFromStep)
+      bar.tick()
 
       await page.close()
     }
@@ -108,9 +172,12 @@ task('perf:run', async () => {
   }
 
   const resultsFile = paths.perfDist('result.json')
+  const normalizedMeasures = normalizeMeasures(measures)
 
-  fs.writeFileSync(resultsFile, JSON.stringify(normalizeMeasures(measures), null, 2))
+  fs.writeFileSync(resultsFile, JSON.stringify(normalizedMeasures, null, 2))
+
   log(colors.green('Results are written to "%s"'), resultsFile)
+  console.log(createMarkdownTable(normalizedMeasures))
 })
 
 task('perf:serve', cb => {
@@ -127,3 +194,4 @@ task('perf:serve:stop', cb => {
 })
 
 task('perf', series('perf:clean', 'perf:build', 'perf:serve', 'perf:run', 'perf:serve:stop'))
+task('perf:debug', series('perf:clean', 'perf:build', 'perf:serve'))
