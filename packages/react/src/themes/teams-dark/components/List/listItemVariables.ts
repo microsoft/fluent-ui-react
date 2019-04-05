e6182cc719ca61540cbc3af6ce2dd39c9b9edc72
manajdov <manajdov@microsoft.com>
2019-04-05 14:07:06 +0200

    -updated list and radio group

--- packages/react/src/themes/teams-dark/components/List/listItemVariables.ts
+++ packages/react/src/themes/teams-dark/components/List/listItemVariables.ts
@@ -1,6 +1,6 @@
 import { ListItemVariables } from 'src/themes/teams/components/List/listItemVariables'
 
 export default (siteVars: any): Partial<ListItemVariables> => ({
-  selectableFocusHoverBackgroundColor: siteVars.gray08,
-  selectedBackgroundColor: siteVars.gray08,
+  selectableFocusHoverBackgroundColor: siteVars.colors.grey.dark08,
+  selectedBackgroundColor: siteVars.colors.grey.dark08,
 })
--- packages/react/src/themes/teams-dark/components/RadioGroup/radioGroupItemVariables.ts
+++ packages/react/src/themes/teams-dark/components/RadioGroup/radioGroupItemVariables.ts
@@ -1,7 +1,7 @@
 import { RadioGroupItemVariables } from 'src/themes/teams/components/RadioGroup/radioGroupItemVariables'
 
 export default (siteVars: any): Partial<RadioGroupItemVariables> => ({
-  textColorDefault: siteVars.gray02,
+  textColorDefault: siteVars.colors.grey.dark02,
   textColorDefaultHoverFocus: siteVars.colors.white,
   textColorChecked: siteVars.colors.white,
 
