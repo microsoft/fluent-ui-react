1d8c578d5b3bf36676875f3ce698af0f7b591d22
Miroslav Stastny <mistastn@microsoft.com>
2019-04-10 13:48:33 +0200

    fix(Theme): Use global REM-base, do not set html font-size in static styles (#1190)
    * fix(Theme): Use global REM-base, do not set html font-size in static styles
    
    * Update changelog

--- packages/react/src/themes/types.ts
+++ packages/react/src/themes/types.ts
@@ -133,7 +133,6 @@ export interface SiteVariablesInput extends ObjectOf<any> {
   contextualColors?: ContextualColors
   emphasisColors?: EmphasisColors
   naturalColors?: NaturalColorsStrict
-  htmlFontSize?: string
 }
 
 export interface SiteVariablesPrepared extends SiteVariablesInput {
