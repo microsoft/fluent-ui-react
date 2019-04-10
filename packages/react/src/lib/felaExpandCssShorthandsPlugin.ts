5c106102a732e6e30ba00273a54b9237b2c67cf2
manajdov <manajdov@microsoft.com>
2019-04-10 15:24:41 +0200

    -added list of handled prop in the css shorthands plugin

--- packages/react/src/lib/felaExpandCssShorthandsPlugin.ts
+++ packages/react/src/lib/felaExpandCssShorthandsPlugin.ts
@@ -5,18 +5,37 @@ import * as _expand from 'css-shorthand-expand'
 // https://github.com/rollup/rollup/issues/1267#issuecomment-446681320
 const expand = (_expand as any).default || _expand
 
+const handledCssProps = [
+  'font',
+  'padding',
+  'margin',
+  'border',
+  'borderWidth',
+  'borderStyle',
+  'borderColor',
+  'borderTop',
+  'borderRight',
+  'borderBottom',
+  'borderLeft',
+  'borderRadius',
+  'background',
+  'outline',
+]
+
 export default () => {
   const expandCssShorthands = (styles: Object) => {
     return _.keys(styles).reduce((acc, cssPropertyName) => {
       const cssPropertyValue = styles[cssPropertyName]
 
-      if (typeof cssPropertyValue === 'object') {
-        return { ...acc, [cssPropertyName]: expandCssShorthands(cssPropertyValue) }
-      }
+      if (_.includes(handledCssProps, cssPropertyName)) {
+        if (typeof cssPropertyValue === 'object') {
+          return { ...acc, [cssPropertyName]: expandCssShorthands(cssPropertyValue) }
+        }
 
-      const expandedProps = expand(_.kebabCase(cssPropertyName), String(cssPropertyValue))
-      if (expandedProps) {
-        return { ...acc, ...convertKeysToCamelCase(expandedProps) }
+        const expandedProps = expand(_.kebabCase(cssPropertyName), String(cssPropertyValue))
+        if (expandedProps) {
+          return { ...acc, ...convertKeysToCamelCase(expandedProps) }
+        }
       }
 
       return { ...acc, [cssPropertyName]: cssPropertyValue }
