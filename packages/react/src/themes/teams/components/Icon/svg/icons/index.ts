c3c9be7fefb6b5b5ce569493413a06f229828aef
Andrei Barbu <a96andrei@gmail.com>
2019-04-11 14:17:06 +0200

    feat(Icon): export call-missed-line icon in Teams theme (#1203)
    * exported icon
    
    * exported outline version. modified changelog.md
    
    * modified changelog.md
    
    * fixed circular dependency

--- packages/react/src/themes/teams/components/Icon/svg/icons/index.ts
+++ packages/react/src/themes/teams/components/Icon/svg/icons/index.ts
@@ -19,6 +19,7 @@ import callRecording from './callRecording'
 import callVideo from './callVideo'
 import callVideoOff from './callVideoOff'
 import canvasAddPage from './canvasAddPage'
+import callMissedLine from './callMissedLine'
 import chat from './chat'
 import chevronDown from './chevronDown'
 import close from './close'
@@ -126,6 +127,7 @@ export default {
   'call-video-off': callVideoOff,
   'call-control-present-new': callControlPresentNew,
   'call-control-stop-presenting-new': callControlStopPresentingNew,
+  'call-missed-line': callMissedLine,
   'call-pstn': callPstn,
   'call-recording': callRecording,
   'canvas-add-page': canvasAddPage,
