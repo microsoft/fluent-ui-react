f335a4a6ee90f32734de7969bc0c998154c6dbfc
manajdov <manajdov@microsoft.com>
2019-04-11 15:05:30 +0200

    -added memoize

--- packages/react/src/lib/felaExpandCssShorthandsPlugin.ts
+++ packages/react/src/lib/felaExpandCssShorthandsPlugin.ts
@@ -1,38 +1,46 @@
 import * as _ from 'lodash'
 import * as _expand from 'css-shorthand-expand'
+import * as memoize from 'fast-memoize'
 
 // `css-shorthand-expand` is a CJS library, there are known issues with them:
 // https://github.com/rollup/rollup/issues/1267#issuecomment-446681320
 const expand = (_expand as any).default || _expand
+// @ts-ignore
+const expandMemoized = memoize(expand)
+// @ts-ignore
+const camelCaseMemoized = memoize(_.camelCase)
 
-const handledCssProps = [
-  'font',
-  'padding',
-  'margin',
-  'border',
-  'borderWidth',
-  'borderStyle',
-  'borderColor',
-  'borderTop',
-  'borderRight',
-  'borderBottom',
-  'borderLeft',
-  'borderRadius',
-  'background',
-  'outline',
-]
+const handledCssPropsMap = {
+  font: 'font',
+  padding: 'padding',
+  margin: 'margin',
+  border: 'border',
+  borderWidth: 'border-width',
+  borderStyle: 'border-style',
+  borderColor: 'border-color',
+  borderTop: 'border-top',
+  borderRight: 'border-right',
+  borderBottom: 'border-bottom',
+  borderLeft: 'border-left',
+  borderRadius: 'border-radius',
+  background: 'background',
+  outline: 'outline',
+}
 
 export default () => {
   const expandCssShorthands = (styles: Object) => {
-    return _.keys(styles).reduce((acc, cssPropertyName) => {
+    return Object.keys(styles).reduce((acc, cssPropertyName) => {
       const cssPropertyValue = styles[cssPropertyName]
 
       if (typeof cssPropertyValue === 'object') {
         return { ...acc, [cssPropertyName]: expandCssShorthands(cssPropertyValue) }
       }
 
-      if (_.includes(handledCssProps, cssPropertyName)) {
-        const expandedProps = expand(_.kebabCase(cssPropertyName), `${cssPropertyValue}`)
+      if (handledCssPropsMap[cssPropertyName]) {
+        const expandedProps = expandMemoized(
+          handledCssPropsMap[cssPropertyName],
+          `${cssPropertyValue}`,
+        )
         if (expandedProps) {
           return { ...acc, ...convertKeysToCamelCase(expandedProps) }
         }
@@ -45,4 +53,4 @@ export default () => {
   return expandCssShorthands
 }
 
-const convertKeysToCamelCase = obj => _.mapKeys(obj, (value, key) => _.camelCase(key))
+const convertKeysToCamelCase = obj => _.mapKeys(obj, (value, key) => camelCaseMemoized(key))
