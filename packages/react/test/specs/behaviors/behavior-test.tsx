4d152e03994839494fa86d4133d690c68f866cb4
PRIYANKAR GUPTA <prigupta@microsoft.com>
2019-04-12 22:47:27 +0530

    Adding up & down key accessibility feature for Tree component

--- packages/react/test/specs/behaviors/behavior-test.tsx
+++ packages/react/test/specs/behaviors/behavior-test.tsx
@@ -30,6 +30,7 @@ import {
   toggleButtonBehavior,
   toolbarBehavior,
   toolbarButtonBehavior,
+  treeBehavior,
   treeTitleBehavior,
   gridBehavior,
   statusBehavior,
@@ -68,6 +69,7 @@ testHelper.addBehavior('toolbarBehavior', toolbarBehavior)
 testHelper.addBehavior('toggleButtonBehavior', toggleButtonBehavior)
 testHelper.addBehavior('toolbarButtonBehavior', toolbarButtonBehavior)
 testHelper.addBehavior('treeTitleBehavior', treeTitleBehavior)
+testHelper.addBehavior('treeBehavior', treeBehavior)
 testHelper.addBehavior('gridBehavior', gridBehavior)
 testHelper.addBehavior('dialogBehavior', dialogBehavior)
 testHelper.addBehavior('statusBehavior', statusBehavior)
