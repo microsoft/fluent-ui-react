4859379c961bef0419cda7e18dee34cfe1ad3e8b
Oleksandr Fediashov <olfedias@microsoft.com>
2019-04-15 17:01:54 +0300

    move styles to Teams theme

--- packages/react/src/themes/base/components/Provider/providerBoxStyles.ts
+++ packages/react/src/themes/base/components/Provider/providerBoxStyles.ts
@@ -1,35 +1,9 @@
-import { ComponentStyleFunctionParam, ICSSInJSStyle } from '../../../types'
-import { ProviderBoxVariables } from './providerBoxVariables'
+import { ICSSInJSStyle } from '../../../types'
 
 export default {
-  root: ({
-    variables: v,
-  }: ComponentStyleFunctionParam<never, ProviderBoxVariables>): ICSSInJSStyle => ({
+  root: ({ variables: v }): ICSSInJSStyle => ({
     background: v.background,
     color: v.color,
     textAlign: 'left',
-
-    '& ::-webkit-scrollbar': {
-      height: v.scrollbarHeight,
-      width: v.scrollbarWidth,
-
-      ':disabled': {
-        display: 'none',
-      },
-    },
-    '& ::-webkit-scrollbar-thumb': {
-      borderRadius: v.scrollbarThumbBorderRadius,
-      border: `solid ${v.scrollbarThumbBorderSize} transparent`,
-      backgroundClip: 'content-box',
-      backgroundColor: v.scrollbarThumbBackgroundColor,
-
-      ':hover': {
-        backgroundColor: v.scrollbarThumbHoverBackgroundColor,
-        border: `solid ${v.scrollbarThumbHoverBorderSize} transparent`,
-      },
-    },
-    '& ::-webkit-scrollbar-track': {
-      background: 'transparent',
-    },
   }),
 }
--- packages/react/src/themes/base/components/Provider/providerBoxVariables.ts
+++ packages/react/src/themes/base/components/Provider/providerBoxVariables.ts
@@ -1,31 +1,9 @@
-import { pxToRem } from '../../../../lib'
-
 export interface ProviderBoxVariables {
   background: string
   color: string
-
-  scrollbarHeight: string
-  scrollbarWidth: string
-
-  scrollbarThumbBackgroundColor: string
-  scrollbarThumbBorderRadius: string
-  scrollbarThumbBorderSize: string
-
-  scrollbarThumbHoverBackgroundColor: string
-  scrollbarThumbHoverBorderSize: string
 }
 
 export default (siteVariables): ProviderBoxVariables => ({
   background: siteVariables.bodyBackground,
   color: siteVariables.bodyColor,
-
-  scrollbarHeight: pxToRem(16),
-  scrollbarWidth: pxToRem(16),
-
-  scrollbarThumbBackgroundColor: siteVariables.gray06,
-  scrollbarThumbBorderRadius: pxToRem(9),
-  scrollbarThumbBorderSize: pxToRem(4),
-
-  scrollbarThumbHoverBackgroundColor: siteVariables.gray04,
-  scrollbarThumbHoverBorderSize: pxToRem(2),
 })
