ebfb357fda982d22bae7e7cf1c20835e81c4f877
Oleksandr Fediashov <olfedias@microsoft.com>
2019-04-16 11:36:06 +0300

    perf(Dropdown): add tests

---
+++ docs/src/examples/components/Dropdown/Performance/Dropdown.perf.tsx
@@ -0,0 +1,13 @@
+import { Dropdown } from '@stardust-ui/react'
+import * as _ from 'lodash'
+import * as React from 'react'
+
+const inputItems = _.times(300, (i: number) => ({
+  content: `content: ${i}`,
+  key: i,
+  header: `header: ${i}`,
+}))
+
+const DropdownPerf = () => <Dropdown items={inputItems} placeholder="Select your hero" />
+
+export default DropdownPerf
