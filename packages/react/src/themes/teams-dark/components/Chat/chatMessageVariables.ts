7c614e7d341eb1d6c9d0c2ec8437094b12b8d094
Brigette Lundeen <brlundee@microsoft.com>
2019-04-17 18:59:11 -0700

    updated Teams dark theme at-mention and important colors

--- packages/react/src/themes/teams-dark/components/Chat/chatMessageVariables.ts
+++ packages/react/src/themes/teams-dark/components/Chat/chatMessageVariables.ts
@@ -6,5 +6,7 @@ export default (siteVars: any): Partial<ChatMessageVariables> => {
     backgroundColorMine: '#3B3C54',
     color: siteVars.colors.white,
     contentFocusOutlineColor: siteVars.brand,
+    hasMentionNubbinColor: siteVars.naturalColors.orange[900], // orange[300] when the new palette pr is checked in
+    isImportantColor: '#e73550', // red[300] when the new palette pr is checked in
   }
 }
--- packages/react/src/themes/teams/components/Chat/chatMessageVariables.ts
+++ packages/react/src/themes/teams/components/Chat/chatMessageVariables.ts
@@ -44,9 +44,9 @@ export default (siteVars): ChatMessageVariables => ({
   badgeShadow: siteVars.shadowLevel1Darker,
   isImportant: false,
   hasMention: false,
-  hasMentionColor: siteVars.naturalColors.orange[900],
+  hasMentionColor: siteVars.naturalColors.orange[900], // orange[300] when the new palette pr is checked in
   hasMentionNubbinColor: siteVars.naturalColors.darkOrange[400], // orange[400] when the new palette pr is checked in
-  isImportantColor: siteVars.colors.red[900],
+  isImportantColor: siteVars.colors.red[900], // red[400] when the new palette pr is checked in
   badgeTextColor: siteVars.colors.white,
   reactionGroupMarginLeft: pxToRem(12),
   timestampColorMine: siteVars.gray02,
