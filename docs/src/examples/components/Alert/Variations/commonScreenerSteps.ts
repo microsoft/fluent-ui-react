ec06a7ed62eafd93785adf313e2599d7cc01be57
Brigette Lundeen <brlundee@microsoft.com>
2019-04-23 14:05:43 -0700

    should focus be on Alert or the action button?

--- docs/src/examples/components/Alert/Variations/commonScreenerSteps.ts
+++ docs/src/examples/components/Alert/Variations/commonScreenerSteps.ts
@@ -1,10 +1,11 @@
 import { Alert } from '@stardust-ui/react'
 
+const alert = `.${Alert.className}`
 const alertActionButton = `.${Alert.slotClassNames.action}`
 
 const getScreenerSteps = (): ScreenerSteps => [
   builder => builder.hover(alertActionButton).snapshot('Hovers the action icon'),
-  builder => builder.focus(alertActionButton).snapshot('Focuses the action icon'),
+  builder => builder.focus(alert).snapshot('Focuses the action icon'),
 ]
 
 export default getScreenerSteps
