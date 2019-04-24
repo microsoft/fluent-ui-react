b614d75ab8823135572ed795e15727147675ed78
Oleksandr Fediashov <olfedias@microsoft.com>
2019-04-24 18:23:12 +0300

    chore(deployment): add dynamic deployment with now.sh

--- build/gulp/plugins/gulp-doctoc.ts
+++ build/gulp/plugins/gulp-doctoc.ts
@@ -1,10 +1,15 @@
+import * as fs from 'fs'
 import * as through2 from 'through2'
+
+import config from '../../../config'
 import sh from '../sh'
+
+const insideGitRepo = fs.existsSync(config.paths.base('.git'))
 
 export default () =>
   through2.obj((file, enc, done) => {
     sh(`doctoc ${file.path} --github --maxlevel 4`)
-      .then(() => sh(`git add ${file.path}`))
+      .then(() => insideGitRepo && sh(`git add ${file.path}`))
       .then(() => {
         done(null, file)
       })
