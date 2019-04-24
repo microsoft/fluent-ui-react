7f07e0b110cf10dc144b45726493a98efbe81473
Brigette Lundeen <brlundee@microsoft.com>
2019-04-24 08:04:31 -0700

    testing focus on alert action button

--- docs/src/examples/components/Alert/Variations/commonScreenerSteps.ts
+++ docs/src/examples/components/Alert/Variations/commonScreenerSteps.ts
@@ -1,15 +1,10 @@
 import { Alert } from '@stardust-ui/react'
 
-const alert = `.${Alert.className}`
 const alertActionButton = `.${Alert.slotClassNames.action}`
 
 const getScreenerSteps = (): ScreenerSteps => [
   builder => builder.hover(alertActionButton).snapshot('Hovers the action icon'),
-  (builder, keys) =>
-    builder
-      .click(alert)
-      .keys(alert, keys.tab)
-      .snapshot('Focuses the action icon'),
+  (builder, keys) => builder.keys('body', keys.tab).snapshot('Focuses the action icon'),
 ]
 
 export default getScreenerSteps
