fd46c360554641545b66ea9687d07d8ae23c892d
Brigette Lundeen <brlundee@microsoft.com>
2019-04-24 06:30:15 -0700

    testing screener tests

--- docs/src/examples/components/Alert/Variations/commonScreenerSteps.ts
+++ docs/src/examples/components/Alert/Variations/commonScreenerSteps.ts
@@ -1,10 +1,10 @@
 import { Alert } from '@stardust-ui/react'
 
 const alert = `.${Alert.className}`
-const alertActionButton = `.${Alert.slotClassNames.action}`
+// const alertActionButton = `.${Alert.slotClassNames.action}`
 
 const getScreenerSteps = (): ScreenerSteps => [
-  builder => builder.hover(alertActionButton).snapshot('Hovers the action icon'),
+  builder => builder.hover(alert).snapshot('Hovers the action icon'),
   builder => builder.focus(alert).snapshot('Focuses the action icon'),
 ]
 
