0d3034d645e6ded8004ce08056f64a79c9bca489
Oleksandr Fediashov <olfedias@microsoft.com>
2019-04-25 10:26:31 +0300

    use delay as knob

--- docs/src/examples/components/Loader/Usage/LoaderExampleDelay.tsx
+++ docs/src/examples/components/Loader/Usage/LoaderExampleDelay.tsx
@@ -1,10 +1,11 @@
 import { Loader } from '@stardust-ui/react'
 import * as React from 'react'
-import { useBooleanKnob } from '@stardust-ui/docs-components'
+import { useBooleanKnob, useRangeKnob } from '@stardust-ui/docs-components'
 
 const LoaderExampleDelay = () => {
   const [mounted] = useBooleanKnob({ name: 'mounted', initialValue: true })
+  const [delay] = useRangeKnob({ name: 'delay', initialValue: 500 })
 
-  return <div style={{ minHeight: 24 }}>{mounted && <Loader delay={500} />}</div>
+  return <div style={{ minHeight: 24 }}>{mounted && <Loader delay={delay as number} />}</div>
 }
 export default LoaderExampleDelay
