b28fef0d57b5377eed94d9f17b355e0fbf458cb6
Oleksandr Fediashov <olfedias@microsoft.com>
2019-05-03 11:08:05 +0200

    docs(pages): fix broken links in docs (#1283)
    * docs(pages): fix broken links in docs
    
    * fix link() usage
    
    * fix review comment and fix more links

--- docs/src/utils/helpers.tsx
+++ docs/src/utils/helpers.tsx
@@ -1,10 +1,20 @@
-import * as React from 'react'
 import { Icon } from '@stardust-ui/react'
+import * as React from 'react'
+import { Link } from 'react-router-dom'
 
 export const code = value => <code>{value}</code>
 
-export const link = (content, href, isExternal = false) => (
-  <a href={href} {...isExternal && { target: 'blank' }}>
-    {content} {isExternal ? <Icon name="external" size="small" /> : ''}
-  </a>
-)
+export const link = (content: string, href: string) => {
+  const isExternal = href.startsWith('http://') || href.startsWith('https://')
+  const isAnchor = !isExternal && href.startsWith('#')
+
+  if (isAnchor || isExternal) {
+    return (
+      <a className={isAnchor && 'anchor-link'} href={href} {...isExternal && { target: 'blank' }}>
+        {content} {isExternal ? <Icon name="external" size="small" /> : ''}
+      </a>
+    )
+  }
+
+  return <Link to={href}>{content}</Link>
+}
