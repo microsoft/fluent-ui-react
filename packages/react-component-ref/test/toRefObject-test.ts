7eac36418e2fb868d709f04086556a9d9dee600e
Oleksandr Fediashov <alexander.mcgarret@gmail.com>
2019-05-07 11:10:23 +0200

    feat(Ref): extract to a separate package, add utils (#1281)
    * feat(Ref): extract to a separate package, add utils
    
    * add mergeRefs test
    
    * fix dependencies
    
    * fix package.json for react-component-ref
    
    * remove pkg/ from .gituignore
    
    * revert type change
    
    * revert propTypes change
    
    * remove @ts-ignore
    
    * add changelog entry
    
    * remove `mergeRefs`

---
+++ packages/react-component-ref/test/toRefObject-test.ts
@@ -0,0 +1,19 @@
+import { toRefObject } from '@stardust-ui/react-component-ref'
+
+describe('toRefObject', () => {
+  it('creates an ref object from an input', () => {
+    const node = document.createElement('div')
+    expect(toRefObject(node)).toHaveProperty('current', node)
+  })
+
+  it('handles "null" as input', () => {
+    expect(toRefObject(null)).toHaveProperty('current', null)
+  })
+
+  it('returned object is memoized', () => {
+    const node = document.createElement('div')
+    const refObject = toRefObject(node)
+
+    expect(toRefObject(node)).toBe(refObject)
+  })
+})
