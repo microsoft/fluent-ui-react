1ed90de0de81e33be6bb6e14ced0ee9deebce8d1
Brigette Lundeen <brlundee@microsoft.com>
2019-05-06 22:03:34 -0700

    adding border for high contrast theme

--- packages/react/src/themes/teams-high-contrast/componentStyles.ts
+++ packages/react/src/themes/teams-high-contrast/componentStyles.ts
@@ -1,3 +1,4 @@
 export { default as Attachment } from './components/Attachment/attachmentStyles'
 export { default as MenuItem } from './components/Menu/menuItemStyles'
 export { default as Alert } from './components/Alert/alertStyles'
+export { default as Dialog } from './components/Dialog/dialogStyles'
