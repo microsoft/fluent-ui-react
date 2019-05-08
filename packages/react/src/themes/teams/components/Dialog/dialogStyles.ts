faefe9a6cbbac289f75248880689a1baa271d1ec
Brigette Lundeen <brlundee@microsoft.com>
2019-05-07 17:14:43 -0700

    removing styles that are duplicated from base theme

--- packages/react/src/themes/teams/components/Dialog/dialogStyles.ts
+++ packages/react/src/themes/teams/components/Dialog/dialogStyles.ts
@@ -6,42 +6,12 @@ type DialogStyleParams = ComponentStyleFunctionParam<DialogProps, DialogVariable
 
 export default {
   root: ({ props: p, variables: v }: DialogStyleParams): ICSSInJSStyle => ({
-    background: v.rootBackground,
-    borderRadius: v.rootBorderRadius,
     boxShadow: v.boxShadow,
     color: v.foregroundColor,
-    outline: 'none',
-    padding: v.rootPadding,
-    position: 'relative',
-    width: v.rootWidth,
-  }),
-
-  actions: (): ICSSInJSStyle => ({
-    textAlign: 'right',
-  }),
-
-  content: ({ variables: v }: DialogStyleParams): ICSSInJSStyle => ({
-    margin: v.contentMargin,
   }),
 
   header: ({ variables: v }: DialogStyleParams): ICSSInJSStyle => ({
     fontSize: v.headerFontSize,
     fontWeight: v.headerFontWeight,
-    margin: v.headerMargin,
-  }),
-
-  overlay: ({ props: p, variables: v }: DialogStyleParams): ICSSInJSStyle => ({
-    alignItems: 'center',
-    background: v.overlayBackground,
-    bottom: 0,
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    left: 0,
-    overflow: 'auto',
-    position: 'fixed',
-    right: 0,
-    top: 0,
-    zIndex: v.overlayZIndex,
   }),
 }
