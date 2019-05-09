34636bf1070ec869c739f35c5a210c84ebccc911
Oleksandr Fediashov <olfedias@microsoft.com>
2019-05-09 15:27:52 +0200

    update comment

--- packages/react/src/components/Popup/createPopperReferenceProxy.ts
+++ packages/react/src/components/Popup/createPopperReferenceProxy.ts
@@ -28,9 +28,11 @@ const referenceProxies = new WeakMap<React.RefObject<HTMLElement>, ReferenceProx
 /**
  * Popper.js does not support ref objects from `createRef()` as referenceElement. If we will pass
  * directly `ref`, `ref.current` will be `null` at the render process. We use memoize to keep the
- * same reference between renders.
+ * same reference between renders, it's necessary because created object will be passed to
+ * `react-popper` which creates PopperJS objects based on props difference.
  *
  * @see https://popper.js.org/popper-documentation.html#referenceObject
+ * @see https://github.com/FezVrasta/react-popper/blob/v1.3.3/src/Popper.js#L166
  */
 const createPopperReferenceProxy = (reference: HTMLElement | React.RefObject<HTMLElement>) => {
   const referenceRef = isRefObject(reference) ? reference : toRefObject(reference)
