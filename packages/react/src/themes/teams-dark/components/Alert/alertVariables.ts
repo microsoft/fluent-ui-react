cfd807b3de34295e65fc6a47eba237dea2c316d7
manajdov <manajdov@microsoft.com>
2019-05-09 13:00:54 +0200

    -removed comments

--- packages/react/src/themes/teams-dark/components/Alert/alertVariables.ts
+++ packages/react/src/themes/teams-dark/components/Alert/alertVariables.ts
@@ -10,9 +10,9 @@ export default (siteVars: any): Partial<AlertVariables> => {
     dangerBackgroundColor: siteVars.colors.red[800],
     dangerBorderColor: siteVars.colors.red[900],
 
-    oofColor: siteVars.colors.pink[200], // pink[200] when new color palette PR goes in
-    oofBackgroundColor: siteVars.colors.pink[800], // pink[800] when new color palette PR goes in
-    oofBorderColor: siteVars.colors.pink[900], // pink[900] when new color palette PR goes in
+    oofColor: siteVars.colors.pink[200],
+    oofBackgroundColor: siteVars.colors.pink[800],
+    oofBorderColor: siteVars.colors.pink[900],
 
     infoColor: siteVars.colors.grey[250],
     infoBackgroundColor: siteVars.colors.grey[550],
