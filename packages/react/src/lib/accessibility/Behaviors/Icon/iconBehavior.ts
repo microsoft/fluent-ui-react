3d5becec0d80ad9a4a451eba09d226292ecc8a03
Milan Turon <mituron@microsoft.com>
2019-05-13 11:40:57 +0200

    changing icon behavior as for some scopes icon could be visible, as icon inside the table

--- packages/react/src/lib/accessibility/Behaviors/Icon/iconBehavior.ts
+++ packages/react/src/lib/accessibility/Behaviors/Icon/iconBehavior.ts
@@ -2,16 +2,19 @@ import { Accessibility } from '../../types'
 
 /**
  * @description
- * Icon is usually only visual representation and therefore is hidden from screen readers.
+ * Icon is usually only visual representation and therefore is hidden from screen readers, unless 'alt' or 'aria-label' property is provided.
  *
  * @specification
- * Adds attribute 'aria-hidden=true' to icon.
+ * Adds role='img'.
+ * Adds attribute 'aria-hidden=true', if there is no 'alt' property provided.
+ * Adds attribute 'aria-hidden=true', if there is no 'aria-label' property provided.
  */
 
 const iconBehavior: Accessibility = (props: any) => ({
   attributes: {
     root: {
-      'aria-hidden': 'true',
+      role: 'img',
+      'aria-hidden': props['alt'] || props['aria-label'] ? undefined : 'true',
     },
   },
 })
