816ac3822b8d29df55128478d94da4494d128d94
Oleksandr Fediashov <olfedias@microsoft.com>
2019-05-16 12:38:08 +0200

    fixing regressions

--- packages/react/src/themes/base/components/Icon/iconNames.ts
+++ packages/react/src/themes/base/components/Icon/iconNames.ts
@@ -5,7 +5,7 @@ const fontIcon = (content: string): ThemeIconSpec => ({
 })
 
 export const icons: ThemeIcons = {
-  'stardust-close': fontIcon('2A09'),
+  'stardust-close': fontIcon('274C'),
   'stardust-arrow-down': fontIcon('25BE'),
   'stardust-arrow-end': fontIcon('25B8'),
   'stardust-arrow-up': fontIcon('25B4'),
