47e715c370e5711f341eb8b82c2dfe5b3a4d1d19
kuzhelov <kuroman@microsoft.com>
2019-05-21 12:57:02 +0200

    introduce changes

--- build/gulp/plugins/gulp-webpack.ts
+++ build/gulp/plugins/gulp-webpack.ts
@@ -1,6 +1,7 @@
 import * as webpack from 'webpack'
 import config from '../../../config'
 
+const { __DEV__ } = config.compiler_globals
 const { log, PluginError } = require('gulp-load-plugins')().util
 
 const webpackPlugin = (webpackConfig, cb, onComplete = (err: any, stats: any) => {}) => {
@@ -14,11 +15,11 @@ const webpackPlugin = (webpackConfig, cb, onComplete = (err: any, stats: any) =>
       log('Webpack compiler encountered a fatal error.')
       throw new PluginError('webpack', err.toString())
     }
-    if (errors.length > 0) {
+    if (!__DEV__ && errors.length > 0) {
       log('Webpack compiler encountered errors.')
       throw new PluginError('webpack', errors.toString())
     }
-    if (warnings.length > 0) {
+    if (!__DEV__ && warnings.length > 0) {
       throw new PluginError('webpack', warnings.toString())
     }
 
--- build/gulp/plugins/util/docgen.ts
+++ build/gulp/plugins/util/docgen.ts
@@ -89,6 +89,8 @@ const defaultOptions: ts.CompilerOptions = {
   jsx: ts.JsxEmit.React,
   module: ts.ModuleKind.CommonJS,
   target: ts.ScriptTarget.Latest,
+  allowUnusedLabels: true,
+  allowUnreachableCode: true,
 }
 
 const reactComponentSymbolNames = [
