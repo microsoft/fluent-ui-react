ba00c4791c03dcae3eb2234bf37f2fd53f85c6f4
silviuavram <silviu.avram91@gmail.com>
2019-05-23 15:06:13 +0200

    improved behaviors

--- packages/react/src/lib/accessibility/Behaviors/Alert/alertWarningBehavior.ts
+++ packages/react/src/lib/accessibility/Behaviors/Alert/alertWarningBehavior.ts
@@ -3,7 +3,7 @@ import { Accessibility } from '../../types'
 /**
  * @specification
  * Adds role 'alert' to 'content' component's part.
- * Adds Adds attribute 'aria-live=polite' to 'content' component's part.
+ * Adds attribute 'aria-live=polite' to 'content' component's part.
  */
 
 const alertWarningBehavior: Accessibility = (props: any) => ({
