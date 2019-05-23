a2f7b6a84ad537b606c73cd2fef85242ad1ad503
Alexandru Buliga <bugaaa92@gmail.com>
2019-05-23 17:46:49 +0200

    fix bad merge

--- packages/docs-components/src/knobs/useSelectKnob.ts
+++ packages/docs-components/src/knobs/useSelectKnob.ts
@@ -2,6 +2,10 @@ import { UseKnobOptions } from './types'
 import useKnob from './useKnob'
 
 const useSelectKnob = <T extends string>(options: UseKnobOptions<T>) =>
-  useKnob<T>({ initialValue: '' as T, type: 'select', ...options })
+  useKnob<T>({
+    initialValue: '' as T,
+    type: 'select',
+    ...options,
+  })
 
 export default useSelectKnob
