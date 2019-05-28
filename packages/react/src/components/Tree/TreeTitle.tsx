bb931de842613dab27b208e04a26fc543e8c60af
silviuavram <silviu.avram91@gmail.com>
2019-05-28 12:23:54 +0200

    implemented left-right arrow navigation

--- packages/react/src/components/Tree/TreeTitle.tsx
+++ packages/react/src/components/Tree/TreeTitle.tsx
@@ -1,3 +1,5 @@
+import * as customPropTypes from '@stardust-ui/react-proptypes'
+import { Ref, handleRef } from '@stardust-ui/react-component-ref'
 import * as _ from 'lodash'
 import * as PropTypes from 'prop-types'
 import * as React from 'react'
@@ -27,6 +29,18 @@ export interface TreeTitleProps
    */
   accessibility?: Accessibility
 
+  /** Ref to the clickable element that contains the title. */
+  contentRef?: React.Ref<HTMLElement>
+
+  /** Title must be able to focus first child title if it exists. */
+  focusFirstChild?: Function
+
+  /** Ref to the clickable element that will be used by parent title to focus this child. */
+  firstTitleChildRef?: React.Ref<HTMLElement>
+
+  /** Title must have a function to focus the parent title. */
+  focusParent?: Function
+
   /**
    * Called on click.
    *
@@ -51,6 +65,10 @@ class TreeTitle extends UIComponent<WithAsProp<TreeTitleProps>> {
 
   static propTypes = {
     ...commonPropTypes.createCommon(),
+    contentRef: customPropTypes.ref,
+    firstTitleChildRef: customPropTypes.ref,
+    focusFirstChild: PropTypes.func,
+    focusParent: PropTypes.func,
     onClick: PropTypes.func,
     open: PropTypes.bool,
     hasSubtree: PropTypes.bool,
@@ -66,16 +84,41 @@ class TreeTitle extends UIComponent<WithAsProp<TreeTitleProps>> {
       e.preventDefault()
       this.handleClick(e)
     },
+    expandOrFocusFirstChild: e => {
+      const { open } = this.props
+      e.preventDefault()
+      if (open) {
+        this.focusFirstChild()
+      } else {
+        this.handleClick(e)
+      }
+    },
+    collapseOrFocusParent: e => {
+      const { open } = this.props
+      e.preventDefault()
+      if (open) {
+        this.handleClick(e)
+      } else {
+        this.focusParent()
+      }
+    },
   }
 
-  handleClick = e => {
+  private handleClick = e => {
     _.invoke(this.props, 'onClick', e, this.props)
   }
 
-  renderComponent({ ElementType, classes, accessibility, unhandledProps, styles, variables }) {
-    const { children, content } = this.props
+  private focusFirstChild = () => {
+    _.invoke(this.props, 'focusFirstChild')
+  }
 
-    return (
+  private focusParent = () => {
+    _.invoke(this.props, 'focusParent')
+  }
+
+  renderComponent({ ElementType, classes, accessibility, unhandledProps, styles, variables }) {
+    const { children, content, contentRef, firstTitleChildRef } = this.props
+    const component = (
       <ElementType
         className={classes.root}
         onClick={this.handleClick}
@@ -86,6 +129,18 @@ class TreeTitle extends UIComponent<WithAsProp<TreeTitleProps>> {
       >
         {childrenExist(children) ? children : content}
       </ElementType>
+    )
+    return contentRef ? (
+      <Ref
+        innerRef={(titleElement: HTMLElement) => {
+          handleRef(contentRef, titleElement)
+          handleRef(firstTitleChildRef, titleElement)
+        }}
+      >
+        {component}
+      </Ref>
+    ) : (
+      component
     )
   }
 }
