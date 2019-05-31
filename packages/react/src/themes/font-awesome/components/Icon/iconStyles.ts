e1fabb2c16fe0f3c9e2129c5b350fc2c02b5906c
Oleksandr Fediashov <olfedias@microsoft.com>
2019-05-31 18:28:29 +0200

    declare condition

--- packages/react/src/themes/font-awesome/components/Icon/iconStyles.ts
+++ packages/react/src/themes/font-awesome/components/Icon/iconStyles.ts
@@ -1,11 +1,19 @@
-import { ComponentSlotStylesInput, ICSSInJSStyle } from '../../../types'
+import { ComponentSlotStylesInput, ICSSInJSStyle, ThemeIconSpec } from '../../../types'
 import { IconProps } from '../../../../components/Icon/Icon'
 import { IconVariables } from '../../../teams/components/Icon/iconVariables'
+import { emptyIcon } from '../../../base/components/Icon/iconNames'
 
 const iconStyles: ComponentSlotStylesInput<IconProps, IconVariables> = {
-  root: (): ICSSInJSStyle => ({
-    fontWeight: 900, // required for the fontAwesome to render
-  }),
+  root: ({ props: p, theme: t }): ICSSInJSStyle => {
+    const iconSpec: ThemeIconSpec = t.icons[p.name] || emptyIcon
+    const isFontIcon = !iconSpec.isSvg
+
+    return (
+      isFontIcon && {
+        fontWeight: 900, // required for the fontAwesome to render
+      }
+    )
+  },
 }
 
 export default iconStyles
