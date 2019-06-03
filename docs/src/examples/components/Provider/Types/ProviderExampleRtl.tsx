864e38d60dba3b4afa06737f4058d931e6e685b0
manajdov <manajdov@microsoft.com>
2019-06-03 16:16:04 +0200

    -added Provider example for rtl and disableAnimations

---
+++ docs/src/examples/components/Provider/Types/ProviderExampleRtl.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { Provider } from '@stardust-ui/react'
+
+const ProviderExampleRtl = () => (
+  <Provider rtl>
+    <div>{'مثال النص'}</div>
+  </Provider>
+)
+
+export default ProviderExampleRtl
--- docs/src/examples/components/Provider/Types/index.tsx
+++ docs/src/examples/components/Provider/Types/index.tsx
@@ -12,6 +12,16 @@ const Types = () => (
         description="A Provider defines the theme for your components."
         examplePath="components/Provider/Types/ProviderExample"
       />
+      <ComponentExample
+        title="Rtl"
+        description="A Provider can specify that the content inside it should be in rtl mode."
+        examplePath="components/Provider/Types/ProviderExampleRtl"
+      />
+      <ComponentExample
+        title="Disable animations"
+        description="A Provider can specify that the animations inside it's content should be disabled."
+        examplePath="components/Provider/Types/ProviderExampleDisableAnimations"
+      />
     </ExampleSection>
     <NonPublicSection title="Types for visual tests">
       <ComponentExample examplePath="components/Provider/Types/ProviderExampleScrollbar" />
