2905fd09b72acafdd2518045521be88781eb7069
manajdov <manajdov@microsoft.com>
2019-06-06 12:33:35 +0200

    -fixed steps

--- docs/src/examples/components/Tooltip/Types/TooltipExample.shorthand.steps.ts
+++ docs/src/examples/components/Tooltip/Types/TooltipExample.shorthand.steps.ts
@@ -4,7 +4,7 @@ const config: ScreenerTestsConfig = {
   themes: ['teams', 'teamsDark', 'teamsHighContrast'],
   steps: [
     builder => builder.hover(`.${Button.className}`).snapshot('Shows tooltip'),
-    builder => builder.hover('#body'),
+    builder => builder.hover('.ui-provider__box').snapshot('Shows tooltip'),
   ],
 }
 
