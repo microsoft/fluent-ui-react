c5a08b958fdbde678a2d3981cac3f0edf2af71e5
manajdov <manajdov@microsoft.com>
2019-06-07 12:32:24 +0200

    -fixed steps in screener toolbar tests

--- docs/src/examples/components/Menu/Usages/MenuExampleToolbar.shorthand.steps.ts
+++ docs/src/examples/components/Menu/Usages/MenuExampleToolbar.shorthand.steps.ts
@@ -11,7 +11,7 @@ const config: ScreenerTestsConfig = {
   themes: ['teams', 'teamsDark', 'teamsHighContrast'],
   steps: [
     (builder, keys) =>
-      getScreenerSteps()[0](builder, keys)
+      getScreenerSteps({ startIdx: 2, endIdx: 4 })[0](builder, keys)
         .click(selectors.lastItem)
         .snapshot('Clicks on the last item and opens submenu')
 
--- docs/src/examples/components/Menu/commonScreenerSteps.ts
+++ docs/src/examples/components/Menu/commonScreenerSteps.ts
@@ -2,6 +2,8 @@ import { Menu } from '@stardust-ui/react'
 
 interface StepsOptions {
   vertical?: boolean
+  startIdx?: number
+  endIdx?: number
 }
 
 const selectors = {
@@ -9,16 +11,18 @@ const selectors = {
   item: (itemIndex: number) => `.${Menu.className} li:nth-child(${itemIndex}) a`,
 }
 
-const getScreenerSteps = ({ vertical }: StepsOptions = {}): ScreenerSteps => [
+const getScreenerSteps = (
+  { vertical, startIdx, endIdx }: StepsOptions = { startIdx: 2, endIdx: 3 },
+): ScreenerSteps => [
   (builder, keys) =>
     builder
-      .hover(selectors.item(2))
+      .hover(selectors.item(startIdx))
       .snapshot('Hovers 2nd item (hover state styles)')
-      .click(selectors.item(2))
+      .click(selectors.item(startIdx))
       .snapshot('Clicks on 2nd item (active state styles)')
-      .keys(selectors.item(2), vertical ? keys.downArrow : keys.rightArrow)
+      .keys(selectors.item(startIdx), vertical ? keys.downArrow : keys.rightArrow)
       .snapshot('Navigates to next item (focus state styles)')
-      .keys(selectors.item(3), vertical ? keys.upArrow : keys.leftArrow)
+      .keys(selectors.item(endIdx), vertical ? keys.upArrow : keys.leftArrow)
       .snapshot('Navigates to previous item (active and focus state styles)'),
 ]
 
