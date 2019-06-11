3097b28dc306331d1457aeb92791e72f55a7d8c5
manajdov <manajdov@microsoft.com>
2019-06-11 18:31:48 +0200

    -fixed pointing examples

--- docs/src/examples/components/Tooltip/Types/TooltipExamplePointing.shorthand.tsx
+++ docs/src/examples/components/Tooltip/Types/TooltipExamplePointing.shorthand.tsx
@@ -1,18 +1,8 @@
 import * as React from 'react'
 import { Button, Tooltip } from '@stardust-ui/react'
-import { useBooleanKnob } from '@stardust-ui/docs-components'
 
-const TooltipExamplePointing = () => {
-  const [pointing] = useBooleanKnob({ name: 'pointing-s', initialValue: true })
-
-  return (
-    <Tooltip
-      open
-      pointing={pointing}
-      trigger={<Button icon="expand" />}
-      content={`The tooltip is ${pointing ? '' : 'not'} pointing.`}
-    />
-  )
-}
+const TooltipExamplePointing = () => (
+  <Tooltip open pointing trigger={<Button icon="expand" />} content={`The tooltip is pointing.`} />
+)
 
 export default TooltipExamplePointing
--- docs/src/examples/components/Tooltip/Types/TooltipExamplePointing.tsx
+++ docs/src/examples/components/Tooltip/Types/TooltipExamplePointing.tsx
@@ -1,15 +1,10 @@
 import * as React from 'react'
 import { Button, Tooltip } from '@stardust-ui/react'
-import { useBooleanKnob } from '@stardust-ui/docs-components'
 
-const TooltipExamplePointing = () => {
-  const [pointing] = useBooleanKnob({ name: 'pointing-s', initialValue: true })
-
-  return (
-    <Tooltip open pointing={pointing} content={`The tooltip is ${pointing ? '' : 'not'} pointing.`}>
-      <Button icon="expand" />
-    </Tooltip>
-  )
-}
+const TooltipExamplePointing = () => (
+  <Tooltip open pointing content={`The tooltip is pointing.`}>
+    <Button icon="expand" />
+  </Tooltip>
+)
 
 export default TooltipExamplePointing
