b92e8e7bc6bcb95ec36cb21e28f52ca8230e2bf4
manajdov <manajdov@microsoft.com>
2019-06-11 14:03:59 +0200

    -improved controlled tooltip example

--- docs/src/examples/components/Tooltip/States/TooltipOpenControlledExample.shorthand.tsx
+++ docs/src/examples/components/Tooltip/States/TooltipOpenControlledExample.shorthand.tsx
@@ -3,11 +3,12 @@ import { Button, Tooltip } from '@stardust-ui/react'
 import { useBooleanKnob } from '@stardust-ui/docs-components'
 
 const TooltipControlledExample = () => {
-  const [open] = useBooleanKnob({ name: 'open-c', initialValue: true })
+  const [open, setOpen] = useBooleanKnob({ name: 'open-c', initialValue: true })
 
   return (
     <Tooltip
       open={open}
+      onOpenChange={(e, data) => setOpen(data.open)}
       trigger={<Button icon="expand" />}
       content={{ content: 'This is a controlled Tooltip' }}
     />
--- docs/src/examples/components/Tooltip/States/TooltipOpenControlledExample.tsx
+++ docs/src/examples/components/Tooltip/States/TooltipOpenControlledExample.tsx
@@ -3,11 +3,12 @@ import { Button, Input, Tooltip } from '@stardust-ui/react'
 import { useBooleanKnob } from '@stardust-ui/docs-components'
 
 const TooltipOpenControlledExample = () => {
-  const [open] = useBooleanKnob({ name: 'open-c', initialValue: true })
+  const [open, setOpen] = useBooleanKnob({ name: 'open-c', initialValue: true })
 
   return (
     <Tooltip
       open={open || undefined}
+      onOpenChange={(e, data) => setOpen(data.open)}
       content={{ content: <Input icon="search" placeholder="Search..." /> }}
     >
       <Button icon="expand" />
