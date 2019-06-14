62bf8edfd074450882a435fab2aaa826d1a8bd09
Natasha Mayur Shah <natshah@microsoft.com>
2019-06-14 10:35:46 -0700

    Add svgs to processed icons and remove exportedAs from icons

--- packages/react/src/themes/teams/components/Icon/svg/ProcessedIcons/icons-poll.tsx
+++ packages/react/src/themes/teams/components/Icon/svg/ProcessedIcons/icons-poll.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import cx from 'classnames'
-import { TeamsSvgIconSpec } from '../types'
+import { TeamsProcessedSvgIconSpec } from '../types'
 import { teamsIconClassNames } from '../teamsIconClassNames'
 
 export default {
@@ -19,4 +19,5 @@ export default {
     </svg>
   ),
   styles: {},
-} as TeamsSvgIconSpec
+  exportedAs: 'poll',
+} as TeamsProcessedSvgIconSpec
