8164273fd4554af2994e4b288015213b51d2c9e6
Brian Calvery <bcalvery@microsoft.com>
2019-06-21 13:56:37 -0700

    fixing changelog entry. fixing sitVar change.

--- packages/react/src/themes/teams/siteVariables.ts
+++ packages/react/src/themes/teams/siteVariables.ts
@@ -17,7 +17,7 @@ export {
 // BORDER STYLES
 //
 export const borderWidth = '1px'
-export const borderRadius = '3px'
+export const borderRadius = '2px'
 export const focusInnerBorderColor = colors.white
 export const focusOuterBorderColor = colors.black
 
