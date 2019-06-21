3ebb79bd1effd0e28aa3d199b4cca59d223d9362
Andrew Martin <andmarti@microsoft.com>
2019-06-21 14:36:41 -0700

    updating icon

--- docs/src/examples/components/Label/Content/LabelExampleIconPosition.shorthand.tsx
+++ docs/src/examples/components/Label/Content/LabelExampleIconPosition.shorthand.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { Label } from '@stardust-ui/react'
 
 const LabelExampleIconPositionShorthand = () => (
-  <Label content="Have a coffee!" icon="coffee" iconPosition="start" />
+  <Label content="Delete this" icon="trash-can" iconPosition="start" />
 )
 
 export default LabelExampleIconPositionShorthand
