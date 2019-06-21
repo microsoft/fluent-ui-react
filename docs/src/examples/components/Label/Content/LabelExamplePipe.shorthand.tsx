bfc585ec4e9f41374ac86e910bca90edae68585b
Andrew Martin <andmarti@microsoft.com>
2019-06-21 13:30:01 -0700

    adding example

---
+++ docs/src/examples/components/Label/Content/LabelExamplePipe.shorthand.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import { Label } from '@stardust-ui/react'
+
+const LabelExampleImageShorthand = () => (
+  <Label content="Account Team" image="public/images/avatar/small/zoe.jpg" imagePosition="end" />
+)
+
+export default LabelExampleImageShorthand
