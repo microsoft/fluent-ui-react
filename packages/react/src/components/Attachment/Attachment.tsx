marst89/react

fix(factories): handle ReactElement as primitive

packages/react/src/components/Attachment/Attachment.tsx
@@ -80,7 +80,7 @@ class Attachment extends UIComponent<WithAsProp<AttachmentProps>, AttachmentStat
     actionable: PropTypes.bool,
     description: customPropTypes.itemShorthand,
     header: customPropTypes.itemShorthand,
-    icon: customPropTypes.itemShorthand,
+    icon: customPropTypes.primitiveShorthand,
     progress: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   }
 

packages/react/src/components/Button/Button.tsx
@@ -94,7 +94,7 @@ class Button extends UIComponent<WithAsProp<ButtonProps>, ButtonState> {
     circular: PropTypes.bool,
     disabled: PropTypes.bool,
     fluid: PropTypes.bool,
-    icon: customPropTypes.itemShorthand,
+    icon: customPropTypes.primitiveShorthand,
     iconOnly: PropTypes.bool,
     iconPosition: PropTypes.oneOf(['before', 'after']),
     onClick: PropTypes.func,

packages/react/src/components/Dropdown/DropdownItem.tsx
@@ -66,7 +66,7 @@ class DropdownItem extends UIComponent<WithAsProp<DropdownItemProps>> {
     active: PropTypes.bool,
     content: customPropTypes.itemShorthand,
     header: customPropTypes.itemShorthand,
-    image: customPropTypes.itemShorthand,
+    image: customPropTypes.primitiveShorthand,
     onClick: PropTypes.func,
     isFromKeyboard: PropTypes.bool,
     selected: PropTypes.bool,

packages/react/src/components/Dropdown/DropdownSelectedItem.tsx
@@ -78,8 +78,8 @@ class DropdownSelectedItem extends UIComponent<WithAsProp<DropdownSelectedItemPr
     }),
     active: PropTypes.bool,
     header: customPropTypes.itemShorthand,
-    icon: customPropTypes.itemShorthand,
-    image: customPropTypes.itemShorthand,
+    icon: customPropTypes.primitiveShorthand,
+    image: customPropTypes.primitiveShorthand,
     onClick: PropTypes.func,
     onKeyDown: PropTypes.func,
     onRemove: PropTypes.func,
@@ -103,7 +103,8 @@ class DropdownSelectedItem extends UIComponent<WithAsProp<DropdownSelectedItemPr
     _.invoke(this.props, 'onKeyDown', e, this.props)
   }
 
-  handleIconOverrides = (predefinedProps: IconProps) => ({
+  handleIconOverrides = props => (predefinedProps: IconProps) => ({
+    ...props,
     onClick: (e: React.SyntheticEvent, iconProps: IconProps) => {
       e.stopPropagation()
       _.invoke(this.props, 'onRemove', e, this.props)
@@ -125,21 +126,6 @@ class DropdownSelectedItem extends UIComponent<WithAsProp<DropdownSelectedItemPr
   }: RenderResultConfig<DropdownSelectedItemProps>) {
     const { active, header, icon, image } = this.props
 
-    const iconElement = Icon.create(icon, {
-      defaultProps: {
-        'aria-label': `Remove ${header} from selection.`, // TODO: Extract this in a behaviour.
-        className: DropdownSelectedItem.slotClassNames.icon,
-        styles: styles.icon,
-      },
-      overrideProps: this.handleIconOverrides,
-    })
-    const imageElement = Image.create(image, {
-      defaultProps: {
-        avatar: true,
-        className: DropdownSelectedItem.slotClassNames.image,
-        styles: styles.image,
-      },
-    })
     const contentElement = Box.create(header, {
       defaultProps: {
         as: 'span',
@@ -147,6 +133,32 @@ class DropdownSelectedItem extends UIComponent<WithAsProp<DropdownSelectedItemPr
         styles: styles.header,
       },
     })
+    const renderIcon = _.isNil(icon)
+      ? icon
+      : render =>
+          render(image, (ComponentType, props) =>
+            Icon.create(icon, {
+              defaultProps: {
+                'aria-label': `Remove ${header} from selection.`, // TODO: Extract this in a behaviour.
+                className: DropdownSelectedItem.slotClassNames.icon,
+                styles: styles.icon,
+              },
+              overrideProps: this.handleIconOverrides(props),
+            }),
+          )
+    const renderImage = _.isNil(image)
+      ? image
+      : render =>
+          render(image, (ComponentType, props) =>
+            Image.create(image, {
+              defaultProps: {
+                avatar: true,
+                className: DropdownSelectedItem.slotClassNames.image,
+                styles: styles.image,
+              },
+              overrideProps: props,
+            }),
+          )
 
     return (
       <Ref innerRef={this.itemRef}>
@@ -158,8 +170,8 @@ class DropdownSelectedItem extends UIComponent<WithAsProp<DropdownSelectedItemPr
           onClick={this.handleClick}
           onKeyDown={this.handleKeyDown}
           content={contentElement}
-          icon={iconElement}
-          image={imageElement}
+          icon={renderIcon}
+          image={renderImage}
           {...unhandledProps}
         />
       </Ref>

packages/react/src/components/RadioGroup/RadioGroupItem.tsx
@@ -106,7 +106,7 @@ class RadioGroupItem extends AutoControlledComponent<
     checked: PropTypes.bool,
     defaultChecked: PropTypes.bool,
     disabled: PropTypes.bool,
-    icon: customPropTypes.itemShorthand,
+    icon: customPropTypes.primitiveShorthand,
     label: customPropTypes.itemShorthand,
     name: PropTypes.string,
     onBlur: PropTypes.func,

packages/react/src/components/Status/Status.tsx
@@ -47,7 +47,7 @@ class Status extends UIComponent<WithAsProp<StatusProps>, any> {
       content: false,
     }),
     color: PropTypes.string,
-    icon: customPropTypes.itemShorthand,
+    icon: customPropTypes.primitiveShorthand,
     size: customPropTypes.size,
     state: PropTypes.oneOf(['success', 'info', 'warning', 'error', 'unknown']),
   }

microsoft/fluent-ui-react

packages/react/src/components/Attachment/Attachment.tsx
@@ -80,7 +80,7 @@ class Attachment extends UIComponent<WithAsProp<AttachmentProps>, AttachmentStat
     actionable: PropTypes.bool,
     description: customPropTypes.itemShorthand,
     header: customPropTypes.itemShorthand,
-    icon: customPropTypes.itemShorthand,
+    icon: customPropTypes.itemShorthandWithoutJSX,
     progress: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   }
 

packages/react/src/components/Button/Button.tsx
@@ -94,7 +94,7 @@ class Button extends UIComponent<WithAsProp<ButtonProps>, ButtonState> {
     circular: PropTypes.bool,
     disabled: PropTypes.bool,
     fluid: PropTypes.bool,
-    icon: customPropTypes.itemShorthand,
+    icon: customPropTypes.itemShorthandWithoutJSX,
     iconOnly: PropTypes.bool,
     iconPosition: PropTypes.oneOf(['before', 'after']),
     onClick: PropTypes.func,

packages/react/src/components/Dropdown/DropdownItem.tsx
@@ -66,7 +66,7 @@ class DropdownItem extends UIComponent<WithAsProp<DropdownItemProps>> {
     active: PropTypes.bool,
     content: customPropTypes.itemShorthand,
     header: customPropTypes.itemShorthand,
-    image: customPropTypes.itemShorthand,
+    image: customPropTypes.itemShorthandWithoutJSX,
     onClick: PropTypes.func,
     isFromKeyboard: PropTypes.bool,
     selected: PropTypes.bool,

packages/react/src/components/Dropdown/DropdownSelectedItem.tsx
@@ -78,8 +78,8 @@ class DropdownSelectedItem extends UIComponent<WithAsProp<DropdownSelectedItemPr
     }),
     active: PropTypes.bool,
     header: customPropTypes.itemShorthand,
-    icon: customPropTypes.itemShorthand,
-    image: customPropTypes.itemShorthand,
+    icon: customPropTypes.itemShorthandWithoutJSX,
+    image: customPropTypes.itemShorthandWithoutJSX,
     onClick: PropTypes.func,
     onKeyDown: PropTypes.func,
     onRemove: PropTypes.func,
@@ -103,7 +103,8 @@ class DropdownSelectedItem extends UIComponent<WithAsProp<DropdownSelectedItemPr
     _.invoke(this.props, 'onKeyDown', e, this.props)
   }
 
-  handleIconOverrides = (predefinedProps: IconProps) => ({
+  handleIconOverrides = props => (predefinedProps: IconProps) => ({
+    ...props,
     onClick: (e: React.SyntheticEvent, iconProps: IconProps) => {
       e.stopPropagation()
       _.invoke(this.props, 'onRemove', e, this.props)
@@ -125,21 +126,6 @@ class DropdownSelectedItem extends UIComponent<WithAsProp<DropdownSelectedItemPr
   }: RenderResultConfig<DropdownSelectedItemProps>) {
     const { active, header, icon, image } = this.props
 
-    const iconElement = Icon.create(icon, {
-      defaultProps: {
-        'aria-label': `Remove ${header} from selection.`, // TODO: Extract this in a behaviour.
-        className: DropdownSelectedItem.slotClassNames.icon,
-        styles: styles.icon,
-      },
-      overrideProps: this.handleIconOverrides,
-    })
-    const imageElement = Image.create(image, {
-      defaultProps: {
-        avatar: true,
-        className: DropdownSelectedItem.slotClassNames.image,
-        styles: styles.image,
-      },
-    })
     const contentElement = Box.create(header, {
       defaultProps: {
         as: 'span',
@@ -147,6 +133,32 @@ class DropdownSelectedItem extends UIComponent<WithAsProp<DropdownSelectedItemPr
         styles: styles.header,
       },
     })
+    const renderIcon = _.isNil(icon)
+      ? icon
+      : render =>
+          render(image, (ComponentType, props) =>
+            Icon.create(icon, {
+              defaultProps: {
+                'aria-label': `Remove ${header} from selection.`, // TODO: Extract this in a behaviour.
+                className: DropdownSelectedItem.slotClassNames.icon,
+                styles: styles.icon,
+              },
+              overrideProps: this.handleIconOverrides(props),
+            }),
+          )
+    const renderImage = _.isNil(image)
+      ? image
+      : render =>
+          render(image, (ComponentType, props) =>
+            Image.create(image, {
+              defaultProps: {
+                avatar: true,
+                className: DropdownSelectedItem.slotClassNames.image,
+                styles: styles.image,
+              },
+              overrideProps: props,
+            }),
+          )
 
     return (
       <Ref innerRef={this.itemRef}>
@@ -158,8 +170,8 @@ class DropdownSelectedItem extends UIComponent<WithAsProp<DropdownSelectedItemPr
           onClick={this.handleClick}
           onKeyDown={this.handleKeyDown}
           content={contentElement}
-          icon={iconElement}
-          image={imageElement}
+          icon={renderIcon}
+          image={renderImage}
           {...unhandledProps}
         />
       </Ref>

packages/react/src/components/RadioGroup/RadioGroupItem.tsx
@@ -106,7 +106,7 @@ class RadioGroupItem extends AutoControlledComponent<
     checked: PropTypes.bool,
     defaultChecked: PropTypes.bool,
     disabled: PropTypes.bool,
-    icon: customPropTypes.itemShorthand,
+    icon: customPropTypes.itemShorthandWithoutJSX,
     label: customPropTypes.itemShorthand,
     name: PropTypes.string,
     onBlur: PropTypes.func,

packages/react/src/components/Status/Status.tsx
@@ -47,7 +47,7 @@ class Status extends UIComponent<WithAsProp<StatusProps>, any> {
       content: false,
     }),
     color: PropTypes.string,
-    icon: customPropTypes.itemShorthand,
+    icon: customPropTypes.itemShorthandWithoutJSX,
     size: customPropTypes.size,
     state: PropTypes.oneOf(['success', 'info', 'warning', 'error', 'unknown']),
   }