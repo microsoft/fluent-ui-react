356b2c35e919cc091ae476ab4a32d7af5ac3f975
Alina Medintu <almedint@microsoft.com>
2019-06-26 15:34:22 +0200

    updating popup best practices

--- docs/src/examples/components/Popup/BestPractices/PopupBestPractices.tsx
+++ docs/src/examples/components/Popup/BestPractices/PopupBestPractices.tsx
@@ -7,6 +7,11 @@ const doList = [
     key: 'one',
     content: 'Use popupFocusTrapBehavior if the focus needs to be trapped inside of the Popup.',
   },
+  {
+    key: 'two',
+    content:
+      "If Popup's content is lazy loaded and focus needs to be trapped inside - make sure to use state change to trigger componentDidUpdate, so the focus can be set correctly to the first tabbable element inside Popup or manually set focus to the element inside once content is loaded.",
+  },
 ]
 
 const AlertBestPractices = () => {
