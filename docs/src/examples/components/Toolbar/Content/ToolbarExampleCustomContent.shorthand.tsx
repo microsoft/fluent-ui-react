af93c25dcd4fb804487dd2aa3533109f8bfeaeb8
Oleksandr Fediashov <olfedias@microsoft.com>
2019-07-01 10:39:04 +0200

    simplify example

--- docs/src/examples/components/Toolbar/Content/ToolbarExampleCustomContent.shorthand.tsx
+++ docs/src/examples/components/Toolbar/Content/ToolbarExampleCustomContent.shorthand.tsx
@@ -1,28 +1,23 @@
 import * as React from 'react'
 import { Button, Text, Toolbar } from '@stardust-ui/react'
 
-const ToolbarExampleCustomContentShorthand = () => {
-  return (
-    <Toolbar
-      items={[
-        {
-          key: 'bold',
-          icon: 'bold',
-        },
-        {
-          key: 'custom-text',
-          kind: 'custom',
-          content: <Text content="text" />,
-        },
-        {
-          key: 'custom-button',
-          kind: 'custom',
-          fitted: 'horizontally',
-          content: <Button content="button" />,
-        },
-      ]}
-    />
-  )
-}
+const ToolbarExampleCustomContentShorthand = () => (
+  <Toolbar
+    items={[
+      { key: 'bold', icon: 'bold' },
+      {
+        key: 'custom-text',
+        kind: 'custom',
+        content: <Text content="text" />,
+      },
+      {
+        key: 'custom-button',
+        kind: 'custom',
+        fitted: 'horizontally',
+        content: <Button content="button" />,
+      },
+    ]}
+  />
+)
 
 export default ToolbarExampleCustomContentShorthand
