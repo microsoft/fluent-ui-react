fd243c3d6e3d86e316d861d1d3d59149dd47d46f
manajdov <manajdov@microsoft.com>
2019-07-09 10:43:48 +0200

    -addressed PR comments

--- packages/react/src/themes/teams/components/Tooltip/tooltipContentStyles.ts
+++ packages/react/src/themes/teams/components/Tooltip/tooltipContentStyles.ts
@@ -2,7 +2,7 @@ import { ComponentSlotStylesInput, ICSSInJSStyle } from '../../../types'
 import { TooltipContentProps } from '../../../../components/Tooltip/TooltipContent'
 import { TooltipContentVariables } from './tooltipContentVariables'
 import getPointerStyles from '../../getPointerStyles'
-import svgContent from './tooltipPointerSvgUrl'
+import tooltipPointerSvg from './tooltipPointerSvgUrl'
 import { PopperChildrenProps } from '../../../../lib/positioner'
 
 const getPointerOffset = (
@@ -22,27 +22,43 @@ const tooltipContentStyles: ComponentSlotStylesInput<
   TooltipContentProps,
   TooltipContentVariables
 > = {
-  root: ({ props: p, variables: v, rtl }): ICSSInJSStyle => ({
-    borderRadius: v.borderRadius,
-    display: 'block',
-    maxWidth: v.maxWidth,
-    color: v.color,
-    background: v.backgroundColor,
-    ...(p.pointing &&
-      getPointerStyles(getPointerOffset(p.placement, v), v.pointerMargin, rtl, p.placement, true)
-        .root),
-  }),
-  pointer: ({ props: p, variables: v, rtl }): ICSSInJSStyle => ({
-    display: 'block',
-    position: 'absolute',
-    overflow: 'hidden',
-    width: v.pointerWidth,
-    height: v.pointerHeight,
-    backgroundImage: svgContent(v.backgroundColor),
+  root: ({ props: p, variables: v, rtl }): ICSSInJSStyle => {
+    const svgPointerStyles = getPointerStyles(
+      getPointerOffset(p.placement, v),
+      v.pointerMargin,
+      rtl,
+      p.placement,
+      true,
+    )
 
-    ...getPointerStyles(getPointerOffset(p.placement, v), v.pointerMargin, rtl, p.placement, true)
-      .pointer,
-  }),
+    return {
+      borderRadius: v.borderRadius,
+      display: 'block',
+      maxWidth: v.maxWidth,
+      color: v.color,
+      background: v.backgroundColor,
+      ...(p.pointing && svgPointerStyles.root),
+    }
+  },
+  pointer: ({ props: p, variables: v, rtl }): ICSSInJSStyle => {
+    const svgPointerStyles = getPointerStyles(
+      getPointerOffset(p.placement, v),
+      v.pointerMargin,
+      rtl,
+      p.placement,
+      true,
+    )
+
+    return {
+      display: 'block',
+      position: 'absolute',
+      overflow: 'hidden',
+      width: v.pointerWidth,
+      height: v.pointerHeight,
+      backgroundImage: tooltipPointerSvg(v.backgroundColor),
+      ...svgPointerStyles.pointer,
+    }
+  },
   content: ({ props: p, variables: v }): ICSSInJSStyle => ({
     display: 'block',
     padding: v.padding,
