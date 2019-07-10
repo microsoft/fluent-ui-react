24cc7b051e4be0eb8e797e7888898cf7352553e4
Alexandru Buliga <bugaaa92@gmail.com>
2019-07-10 17:44:13 +0200

    add screenshot tests

--- docs/src/examples/components/Slider/Types/SliderExample.shorthand.steps.ts
+++ docs/src/examples/components/Slider/Types/SliderExample.shorthand.steps.ts
@@ -1,3 +1,32 @@
-export const config: ScreenerTestsConfig = { themes: ['base', 'teams'] }
+import { Slider } from '@stardust-ui/react/src'
+
+const selectors = {
+  input: `.${Slider.slotClassNames.input}`,
+}
+
+const config: ScreenerTestsConfig = {
+  themes: ['base', 'teams', 'teamsDark', 'teamsHighContrast'],
+  steps: [
+    (builder, keys) => builder.keys(selectors.input, keys.tab).snapshot('Focuses the slider'),
+    (builder, keys) =>
+      builder
+        .keys(selectors.input, keys.tab)
+        .keys(selectors.input, keys.rightArrow)
+        .keys(selectors.input, keys.rightArrow)
+        .keys(selectors.input, keys.rightArrow)
+        .keys(selectors.input, keys.rightArrow)
+        .keys(selectors.input, keys.rightArrow)
+        .snapshot('Navigates to the right with the right arrow key'),
+    (builder, keys) =>
+      builder
+        .keys(selectors.input, keys.tab)
+        .keys(selectors.input, keys.downArrow)
+        .keys(selectors.input, keys.downArrow)
+        .keys(selectors.input, keys.downArrow)
+        .keys(selectors.input, keys.downArrow)
+        .keys(selectors.input, keys.downArrow)
+        .snapshot('Navigates to the right with the dow arrow key'),
+  ],
+}
 
 export default config
