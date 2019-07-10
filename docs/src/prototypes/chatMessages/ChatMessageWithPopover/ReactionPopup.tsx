3d6cb81c9f17f7ead6c646f30c22658da68a6ea0
manajdov <manajdov@microsoft.com>
2019-07-10 16:05:18 +0200

    -keyboard event fixes

--- docs/src/prototypes/chatMessages/ChatMessageWithPopover/ReactionPopup.tsx
+++ docs/src/prototypes/chatMessages/ChatMessageWithPopover/ReactionPopup.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import keyboardKey from 'keyboard-key'
-import { Popup, Menu, Reaction, ReactionProps } from '@stardust-ui/react'
+import { Popup, Menu, Reaction, ReactionProps, popupAutoFocusBehavior } from '@stardust-ui/react'
 
 const getAriaLabel = ({ content: numberOfPersons, icon: emojiType }: ReactionProps) => {
   if (numberOfPersons === 1) {
@@ -47,6 +47,7 @@ class ReactionPopup extends React.Component<ReactionProps, { open: boolean }> {
         on="hover"
         open={this.state.open}
         onOpenChange={this.handleOpenChange}
+        accessibility={popupAutoFocusBehavior}
       />
     )
   }
