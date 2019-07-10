b32748b616840109e6c497b056fba283abeacf67
Juraj Kapsiar <jukapsia@microsoft.com>
2019-07-10 09:30:20 +0200

    v2

--- docs/src/prototypes/treeActions/TreeActions.tsx
+++ docs/src/prototypes/treeActions/TreeActions.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react'
 import keyboardKey from 'keyboard-key'
-import { Tree, ItemLayout, Image, ContextMenu, TreeTitle, Toolbar } from '@stardust-ui/react'
+import {
+  Checkbox,
+  Tree,
+  ItemLayout,
+  Image,
+  ContextMenu,
+  TreeTitle,
+  Toolbar,
+} from '@stardust-ui/react'
 
 const itemMenu = { items: ['Add', 'Remove'] }
-const titleMenu = { items: ['All'] }
+// const titleMenu = { items: ['All'] }
 
 const images = [
   'ade.jpg',
@@ -15,24 +23,32 @@ const images = [
   'jenny.jpg',
 ]
 
-const title = name => ({
-  onKeyDown: handleItemKeyDown,
-  content: (
-    <ItemLayout
-      header={name}
-      headerMedia={<Toolbar items={['All']} onKeyDown={handleToolbarKeyDown} />}
-    />
-  ),
-})
+// const title = name => ({
+// onKeyDown: handleItemKeyDown,
+// content: (name
+// <ItemLayout
+//   header={name}
+//   headerMedia={<Toolbar aria-hidden={true} items={['All']} onKeyDown={handleToolbarKeyDown} />}
+// />
+// ),
+// })
 
-const item = (name, img) => ({
-  onKeyDown: handleItemKeyDown,
+const item = (name, img, hidden) => ({
+  ...(!hidden && { onKeyDown: handleItemKeyDown }),
   content: (
     <ItemLayout
       media={<Image src={`public/images/avatar/small/${images[img]}`} avatar />}
       header={name}
       headerMedia={
-        <Toolbar onKeyDown={handleToolbarKeyDown} items={[{ icon: 'plus' }, { icon: 'delete' }]} />
+        <Toolbar
+          role={hidden ? 'presentation' : 'toolbar'}
+          aria-hidden={hidden}
+          onKeyDown={handleToolbarKeyDown}
+          items={[
+            { icon: 'plus', title: 'Accept', as: hidden ? 'span' : 'button' },
+            { icon: 'delete', title: 'Decline', as: hidden ? 'span' : 'button' },
+          ]}
+        />
       }
     />
   ),
@@ -53,42 +69,48 @@ const handleToolbarKeyDown = e => {
   }
 }
 
-const itemWithMenu = (name, img) => render =>
-  render(item(name, img), (ComponentType, props) => (
-    <ContextMenu menu={itemMenu} trigger={<ComponentType {...props} />} />
-  ))
+const itemWithMenu = (name, img, hidden) => render =>
+  render(item(name, img, hidden), (ComponentType, props) =>
+    hidden ? (
+      <ContextMenu menu={itemMenu} trigger={<ComponentType {...props} />} />
+    ) : (
+      <ComponentType {...props} />
+    ),
+  )
 
-const titleWithMenu = name => render =>
-  render(title(name), (ComponentType, props) => (
-    <ContextMenu menu={titleMenu} trigger={<ComponentType {...props} />} />
-  ))
+// const titleWithMenu = name => render =>
+//   render(name, (ComponentType, props) => (
+//     <ContextMenu menu={titleMenu} trigger={<ComponentType {...props} />} />
+//   ))
 
-class TreeActions extends React.Component<{}, {}> {
+class TreeActions extends React.Component<{}, { hidden: boolean }> {
   // state = {
   //   selectedBannerName: bannerRadioItems[0].value as BannerName,
   //   open: true,
   // }
 
-  items = [
+  state = { hidden: false }
+
+  items = hidden => [
     {
       key: '1',
-      title: titleWithMenu('Participants'),
+      title: 'Participants', // titleWithMenu('Participants'),
       items: [
         {
           key: '11',
-          title: itemWithMenu('Tywin', 1),
+          title: itemWithMenu('Tywin', 1, hidden),
         },
         {
           key: '12',
-          title: itemWithMenu('Jaime', 2),
+          title: itemWithMenu('Jaime', 2, hidden),
         },
         {
           key: '13',
-          title: itemWithMenu('Cersei', 3),
+          title: itemWithMenu('Cersei', 3, hidden),
         },
         {
           key: '14',
-          title: itemWithMenu('Tyrion', 4),
+          title: itemWithMenu('Tyrion', 4, hidden),
         },
       ],
     },
@@ -98,22 +120,31 @@ class TreeActions extends React.Component<{}, {}> {
       items: [
         {
           key: '211',
-          title: itemWithMenu('Lancel', 5),
+          title: itemWithMenu('Lancel', 5, hidden),
         },
         {
           key: '212',
-          title: itemWithMenu('Willem', 6),
+          title: itemWithMenu('Willem', 6, hidden),
         },
         {
           key: '213',
-          title: itemWithMenu('Martyn', 0),
+          title: itemWithMenu('Martyn', 0, hidden),
         },
       ],
     },
   ]
 
   render() {
-    return <Tree styles={{ width: '300px' }} items={this.items} />
+    return (
+      <>
+        <Checkbox
+          label="Hide toolbar from screen reader (click to open context menu with actions)"
+          onChange={(_, props) => this.setState({ hidden: props.checked })}
+        />
+
+        <Tree styles={{ width: '300px' }} items={this.items(this.state && this.state.hidden)} />
+      </>
+    )
   }
 }
 
