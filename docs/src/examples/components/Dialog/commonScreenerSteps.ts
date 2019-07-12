78aa7c7e6657edeef9bda27bb65b06d513082737
manajdov <manajdov@microsoft.com>
2019-07-12 13:04:50 +0200

    -renamed screener description

--- docs/src/examples/components/Dialog/commonScreenerSteps.ts
+++ docs/src/examples/components/Dialog/commonScreenerSteps.ts
@@ -3,7 +3,7 @@ import { Button } from '@stardust-ui/react'
 const button = `.${Button.className}`
 
 const getScreenerSteps = (): ScreenerSteps => [
-  builder => builder.click(button).snapshot('Clicks the first button'),
+  builder => builder.click(button).snapshot('Clicks the trigger button'),
 ]
 
 export default getScreenerSteps
