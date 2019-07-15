45e1faff65d0beb55d7877ccfb689bedd83c5394
manajdov <manajdov@microsoft.com>
2019-07-15 16:07:00 +0200

    simplified toolbar example

--- docs/src/examples/components/Toolbar/Usage/ToolbarExampleWithTooltip.shorthand.tsx
+++ docs/src/examples/components/Toolbar/Usage/ToolbarExampleWithTooltip.shorthand.tsx
@@ -9,50 +9,8 @@ import {
 } from '@stardust-ui/react'
 import { useBooleanKnob } from '@stardust-ui/docs-components'
 
-const fields = [
-  {
-    label: 'First name',
-    name: 'firstName',
-    id: 'first-name-inline-shorthand',
-    key: 'first-name',
-    required: true,
-    inline: true,
-  },
-  {
-    label: 'Last name',
-    name: 'lastName',
-    id: 'last-name-inline-shorthand',
-    key: 'last-name',
-    required: true,
-    inline: true,
-  },
-  {
-    label: 'I agree to the Terms and Conditions',
-    control: {
-      as: 'input',
-    },
-    type: 'checkbox',
-    id: 'conditions-inline-shorthand',
-    key: 'conditions',
-  },
-  {
-    control: {
-      as: Button,
-      content: 'Submit',
-    },
-    key: 'submit',
-  },
-]
-
-const HighlightPopup = ({ onConfirm }) => {
-  return <Form onSubmit={onConfirm} fields={fields} />
-}
-
 const ToolbarExampleShorthand = () => {
   const [isBold, setBold] = useBooleanKnob({ name: 'bold', initialValue: true })
-  const [isItalic, setItalic] = useBooleanKnob({ name: 'isItalic', initialValue: false })
-  const [isUnderline, setUnderline] = useBooleanKnob({ name: 'isUnderline', initialValue: false })
-  const [isStrike, setStrike] = useBooleanKnob({ name: 'isStrike', initialValue: false })
 
   const [highlightOpen, setHighlightOpen] = useBooleanKnob({
     name: 'highlightOpen',
@@ -68,14 +26,8 @@ const ToolbarExampleShorthand = () => {
     initialValue: false,
   })
 
-  const [log, setLog] = React.useState<string[]>([])
-  const writeLog = message => {
-    setLog(prevLog => [`${new Date().toLocaleTimeString()}: ${message}`, ...prevLog])
-  }
-
   const [bulletListActive, setBulletListActive] = React.useState(false)
   const [numberListActive, setNumberListActive] = React.useState(false)
-  const [toDoListActive, setToDoListActive] = React.useState(false)
 
   return (
     <>
@@ -89,38 +41,6 @@ const ToolbarExampleShorthand = () => {
             icon: { name: 'bold', outline: true },
             onClick: () => {
               setBold(!isBold)
-            },
-          },
-          {
-            key: 'italic',
-            kind: 'toggle' as ToolbarItemShorthandKinds,
-            active: isItalic,
-            tooltip: 'Italic',
-            icon: { name: 'italic', outline: true },
-            onClick: () => {
-              setItalic(!isItalic)
-            },
-          },
-          {
-            key: 'underline',
-            kind: 'toggle' as ToolbarItemShorthandKinds,
-            active: isUnderline,
-            tooltip: 'Underline',
-            icon: { name: 'underline', outline: true },
-            onClick: () => {
-              setUnderline(!isUnderline)
-            },
-          },
-          {
-            key: 'strike',
-            kind: 'toggle' as ToolbarItemShorthandKinds,
-            active: isStrike,
-            disabled: true,
-            // TODO: should this have a tooltip? And be focusable
-            // tooltip: 'Strike',
-            icon: { name: 'strike', outline: true },
-            onClick: () => {
-              setStrike(!isStrike)
             },
           },
           { key: 'divider1', kind: 'divider' as ToolbarItemShorthandKinds },
@@ -155,13 +75,6 @@ const ToolbarExampleShorthand = () => {
               },
             },
           },
-          { key: 'font-size', icon: { name: 'font-size', outline: true }, tooltip: 'Font size' },
-          {
-            key: 'remove-format',
-            icon: { name: 'remove-format', outline: true },
-            tooltip: 'Remove format',
-          },
-          { key: 'divider2', kind: 'divider' as ToolbarItemShorthandKinds },
           {
             key: 'radiogroup',
             kind: 'group' as ToolbarItemShorthandKinds,
@@ -173,10 +86,8 @@ const ToolbarExampleShorthand = () => {
                 active: bulletListActive,
                 onClick: () => {
                   setBulletListActive(!bulletListActive)
-
-                  // deselect other radio items
+                  // deselect other radio item
                   setNumberListActive(false)
-                  setToDoListActive(false)
                 },
                 'aria-label': 'bullet list',
               },
@@ -187,26 +98,10 @@ const ToolbarExampleShorthand = () => {
                 tooltip: 'Number list',
                 onClick: () => {
                   setNumberListActive(!numberListActive)
-
-                  // deselect other radio items
+                  // deselect other radio item
                   setBulletListActive(false)
-                  setToDoListActive(false)
                 },
                 'aria-label': 'number list',
-              },
-              {
-                key: 'to-do-list',
-                icon: { name: 'to-do-list', outline: true },
-                active: toDoListActive,
-                tooltip: 'To do list',
-                onClick: () => {
-                  setToDoListActive(!toDoListActive)
-
-                  // deselect other radio items
-                  setBulletListActive(false)
-                  setNumberListActive(false)
-                },
-                'aria-label': 'to do list',
               },
             ].map(radioItem => render =>
               render(radioItem, (RadioItem, props) => {
@@ -215,10 +110,6 @@ const ToolbarExampleShorthand = () => {
               }),
             ),
           },
-          { key: 'divider3', kind: 'divider' as ToolbarItemShorthandKinds },
-          { key: 'outdent', icon: { name: 'outdent', outline: true }, tooltip: 'Outdent' },
-          { key: 'indent', icon: { name: 'indent', outline: true }, tooltip: 'Indent' },
-          { key: 'divider4', kind: 'divider' as ToolbarItemShorthandKinds },
           {
             key: 'more',
             icon: { name: 'more', outline: true },
@@ -228,27 +119,14 @@ const ToolbarExampleShorthand = () => {
               {
                 content: 'Quote',
                 icon: 'quote',
-                onClick: () => {
-                  writeLog('... -> Quote')
-                },
-              },
-              {
-                content: 'Link',
-                icon: 'link',
-                disabled: true,
-                onClick: () => {
-                  writeLog('SHOULD NOT BE CALLED, ITEM IS DISABLED... -> Link')
-                },
               },
               {
                 content: 'Code snippet',
                 icon: 'code-snippet',
-                onClick: () => writeLog('... -> Code snippet'),
               },
             ],
             menuOpen: moreMenuOpen,
             onMenuOpenChange: (e, { menuOpen }) => {
-              writeLog(`setting menu to ${menuOpen ? 'open' : 'close'}`)
               setMoreMenuOpen(menuOpen)
             },
           },
@@ -268,15 +146,39 @@ const ToolbarExampleShorthand = () => {
             : item,
         )}
       />
-      <br />
-      <button onClick={() => setLog([])}>Clear log</button>
-      <pre>
-        {log.map((e, i) => (
-          <div key={i}>{e}</div>
-        ))}
-      </pre>
     </>
   )
+}
+
+const fields = [
+  {
+    label: 'Full name',
+    name: 'fullName',
+    id: 'full-name-inline-shorthand',
+    key: 'full-name',
+    required: true,
+    inline: true,
+  },
+  {
+    label: 'I agree to the Terms and Conditions',
+    control: {
+      as: 'input',
+    },
+    type: 'checkbox',
+    id: 'conditions-inline-shorthand',
+    key: 'conditions',
+  },
+  {
+    control: {
+      as: Button,
+      content: 'Submit',
+    },
+    key: 'submit',
+  },
+]
+
+const HighlightPopup = ({ onConfirm }) => {
+  return <Form onSubmit={onConfirm} fields={fields} />
 }
 
 export default ToolbarExampleShorthand
