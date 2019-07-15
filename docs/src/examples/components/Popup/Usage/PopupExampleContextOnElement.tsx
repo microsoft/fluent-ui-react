42a96fa5f5e602c61c3e92b377bd422c46b4a046
Oleksandr Fediashov <olfedias@microsoft.com>
2019-07-15 16:38:57 +0200

    simplify example

--- docs/src/examples/components/Popup/Usage/PopupExampleContextOnElement.tsx
+++ docs/src/examples/components/Popup/Usage/PopupExampleContextOnElement.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react'
 import { Alert, Button, Flex, Popup } from '@stardust-ui/react'
 
-const contentWithButtons = {
-  content: (
-    <Flex gap="gap.smaller">
-      <Button>First</Button>
-      <Button primary>Second</Button>
-    </Flex>
-  ),
-}
+const contentWithButtons = (
+  <Flex gap="gap.smaller">
+    <Button>First</Button>
+    <Button primary>Second</Button>
+  </Flex>
+)
 
 class PopupContextOnElement extends React.Component {
   state = { alert: false }
