da3942cba754193728e845495e0ce4e5ca43ddc9
manajdov <manajdov@microsoft.com>
2019-07-16 12:37:16 +0200

    -fixed screener steps

--- docs/src/examples/components/Menu/Usages/MenuExampleWithTooltip.shorthand.steps.ts
+++ docs/src/examples/components/Menu/Usages/MenuExampleWithTooltip.shorthand.steps.ts
@@ -1,8 +1,21 @@
-import getScreenerSteps from '../commonScreenerSteps'
+import { Menu } from '@stardust-ui/react'
+
+const selectors = {
+  menu: `.${Menu.className}`,
+  item: (itemIndex: number) => `.${Menu.className} li:nth-child(${itemIndex}) a`,
+}
 
 const config: ScreenerTestsConfig = {
   themes: ['teams', 'teamsDark', 'teamsHighContrast'],
-  steps: getScreenerSteps(),
+  steps: [
+    (builder, keys) =>
+      builder
+        .hover(selectors.item(1))
+        .snapshot('Hovers 1st item (show tooltip)')
+        .click(selectors.item(1))
+        .keys(selectors.item(1), keys.rightArrow)
+        .snapshot('Navigates to next item (shows tooltip)'),
+  ],
 }
 
 export default config
--- docs/src/examples/components/Toolbar/Usage/ToolbarExampleWithTooltip.shorthand.steps.ts
+++ docs/src/examples/components/Toolbar/Usage/ToolbarExampleWithTooltip.shorthand.steps.ts
@@ -1,8 +1,21 @@
-import getScreenerSteps from '../commonScreenerSteps'
+import { Toolbar } from '@stardust-ui/react'
+
+const selectors = {
+  menu: `.${Toolbar.className}`,
+  item: (itemIndex: number) => `.${Toolbar.className} :nth-child(${itemIndex}) a`,
+}
 
 const config: ScreenerTestsConfig = {
   themes: ['teams', 'teamsDark', 'teamsHighContrast'],
-  steps: getScreenerSteps(),
+  steps: [
+    (builder, keys) =>
+      builder
+        .hover(selectors.item(1))
+        .snapshot('Hovers 1st item (show tooltip)')
+        .click(selectors.item(1))
+        .keys(selectors.item(1), keys.rightArrow)
+        .snapshot('Navigates to next item (shows tooltip)'),
+  ],
 }
 
 export default config
