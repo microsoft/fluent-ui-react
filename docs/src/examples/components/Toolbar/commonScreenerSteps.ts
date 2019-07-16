12abc6f23cdb6ab608bbf2070f63e3e5011dfc7b
manajdov <manajdov@microsoft.com>
2019-07-16 12:13:04 +0200

    -added screener steps

---
+++ docs/src/examples/components/Toolbar/commonScreenerSteps.ts
@@ -0,0 +1,31 @@
+import { Toolbar } from '@stardust-ui/react'
+
+interface StepsOptions {
+  vertical?: boolean
+  startItem?: number
+  endItem?: number
+}
+
+const selectors = {
+  menu: `.${Toolbar.className}`,
+  item: (itemIndex: number) => `.${Toolbar.className} li:nth-child(${itemIndex}) a`,
+}
+
+const getScreenerSteps = ({
+  vertical,
+  startItem = 2,
+  endItem = 3,
+}: StepsOptions = {}): ScreenerSteps => [
+  (builder, keys) =>
+    builder
+      .hover(selectors.item(startItem))
+      .snapshot('Hovers 2nd item (hover state styles)')
+      .click(selectors.item(startItem))
+      .snapshot('Clicks on 2nd item (active state styles)')
+      .keys(selectors.item(startItem), vertical ? keys.downArrow : keys.rightArrow)
+      .snapshot('Navigates to next item (focus state styles)')
+      .keys(selectors.item(endItem), vertical ? keys.upArrow : keys.leftArrow)
+      .snapshot('Navigates to previous item (active and focus state styles)'),
+]
+
+export default getScreenerSteps
