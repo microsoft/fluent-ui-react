68b1350c99e157b1012e395806a7d1675b4a947c
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2019-07-18 10:32:34 -0700

    Fix typo and remove unused g tag

--- packages/react/src/themes/teams/components/Icon/svg/icons/index.ts
+++ packages/react/src/themes/teams/components/Icon/svg/icons/index.ts
@@ -73,7 +73,7 @@ import giphy from './giphy'
 import hand from './hand'
 import highlight from './highlight'
 import horizontalRule from './horizontalRule'
-import imageUnavailabe from './image-unavailable'
+import imageUnavailable from './image-unavailable'
 import indent from './indent'
 import info from './info'
 import italic from './italic'
@@ -209,7 +209,7 @@ export default {
   giphy,
   highlight,
   'horizontal-rule': horizontalRule,
-  'image-unavailable': imageUnavailabe,
+  'image-unavailable': imageUnavailable,
   indent,
   info,
   italic,
