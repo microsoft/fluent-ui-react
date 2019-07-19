b484bcea85c44e3aa6987a810c9454ecbe6305e5
Brigette Lundeen <brlundee@microsoft.com>
2019-07-18 18:12:17 -0700

    adding support for inverted color for inputs

--- packages/react/src/themes/teams-high-contrast/components/Input/inputVariables.ts
+++ packages/react/src/themes/teams-high-contrast/components/Input/inputVariables.ts
@@ -1,12 +1,10 @@
 import { InputVariables } from '../../../base/components/Input/inputVariables'
+import { pxToRem } from '../../../../lib'
 
 export default (siteVars: any): Partial<InputVariables> => {
   return {
-    backgroundColor: siteVars.bodyBackground,
     borderColor: siteVars.bodyColor,
-    fontColor: siteVars.bodyColor,
-    iconColor: siteVars.bodyColor,
+    borderWidth: `${pxToRem(1)} ${pxToRem(1)} ${pxToRem(2)} ${pxToRem(1)}`,
     inputFocusBorderColor: `transparent transparent ${siteVars.accessibleYellow} transparent`,
-    placeholderColor: siteVars.colors.white,
   }
 }
