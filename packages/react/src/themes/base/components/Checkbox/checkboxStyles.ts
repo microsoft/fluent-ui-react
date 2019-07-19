5f31da2ed1ef961faf5211ea38acf566b3a4f7ed
Oleksandr Fediashov <olfedias@microsoft.com>
2019-07-19 11:03:00 +0200

    polish styles

--- packages/react/src/themes/base/components/Checkbox/checkboxStyles.ts
+++ packages/react/src/themes/base/components/Checkbox/checkboxStyles.ts
@@ -7,9 +7,10 @@ const checkboxStyles: ComponentSlotStylesInput<CheckboxProps & CheckboxState, Ch
     alignItems: 'center',
     position: 'relative',
 
+    // CSS Grid is polifilled only with latest inline-style-prefixer
     // @ts-ignore is supported by fallback values plugin
-    display: ['inline-grid', '-ms-grid'],
-    // Gap for IE 11 is done via virtual column
+    display: ['inline-grid', '-ms-inline-grid'],
+    // IE11: Gap is done via virtual column as in autoprefixer
     '-ms-grid-columns':
       p.labelPosition === 'start' ? `1fr ${v.checkboxGap} auto` : `auto ${v.checkboxGap} 1fr`,
     gridTemplateColumns: p.labelPosition === 'start' ? '1fr auto' : 'auto 1fr',
@@ -56,7 +57,7 @@ const checkboxStyles: ComponentSlotStylesInput<CheckboxProps & CheckboxState, Ch
 
   label: ({ props: p }): ICSSInJSStyle => ({
     '-ms-grid-column': p.labelPosition === 'start' ? 1 : 3,
-    display: 'block',
+    display: 'block', // IE11: should be forced `block`, `inline-block` will be broken
   }),
 
   toggle: ({ props: p, variables: v }): ICSSInJSStyle => ({
