4c17d24a26f4396a7caca7be08f4c6ae5f72b10b
kuzhelov <kuroman@microsoft.com>
2019-07-22 12:56:08 +0200

    add comment

--- build/dangerjs/detectNonApprovedDependencies/utils/getRuntimeDependencies.ts
+++ build/dangerjs/detectNonApprovedDependencies/utils/getRuntimeDependencies.ts
@@ -4,6 +4,12 @@ import config from '../../../../config'
 
 const { paths } = config
 
+/**
+ * It is necessary to cotrresppondign Gulp task in a separate process.
+ * This task relies on webpack's crawling through the imported modules,
+ * but when this is run from DangerJS process, there is no crawling happening.
+ * This is because of the way DangerJS handles imports: https://spectrum.chat/danger/javascript/danger-js-actually-runs-your-imports-as-globals~0a005b56-31ec-4919-9a28-ced623949d4d
+ */
 const getRuntimeDependencies = (stardustPackageName: string) => {
   const dependencyRegex = /^dependency:\s+(.*)$/
   const result = spawnSync(
