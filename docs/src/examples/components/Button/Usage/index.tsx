bda3bf773ef36551722cd80bd809476e8c50b9b6
Levi Thomason <me@levithomason.com>
2019-07-22 11:51:50 -0700

    docs(Button): separate overflow example

--- docs/src/examples/components/Button/Usage/index.tsx
+++ docs/src/examples/components/Button/Usage/index.tsx
@@ -6,6 +6,11 @@ import ExampleSection from 'docs/src/components/ComponentDoc/ExampleSection'
 const Usage = () => (
   <ExampleSection title="Usage">
     <ComponentExample
+      title="Overflow"
+      description="A button can have content that overflows."
+      examplePath="components/Button/Usage/ButtonExampleOverflow"
+    />
+    <ComponentExample
       title="Tinted Example"
       description='A button used in cards is a "tinted" version of a default button.'
       examplePath="components/Button/Usage/ButtonUsageExample"
