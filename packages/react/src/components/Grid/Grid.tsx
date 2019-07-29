172fbc24430a618aa3bc74427700a546e86a2341
Sofiya Huts <sohuts@microsoft.com>
2019-07-29 14:43:37 +0200

    feat(Grid): Expose `isFromKeyboard` prop

--- packages/react/src/components/Grid/Grid.tsx
+++ packages/react/src/components/Grid/Grid.tsx
@@ -1,6 +1,8 @@
 import * as customPropTypes from '@stardust-ui/react-proptypes'
 import * as PropTypes from 'prop-types'
 import * as React from 'react'
+import * as _ from 'lodash'
+
 import {
   UIComponent,
   childrenExist,
@@ -10,8 +12,9 @@ import {
   commonPropTypes,
   ContentComponentProps,
   rtlTextContainer,
+  isFromKeyboard,
 } from '../../lib'
-import { WithAsProp, withSafeTypeForAs } from '../../types'
+import { WithAsProp, withSafeTypeForAs, ComponentEventHandler } from '../../types'
 import { Accessibility } from '../../lib/accessibility/types'
 
 export interface GridProps extends UIComponentProps, ChildrenComponentProps, ContentComponentProps {
@@ -26,9 +29,27 @@ export interface GridProps extends UIComponentProps, ChildrenComponentProps, Con
 
   /** The rows of the grid with a space-separated list of values. The values represent the track size, and the space between them represents the grid line. */
   rows?: string | number
+
+  /**
+   * Called after user's focus.
+   * @param {SyntheticEvent} event - React's original SyntheticEvent.
+   * @param {object} data - All props.
+   */
+  onFocus?: ComponentEventHandler<GridProps>
+
+  /**
+   * Called after item blur.
+   * @param {SyntheticEvent} event - React's original SyntheticEvent.
+   * @param {object} data - All props.
+   */
+  onBlur?: ComponentEventHandler<GridProps>
 }
 
-class Grid extends UIComponent<WithAsProp<GridProps>, any> {
+interface GridState {
+  isFromKeyboard: boolean
+}
+
+class Grid extends UIComponent<WithAsProp<GridProps>, GridState> {
   static displayName = 'Grid'
 
   static className = 'ui-grid'
@@ -46,10 +67,24 @@ class Grid extends UIComponent<WithAsProp<GridProps>, any> {
       ]),
     ]),
     rows: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    onFocus: PropTypes.func,
+    onBlur: PropTypes.func,
   }
 
   static defaultProps: WithAsProp<GridProps> = {
     as: 'div',
+  }
+
+  handleBlur = (e: React.SyntheticEvent) => {
+    this.setState({ isFromKeyboard: false })
+
+    _.invoke(this.props, 'onBlur', e, this.props)
+  }
+
+  handleFocus = (e: React.SyntheticEvent) => {
+    this.setState({ isFromKeyboard: isFromKeyboard() })
+
+    _.invoke(this.props, 'onFocus', e, this.props)
   }
 
   renderComponent({
@@ -63,6 +98,8 @@ class Grid extends UIComponent<WithAsProp<GridProps>, any> {
     return (
       <ElementType
         className={classes.root}
+        onFocus={this.handleFocus}
+        onBlur={this.handleBlur}
         {...rtlTextContainer.getAttributes({ forElements: [children, content] })}
         {...accessibility.attributes.root}
         {...unhandledProps}
