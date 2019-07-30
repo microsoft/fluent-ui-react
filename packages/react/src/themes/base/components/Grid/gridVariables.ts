35dfffa76ac495ae08ea4e214cf1610e8ade9166
Oleksandr Fediashov <olfedias@microsoft.com>
2019-07-30 18:11:54 +0200

    remove default values

--- packages/react/src/themes/base/components/Grid/gridVariables.ts
+++ packages/react/src/themes/base/components/Grid/gridVariables.ts
@@ -1,14 +1,12 @@
 export interface GridVariables {
-  height: string
-  width: string
+  height?: string
+  width?: string
   defaultColumnCount: number
   gridGap?: string
   padding?: string
 }
 
 export default (): GridVariables => ({
-  height: 'auto',
-  width: '100%',
   defaultColumnCount: 5,
   gridGap: undefined,
   padding: undefined,
