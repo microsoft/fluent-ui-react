ca48934c96354f59c002c704793a199acfed2a6c
Brigette Lundeen <brlundee@microsoft.com>
2019-08-05 14:53:10 -0700

    updated text button example to have all 3 examples

--- docs/src/examples/components/Button/Types/ButtonExampleText.shorthand.tsx
+++ docs/src/examples/components/Button/Types/ButtonExampleText.shorthand.tsx
@@ -1,6 +1,16 @@
 import * as React from 'react'
 import { Button } from '@stardust-ui/react'
 
-const ButtonExampleTextShorthand = () => <Button text content="A text button" />
+const ButtonExampleTextShorthand = () => (
+  <div>
+    <Button text content="A text button" />
+    <br />
+    <br />
+    <Button icon="call-video" text content="A text button with an icon" />
+    <br />
+    <br />
+    <Button icon="team-create" text iconOnly />
+  </div>
+)
 
 export default ButtonExampleTextShorthand
--- docs/src/examples/components/Button/Types/ButtonExampleText.tsx
+++ docs/src/examples/components/Button/Types/ButtonExampleText.tsx
@@ -1,10 +1,23 @@
 import * as React from 'react'
-import { Button, Text } from '@stardust-ui/react'
+import { Button, Text, Icon } from '@stardust-ui/react'
 
 const ButtonExampleText = () => (
-  <Button text>
-    <Text content="A text button" />
-  </Button>
+  <div>
+    <Button text>
+      <Text content="A text button" />
+    </Button>
+    <br />
+    <br />
+    <Button text>
+      <Icon name="call-video" />
+      <Text content="A text button with an icon" />
+    </Button>
+    <br />
+    <br />
+    <Button text iconOnly>
+      <Icon name="call-video" />
+    </Button>
+  </div>
 )
 
 export default ButtonExampleText
