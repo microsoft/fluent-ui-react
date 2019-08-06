b58d25e9796bf79c505d1e816547605bb74439b9
Milan Turon <mituron@microsoft.com>
2019-08-06 13:54:05 +0200

    changes after PR

--- docs/src/examples/components/Dialog/BestPractices/DialogBestPractices.tsx
+++ docs/src/examples/components/Dialog/BestPractices/DialogBestPractices.tsx
@@ -1,24 +1,17 @@
 import * as React from 'react'
 import ComponentBestPractices from 'docs/src/components/ComponentBestPractices'
-import { link } from '../../../../utils/helpers'
+import { link, code } from '../../../../utils/helpers'
 import { Text } from '@stardust-ui/react'
 
 const doList = [
   'Do use `trapFocus` prop to control focus trapping behavior.',
   <Text>
-    Recommended{' '}
+    Do use {code('aria-describedby')} prop to override or omit description if the dialog content is
+    complex (contains actionable elements). Refer to{' '}
     {link(
       'aria pattern',
       'https://www.w3.org/TR/wai-aria-practices-1.1/examples/dialog-modal/dialog.html',
-    )}{' '}
-    how to label dialog is following:
-    <ul>
-      <li> aria-labelledby="id of the dialog title" </li>
-      <li> aria-describedby="id of the dialog content" </li>
-    </ul>
-    This approach is automatically applied by stardust behavior. In case your dialog content is
-    huge, then consider overriding aria-describedby to shorted description, or don't use the
-    attribute.
+    )}
   </Text>,
 ]
 
--- docs/src/examples/components/Form/BestPractices/FormBestPractices.tsx
+++ docs/src/examples/components/Form/BestPractices/FormBestPractices.tsx
@@ -1,20 +1,15 @@
 import * as React from 'react'
 
 import ComponentBestPractices from 'docs/src/components/ComponentBestPractices'
-import { link } from '../../../../utils/helpers'
+import { link, code } from '../../../../utils/helpers'
 import { Text } from '@stardust-ui/react'
 
 const doList = [
   'Provide label by using `aria-label`, or `aria-labelledby` prop.',
   <Text>
-    Recommended {link('pattern', 'https://www.w3schools.com/tags/tag_label.asp')} how to label input
-    in the form is following:
-    <ul>
-      <li> label tag has attribute for="id of input" </li>
-      <li> input has "id" attribute </li>
-    </ul>
-    This approach is automatically applied by stardust behavior. In case stardust form component is
-    not used, then follow the labelling of input in form as described above.
+    Do attach label to each input element. By using {code('Form.Field')} component or by using for
+    property on the input. Seee{' '}
+    {link('element documentation', 'https://www.w3schools.com/tags/tag_label.asp')} for details.
   </Text>,
 ]
 
