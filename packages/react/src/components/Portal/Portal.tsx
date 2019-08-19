11e0ba4250631c4dc5631cb78ccc4e39b82d6497
Oleksandr Fediashov <olfedias@microsoft.com>
2019-08-19 11:31:12 +0200

    fix(Provider): use `target` in context (#1810)
    * fix(Provider): use `target` in context
    
    * wipwip
    
    * wipwipwip
    
    * fix UT
    
    * remove default props
    
    * remove one more default prop
    
    * add changelog entry
    
    * fixfixfix

--- packages/react/src/components/Portal/Portal.tsx
+++ packages/react/src/components/Portal/Portal.tsx
@@ -1,5 +1,5 @@
-import { documentRef, EventListener } from '@stardust-ui/react-component-event-listener'
-import { handleRef, Ref } from '@stardust-ui/react-component-ref'
+import { EventListener } from '@stardust-ui/react-component-event-listener'
+import { handleRef, Ref, toRefObject } from '@stardust-ui/react-component-ref'
 import * as customPropTypes from '@stardust-ui/react-proptypes'
 import * as PropTypes from 'prop-types'
 import * as React from 'react'
@@ -121,8 +121,10 @@ class Portal extends AutoControlledComponent<PortalProps, PortalState> {
   renderPortal(): JSX.Element | undefined {
     const { children, content, trapFocus } = this.props
     const { open } = this.state
+
     const contentToRender = childrenExist(children) ? children : content
     const focusTrapZoneProps = (_.keys(trapFocus).length && trapFocus) || {}
+    const targetRef = toRefObject(this.context.target)
 
     return (
       open && (
@@ -137,11 +139,7 @@ class Portal extends AutoControlledComponent<PortalProps, PortalState> {
             ) : (
               contentToRender
             )}
-            <EventListener
-              listener={this.handleDocumentClick}
-              targetRef={documentRef}
-              type="click"
-            />
+            <EventListener listener={this.handleDocumentClick} targetRef={targetRef} type="click" />
           </PortalInner>
         </Ref>
       )
