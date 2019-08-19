0bbd0196549cc0e8fe68e3ed5fe53186db04e79b
Brian Calvery <bcalvery@microsoft.com>
2019-08-19 15:01:03 -0700

    Fixing incorrect tooltip shadow

--- packages/react/src/themes/teams-dark/components/Tooltip/tooltipContentVariables.ts
+++
@@ -1,5 +0,0 @@
-import { TooltipContentVariables } from '../../../teams/components/Tooltip/tooltipContentVariables'
-
-export default (siteVars: any): Partial<TooltipContentVariables> => ({
-  boxShadowColor: siteVars.colors.black,
-})
