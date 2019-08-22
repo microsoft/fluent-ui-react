972dc82fdf309cf8bd6079cf7edcd6fd735c40f6
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2019-08-22 12:01:10 +0200

    add alertProps.ts file

---
+++ packages/react/src/lib/accessibility/Behaviors/Alert/alertProps.ts
@@ -0,0 +1,10 @@
+type AlertProps = {
+  /** An alert may be formatted to display a danger message. */
+  danger?: boolean
+  /** An alert may be formatted to display a warning message. */
+  warning?: boolean
+  /** id of the alert body element. */
+  bodyId?: string
+}
+
+export default AlertProps
