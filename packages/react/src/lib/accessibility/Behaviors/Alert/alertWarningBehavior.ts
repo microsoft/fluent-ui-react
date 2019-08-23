93087e18226f4d742eb7a294061cef61f33fce44
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2019-08-23 12:22:06 +0200

    remove hardcoded aria-label

--- packages/react/src/lib/accessibility/Behaviors/Alert/alertWarningBehavior.ts
+++ packages/react/src/lib/accessibility/Behaviors/Alert/alertWarningBehavior.ts
@@ -14,7 +14,6 @@ const alertWarningBehavior: Accessibility<AlertProps> = props => ({
     },
     dismissAction: {
       'aria-describedby': props.bodyId,
-      'aria-label': 'dismiss',
     },
   },
 })
