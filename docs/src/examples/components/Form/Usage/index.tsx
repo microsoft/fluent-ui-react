00977a9cf3141f3b290f26bd407f1a31305c57fa
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2019-08-23 17:53:46 +0200

    move some exmaples to Usage

--- docs/src/examples/components/Form/Usage/index.tsx
+++ docs/src/examples/components/Form/Usage/index.tsx
@@ -9,6 +9,16 @@ const Usage = () => (
       description="A form can have a Checkbox as a field."
       examplePath="components/Form/Usage/FormExampleCheckbox"
     />
+    <ComponentExample
+      title="Dropdown"
+      description="A form can have a Dropdown as a field."
+      examplePath="components/Form/Usage/FormExampleDropdown"
+    />
+    <ComponentExample
+      title="Slider"
+      description="A form can have a Slider as a field."
+      examplePath="components/Form/Usage/FormExampleSlider"
+    />
   </ExampleSection>
 )
 
