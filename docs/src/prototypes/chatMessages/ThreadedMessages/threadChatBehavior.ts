0101817bb400de24e44f6d8db3dc7d481eaec27a
Sofiya Huts <8460706+sophieH29@users.noreply.github.com>
2019-08-26 14:17:46 +0200

    Update docs/src/prototypes/chatMessages/ThreadedMessages/threadChatBehavior.ts
    Co-Authored-By: Oleksandr Fediashov <olfedias@microsoft.com>

--- docs/src/prototypes/chatMessages/ThreadedMessages/threadChatBehavior.ts
+++ docs/src/prototypes/chatMessages/ThreadedMessages/threadChatBehavior.ts
@@ -16,7 +16,7 @@ const getLastTabbableElement = (root: HTMLElement): HTMLElement => {
   // In real chat, it should focus the message with data-last-visible="true"
   // Since we don't have this logic in Stardust, overriding a selector to focus the last thread message
   const chatItemsElements = root.querySelectorAll(
-    `[chat-focuszone] .ui-chat__message__thread[data-is-focusable="true"]`,
+    `[chat-focuszone] .${classNames.threadedMessage.thread}[data-is-focusable="true"]`,
   )
   return chatItemsElements.length > 0
     ? (chatItemsElements[chatItemsElements.length - 1] as HTMLElement)
