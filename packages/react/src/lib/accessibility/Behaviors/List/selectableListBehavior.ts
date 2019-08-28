1df7be0ed75d8d07ea5beb7d8e147cc4d0758884
Sofiya Huts <sohuts@microsoft.com>
2019-08-28 15:37:27 +0200

    small improvement

--- packages/react/src/lib/accessibility/Behaviors/List/selectableListBehavior.ts
+++ packages/react/src/lib/accessibility/Behaviors/List/selectableListBehavior.ts
@@ -16,7 +16,7 @@ const selectableListBehavior: Accessibility<ListBehaviorProps> = props => ({
   attributes: {
     root: {
       role: 'listbox',
-      tabIndex: props.selectable ? (props.isItemFocused ? -1 : 0) : undefined,
+      tabIndex: props.isItemFocused ? -1 : 0,
       ...(props.horizontal && {
         'aria-orientation': 'horizontal',
       }),
