b3712f9f3c3f0f374bc8af94784a68b9e964fe2f
Oleksandr Fediashov <olfedias@microsoft.com>
2019-08-28 16:40:44 +0200

    update name

--- docs/src/examples/components/Checkbox/commonScreenerSteps.ts
+++ docs/src/examples/components/Checkbox/commonScreenerSteps.ts
@@ -1,8 +1,8 @@
 import { Checkbox } from '@stardust-ui/react'
 
 const getScreenerSteps = (): ScreenerSteps => [
-  builder => builder.click(`.${Checkbox.className}`).snapshot('Can be checked'),
-  (builder, keys) => builder.keys('body', keys.tab).snapshot('Can be focused'),
+  builder => builder.click(`.${Checkbox.className}`).snapshot('Checks checkbox'),
+  (builder, keys) => builder.keys('body', keys.tab).snapshot('Focuses checkbox'),
 ]
 
 export default getScreenerSteps
