a9c98f0caa5147213aab2221e5458f3d1ca14dd3
Oleksandr Fediashov <olfedias@microsoft.com>
2019-08-29 15:59:54 +0200

    add knob

--- docs/src/examples/components/Dialog/Variations/DialogExampleInert.shorthand.tsx
+++ docs/src/examples/components/Dialog/Variations/DialogExampleInert.shorthand.tsx
@@ -1,14 +1,19 @@
+import { useBooleanKnob } from '@stardust-ui/docs-components'
 import { Button, Dialog } from '@stardust-ui/react'
 import * as React from 'react'
 
-const DialogExampleInert = () => (
-  <Dialog
-    cancelButton="Cancel"
-    confirmButton="Confirm"
-    header="Action confirmation"
-    inert
-    trigger={<Button content="Open a dialog" />}
-  />
-)
+const DialogExampleInert = () => {
+  const [inert] = useBooleanKnob({ name: 'inert', initialValue: true })
+
+  return (
+    <Dialog
+      cancelButton="Cancel"
+      confirmButton="Confirm"
+      header="Action confirmation"
+      inert={inert}
+      trigger={<Button content="Open a dialog" />}
+    />
+  )
+}
 
 export default DialogExampleInert
