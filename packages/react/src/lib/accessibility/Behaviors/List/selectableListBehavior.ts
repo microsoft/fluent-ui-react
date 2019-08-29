545bab07d87d0f7c0a7e8e1e9207f034ee78df04
Sofiya Huts <sohuts@microsoft.com>
2019-08-29 12:05:26 +0200

    add test and changelog

--- packages/react/src/lib/accessibility/Behaviors/List/selectableListBehavior.ts
+++ packages/react/src/lib/accessibility/Behaviors/List/selectableListBehavior.ts
@@ -8,6 +8,7 @@ import { FocusZoneDirection } from '../../FocusZone'
  *
  * @specification
  * Adds role='listbox'.
+ * Adds attribute 'tabIndex=-1' to 'root' slot.
  * Adds attribute 'aria-orientation=horizontal' to 'root' slot if 'horizontal' property is true. Does not set the attribute otherwise.
  * Embeds component into FocusZone.
  * Provides arrow key navigation in bidirectionalDomOrder direction.
