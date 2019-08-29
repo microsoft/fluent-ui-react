2de17c2b1ad94ee0fb729f3321e09e5f378a0aac
Oleksandr Fediashov <olfedias@microsoft.com>
2019-08-29 17:34:18 +0200

    Update docs/src/examples/components/Tree/Usage/index.tsx

--- docs/src/examples/components/Tree/Usage/index.tsx
+++ docs/src/examples/components/Tree/Usage/index.tsx
@@ -4,7 +4,7 @@ import ComponentExample from 'docs/src/components/ComponentDoc/ComponentExample'
 import ExampleSection from 'docs/src/components/ComponentDoc/ExampleSection'
 
 const Usage = () => (
-  <ExampleSection title="">
+  <ExampleSection title="Usage">
     <ComponentExample
       title="Custom Title"
       description="A Tree with customized title rendering."
