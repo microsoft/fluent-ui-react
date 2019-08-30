478b8970e21c52ee2d7b7d99ca2cd23709cbfe11
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2019-08-30 16:11:27 +0200

    rework index.tsx using React.FC

--- docs/src/prototypes/CopyToClipboard/index.tsx
+++ docs/src/prototypes/CopyToClipboard/index.tsx
@@ -3,46 +3,41 @@ import { Flex, Text } from '@stardust-ui/react/src'
 import CopyToClipboard from './CopyToClipboard'
 import { PrototypeSection, ComponentPrototype } from '../Prototypes'
 
-class CopyToClipboardPrototypeAttached extends React.Component {
-  render() {
-    const commitID = '3422f7d'
-    return (
-      <Flex gap="gap.medium" vAlign="center" padding="padding.medium">
-        <Text content="Commit: " />
-        <Text content={commitID} color="brand" />
-        <CopyToClipboard attached pointing value={commitID} copyPrompt="Copy commit ID" />
-      </Flex>
-    )
-  }
+type CopyToClipboardPrototypeProps = {
+  value: string
+  attached?: boolean
 }
 
-class CopyToClipboardPrototypeNotAttached extends React.Component {
-  render() {
-    const commitID = '3421f7d'
-    return (
-      <Flex gap="gap.medium" vAlign="center" padding="padding.medium">
-        <Text content="Commit: " />
-        <Text content={commitID} color="brand" />
-        <CopyToClipboard pointing value={commitID} copyPrompt="Copy commit ID" />
-      </Flex>
-    )
-  }
+const CopyToClipboardPrototype: React.FC<CopyToClipboardPrototypeProps> = props => {
+  return (
+    <Flex gap="gap.medium" vAlign="center" padding="padding.medium">
+      <Text content="Commit: " />
+      <Text content={props.value} color="brand" />
+      <CopyToClipboard
+        attached={props.attached}
+        pointing
+        value={props.value}
+        copyPrompt="Copy commit ID"
+      />
+    </Flex>
+  )
 }
 
 const CopyToClipboardPrototypes: React.FC = () => {
+  const commitID = '3422f7d'
   return (
     <PrototypeSection title="Copy to Clipboard">
       <ComponentPrototype
         title="Attached"
         description="Attached version of Copy to Clipboard prototype"
       >
-        <CopyToClipboardPrototypeAttached />
+        <CopyToClipboardPrototype attached value={commitID} />
       </ComponentPrototype>
       <ComponentPrototype
         title="Not Attached"
         description="Not attached version of Copy to Clipboard prototype"
       >
-        <CopyToClipboardPrototypeNotAttached />
+        <CopyToClipboardPrototype value={commitID} />
       </ComponentPrototype>
     </PrototypeSection>
   )
