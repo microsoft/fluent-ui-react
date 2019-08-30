a5715c7c706144761fb23da2a96fffebfb2c1654
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2019-08-30 15:53:06 +0200

    rewrite using FunctionalComonent

--- docs/src/prototypes/CopyToClipboard/CopyToClipboard.tsx
+++ docs/src/prototypes/CopyToClipboard/CopyToClipboard.tsx
@@ -10,126 +10,119 @@ export type CopyToClipboardProps = {
   pointing?: boolean
 }
 
-class CopyToClipboard extends React.Component<CopyToClipboardProps> {
-  state = {
-    copied: false,
-    copiedHeight: 0,
-    copiedWidth: 0,
-  }
-  timeoutId = undefined
-  copiedTextRef = React.createRef<HTMLElement>()
+const CopyToClipboard: React.FC<CopyToClipboardProps> = props => {
+  const [copied, setCopied] = React.useState<boolean>(false)
+  const [copiedHeight, setCopiedHeight] = React.useState<number>(0)
+  const [copiedWidth, setCopiedWidth] = React.useState<number>(0)
+  let timeoutId = undefined
+  const copiedTextRef = React.useRef<HTMLElement>(null)
 
-  static defaultProps = {
-    timeout: 4000,
-    attached: false,
-    pointing: false,
-  }
-
-  handleClick = () => {
-    if (this.timeoutId !== undefined) {
-      clearTimeout(this.timeoutId)
+  const handleClick = () => {
+    if (timeoutId !== undefined) {
+      clearTimeout(timeoutId)
     }
-    const { timeout, value } = this.props
-    this.setState({
-      copied: true,
-    })
-    this.timeoutId = setTimeout(() => {
-      this.setState({ copied: false })
+    const { timeout, value } = props
+    setCopied(true)
+    timeoutId = setTimeout(() => {
+      setCopied(false)
     }, timeout)
     copyToClipboard(value)
   }
 
-  componentDidMount() {
-    if (this.copiedTextRef.current === null) {
+  React.useEffect(() => {
+    if (copiedTextRef.current === null) {
       return
     }
-    this.setState({
-      copiedHeight: this.copiedTextRef.current.clientHeight,
-      copiedWidth: this.copiedTextRef.current.clientWidth,
-    })
+    setCopiedHeight(copiedTextRef.current.clientHeight)
+    setCopiedWidth(copiedTextRef.current.clientWidth)
+  })
+
+  const { attached, pointing, copyPrompt } = props
+  const hideContent = copied && !attached
+  const copiedStr = 'Copied to clipboard'
+  const copiedText = hideContent ? (
+    <Text size="larger" styles={{ padding: '5px 5px 5px 5px' }}>
+      {copiedStr}
+    </Text>
+  ) : (
+    <Text>{copiedStr}</Text>
+  )
+  const copyText = <Text>{copyPrompt}</Text>
+  let popupContent = copied ? copiedText : copyText
+  if (hideContent) {
+    popupContent = <Text />
+  }
+  const button = (
+    <Button
+      icon="clipboard-copied-to"
+      iconOnly
+      onClick={handleClick}
+      variables={siteVariables => ({
+        colorHover: !copied && siteVariables.colors.brand[400],
+      })}
+    />
+  )
+  const hiddenPopupVariables = {
+    contentBackgroundColor: 'transparent',
+  }
+  const hiddenPopupContentVariables = {
+    boxShadow: 'none',
+    borderColor: 'transparent',
+  }
+  const portalContentStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'fixed' as 'fixed',
+    backgroundColor: 'black',
+    color: 'white',
+    zIndex: 1000,
+    top: `calc(50% - ${copiedHeight / 2}px)`,
+    left: `calc(50% - ${copiedWidth / 2}px)`,
   }
 
-  render() {
-    const { attached, pointing, copyPrompt } = this.props
-    const hideContent = this.state.copied && !attached
-    const copiedStr = 'Copied to clipboard'
-    const copiedText = hideContent ? (
-      <Text size="larger" styles={{ padding: '5px 5px 5px 5px' }}>
-        {copiedStr}
-      </Text>
-    ) : (
-      <Text>{copiedStr}</Text>
-    )
-    const copyText = <Text>{copyPrompt}</Text>
-    let popupContent = this.state.copied ? copiedText : copyText
-    if (hideContent) {
-      popupContent = <Text />
-    }
-    const button = (
-      <Button
-        icon="clipboard-copied-to"
-        iconOnly
-        onClick={this.handleClick}
+  return (
+    <>
+      <Popup
+        pointing={pointing}
+        position="below"
+        align="center"
+        trigger={button}
+        content={{
+          content: popupContent,
+          variables: {
+            ...(hideContent && hiddenPopupContentVariables),
+          },
+        }}
+        on={['hover', 'context']}
         variables={siteVariables => ({
-          colorHover: !this.state.copied && siteVariables.colors.brand[400],
+          contentColor: copied
+            ? siteVariables.colorScheme.brand.foreground4
+            : siteVariables.colors.black,
+          contentBackgroundColor: copied
+            ? siteVariables.colorScheme.brand.background4
+            : siteVariables.colors.white,
+          ...(hideContent && hiddenPopupVariables),
         })}
       />
-    )
-    const hiddenPopupVariables = {
-      contentBackgroundColor: 'transparent',
-    }
-    const hiddenPopupContentVariables = {
-      boxShadow: 'none',
-      borderColor: 'transparent',
-    }
-    const portalContentStyle = {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      position: 'fixed' as 'fixed',
-      backgroundColor: 'black',
-      color: 'white',
-      zIndex: 1000,
-      top: `calc(50% - ${this.state.copiedHeight / 2}px)`,
-      left: `calc(50% - ${this.state.copiedWidth / 2}px)`,
-    }
-    return (
-      <>
-        <Popup
-          pointing={pointing}
-          position="below"
-          align="center"
-          trigger={button}
-          content={{
-            content: popupContent,
-            variables: {
-              ...(hideContent && hiddenPopupContentVariables),
-            },
-          }}
-          on={['hover', 'context']}
-          variables={siteVariables => ({
-            contentColor: this.state.copied
-              ? siteVariables.colorScheme.brand.foreground4
-              : siteVariables.colors.black,
-            contentBackgroundColor: this.state.copied
-              ? siteVariables.colorScheme.brand.background4
-              : siteVariables.colors.white,
-            ...(hideContent && hiddenPopupVariables),
-          })}
+      {hideContent && (
+        <Portal
+          open={true}
+          content={
+            <div style={portalContentStyle}>
+              <Ref innerRef={copiedTextRef}>{copiedText}</Ref>
+            </div>
+          }
         />
-        {hideContent && (
-          <Portal
-            open={true}
-            content={
-              <div style={portalContentStyle}>
-                <Ref innerRef={this.copiedTextRef}>{copiedText}</Ref>
-              </div>
-            }
-          />
-        )}
-      </>
-    )
-  }
+      )}
+    </>
+  )
+}
+
+CopyToClipboard.defaultProps = {
+  timeout: 4000,
+  attached: false,
+  pointing: false,
 }
 
 export default CopyToClipboard
