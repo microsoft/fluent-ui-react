350db3a029c8fa5c19ec602a20b61bbf5ab7b180
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2019-09-02 10:09:39 +0200

    initial commit

---
+++ packages/react/src/themes/teams-dark/components/Input/inputVariables.ts
@@ -0,0 +1,5 @@
+import { InputVariables } from '../../../base/components/Input/inputVariables'
+
+export default (siteVars): Partial<InputVariables> => ({
+  backgroundColor: siteVars.colors.grey[650],
+})
