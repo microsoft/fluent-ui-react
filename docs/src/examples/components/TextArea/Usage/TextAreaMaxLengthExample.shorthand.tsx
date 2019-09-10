150b978eb94aab55e3ec0b5b19161615e8aea9e8
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2019-09-10 16:47:18 +0200

    add notice on limited number of characters

--- docs/src/examples/components/TextArea/Usage/TextAreaMaxLengthExample.shorthand.tsx
+++ docs/src/examples/components/TextArea/Usage/TextAreaMaxLengthExample.shorthand.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react'
 import { TextArea } from '@stardust-ui/react'
 
+const maxLength = 5
+
 const TextAreaMaxLengthExample = () => (
-  <TextArea placeholder="Try to type something..." maxLength={5} />
+  <>
+    <TextArea
+      placeholder={`No more than ${maxLength} letters can be here...`}
+      maxLength={maxLength}
+    />
+  </>
 )
 
 export default TextAreaMaxLengthExample
