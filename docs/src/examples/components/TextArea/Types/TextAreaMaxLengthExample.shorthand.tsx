e0973f48b7a75b242758966369b2a23b7b78585e
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2019-09-10 11:19:47 +0200

    fix example

--- docs/src/examples/components/TextArea/Types/TextAreaMaxLengthExample.shorthand.tsx
+++ docs/src/examples/components/TextArea/Types/TextAreaMaxLengthExample.shorthand.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { TextArea } from '@stardust-ui/react'
 
 const TextAreaMaxLengthExample = () => (
-  <TextArea placeholder="Try to type something..." maxLength={5} />
+  <TextArea placeholder="Try to type something..." maxlength={5} />
 )
 
 export default TextAreaMaxLengthExample
