9571bde62bf0bd54e35eeb8b4f20d2e113061441
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2019-09-10 18:19:50 +0200

    inline constants

--- docs/src/examples/components/TextArea/Usage/TextAreaMaxLengthExample.shorthand.tsx
+++ docs/src/examples/components/TextArea/Usage/TextAreaMaxLengthExample.shorthand.tsx
@@ -1,14 +1,9 @@
 import * as React from 'react'
 import { TextArea } from '@stardust-ui/react'
 
-const maxLength = 5
-
 const TextAreaMaxLengthExample = () => (
   <>
-    <TextArea
-      placeholder={`No more than ${maxLength} letters can be here...`}
-      maxLength={maxLength}
-    />
+    <TextArea placeholder={`No more than 5 letters can be here...`} maxLength={5} />
   </>
 )
 
--- packages/react/test/specs/components/TextArea/TextArea-test.tsx
+++ packages/react/test/specs/components/TextArea/TextArea-test.tsx
@@ -3,31 +3,29 @@ import { mountWithProvider as mount } from 'test/utils'
 import TextArea from 'src/components/TextArea/TextArea'
 import { isConformant } from 'test/specs/commonTests'
 
-const testValue = 'test value'
-
 describe('TextArea', () => {
   isConformant(TextArea)
 
   describe('defaultValue', () => {
     test('sets "defaultValue" as initial "value"', () => {
-      const wrapper = mount(<TextArea defaultValue={testValue} />)
+      const wrapper = mount(<TextArea defaultValue={'foo'} />)
 
-      expect(wrapper.find('textarea').prop('value')).toBe(testValue)
+      expect(wrapper.find('textarea').prop('value')).toBe('foo')
     })
   })
 
   describe('auto-controlled', () => {
     it('sets TextArea value from user when the value prop is not set (non-controlled mode)', () => {
       const wrapper = mount(<TextArea />)
-      wrapper.simulate('change', { target: { value: testValue } })
+      wrapper.simulate('change', { target: { value: 'foo' } })
 
-      expect(wrapper.find('textarea').prop('value')).toBe(testValue)
+      expect(wrapper.find('textarea').prop('value')).toBe('foo')
     })
 
     it('cannot set TextArea value from user when the value prop is already set (controlled mode)', () => {
       const controlledTextAreaValue = 'controlled TextArea value'
       const wrapper = mount(<TextArea value={controlledTextAreaValue} />)
-      wrapper.simulate('change', { target: { value: testValue } })
+      wrapper.simulate('change', { target: { value: 'foo' } })
 
       expect(wrapper.find('textarea').prop('value')).toBe(controlledTextAreaValue)
     })
@@ -38,11 +36,11 @@ describe('TextArea', () => {
       const onChange = jest.fn()
       const wrapper = mount(<TextArea onChange={onChange} />)
 
-      wrapper.simulate('change', { target: { value: testValue } })
+      wrapper.simulate('change', { target: { value: 'foo' } })
       expect(onChange).toBeCalledTimes(1)
       expect(onChange).toBeCalledWith(
         expect.objectContaining({ type: 'change' }),
-        expect.objectContaining({ value: testValue }),
+        expect.objectContaining({ value: 'foo' }),
       )
     })
   })
