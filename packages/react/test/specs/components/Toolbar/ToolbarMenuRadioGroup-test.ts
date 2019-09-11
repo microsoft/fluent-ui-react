e279c26030123327bb3be836f39f7eb522492240
Oleksandr Fediashov <olfedias@microsoft.com>
2019-09-11 17:23:26 +0200

    add UT

---
+++ packages/react/test/specs/components/Toolbar/ToolbarMenuRadioGroup-test.ts
@@ -0,0 +1,6 @@
+import { isConformant } from 'test/specs/commonTests'
+import ToolbarMenuRadioGroup from 'src/components/Toolbar/ToolbarMenuRadioGroup'
+
+describe('ToolbarMenuDivider', () => {
+  isConformant(ToolbarMenuRadioGroup)
+})
