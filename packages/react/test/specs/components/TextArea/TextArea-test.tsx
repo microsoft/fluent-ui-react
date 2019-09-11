5fa604970d84b41dcc1aa5ab64481a8554927369
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2019-09-11 11:09:32 +0200

    remove useless tests

--- packages/react/test/specs/components/TextArea/TextArea-test.tsx
+++ packages/react/test/specs/components/TextArea/TextArea-test.tsx
@@ -16,24 +16,6 @@ describe('TextArea', () => {
     })
   })
 
-  describe('auto-controlled', () => {
-    it('sets TextArea value from user when the value prop is not set (non-controlled mode)', () => {
-      const value = faker.lorem.words()
-      const wrapper = mount(<TextArea />)
-      wrapper.simulate('change', { target: { value } })
-
-      expect(wrapper.find('textarea').prop('value')).toBe(value)
-    })
-
-    it('cannot set TextArea value from user when the value prop is already set (controlled mode)', () => {
-      const controlledTextAreaValue = 'controlled TextArea value'
-      const wrapper = mount(<TextArea value={controlledTextAreaValue} />)
-      wrapper.simulate('change', { target: { value: 'foo' } })
-
-      expect(wrapper.find('textarea').prop('value')).toBe(controlledTextAreaValue)
-    })
-  })
-
   describe('onChange', () => {
     test('is called with (e, data)', () => {
       const value = faker.lorem.words()
