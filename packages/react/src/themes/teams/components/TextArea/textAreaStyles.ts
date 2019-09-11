7d65f2f556c54752a652830bd97bb22b0aecbf0b
manajdov <manajdov@microsoft.com>
2019-09-11 12:13:34 -0700

    -fixed import

--- packages/react/src/themes/teams/components/TextArea/textAreaStyles.ts
+++ packages/react/src/themes/teams/components/TextArea/textAreaStyles.ts
@@ -1,6 +1,6 @@
 import { TextAreaVariables } from './textAreaVariables'
 import { ComponentSlotStylesInput, ICSSInJSStyle } from '../../../types'
-import { TextAreaProps } from 'packages/react/src/components/TextArea/TextArea'
+import { TextAreaProps } from '../../../../components/TextArea/TextArea'
 
 const textAreaStyles: ComponentSlotStylesInput<TextAreaProps, TextAreaVariables> = {
   root: ({ props: p, variables: v }): ICSSInJSStyle => ({
