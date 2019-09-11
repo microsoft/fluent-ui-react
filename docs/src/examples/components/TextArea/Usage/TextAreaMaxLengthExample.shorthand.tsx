a56c43fff92405d32aacd6a824579e75d5f85437
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2019-09-11 14:08:32 +0200

    try to fix prettier error

--- docs/src/examples/components/TextArea/Usage/TextAreaMaxLengthExample.shorthand.tsx
+++ docs/src/examples/components/TextArea/Usage/TextAreaMaxLengthExample.shorthand.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { TextArea } from '@stardust-ui/react'
 
 const TextAreaMaxLengthExample = () => (
-  <TextArea placeholder='No more than 5 letters can be here...' maxLength={5} />
+  <TextArea placeholder="No more than 5 letters can be here..." maxLength={5} />
 )
 
 export default TextAreaMaxLengthExample
