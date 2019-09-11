9ad2b3fbf0cfd53c06db86d57f00cd3b531078a5
manajdov <manajdov@microsoft.com>
2019-09-10 18:12:09 -0700

    -removed base theme tests for Checkbox component

--- docs/src/examples/components/Checkbox/Types/CheckboxExample.shorthand.steps.ts
+++ docs/src/examples/components/Checkbox/Types/CheckboxExample.shorthand.steps.ts
@@ -1,7 +1,6 @@
 import getScreenerSteps from '../commonScreenerSteps'
 
 export const config: ScreenerTestsConfig = {
-  themes: ['teams', 'teamsDark', 'teamsHighContrast'],
   steps: getScreenerSteps(),
 }
 
