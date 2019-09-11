6479901af552ca0b39712d0a1a7ceb90299f1e38
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2019-09-11 11:07:35 +0200

    use faker in tests

--- packages/react/test/specs/components/TextArea/TextArea-test.tsx
+++ packages/react/test/specs/components/TextArea/TextArea-test.tsx
@@ -2,24 +2,27 @@ import * as React from 'react'
 import { mountWithProvider as mount } from 'test/utils'
 import TextArea from 'src/components/TextArea/TextArea'
 import { isConformant } from 'test/specs/commonTests'
+import * as faker from 'faker'
 
 describe('TextArea', () => {
   isConformant(TextArea)
 
   describe('defaultValue', () => {
     test('sets "defaultValue" as initial "value"', () => {
-      const wrapper = mount(<TextArea defaultValue={'foo'} />)
+      const value = faker.lorem.words()
+      const wrapper = mount(<TextArea defaultValue={value} />)
 
-      expect(wrapper.find('textarea').prop('value')).toBe('foo')
+      expect(wrapper.find('textarea').prop('value')).toBe(value)
     })
   })
 
   describe('auto-controlled', () => {
     it('sets TextArea value from user when the value prop is not set (non-controlled mode)', () => {
+      const value = faker.lorem.words()
       const wrapper = mount(<TextArea />)
-      wrapper.simulate('change', { target: { value: 'foo' } })
+      wrapper.simulate('change', { target: { value } })
 
-      expect(wrapper.find('textarea').prop('value')).toBe('foo')
+      expect(wrapper.find('textarea').prop('value')).toBe(value)
     })
 
     it('cannot set TextArea value from user when the value prop is already set (controlled mode)', () => {
@@ -33,14 +36,15 @@ describe('TextArea', () => {
 
   describe('onChange', () => {
     test('is called with (e, data)', () => {
+      const value = faker.lorem.words()
       const onChange = jest.fn()
       const wrapper = mount(<TextArea onChange={onChange} />)
 
-      wrapper.simulate('change', { target: { value: 'foo' } })
+      wrapper.simulate('change', { target: { value } })
       expect(onChange).toBeCalledTimes(1)
       expect(onChange).toBeCalledWith(
         expect.objectContaining({ type: 'change' }),
-        expect.objectContaining({ value: 'foo' }),
+        expect.objectContaining({ value }),
       )
     })
   })
