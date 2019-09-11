5058d9af242744cfa0a18295f0e276f639adbc5d
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2019-09-11 14:49:43 +0200

    remove useless fragment

--- docs/src/components/ComponentDoc/UsageSection.tsx
+++ docs/src/components/ComponentDoc/UsageSection.tsx
@@ -7,11 +7,9 @@ const gridStyle: React.CSSProperties = {
 
 // minmax = prevent example overflow - https://stackoverflow.com/a/43312314
 const ExampleSection: React.FC = ({ children }) => (
-  <>
-    <Grid styles={gridStyle} variables={{ gridGap: '2rem' }} columns="minmax(550px, 1fr)">
-      {children}
-    </Grid>
-  </>
+  <Grid styles={gridStyle} variables={{ gridGap: '2rem' }} columns="minmax(550px, 1fr)">
+    {children}
+  </Grid>
 )
 
 export default ExampleSection
