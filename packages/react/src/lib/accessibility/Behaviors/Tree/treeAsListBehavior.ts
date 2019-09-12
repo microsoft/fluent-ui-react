492884d18ac2d8e33f86583c4274888d7ec90cea
silviuavram <silviu.avram91@gmail.com>
2019-09-12 15:07:36 +0200

    use default behaviors to create list ones

--- packages/react/src/lib/accessibility/Behaviors/Tree/treeAsListBehavior.ts
+++ packages/react/src/lib/accessibility/Behaviors/Tree/treeAsListBehavior.ts
@@ -1,39 +1,22 @@
-import * as keyboardKey from 'keyboard-key'
-import { Accessibility, AccessibilityAttributes, FocusZoneMode } from '../../types'
-import { FocusZoneDirection } from '../../FocusZone'
+import * as _ from 'lodash'
+import { Accessibility, AccessibilityAttributes } from '../../types'
+import treeBehavior from './treeBehavior'
 
 /**
  * @specification
- * Adds role 'tree' to 'root' slot.
- * Adds attribute 'aria-labelledby' based on the property 'aria-labelledby' to 'root' slot.
- * Embeds component into FocusZone.
- * Provides arrow key navigation in vertical direction.
- * Triggers 'expandSiblings' action with '*' on 'root'.
+ * Adds role 'list' to 'root' slot.
  */
-const treeBehavior: Accessibility<TreeBehaviorProps> = props => {
-  return {
+const treeAsListBehavior: Accessibility<TreeBehaviorProps> = props => {
+  const behavior = treeBehavior(props)
+  return _.merge(behavior, {
     attributes: {
       root: {
         role: 'list',
-        'aria-labelledby': props['aria-labelledby'],
       },
     },
-    keyActions: {
-      root: {
-        expandSiblings: {
-          keyCombinations: [{ keyCode: keyboardKey['*'] }],
-        },
-      },
-    },
-    focusZone: {
-      mode: FocusZoneMode.Embed,
-      props: {
-        direction: FocusZoneDirection.vertical,
-      },
-    },
-  }
+  })
 }
 
 type TreeBehaviorProps = {} & Pick<AccessibilityAttributes, 'aria-labelledby'>
 
-export default treeBehavior
+export default treeAsListBehavior
