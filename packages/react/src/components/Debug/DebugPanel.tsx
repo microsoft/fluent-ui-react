b52315111212efc752f6b3de8436ed9dec9e5c3c
manajdov <manajdov@microsoft.com>
2019-09-14 17:24:09 -0700

    -added styles panel -added search and highlighting of the styles

--- packages/react/src/components/Debug/DebugPanel.tsx
+++ packages/react/src/components/Debug/DebugPanel.tsx
@@ -3,19 +3,19 @@ import * as _ from 'lodash'
 import debugData from './debugData'
 import PortalInner from '../Portal/PortalInner'
 
-const DebigPanelitem = props => {
+const VariablesDebugPanel = props => {
   const { name, data } = props
   return (
     <div>
       <b>{`${_.upperCase(name)}:`}</b>
       <hr />
-      {data.map((theme, idx) => (
+      {data.reverse().map((theme, idx) => (
         <div key={idx} style={{ marginBottom: '10px' }}>
           {' '}
           {/* fix the key */}
           <i>{`Theme ${idx}`}</i>
           <br />
-          <p>
+          <pre>
             {'{'}
             <br />
             {Object.keys(theme.input).map(key => (
@@ -31,7 +31,7 @@ const DebigPanelitem = props => {
                     typeof theme.resolved[key] === 'string'
                       ? `"${theme.resolved[key]}"`
                       : theme.resolved[key]
-                  }`}{' '}
+                  }`}
                   {','}{' '}
                   {theme.input[key] !== theme.resolved[key] && (
                     <small style={{ color: '#999' }}>{`// ${theme.input[key]}`}</small>
@@ -41,9 +41,108 @@ const DebigPanelitem = props => {
               </>
             ))}
             {'}'}
-          </p>
+          </pre>
         </div>
       ))}
+    </div>
+  )
+}
+
+const Data = props => {
+  const { data, indent = 2, highlightKey } = props
+
+  if (typeof data !== 'object' || data === null) {
+    if (typeof data === 'string') {
+      return `"${data}"`
+    }
+    return data
+  }
+
+  return (
+    <>
+      {'{'}
+      <br />
+      {Object.keys(data).map(key => {
+        return (
+          <>
+            <span
+              key={key}
+              style={{
+                // ...(row.overriden && { textDecoration: 'line-through' }),
+                ...(highlightKey !== '' &&
+                  _.toLower(key).indexOf(_.toLower(highlightKey)) !== -1 && {
+                    backgroundColor: 'rgb(255,255,224)',
+                    color: 'black',
+                  }),
+              }}
+            >
+              {' '.repeat(indent)}
+              {`${key}: `}
+              <Data data={data[key]} indent={indent + 2} highlightKey={highlightKey} />
+              {','}{' '}
+            </span>
+            <br />
+          </>
+        )
+      })}
+      {`${indent > 2 ? ' '.repeat(indent - 2) : ''}}`}
+    </>
+  )
+}
+
+const StylesDebugPanel = props => {
+  const [value, setValue] = React.useState('')
+  const { name, data } = props
+
+  const filterR = (search, theme) => {
+    let result = false
+    // TODO add breaks
+    Object.keys(theme).forEach(key => {
+      if (_.toLower(key).indexOf(_.toLower(search)) !== -1) {
+        result = true
+      }
+      if (typeof theme[key] === 'object' && filterR(search, theme[key])) {
+        result = true
+      }
+    })
+
+    return result
+  }
+
+  return (
+    <div>
+      <b>{`${_.upperCase(name)}:`}</b>
+      <hr />
+      <input onChange={e => setValue(e.target.value)} style={{ width: '100%' }} />
+      {data.reverse().map((theme, idx) => {
+        const filteredTheme =
+          value === ''
+            ? theme
+            : Object.keys(theme)
+                .filter(key => {
+                  if (_.toLower(key).indexOf(_.toLower(value)) !== -1) {
+                    return true
+                  }
+                  if (typeof theme[key] === 'object' && theme[key] !== null) {
+                    return filterR(value, theme[key])
+                  }
+                  return false
+                })
+                .reduce((obj, key) => {
+                  obj[key] = theme[key]
+                  return obj
+                }, {})
+        return (
+          <div key={idx} style={{ marginBottom: '10px' }}>
+            {/* fix the key */}
+            <i>{`Theme ${idx}`}</i>
+            <br />
+            <pre>
+              <Data data={filteredTheme} highlightKey={value} />
+            </pre>
+          </div>
+        )
+      })}
     </div>
   )
 }
@@ -81,8 +180,8 @@ const DebugPanel = props => {
           }}
         >
           {/* <SiteVariables data={debugData.siteVariables}/> */}
-          <DebigPanelitem data={debugData.variables} name="variables" />
-          {/* <Styles data={debugData.styles}/> */}
+          <VariablesDebugPanel data={debugData.variables} name="variables" />
+          <StylesDebugPanel data={debugData.styles.root} name={'styles'} />
         </p>
       </div>
     </PortalInner>
