57b8ee72526fe3d341ae87da2b743eb0eb6a5889
manajdov <manajdov@microsoft.com>
2019-09-16 13:54:34 -0700

    -added dock left and right options

--- packages/react/src/components/Debug/DebugPanel.tsx
+++ packages/react/src/components/Debug/DebugPanel.tsx
@@ -3,37 +3,63 @@ import PortalInner from '../Portal/PortalInner'
 import VariablesDebugPanel from './VariablesDebugPanel'
 import StylesDebugPanel from './StylesDebugPanel'
 
+const debugPanelRoot = (left): React.CSSProperties => ({
+  position: 'fixed',
+  [left ? 'left' : 'right']: '0px',
+  bottom: '0px',
+  zIndex: 9999999,
+  width: '300px',
+  background: '#222',
+  color: '#EEE',
+  height: '100%',
+  marginLeft: '10px',
+  overflowY: 'auto',
+})
+
+const debugPanelIcon = (left, isLeftActive) => ({
+  display: 'inline-block',
+  border: '2px solid white',
+  [left ? 'borderLeftWidth' : 'borderRightWidth']: '5px',
+  width: '15px',
+  height: '15px',
+  ...(left && {
+    marginRight: '10px',
+  }),
+  ...(left === isLeftActive && {
+    borderColor: '#00b5ad',
+  }),
+})
+
+const debugPanelOptions: React.CSSProperties = {
+  float: 'right',
+  padding: '10px',
+}
+
+const debugPanelBody: React.CSSProperties = {
+  marginTop: '20px',
+  padding: '10px',
+  overflowWrap: 'break-word',
+  wordWrap: 'break-word',
+  wordBreak: 'break-all',
+  hyphens: 'auto',
+}
+
 const DebugPanel = props => {
+  const [left, setLeft] = React.useState(false)
   const { debugData } = props
 
   const node = document.createElement('div')
   document.body.parentElement.appendChild(node)
   document.body.setAttribute('style', 'width: 80%')
+
   return (
     <PortalInner mountNode={node}>
-      <div
-        style={{
-          position: 'fixed',
-          right: '0px',
-          bottom: '0px',
-          zIndex: 9999999,
-          width: '300px',
-          background: '#222',
-          color: '#EEE',
-          height: '100%',
-          marginLeft: '10px',
-          overflowY: 'auto',
-        }}
-      >
-        <div
-          style={{
-            padding: '10px',
-            overflowWrap: 'break-word',
-            wordWrap: 'break-word',
-            wordBreak: 'break-all',
-            hyphens: 'auto',
-          }}
-        >
+      <div style={debugPanelRoot(left)}>
+        <div style={debugPanelOptions}>
+          <div style={debugPanelIcon(true, left)} onClick={e => setLeft(true)} />
+          <div style={debugPanelIcon(false, left)} onClick={e => setLeft(false)} />
+        </div>
+        <div style={debugPanelBody}>
           {/* <SiteVariablesDebugPanel data={debugData.siteVariables}/> */}
           <VariablesDebugPanel data={debugData.variables} name="variables" />
           <StylesDebugPanel data={debugData.styles.root} name={'styles'} />
