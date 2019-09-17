26a86389d5b6764e3dc49e977847eb8481067ea3
Oleksandr Fediashov <olfedias@microsoft.com>
2019-09-17 17:14:49 +0200

    wip!

--- packages/react/src/themes/teams-dark/components/Chat/chatMessageVariables.ts
+++ packages/react/src/themes/teams-dark/components/Chat/chatMessageVariables.ts
@@ -1,7 +1,12 @@
-import { ChatTimestampVariables } from '../../../teams/components/Chat/chatTimestampVariables'
+import { ChatMessageVariables } from '../../../teams/components/Chat/chatMessageVariables'
 
-export default (siteVars: any): Partial<ChatTimestampVariables> => {
+export default (siteVars: any): Partial<ChatMessageVariables> => {
   return {
-    mineColor: siteVars.colors.grey[250],
+    backgroundColor: siteVars.colors.grey[600],
+    backgroundColorMine: siteVars.colors.brand[900],
+    contentColor: siteVars.colors.white,
+    color: siteVars.colors.white,
+    hasMentionNubbinColor: siteVars.colors.orange[300],
+    isImportantColor: siteVars.colors.red[300],
   }
 }
