94c790590d538f9334e631985f854816db90db58
Oleksandr Fediashov <olfedias@microsoft.com>
2019-09-18 11:53:37 +0200

    feat(Toolbar): add support for radio & checkbox groups (#1920)
    * wip!
    
    * wip
    
    * wip
    
    * add UT
    
    * wip!
    
    * wip!
    
    * add behaviors to tests
    
    * fix UTs
    
    * add CH
    
    * rename to `toggle`
    
    * rename to `toggle`
    
    * fix TS issues
    
    * nit fixes
    
    * fix CI
    
    * add role presentation on li to enable reader narrate position
    
    * restore type change
    
    * add UT, fix TS error
    
    * remove tabIndex
    
    * add activeIndex to ToolbarRadioGroup
    
    * remove test for `tabIndex`
    
    * rename to active

--- packages/react/src/themes/teams/componentVariables.ts
+++ packages/react/src/themes/teams/componentVariables.ts
@@ -85,6 +85,9 @@ export { default as ToolbarDivider } from './components/Toolbar/toolbarDividerVa
 export { default as ToolbarMenu } from './components/Toolbar/toolbarMenuVariables'
 export { default as ToolbarMenuDivider } from './components/Toolbar/toolbarMenuDividerVariables'
 export { default as ToolbarMenuItem } from './components/Toolbar/toolbarMenuItemVariables'
+export {
+  default as ToolbarMenuRadioGroup,
+} from './components/Toolbar/toolbarMenuRadioGroupVariables'
 
 export { default as TreeTitle } from './components/Tree/treeTitleVariables'
 
