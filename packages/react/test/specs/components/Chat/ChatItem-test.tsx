3b303c8609a0e66063c1c04ba02b8cec5a67daed
Oleksandr Fediashov <olfedias@microsoft.com>
2019-09-18 11:59:53 +0200

    fix more UTs

--- packages/react/test/specs/components/Chat/ChatItem-test.tsx
+++ packages/react/test/specs/components/Chat/ChatItem-test.tsx
@@ -1,12 +1,13 @@
-import { isConformant, implementsShorthandProp } from 'test/specs/commonTests'
+import ChatGutter from 'src/components/Chat/ChatGutter'
 import ChatItem from 'src/components/Chat/ChatItem'
-import Box from 'src/components/Box/Box'
+import ChatMessage from 'src/components/Chat/ChatMessage'
+import { isConformant, implementsShorthandProp } from 'test/specs/commonTests'
 
 const chatItemImplementsShorthandProp = implementsShorthandProp(ChatItem)
 
 describe('ChatItem', () => {
   isConformant(ChatItem)
 
-  chatItemImplementsShorthandProp('gutter', Box, { mapsValueToProp: 'children' })
-  chatItemImplementsShorthandProp('message', Box, { mapsValueToProp: 'children' })
+  chatItemImplementsShorthandProp('gutter', ChatGutter)
+  chatItemImplementsShorthandProp('message', ChatMessage)
 })
