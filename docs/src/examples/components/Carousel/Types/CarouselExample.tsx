43492072fcf937f0073e058f469d4cb4273291b3
Silviu Avram <silviu.avram91@gmail.com>
2019-09-26 17:22:40 +0300

    add Carousel example

---
+++ docs/src/examples/components/Carousel/Types/CarouselExample.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import { Carousel } from '@stardust-ui/react'
+
+const carouselItems = [{ content: 'Carousel' }]
+
+const CarouselExample = () => <Carousel items={carouselItems} />
+
+export default CarouselExample
