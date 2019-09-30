3a7ec6a4f0e1013f5432b29079c58170b43bf64c
Levi Thomason <me@levithomason.com>
2019-09-30 11:34:50 -0700

    fix current issues, add vertical menu problem

--- docs/src/prototypes/customScrollbar/index.tsx
+++ docs/src/prototypes/customScrollbar/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import * as _ from 'lodash'
 import Scrollbars from 'react-custom-scrollbars'
-import { Text, Menu, List, Button, Popup, Dialog } from '@stardust-ui/react'
+import { Text, Menu, List, Button, Popup, Dialog, Dropdown } from '@stardust-ui/react'
 import { PrototypeSection, ComponentPrototype } from '../Prototypes'
 
 const ScrollbarMenuPrototype = () => {
@@ -9,20 +9,10 @@ const ScrollbarMenuPrototype = () => {
     {
       key: 'with-scrollbar',
       content: 'Submenu with scrollbar',
-      menu: render =>
-        render(
-          {
-            items: _.range(50).map((i: number) => `Menu Item No. ${i}`),
-          },
-          (ComponentType, componentProps) => {
-            const { style, ...rest } = componentProps
-            return (
-              <Scrollbars style={{ ...style, height: '20rem' }}>
-                <ComponentType {...rest} />
-              </Scrollbars>
-            )
-          },
-        ),
+      menu: {
+        as: props => <Menu {...props} as={Scrollbars} style={{ height: '20rem' }} />,
+        items: _.range(50).map((i: number) => `Menu Item No. ${i}`),
+      },
     },
     {
       key: 'without-scrollbar',
@@ -34,6 +24,31 @@ const ScrollbarMenuPrototype = () => {
   return <Menu items={items} />
 }
 
+const ScrollbarMenuVerticalPrototype = () => {
+  const items = [
+    {
+      key: 'with-scrollbar',
+      content: 'Submenu with scrollbar',
+      menu: {
+        items: _.range(50).map((i: number) => `Menu Item No. ${i}`),
+      },
+    },
+    {
+      key: 'without-scrollbar',
+      content: 'Submenu without scrollbar',
+      menu: _.range(5).map((i: number) => `Menu Item No. ${i}`),
+    },
+  ].concat(_.range(50).map((i: number) => `Menu Item No. ${i}`) as any)
+
+  return (
+    <Menu
+      as={props => <Menu {...props} as={Scrollbars} style={{ width: '20rem', height: '20rem' }} />}
+      vertical
+      items={items}
+    />
+  )
+}
+
 const ScrollbarPopupPrototype = () => {
   const lines = _.range(50).map((i: number) => <p key={i}>Long long text line {i}</p>)
 
@@ -41,6 +56,8 @@ const ScrollbarPopupPrototype = () => {
     <Popup
       trigger={<Button content="Open popup" />}
       content={{
+        // NOTE: because scrollbars uses an abs positioned container to fake scroll
+        //       the consumer must specify a width/height value to show the scrollable area
         styles: { width: '20rem' },
         content: <Scrollbars style={{ height: '20rem' }}>{lines}</Scrollbars>,
       }}
@@ -78,6 +95,16 @@ const ScrollbarListPrototype = () => {
   )
 }
 
+const ScrollbarDropdownPrototype = () => {
+  const items = _.range(50).map((i: number) => ({
+    header: `Header ${i}`,
+    content: `Content ${i}`,
+    key: `item-${i}`,
+  }))
+
+  return <Dropdown open items={items} />
+}
+
 const CustomScrollbarPrototypes: React.FC = () => {
   return (
     <PrototypeSection title="Custom Scrollbar">
@@ -85,8 +112,14 @@ const CustomScrollbarPrototypes: React.FC = () => {
         Note: Stardust does not provide custom scrollbars. It is possible to integrate Stardust
         components with any custom scrollbars framework.
       </Text>
+      <ComponentPrototype title="Dropdown" description="Scrollbar can be integrated in a Dropdown">
+        <ScrollbarDropdownPrototype />
+      </ComponentPrototype>
       <ComponentPrototype title="Menu" description="Scrollbar can be integrated in Menu">
         <ScrollbarMenuPrototype />
+      </ComponentPrototype>
+      <ComponentPrototype title="Menu Vertical" description="Scrollbar can be integrated in Menu">
+        <ScrollbarMenuVerticalPrototype />
       </ComponentPrototype>
       <ComponentPrototype title="Popup" description="Scrollbar can be integrated in Popup content">
         <ScrollbarPopupPrototype />
