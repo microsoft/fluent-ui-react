6fd7fb083812d48519f418bd565d603e28db9a4a
Juraj Kapsiar <jukapsia@microsoft.com>
2019-10-02 00:53:55 +0200

    comment not working pieces, fix menu

--- docs/src/prototypes/customScrollbar/index.tsx
+++ docs/src/prototypes/customScrollbar/index.tsx
@@ -1,18 +1,31 @@
 import * as React from 'react'
 import * as _ from 'lodash'
 import Scrollbars from 'react-custom-scrollbars'
-import { Text, Menu, List, Button, Popup, Dialog, Dropdown } from '@stardust-ui/react'
+import {
+  Text,
+  Menu,
+  List,
+  Button,
+  Popup,
+  Dialog,
+  ShorthandValue,
+  MenuProps,
+} from '@stardust-ui/react'
 import { PrototypeSection, ComponentPrototype } from '../Prototypes'
+
+const submenuWithScrollbars = (shorthand: ShorthandValue<MenuProps>, height: string) => {
+  return render => render(shorthand, (C, p) => <C {...p} as={Scrollbars} style={{ height }} />)
+}
 
 const ScrollbarMenuPrototype = () => {
   const items = [
     {
       key: 'with-scrollbar',
       content: 'Submenu with scrollbar',
-      menu: {
-        as: props => <Menu {...props} as={Scrollbars} style={{ height: '20rem' }} />,
-        items: _.range(50).map((i: number) => `Menu Item No. ${i}`),
-      },
+      menu: submenuWithScrollbars(
+        { items: _.range(50).map((i: number) => `Menu Item No. ${i}`) },
+        '20rem',
+      ),
     },
     {
       key: 'without-scrollbar',
@@ -24,30 +37,30 @@ const ScrollbarMenuPrototype = () => {
   return <Menu items={items} />
 }
 
-const ScrollbarMenuVerticalPrototype = () => {
-  const items = [
-    {
-      key: 'with-scrollbar',
-      content: 'Submenu with scrollbar',
-      menu: {
-        items: _.range(50).map((i: number) => `Menu Item No. ${i}`),
-      },
-    },
-    {
-      key: 'without-scrollbar',
-      content: 'Submenu without scrollbar',
-      menu: _.range(5).map((i: number) => `Menu Item No. ${i}`),
-    },
-  ].concat(_.range(50).map((i: number) => `Menu Item No. ${i}`) as any)
+// const ScrollbarMenuVerticalPrototype = () => {
+//   const items = [
+//     {
+//       key: 'with-scrollbar',
+//       content: 'Submenu with scrollbar',
+//       menu: {
+//         items: _.range(50).map((i: number) => `Menu Item No. ${i}`),
+//       },
+//     },
+//     {
+//       key: 'without-scrollbar',
+//       content: 'Submenu without scrollbar',
+//       menu: _.range(5).map((i: number) => `Menu Item No. ${i}`),
+//     },
+//   ].concat(_.range(50).map((i: number) => `Menu Item No. ${i}`) as any)
 
-  return (
-    <Menu
-      as={props => <Menu {...props} as={Scrollbars} style={{ width: '20rem', height: '20rem' }} />}
-      vertical
-      items={items}
-    />
-  )
-}
+//   return (
+//     <Menu
+//       as={props => <Menu {...props} as={Scrollbars} style={{ width: '20rem', height: '20rem' }} />}
+//       vertical
+//       items={items}
+//     />
+//   )
+// }
 
 const ScrollbarPopupPrototype = () => {
   const lines = _.range(50).map((i: number) => <p key={i}>Long long text line {i}</p>)
@@ -95,15 +108,15 @@ const ScrollbarListPrototype = () => {
   )
 }
 
-const ScrollbarDropdownPrototype = () => {
-  const items = _.range(50).map((i: number) => ({
-    header: `Header ${i}`,
-    content: `Content ${i}`,
-    key: `item-${i}`,
-  }))
+// const ScrollbarDropdownPrototype = () => {
+//   const items = _.range(50).map((i: number) => ({
+//     header: `Header ${i}`,
+//     content: `Content ${i}`,
+//     key: `item-${i}`,
+//   }))
 
-  return <Dropdown open items={items} />
-}
+//   return <Dropdown open items={items} />
+// }
 
 const CustomScrollbarPrototypes: React.FC = () => {
   return (
@@ -112,15 +125,15 @@ const CustomScrollbarPrototypes: React.FC = () => {
         Note: Stardust does not provide custom scrollbars. It is possible to integrate Stardust
         components with any custom scrollbars framework.
       </Text>
-      <ComponentPrototype title="Dropdown" description="Scrollbar can be integrated in a Dropdown">
+      {/* <ComponentPrototype title="Dropdown" description="Scrollbar can be integrated in a Dropdown">
         <ScrollbarDropdownPrototype />
-      </ComponentPrototype>
+      </ComponentPrototype> */}
       <ComponentPrototype title="Menu" description="Scrollbar can be integrated in Menu">
         <ScrollbarMenuPrototype />
       </ComponentPrototype>
-      <ComponentPrototype title="Menu Vertical" description="Scrollbar can be integrated in Menu">
+      {/* <ComponentPrototype title="Menu Vertical" description="Scrollbar can be integrated in Menu">
         <ScrollbarMenuVerticalPrototype />
-      </ComponentPrototype>
+      </ComponentPrototype> */}
       <ComponentPrototype title="Popup" description="Scrollbar can be integrated in Popup content">
         <ScrollbarPopupPrototype />
       </ComponentPrototype>
