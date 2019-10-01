38f4c87f71e730e5c5b08ff8811d5686d531bb7d
Brian Calvery <bcalvery@microsoft.com>
2019-10-01 13:31:19 -0700

    fixing buttonVariables bugs in hc and dark

--- packages/react/src/themes/teams-dark/components/Button/buttonVariables.ts
+++ packages/react/src/themes/teams-dark/components/Button/buttonVariables.ts
@@ -4,10 +4,8 @@ export default (siteVars: any): Partial<ButtonVariables> => {
   return {
     color: siteVars.colors.white,
     colorHover: siteVars.colors.white,
-    colorFocus: siteVars.colors.white,
     backgroundColor: 'transparent',
     backgroundColorHover: siteVars.colors.grey[700],
-    backgroundColorFocus: siteVars.colors.grey[500],
     backgroundColorActive: siteVars.colors.grey[500],
     backgroundColorDisabled: siteVars.colors.grey[550],
     borderColor: siteVars.colors.grey[500],
@@ -15,13 +13,11 @@ export default (siteVars: any): Partial<ButtonVariables> => {
 
     primaryBackgroundColorActive: siteVars.colors.brand[500],
     primaryBackgroundColorHover: siteVars.colors.brand[500],
-    primaryBackgroundColorFocus: siteVars.colors.brand[800],
 
     circularColor: siteVars.colors.grey[250],
     circularColorActive: siteVars.colors.grey[800],
     circularBackgroundColorActive: siteVars.colors.grey[250],
     circularBackgroundColorHover: siteVars.colors.grey[300],
-    circularBackgroundColorFocus: siteVars.colors.grey[250],
     circularBorderColor: siteVars.colors.grey[250],
   }
 }
--- packages/react/src/themes/teams-high-contrast/components/Button/buttonVariables.ts
+++ packages/react/src/themes/teams-high-contrast/components/Button/buttonVariables.ts
@@ -6,18 +6,15 @@ export default (siteVars: any): Partial<ButtonVariables> => {
     backgroundColor: 'transparent',
     backgroundColorActive: siteVars.accessibleYellow,
     backgroundColorHover: siteVars.accessibleYellow,
-    backgroundColorFocus: siteVars.accessibleYellow,
     backgroundColorDisabled: siteVars.accessibleGreen,
     borderColor: siteVars.colors.white,
     borderColorHover: 'transparent',
 
     primaryColor: siteVars.colors.white,
     primaryColorHover: siteVars.colors.black,
-    primaryColorFocus: siteVars.colors.black,
     primaryBackgroundColor: siteVars.colors.black,
     primaryBackgroundColorActive: siteVars.accessibleYellow,
     primaryBackgroundColorHover: siteVars.accessibleYellow,
-    primaryBackgroundColorFocus: siteVars.accessibleYellow,
     primaryBorderColor: siteVars.colors.white,
 
     circularColor: siteVars.colors.white,
@@ -25,10 +22,8 @@ export default (siteVars: any): Partial<ButtonVariables> => {
     circularBackgroundColor: siteVars.colors.black,
     circularBackgroundColorActive: siteVars.accessibleYellow,
     circularBackgroundColorHover: siteVars.accessibleYellow,
-    circularBackgroundColorFocus: siteVars.accessibleYellow,
     circularBorderColor: siteVars.colors.white,
     circularBorderColorHover: siteVars.colors.white,
-    circularBorderColorFocus: siteVars.colors.white,
 
     boxShadow: 'none',
   }
