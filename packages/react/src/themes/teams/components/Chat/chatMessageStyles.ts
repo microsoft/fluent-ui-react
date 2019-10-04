4db861e0557f572518a18868628437ca97da7d29
Brigette Lundeen <brlundee@microsoft.com>
2019-10-04 16:54:11 -0700

    fixing error

--- packages/react/src/themes/teams/components/Chat/chatMessageStyles.ts
+++ packages/react/src/themes/teams/components/Chat/chatMessageStyles.ts
@@ -85,7 +85,7 @@ const chatMessageStyles: ComponentSlotStylesPrepared<
   actionMenu: ({ props: p, variables: v }): ICSSInJSStyle => ({
     backgroundColor: v.backgroundColor,
     border: '1px solid',
-    borderColor: v.reactionGroupBorder,
+    borderColor: v.reactionGroupBorderColor,
     borderRadius: v.borderRadius,
     boxShadow: v.actionMenuBoxShadow,
     // we need higher zIndex for the action menu in order to be displayed above the focus border of the chat message
