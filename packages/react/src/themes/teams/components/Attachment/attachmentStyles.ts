ee11359b4b885a3e4aeaedc129b7de272e8ae0c8
Andrew Martin <andmarti@microsoft.com>
2019-10-29 13:47:30 -0700

    updating colors

--- packages/react/src/themes/teams/components/Attachment/attachmentStyles.ts
+++ packages/react/src/themes/teams/components/Attachment/attachmentStyles.ts
@@ -32,7 +32,7 @@ const attachmentStyles: ComponentSlotStylesPrepared<AttachmentProps, AttachmentV
       cursor: 'pointer',
 
       ':hover': {
-        background: siteVariables.colorScheme.default.backgroundHover1,
+        background: v.backgroundColorHover,
         color: v.textColorHover,
       },
     }),
