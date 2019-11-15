b40ae4fa0a3a235e86b6676158cdfb0d11bc719d
Andrew Martin <andmarti@microsoft.com>
2019-11-15 15:35:13 -0800

    adding small documentation

---
+++ docs/src/examples/components/SplitButton/Types/SplitButtonExampleSmall.shorthand.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import { SplitButton } from '@stardust-ui/react'
+
+const SplitButtonExampleSmallShorthand = () => (
+  <>
+    <SplitButton
+      menu={[
+        { key: 'group', content: 'New group message' },
+        { key: 'channel', content: 'New channel message' },
+      ]}
+      button={{
+        content: 'small',
+        'aria-roledescription': 'splitbutton',
+        'aria-describedby': 'instruction-message',
+      }}
+      small
+      toggleButton={{ 'aria-label': 'options' }}
+      onMainButtonClick={() => alert('button was clicked')}
+    />
+    <span aria-hidden="true" id="instruction-message" style={{ opacity: 0 }}>
+      to open menu, press Alt + Arrow Down
+    </span>
+  </>
+)
+
+export default SplitButtonExampleSmallShorthand
