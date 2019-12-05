69b94586aea4295570a9d829d3ac7f1e23d568f3
JD Huntington <jdh@microsoft.com>
2019-12-05 10:14:41 -0800

    rendering something from the context.

--- docs/src/examples/components/Provider/Types/ProviderShared.tsx
+++ docs/src/examples/components/Provider/Types/ProviderShared.tsx
@@ -1,14 +1,34 @@
 import { Fabric, PrimaryButton } from 'office-ui-fabric-react'
+import { Provider } from '@fluentui/react'
 import * as React from 'react'
 
+/*
+const ProviderShared = () => (
+  <Provider.Consumer
+    render={(ctx: any) => (
+      <div>
+        <pre>{JSON.stringify(ctx.theme, null, 2)}</pre>
+        <div>
+          <Fabric>
+            <PrimaryButton text="just a button" />
+          </Fabric>
+        </div>
+      </div>
+    )}
+  />
+)
+*/
 const ProviderShared = () => (
   <div>
-    {' '}
+    <Provider.Consumer
+      render={theme => <code>theme.siteVariables.brand = {theme.siteVariables.bodyColor}</code>}
+    />
+
     <div>
       <Fabric>
         <PrimaryButton text="just a button" />
       </Fabric>
-    </div>{' '}
+    </div>
   </div>
 )
 
