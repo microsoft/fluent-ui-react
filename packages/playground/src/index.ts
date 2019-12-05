4d5772ebdd023821a9e9bfc850a1dad45646a461
Ken Chau <kchau@microsoft.com>
2019-12-05 13:46:08 -0800

    @types/react match version

--- packages/playground/src/index.ts
+++ packages/playground/src/index.ts
@@ -1,6 +1,5 @@
 export * from './components/Button/Button.base'
 export * from './components/Link/Link.base'
 export * from './components/Link/Link'
-export * from './components/NativeSlider/NativeSlider'
 export * from './components/Slider/Slider.base'
 export * from './components/Slider/Slider'
