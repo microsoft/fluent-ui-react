b98e8a06c218173bec52adf1db3976ab29300c1f
Ken Chau <kchau@microsoft.com>
2019-12-05 13:27:26 -0800

    moves playground into main

---
+++ packages/playground/.eslintrc.js
@@ -0,0 +1,2 @@
+const config = require('@fluentui/scripts/config/eslint/eslint.config');
+module.exports = config;
---
+++ packages/playground/src/index.ts
@@ -0,0 +1,6 @@
+export * from './components/Button/Button.base'
+export * from './components/Link/Link.base'
+export * from './components/Link/Link'
+export * from './components/NativeSlider/NativeSlider'
+export * from './components/Slider/Slider.base'
+export * from './components/Slider/Slider'
