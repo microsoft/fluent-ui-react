6a5715edaade1533930e884babc755c1c24ed8b9
Milan Turon <mituron@microsoft.com>
2019-12-06 15:44:34 +0100

    improving the doc

--- docs/src/examples/components/Table/BestPractices/TableBestPractices.tsx
+++ docs/src/examples/components/Table/BestPractices/TableBestPractices.tsx
@@ -9,18 +9,15 @@ const doList = [
     Choose desired accessibility behavior for grid cell depending on the use case.(Check the{' '}
     {link('Behaviors', '/components/table/accessibility')} section).
   </Text>,
-  'Provide label to the Table component using `aria-label` or `aria-labelledby` prop.',
-  'Provide label to the Row component using `aria-label` or `aria-labelledby` prop. If not, then each cell of the row is narrated by screen reader.',
+  'Provide label to the Table component using `aria-label` or `aria-labelledby` prop. If no label is provided, the whole header will be used as label for the table.',
+  'Consider providing label to the `TableRow` component using `aria-label` or `aria-labelledby` prop. When not provided, the whole row (all cells) will ne narrated by the screen reader when the user navigates to the row.',
   'Provide label to the table header column, if cell has no content.',
-  'Stop event propagation, when you will add actionable element into the grid cell.',
-]
-
-const dontList = [
-  "Don't set onClick action on row, which will be not available in the cell of grid, as well. With screen reader navigation user can navigate only cell by cell.",
+  'If you use actionable element or component in the cell, stop propagation of the click event in order to avoid performing row click action.',
+  'If you use `onClick` on the row, make also one (preferably the first) cell actionable and add `onClick` handler with the same action to it. Screen readers with virtual cursor do not navigate to rows, so actions need to be available on cells as well.',
 ]
 
 const TableBestPractices = () => {
-  return <ComponentBestPractices doList={doList} dontList={dontList} />
+  return <ComponentBestPractices doList={doList} />
 }
 
 export default TableBestPractices
