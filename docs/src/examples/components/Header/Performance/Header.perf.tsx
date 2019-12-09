dd494d406415e5399ab3cdf8b89441a3999e09ef
Jason Gore <jagore@microsoft.com>
2019-12-09 12:16:58 -0800

    Various perf improvements: table changes, automatic consumption of new perf stories, etc. (#2157)

--- docs/src/examples/components/Header/Performance/Header.perf.tsx
+++ docs/src/examples/components/Header/Performance/Header.perf.tsx
@@ -3,4 +3,6 @@ import * as React from 'react'
 
 const HeaderPerf = () => <Header as="h2" content="Account Settings" />
 
+HeaderPerf.iterations = 5000
+
 export default HeaderPerf
--- packages/perf-test/stories/Button.perf.tsx
+++ packages/perf-test/stories/Button.perf.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react'
-import { Button as ButtonFabric } from 'office-ui-fabric-react'
+import { DefaultButton as ButtonFabric } from 'office-ui-fabric-react'
 import { Button as ButtonFluent } from '@fluentui/react'
+
+export default {
+  iterations: 5000,
+}
 
 export const Fabric = () => <ButtonFabric />
 export const Fluent = () => <ButtonFluent />
