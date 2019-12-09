4eb973b11710a1cb8d807046e00a1606be898750
Jason Gore <jagore@microsoft.com>
2019-12-08 20:37:13 -0800

    Various perf improvements: table changes, automatic consumption of new perf stories, etc.

--- packages/perf-test/stories/Slider.perf.tsx
+++ packages/perf-test/stories/Slider.perf.tsx
@@ -2,5 +2,9 @@ import * as React from 'react'
 import { Slider as SliderFabric } from 'office-ui-fabric-react'
 import { Slider as SliderFluent } from '@fluentui/react'
 
+export default {
+  iterations: 5000,
+}
+
 export const Fabric = () => <SliderFabric />
 export const Fluent = () => <SliderFluent />
