e40c59eecf649323d517cacea6597c75006f56be
Miroslav Stastny <mistastn@microsoft.com>
2019-12-11 13:06:26 -0800

    perf: run flamegrill in CI (#2163)
    * store flamegrill results to json
    
    * run flamegrill in circleci
    
    * remove cruft console.log
    
    * store flamegrill stats to db
    
    * correctly name flamegrill data in db

--- build/gulp/tasks/stats.ts
+++ build/gulp/tasks/stats.ts
@@ -143,6 +143,34 @@ function readSummaryPerfStats() {
     .value()
 }
 
+function readFlamegrillStats() {
+  return require(paths.packageDist('perf-test', 'perfCounts.json'))
+}
+
+// 1. iterate over all perf-test results
+// 2. the ones which have filename are docsite perf examples
+//    -> use camelCase name (docsite perf examples convention)
+//    -> and merge yarn perf (summaryPerf) and yarn perf:test (flamegrill) data
+// 3. the others are perf-test only examples -> store
+function mergePerfStats(summaryPerfStats, perfTestStats) {
+  return _.transform(
+    perfTestStats,
+    (result, value, key: string) => {
+      const flamegrill = _.pick(value, ['profile.metrics', 'analysis', 'extended'])
+      if (value['extended'].filename) {
+        const docsiteFilename = _.camelCase(value['extended'].filename)
+        result[docsiteFilename] = {
+          ...summaryPerfStats[docsiteFilename],
+          flamegrill,
+        }
+      } else {
+        result[key.replace(/\./g, '_')] = { flamegrill } // mongodb does not allow dots in keys
+      }
+    },
+    {},
+  )
+}
+
 function readCurrentBundleStats() {
   return _.mapKeys(require(currentStatsFilePath), (value, key) => _.camelCase(key)) // mongodb does not allow dots in keys
 }
@@ -158,6 +186,9 @@ task('stats:save', async () => {
   )
   const bundleStats = readCurrentBundleStats()
   const perfStats = readSummaryPerfStats()
+  const flamegrillStats = readFlamegrillStats()
+
+  const mergedPerfStats = mergePerfStats(perfStats, flamegrillStats)
 
   const prUrl =
     process.env.CIRCLE_PULL_REQUEST ||
@@ -170,7 +201,7 @@ task('stats:save', async () => {
     build: process.env.BUILD_BUILDID || process.env.CIRCLE_BUILD_NUM,
     ...commandLineArgs, // allow command line overwrites
     bundleSize: bundleStats,
-    performance: perfStats,
+    performance: mergedPerfStats,
     ts: new Date(),
   }
 
