fe3d445fffa58a2d8159d904168d25297c63fe50
Oleksandr Fediashov <olfedias@microsoft.com>
2019-12-11 17:04:29 +0100

    use --no-sandbox

--- build/puppeteer.config.ts
+++ build/puppeteer.config.ts
@@ -5,6 +5,7 @@ const chromiumArgs: LaunchOptions['args'] = [
   '--js-flags=--expose-gc', // exposes "gc()" function
   // Workaround for newPage hang in CircleCI: https://github.com/GoogleChrome/puppeteer/issues/1409#issuecomment-453845568
   process.env.CI && '--single-process',
+  process.env.CI && '--no-sandbox',
 ].filter(Boolean)
 
 export const safeLaunchOptions = (launchOptions?: LaunchOptions) => {
