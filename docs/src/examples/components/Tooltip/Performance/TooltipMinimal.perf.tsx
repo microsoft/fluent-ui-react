3b3db636d892886d982ea31f97003f274aa61cd2
Levi Thomason <me@levithomason.com>
2019-12-10 22:53:01 -0800

    fix tooltip minimal example

--- docs/src/examples/components/Tooltip/Performance/TooltipMinimal.perf.tsx
+++ docs/src/examples/components/Tooltip/Performance/TooltipMinimal.perf.tsx
@@ -1,7 +1,11 @@
 import { Tooltip } from '@fluentui/react'
 import * as React from 'react'
 
-const TooltipMinimalPerf = () => <Tooltip />
+const TooltipMinimalPerf = () => (
+  <Tooltip>
+    <div />
+  </Tooltip>
+)
 
 TooltipMinimalPerf.iterations = 5000
 TooltipMinimalPerf.filename = 'TooltipMinimal.perf.tsx'
