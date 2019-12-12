1854a2195ba3f7d46ead90e869b369c5d019fdaa
Miroslav Stastny <mistastn@microsoft.com>
2019-12-12 11:00:52 -0800

    perf: Add Fluent to Fabric perf comparision to dangerjs

--- build/dangerjs/checkPerfRegressions.ts
+++ build/dangerjs/checkPerfRegressions.ts
@@ -1,7 +1,45 @@
-import config from '../../config'
+import * as _ from 'lodash'
 import { DangerJS } from './types'
+import config from '../../config'
 
-export default ({ danger, fail, warn }: DangerJS) => {
-  const perfCounts = require(config.paths.packageDist('perf-test', 'perfCounts.json'))
-  console.log('perfCounts', perfCounts)
+function fluentFabricComparision(danger, markdown, warn) {
+  let perfCounts
+  try {
+    perfCounts = require(config.paths.packageDist('perf-test', 'perfCounts.json'))
+  } catch {
+    warn('No perf measurements available')
+    return
+  }
+  const results = _.mapValues(
+    _.pickBy(perfCounts, (value, key) => key.endsWith('.Fluent')),
+    stats => {
+      const fluentTpi = _.get(stats, 'extended.tpi')
+      const fabricTpi = _.get(stats, 'extended.fabricTpi')
+      return {
+        numTicks: stats.analysis.numTicks,
+        iterations: stats.extended.iterations,
+        fluentTpi,
+        fabricTpi,
+        fluentToFabric: Math.round((fluentTpi / fabricTpi) * 100),
+      }
+    },
+  )
+
+  markdown(
+    [
+      '## Fluent UI to Fabric perf comparision',
+      '',
+      'Kind | Fluent UI | Fabric | % | iterations | PR Ticks',
+      '--- | ---:| ---:| ---:| ---:| ---:',
+      ..._.map(
+        results,
+        (result, key) =>
+          `${key} | ${result.fluentTpi} | ${result.fabricTpi} | ${result.fluentToFabric} | ${result.iterations} | ${result.numTicks}`,
+      ),
+    ].join('\n'),
+  )
+}
+
+export default ({ danger, markdown, warn }: DangerJS) => {
+  fluentFabricComparision(danger, markdown, warn)
 }
