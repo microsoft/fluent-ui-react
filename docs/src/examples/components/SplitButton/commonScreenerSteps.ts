85db332ef35d2425db75454f1c5e57821dfeae23
Brigette Lundeen <brlundee@microsoft.com>
2019-12-16 13:17:20 -0800

    adding screener tests

---
+++ docs/src/examples/components/SplitButton/commonScreenerSteps.ts
@@ -0,0 +1,19 @@
+import { SplitButton } from '@fluentui/react'
+
+const selectors = {
+  button: `.${SplitButton.className}`,
+  toggleButton: `.${SplitButton.slotClassNames.toggleButton}`,
+}
+
+const getScreenerSteps = (): ScreenerSteps => [
+  (builder, keys) =>
+    builder
+      .hover(selectors.button)
+      .snapshot('Hovers split button')
+      .click(selectors.toggleButton)
+      .snapshot('Clicks on toggle button should show menu')
+      .keys(selectors.toggleButton, keys.downArrow)
+      .snapshot('Navigates through menu of toggle button'),
+]
+
+export default getScreenerSteps
