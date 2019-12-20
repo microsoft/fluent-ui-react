a3268c58058abcd7daad625140a83a4cf31b094d
Silviu Alexandru Avram <silviu.avram91@gmail.com>
2019-12-20 15:56:38 +0200

    fix(Tooltip): make tooltipAsLabelBehavior as default behavior (#2195)
    * improve behavior tests
    
    * rename behavior and change default one
    
    * changelog
    
    * changelog update

--- docs/src/examples/components/Tooltip/BestPractices/TooltipBestPractices.tsx
+++ docs/src/examples/components/Tooltip/BestPractices/TooltipBestPractices.tsx
@@ -4,6 +4,7 @@ import ComponentBestPractices from 'docs/src/components/ComponentBestPractices'
 
 const doList = [
   'Use `tooltipAsLabelBehavior` if adding tooltip to icon-only button or to another visual-only element without any text, title or label.',
+  'Use `tooltipAsDescriptionBehavior` if adding tooltip to a button or to another visual-only element with text, title or label.',
 ]
 
 const DropdownBestPractices: React.FunctionComponent<{}> = () => {
