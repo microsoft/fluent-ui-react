8fee040f27449169233bf70d4ec3c3f0ea5f93a8
Levi Thomason <me@levithomason.com>
2020-01-06 13:33:08 -0800

    perf: add more perf-test comparisons (#2184)
    * perf: add more perf-test comparisons
    
    * perf: adjust perf-test iterations
    
    * perf: fix perf-test imports and required props
    
    * perf: add status to perf chart

--- packages/perf-test/stories/Checkbox.perf.tsx
+++ packages/perf-test/stories/Checkbox.perf.tsx
@@ -3,7 +3,7 @@ import { Checkbox as CheckboxFabric } from 'office-ui-fabric-react'
 import { Checkbox as CheckboxFluent } from '@fluentui/react'
 
 export default {
-  iterations: 5000,
+  iterations: 1000,
 }
 
 export const Fabric = () => <CheckboxFabric />
