ad662f4a47a3ec2a0124b1b29d3a216b0231c4ae
Marija Najdova <mnajdova@gmail.com>
2020-01-08 15:26:17 +0100

    -fixed for screener tests

--- docs/src/examples/components/TextArea/Variations/TextAreaExampleInverted.shorthand.tsx
+++ docs/src/examples/components/TextArea/Variations/TextAreaExampleInverted.shorthand.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Grid, TextArea } from '@fluentui/react'
+import { Grid, Provider, TextArea } from '@fluentui/react'
 
 const TextAreaExampleInverted = () => (
   <div>
@@ -11,14 +11,19 @@ const TextAreaExampleInverted = () => (
     >
       <TextArea inverted placeholder="Inverted color text area..." />
     </Grid>
-    <Grid
-      styles={({ theme: { siteVariables } }) => ({
-        backgroundColor: siteVariables.colorScheme.default.background,
-        padding: '20px',
-      })}
-    >
-      <TextArea placeholder="Default text area..." />
-    </Grid>
+    {/* TODO: figure this out - changed because of restricting props styles to be only object :\ */}
+    <Provider.Consumer
+      render={theme => (
+        <Grid
+          styles={{
+            backgroundColor: theme.siteVariables.colorScheme.default.background,
+            padding: '20px',
+          }}
+        >
+          <TextArea placeholder="Default text area..." />
+        </Grid>
+      )}
+    />
   </div>
 )
 
