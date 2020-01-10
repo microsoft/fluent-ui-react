5bcf3bc351bc5ed68aa97ada24b78225e227b300
Milan Turon <mituron@microsoft.com>
2020-01-10 09:11:27 +0100

    addings specification for carouselItemBehavior

--- packages/accessibility/src/behaviors/Carousel/carouselItemBehavior.ts
+++ packages/accessibility/src/behaviors/Carousel/carouselItemBehavior.ts
@@ -2,7 +2,7 @@ import { Accessibility } from '../../types'
 
 /**
  * @specification
- * Adds attribute 'role=tabpanel' to 'root' slot.
+ * Adds attribute 'role=tabpanel' to 'root' slot if 'navigation' property is true. Sets the attribute to 'group' otherwise.
  * Adds attribute 'aria-hidden=false' to 'root' slot if 'active' property is true. Sets the attribute to 'true' otherwise.
  * Adds attribute 'tabIndex=0' to 'root' slot if 'active' property is true. Sets the attribute to '-1' otherwise.
  */
