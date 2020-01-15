8d610c3aa9c4e7626bbcb0ee2eb0e5c867f0e8d6
Elizabeth Craig <elcraig@microsoft.com>
2020-01-15 02:56:31 -0800

    fix build?

--- gulpfile.ts
+++ gulpfile.ts
@@ -4,16 +4,18 @@ import * as tsPaths from 'tsconfig-paths'
 
 import config from './build/config'
 
-const { compilerOptions } = require('./build/tsconfig.docs.json')
-
-// add node_modules/.bin to the path so we can invoke .bin CLIs in tasks
-process.env.PATH =
-  process.env.PATH + path.delimiter + path.resolve(__dirname, 'node_modules', '.bin')
+// Ensure that paths to local packages are interpreted properly
+// (see https://github.com/microsoft/fluent-ui-react/pull/837)
+const { compilerOptions } = require('./tsconfig.json')
 
 tsPaths.register({
   baseUrl: config.path_base,
   paths: compilerOptions.paths,
 })
+
+// add node_modules/.bin to the path so we can invoke .bin CLIs in tasks
+process.env.PATH =
+  process.env.PATH + path.delimiter + path.resolve(__dirname, 'node_modules', '.bin')
 
 // load tasks in order of dependency usage
 require('./build/gulp/tasks/bundle')
