8c017c0879dced25591a714d81bf5de8fdf69369
Elizabeth Craig <elcraig@microsoft.com>
2020-01-16 00:56:02 -0800

    Update main to point to src, and remove most paths

--- scripts/tasks/publishPrepareTask.ts
+++ scripts/tasks/publishPrepareTask.ts
@@ -8,11 +8,23 @@ export function publishPrepareTask() {
   const allInfo = getAllPackageInfo()
 
   for (const info of Object.values(allInfo)) {
-    if (info.packageJson.main && info.packageJson.main.startsWith('src/index')) {
+    if (info.packageJson.main && info.packageJson.main.startsWith('src/')) {
       const newPackageJson = {
         ...info.packageJson,
-        main: info.packageJson.main.replace('src/', 'lib/').replace(/\.tsx?/, '.js'),
       }
+
+      if (fs.existsSync(path.join(info.packagePath, 'just.config.ts'))) {
+        newPackageJson.main = info.packageJson.main.replace('src/', 'lib/').replace(/\.tsx?/, '.js')
+      } else {
+        const entry = info.packageJson.main.replace('src/', 'dist/es/').replace(/\.tsx?/, '.js')
+        Object.assign(newPackageJson, {
+          main: entry.replace('/es/', '/commonjs/'),
+          module: entry,
+          'jsnext:main': entry,
+          types: entry.replace('.js', '.d.ts'),
+        })
+      }
+
       fs.writeFileSync(
         path.join(root, info.packagePath, 'package.json'),
         `${JSON.stringify(newPackageJson, null, 2)}\n`,
