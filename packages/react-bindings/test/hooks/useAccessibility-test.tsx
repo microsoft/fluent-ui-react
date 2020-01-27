0568136762898e9c8781c55874f327494e5b89a9
Oleksandr Fediashov <olfedias@microsoft.com>
2020-01-27 15:11:56 +0100

    improve types

--- packages/react-bindings/test/hooks/useAccessibility-test.tsx
+++ packages/react-bindings/test/hooks/useAccessibility-test.tsx
@@ -57,7 +57,7 @@ const TestComponent: React.FunctionComponent<TestComponentProps> = props => {
     },
   })
 
-  return getA11Props.unstable_withFocusZone(
+  return getA11Props.unstable_wrapWithFocusZone(
     <div {...getA11Props('root', { onKeyDown, ...rest })}>
       <img
         {...getA11Props('img', {
@@ -77,7 +77,7 @@ const FocusZoneComponent: React.FunctionComponent<FocusZoneComponentProps> = pro
   const { as: ElementType = 'div', children, rtl = false } = props
   const getA11Props = useAccessibility(focusZoneBehavior, { rtl })
 
-  return getA11Props.unstable_withFocusZone(
+  return getA11Props.unstable_wrapWithFocusZone(
     <ElementType {...getA11Props('root', {})}>{children}</ElementType>,
   )
 }
