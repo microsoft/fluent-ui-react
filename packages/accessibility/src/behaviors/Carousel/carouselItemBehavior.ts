c979062a2d30487fbb8650debeee0063fcb03c31
Milan Turon <mituron@microsoft.com>
2020-01-28 16:54:15 +0100

    adding tests for carousel item behavior

--- packages/accessibility/src/behaviors/Carousel/carouselItemBehavior.ts
+++ packages/accessibility/src/behaviors/Carousel/carouselItemBehavior.ts
@@ -2,10 +2,12 @@ import { Accessibility } from '../../types'
 import * as keyboardKey from 'keyboard-key'
 
 /**
+ * @description
+ * Adds attribute 'tabIndex=0' to 'root' slot if 'active' property and 'navigation' property is true. Sets the attribute to '-1' otherwise.
+ *
  * @specification
  * Adds attribute 'role=tabpanel' to 'root' slot if 'navigation' property is true. Sets the attribute to 'group' otherwise.
  * Adds attribute 'aria-hidden=false' to 'root' slot if 'active' property is true. Sets the attribute to 'true' otherwise.
- * Adds attribute 'tabIndex=0' to 'root' slot if 'active' property is true. Sets the attribute to '-1' otherwise.
  */
 const carouselItemBehavior: Accessibility<CarouselItemProps> = props => ({
   attributes: {
