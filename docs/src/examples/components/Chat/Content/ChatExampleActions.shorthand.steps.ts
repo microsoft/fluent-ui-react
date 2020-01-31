8b8cc94ba7b264234a3d420a2aa1476b9b9213bd
Oleksandr Fediashov <olfedias@microsoft.com>
2020-01-31 12:07:54 +0100

    fix(Chat): positiong fixes for actions

---
+++ docs/src/examples/components/Chat/Content/ChatExampleActions.shorthand.steps.ts
@@ -0,0 +1,17 @@
+import { ChatMessage } from '@fluentui/react'
+
+const selectors = {
+  message: `.${ChatMessage.className}`,
+}
+
+const config: ScreenerTestsConfig = {
+  themes: ['teams', 'teamsDark', 'teamsHighContrast'],
+  steps: [
+    builder => builder.hover(selectors.message).snapshot('Hovers the first message'),
+    builder => builder.click(selectors.message).snapshot('Focus the first message via mouse click'),
+    (builder, keys) =>
+      builder.keys('body', keys.tab).snapshot('Focuses the first message via keyboard'),
+  ],
+}
+
+export default config
