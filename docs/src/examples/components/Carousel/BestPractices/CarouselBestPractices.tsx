4c8063efaa7465244544e5cf6ab1168e83404fc5
Milan Turon <mituron@microsoft.com>
2020-02-07 12:01:53 +0100

    change of do description

--- docs/src/examples/components/Carousel/BestPractices/CarouselBestPractices.tsx
+++ docs/src/examples/components/Carousel/BestPractices/CarouselBestPractices.tsx
@@ -17,7 +17,7 @@ const doList = [
     If carousel contains `navigation`: 
     <ul>
       <li> provide label to `navigation` and to navigation item using `aria-label` attribute</li> 
-      <li> add `aria-controls` attribute to navigation item referencing by `carouselItem` id </li>
+      <li> add `aria-controls` attribute to navigation item referencing to `carouselItem` id </li>
     </ul>
   </Box>,
 ]
