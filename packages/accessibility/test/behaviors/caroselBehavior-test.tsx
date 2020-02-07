0d8fe8d86c1569570d25b69bf5197ea8c0b2b8ac
Milan Turon <mituron@microsoft.com>
2020-02-07 11:31:13 +0100

    adresing changes from review

--- packages/accessibility/test/behaviors/caroselBehavior-test.tsx
+++ packages/accessibility/test/behaviors/caroselBehavior-test.tsx
@@ -6,17 +6,17 @@ const label = "portrait collection"
 describe('carouselBehavior.ts', () => {
 
   describe('root', () => {
-    test(`set "role=region" when carousel has NO navigation`, () => {
+    test(`sets "role=region" when carousel has NO navigation`, () => {
       const expectedResult = carouselBehavior({ ariaLiveOn: false, navigation: false })
       expect(expectedResult.attributes.root.role).toEqual('region')
     })
 
-    test('set "aria-roledescription" when carousel has NO navigation', () => {
+    test('sets "aria-roledescription" when carousel has NO navigation', () => {
       const expectedResult = carouselBehavior({ ariaLiveOn: false, navigation: false, ariaRoleDescription: roleDescription })
       expect(expectedResult.attributes.root['aria-roledescription']).toEqual(roleDescription)
     })
 
-    test('set "aria-label" when carousel has NO navigation', () => {
+    test('sets "aria-label" when carousel has NO navigation', () => {
       const expectedResult = carouselBehavior({ ariaLiveOn: false, navigation: false, ariaLabel: label })
       expect(expectedResult.attributes.root['aria-label']).toEqual(label)
     })
@@ -38,12 +38,12 @@ describe('carouselBehavior.ts', () => {
   })
 
   describe('itemsContainer', () => {
-    test('set "aria-roledescription" when carousel has navigation', () => {
+    test('sets "aria-roledescription" when carousel has navigation', () => {
       const expectedResult = carouselBehavior({ ariaLiveOn: false, navigation: true, ariaRoleDescription: roleDescription })
       expect(expectedResult.attributes.itemsContainer['aria-roledescription']).toEqual(roleDescription)
     })
 
-    test('set "aria-label" when carousel has navigation', () => {
+    test('sets "aria-label" when carousel has navigation', () => {
       const expectedResult = carouselBehavior({ ariaLiveOn: false, navigation: true, ariaLabel: label })
       expect(expectedResult.attributes.itemsContainer['aria-label']).toEqual(label)
     })
--- packages/accessibility/test/behaviors/caroseltemBehavior-test.tsx
+++ packages/accessibility/test/behaviors/caroseltemBehavior-test.tsx
@@ -1,22 +1,22 @@
 import { carouselItemBehavior } from '@fluentui/accessibility'
 
 describe('carouselItemBehavior.ts', () => {
-  test('set tabIndex="0" when carousel has navigation and item is visible ', () => {
+  test('sets tabIndex="0" on root when carousel has navigation and item is visible ', () => {
     const expectedResult = carouselItemBehavior({ navigation: true, active: true })
     expect(expectedResult.attributes.root.tabIndex).toEqual(0)
   })
 
-  test('set tabIndex="-1" when carousel has navigation and item is NOT visible ', () => {
+  test('sets tabIndex="-1" on root when carousel has navigation and item is NOT visible ', () => {
     const expectedResult = carouselItemBehavior({ navigation: true, active: false })
     expect(expectedResult.attributes.root.tabIndex).toEqual(-1)
   })
 
-  test('set tabIndex="-1" when carousel has NO navigation and item is visible', () => {
+  test('sets tabIndex="-1" on root when carousel has NO navigation and item is visible', () => {
     const expectedResult = carouselItemBehavior({ navigation: false, active: true })
     expect(expectedResult.attributes.root.tabIndex).toEqual(-1)
   })
 
-  test('set tabIndex="-1" when carousel has NO navigation and item is NOT visible', () => {
+  test('sets tabIndex="-1" on root when carousel has NO navigation and item is NOT visible', () => {
     const expectedResult = carouselItemBehavior({ navigation: false, active: false })
     expect(expectedResult.attributes.root.tabIndex).toEqual(-1)
   })
