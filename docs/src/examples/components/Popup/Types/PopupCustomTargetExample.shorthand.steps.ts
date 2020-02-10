01b70b687fefcd65be4a3cdf4fd17abc1fb415c2
Oleksandr Fediashov <olfedias@microsoft.com>
2020-02-10 13:06:21 +0100

    revert change in screener test

--- docs/src/examples/components/Popup/Types/PopupCustomTargetExample.shorthand.steps.ts
+++ docs/src/examples/components/Popup/Types/PopupCustomTargetExample.shorthand.steps.ts
@@ -1,7 +1,6 @@
 import { Button } from '@fluentui/react'
 
 const config: ScreenerTestsConfig = {
-  themes: ['teams', 'teamsDark', 'teamsHighContrast'],
   steps: [builder => builder.click(`.${Button.className}`).snapshot('Shows popup')],
 }
 
