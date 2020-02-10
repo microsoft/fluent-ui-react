452936863e1f574a0670946a1e02c26846903b76
Oleksandr Fediashov <olfedias@microsoft.com>
2020-02-10 16:52:03 +0100

    chore(fela-plugins): use inline-style-expand-shorthand

--- packages/react/src/utils/felaExpandCssShorthandsPlugin.ts
+++ packages/react/src/utils/felaExpandCssShorthandsPlugin.ts
@@ -1,5 +1,6 @@
-import { expandProperty } from 'inline-style-expand-shorthand'
+import { ICSSInJSStyle } from '@fluentui/styles'
 import * as CSS from 'csstype'
+import { expandProperty } from 'inline-style-expand-shorthand'
 
 const handledCssProps: (keyof CSS.Properties)[] = [
   // 'font', Oops, is not supported by inline-style-expand-shorthand
@@ -19,24 +20,27 @@ const handledCssProps: (keyof CSS.Properties)[] = [
 ]
 
 export default () => {
-  const expandCssShorthands = (styles: Object) => {
-    return Object.keys(styles).reduce((acc, cssPropertyName) => {
-      const cssPropertyValue = styles[cssPropertyName]
+  const expandCssShorthands = (styles: ICSSInJSStyle) => {
+    return Object.keys(styles).reduce(
+      (acc: ICSSInJSStyle, cssPropertyName: keyof CSS.Properties) => {
+        const cssPropertyValue = styles[cssPropertyName]
 
-      if (typeof cssPropertyValue === 'object') {
-        return { ...acc, [cssPropertyName]: expandCssShorthands(cssPropertyValue) }
-      }
-
-      if (handledCssProps[cssPropertyName]) {
-        const expandedProps = expandProperty(cssPropertyName, `${cssPropertyValue}`)
-
-        if (expandedProps) {
-          return { ...acc, expandedProps }
+        if (typeof cssPropertyValue === 'object') {
+          return { ...acc, [cssPropertyName]: expandCssShorthands(cssPropertyValue) }
         }
-      }
 
-      return { ...acc, [cssPropertyName]: cssPropertyValue }
-    }, {})
+        if (handledCssProps.indexOf(cssPropertyName) !== -1) {
+          const expandedProps = expandProperty(cssPropertyName, `${cssPropertyValue}`)
+
+          if (expandedProps) {
+            return { ...acc, ...expandedProps }
+          }
+        }
+
+        return { ...acc, [cssPropertyName]: cssPropertyValue }
+      },
+      {},
+    )
   }
 
   return expandCssShorthands
