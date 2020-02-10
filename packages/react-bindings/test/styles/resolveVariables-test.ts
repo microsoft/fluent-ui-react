79f2ce20bfb2284643078af3573ab37a1cf0dc82
Marija Najdova <mnajdova@gmail.com>
2020-02-10 17:04:09 +0100

    prettier fixes

--- packages/react-bindings/test/styles/resolveVariables-test.ts
+++ packages/react-bindings/test/styles/resolveVariables-test.ts
@@ -1,19 +1,17 @@
-import resolveVariables  from '../../src/styles/resolveVariables'
-import {
-  ComponentVariablesPrepared,
-  emptyTheme,
-  ThemePrepared,
-} from '@fluentui/styles'
+import resolveVariables from '../../src/styles/resolveVariables'
+import { ComponentVariablesPrepared, emptyTheme, ThemePrepared } from '@fluentui/styles'
 
 const siteVariables = {
   ...emptyTheme.siteVariables,
   brand: 'blue',
 }
-const componentVariables: ComponentVariablesPrepared = (siteVariables = emptyTheme.siteVariables) => ({
+const componentVariables: ComponentVariablesPrepared = (
+  siteVariables = emptyTheme.siteVariables,
+) => ({
   backgroundColor: siteVariables.brand,
 })
 
-const createTheme: (displayName?: string) => ThemePrepared = (displayName) => ({
+const createTheme: (displayName?: string) => ThemePrepared = displayName => ({
   ...emptyTheme,
   siteVariables,
   componentVariables: {
@@ -35,7 +33,7 @@ describe('resolveVariables', () => {
     expect(variables).toMatchObject({ backgroundColor: 'blue', color: 'red' })
   })
 
-  test('allows input variabes to override theme\'s', () => {
+  test("allows input variabes to override theme's", () => {
     const propsVariables = {
       backgroundColor: 'green',
     }
@@ -44,7 +42,7 @@ describe('resolveVariables', () => {
   })
 
   test('caches resolved variables', () => {
-    const componentVariablesMock = jest.fn().mockReturnValue({ backgroundColor: 'blue'})
+    const componentVariablesMock = jest.fn().mockReturnValue({ backgroundColor: 'blue' })
     const theme = createTheme()
     theme.componentVariables['Test'] = componentVariablesMock
 
@@ -56,8 +54,8 @@ describe('resolveVariables', () => {
   })
 
   test('considers displayName while caching resolved variables', () => {
-    const componentVariablesMock1 = jest.fn().mockReturnValue({ backgroundColor: 'blue'})
-    const componentVariablesMock2 = jest.fn().mockReturnValue({ color: 'red'})
+    const componentVariablesMock1 = jest.fn().mockReturnValue({ backgroundColor: 'blue' })
+    const componentVariablesMock2 = jest.fn().mockReturnValue({ color: 'red' })
     const theme = createTheme()
     theme.componentVariables['Test1'] = componentVariablesMock1
     theme.componentVariables['Test2'] = componentVariablesMock2
@@ -65,14 +63,14 @@ describe('resolveVariables', () => {
     const variables = resolveVariables('Test1', theme, {}, true)
     const secondVariables = resolveVariables('Test2', theme, {}, true)
 
-    expect(variables).toMatchObject({ backgroundColor: 'blue'})
-    expect(secondVariables).toMatchObject({ color: 'red'})
+    expect(variables).toMatchObject({ backgroundColor: 'blue' })
+    expect(secondVariables).toMatchObject({ color: 'red' })
     expect(componentVariablesMock1).toHaveBeenCalledTimes(1)
     expect(componentVariablesMock2).toHaveBeenCalledTimes(1)
   })
 
   test('considers theme while caching resolved variables', () => {
-    const componentVariablesMock = jest.fn().mockReturnValue({ backgroundColor: 'blue'})
+    const componentVariablesMock = jest.fn().mockReturnValue({ backgroundColor: 'blue' })
     const theme = createTheme('Test')
     const secondTheme = createTheme('Test')
     theme.componentVariables['Test'] = componentVariablesMock
@@ -81,8 +79,8 @@ describe('resolveVariables', () => {
     const variables = resolveVariables('Test', theme, {}, true)
     const secondVariables = resolveVariables('Test', secondTheme, {}, true)
 
-    expect(variables).toMatchObject({ backgroundColor: 'blue'})
-    expect(secondVariables).toMatchObject({ backgroundColor: 'blue'})
+    expect(variables).toMatchObject({ backgroundColor: 'blue' })
+    expect(secondVariables).toMatchObject({ backgroundColor: 'blue' })
     expect(componentVariablesMock).toHaveBeenCalledTimes(2)
   })
 })
