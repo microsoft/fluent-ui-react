4add0e68d5d1f5cd7f94c733a1da080a6dfea6ef
Silviu Avram <silviu.avram91@gmail.com>
2020-02-12 10:12:21 +0100

    add example for e2e test

---
+++ e2e/tests/toolbarMenuOverflow-example.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import _ from 'lodash'
+import { Toolbar, ToolbarItem } from '@fluentui/react'
+
+export const selectors = {
+  toolbarItem: ToolbarItem.className,
+  toolbar: Toolbar.className,
+}
+
+const ToolbarExampleOverflow = () => {
+  const icons = ['bold', 'italic', 'underline']
+
+  const itemData = _.times(40, i => ({
+    key: `b${i}`,
+    content: `${icons[i % icons.length]} #${i}`,
+    icon: icons[i % icons.length],
+    title: `${icons[i % icons.length]} #${i}`,
+    id: `item${i}`,
+  }))
+
+  const toolbarItems = itemData.map(item => {
+    return { ...item, content: undefined }
+  })
+  const [overflowOpen, setOverflowOpen] = React.useState(false)
+  return (
+    <Toolbar
+      aria-label="Toolbar overflow menu"
+      items={toolbarItems}
+      overflow
+      overflowOpen={overflowOpen}
+      overflowItem={{
+        title: 'More',
+      }}
+      onOverflowOpenChange={(e, { overflowOpen }) => {
+        setOverflowOpen(overflowOpen)
+      }}
+      getOverflowItems={startIndex => itemData.slice(startIndex)}
+    />
+  )
+}
+
+export default ToolbarExampleOverflow
