550f9e494964dee2fdc4d64b3951dd5c337b39b7
Marija Najdova <mnajdova@gmail.com>
2020-02-12 15:01:22 +0100

    -added tests

--- packages/react/test/specs/components/Animation/Animation-test.tsx
+++ packages/react/test/specs/components/Animation/Animation-test.tsx
@@ -2,11 +2,22 @@ import * as React from 'react'
 import { isConformant } from 'test/specs/commonTests'
 
 import Animation from 'src/components/Animation/Animation'
+import { consoleUtil, mountWithProvider } from 'test/utils'
 
 describe('Animation', () => {
   isConformant(Animation, {
     hasAccessibilityProp: false,
     requiredProps: { children: <div /> },
     handlesAsProp: false,
+  })
+
+  test('does not throw if children is not passed', () => {
+    consoleUtil.disableOnce()
+    // @ts-ignore
+    expect(() => mountWithProvider(<Animation />)).not.toThrowError()
+
+    consoleUtil.disableOnce()
+    // @ts-ignore
+    expect(() => mountWithProvider(<Animation children={() => undefined} />)).not.toThrowError()
   })
 })
