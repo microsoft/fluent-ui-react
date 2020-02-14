0241d08b259bcf16d6bc45fb20f7df8bc86371f0
Marija Najdova <mnajdova@gmail.com>
2020-02-14 14:19:49 +0100

    remove logs

--- packages/react-bindings/src/styles/calculateAnimationTimeout.ts
+++ packages/react-bindings/src/styles/calculateAnimationTimeout.ts
@@ -1,13 +1,7 @@
 import convertCssTimeToNumber from './convertCssTimeToNumber'
 
 const calculateAnimationTimeout = (duration: string, delay: string): number => {
-  console.log("duration " + duration)
-  console.log("delay " + delay)
-  const result = convertCssTimeToNumber(duration) + convertCssTimeToNumber(delay)
-  console.log("duration in num " + convertCssTimeToNumber(duration))
-  console.log("delay in num " + convertCssTimeToNumber(delay))
-  console.log("result "+ result)
-  return result
+  return convertCssTimeToNumber(duration) + convertCssTimeToNumber(delay)
 }
 
 export default calculateAnimationTimeout
