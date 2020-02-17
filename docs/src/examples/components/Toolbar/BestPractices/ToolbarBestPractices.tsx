bb974672508a4250b19d3fe9a1135b48d56ab00b
Milan Turon <mituron@microsoft.com>
2020-02-17 16:25:43 +0100

    removing focus handling after onClick, extend best practice about this information

--- docs/src/examples/components/Toolbar/BestPractices/ToolbarBestPractices.tsx
+++ docs/src/examples/components/Toolbar/BestPractices/ToolbarBestPractices.tsx
@@ -9,6 +9,7 @@ const doList = [
     `aria-labelledby` props). Refer to{' '}
     {link('toolbar(role)', 'https://www.w3.org/WAI/PF/aria/roles#toolbar')} for details.
   </Text>,
+  'If `Toolbar` contains menu, then focus will need be handled after `onClick` is executed on `menuItem`.'
 ]
 
 const ToolbarBestPractices: React.FunctionComponent<{}> = () => {
