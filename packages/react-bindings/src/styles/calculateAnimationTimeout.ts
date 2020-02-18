6344e391c2cedb55033c67c4558e1590d65918fc
Marija Najdova <mnajdova@gmail.com>
2020-02-18 10:24:48 +0100

    -refactored

--- packages/react-bindings/src/styles/calculateAnimationTimeout.ts
+++ packages/react-bindings/src/styles/calculateAnimationTimeout.ts
@@ -1,9 +1,9 @@
 import convertCssTimeToNumber from './convertCssTimeToNumber'
 import * as _ from 'lodash'
 
-const calculateAnimationTimeout = (duration: string, delay: string | undefined): number => {
+const calculateAnimationTimeout = (duration: string, delay?: string): number => {
   if (_.isNil(duration) || duration === 'inherit' || duration === 'initial') return 0
-  if (!_.isNil(delay) && (delay === 'inherit' || delay === 'initial')) return 0
+  if (delay === 'inherit' || delay === 'initial') return 0
   return convertCssTimeToNumber(duration) + convertCssTimeToNumber(delay || '')
 }
 
