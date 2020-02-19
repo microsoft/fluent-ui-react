e5dce0f836ed5175d5988406703f9f63c2ae5b31
Oleksandr Fediashov <olfedias@microsoft.com>
2020-02-19 17:22:34 +0100

    cleanup & remove comments

--- packages/react/src/types.ts
+++ packages/react/src/types.ts
@@ -21,7 +21,6 @@ export type DebounceResultFn<T> = T & {
 
 // Source: https://github.com/emotion-js/emotion/blob/master/packages/styled-base/types/helper.d.ts
 export type PropsOfElement<
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   E extends keyof JSX.IntrinsicElements | React.JSXElementConstructor<any>
 > = JSX.LibraryManagedAttributes<E, React.ComponentPropsWithRef<E>>
 
