6d71ac45747887b3a675f37b50298c50b8350125
pompomon <pompomon@gmail.com>
2020-02-21 10:02:04 +0100

    Adding a11y attributes

--- docs/src/prototypes/table/InteractiveTable.tsx
+++ docs/src/prototypes/table/InteractiveTable.tsx
@@ -1,10 +1,18 @@
-import { Icon, Popup, Table, tableHeaderCellBehavior } from '@fluentui/react'
+import {
+  Icon,
+  Popup,
+  Table,
+  gridNestedBehavior,
+  gridHeaderCellBehavior,
+  gridRowBehavior,
+  gridCellBehavior,
+} from '@fluentui/react'
 import * as React from 'react'
 
 const InteractiveTable = () => {
   return (
-    <Table aria-label="table">
-      <Table.Row header>
+    <Table aria-label="table" accessibility={gridNestedBehavior}>
+      <Table.Row header accessibility={gridRowBehavior}>
         <Table.Cell
           content={
             <>
@@ -17,29 +25,33 @@ const InteractiveTable = () => {
             </>
           }
           key="id"
-          accessibility={tableHeaderCellBehavior}
+          accessibility={gridHeaderCellBehavior}
         />
-        <Table.Cell content="Name" key="name" accessibility={tableHeaderCellBehavior} />
-        <Table.Cell content="Picture" key="pic" accessibility={tableHeaderCellBehavior} />
-        <Table.Cell content="Age" key="age" accessibility={tableHeaderCellBehavior} />
+        <Table.Cell content="Name" key="name" accessibility={gridHeaderCellBehavior} />
+        <Table.Cell content="Picture" key="pic" accessibility={gridHeaderCellBehavior} />
+        <Table.Cell content="Age" key="age" accessibility={gridHeaderCellBehavior} />
       </Table.Row>
-      <Table.Row key="1">
-        <Table.Cell content="1" key="1-1" />
-        <Table.Cell content="Roman van von der Longername" key="1-2" />
-        <Table.Cell content="None" key="1-3" />
-        <Table.Cell content="30 years" key="1-4" />
+      <Table.Row key="1" accessibility={gridRowBehavior}>
+        <Table.Cell content="1" key="1-1" accessibility={gridCellBehavior} />
+        <Table.Cell
+          content="Roman van von der Longername"
+          key="1-2"
+          accessibility={gridCellBehavior}
+        />
+        <Table.Cell content="None" key="1-3" accessibility={gridCellBehavior} />
+        <Table.Cell content="30 years" key="1-4" accessibility={gridCellBehavior} />
       </Table.Row>
-      <Table.Row key="2">
-        <Table.Cell content="2" key="1-1" />
-        <Table.Cell content="Alex" key="1-2" />
-        <Table.Cell content="None" key="1-3" />
-        <Table.Cell content="1 year" key="1-4" />
+      <Table.Row key="2" accessibility={gridRowBehavior}>
+        <Table.Cell content="2" key="1-1" accessibility={gridCellBehavior} />
+        <Table.Cell content="Alex" key="1-2" accessibility={gridCellBehavior} />
+        <Table.Cell content="None" key="1-3" accessibility={gridCellBehavior} />
+        <Table.Cell content="1 year" key="1-4" accessibility={gridCellBehavior} />
       </Table.Row>
-      <Table.Row key="3">
-        <Table.Cell content="3" key="1-1" />
-        <Table.Cell content="Ali" key="1-2" />
-        <Table.Cell content="None" key="1-3" />
-        <Table.Cell content="30000000000000 years" key="1-4" />
+      <Table.Row key="3" accessibility={gridRowBehavior}>
+        <Table.Cell content="3" key="1-1" accessibility={gridCellBehavior} />
+        <Table.Cell content="Ali" key="1-2" accessibility={gridCellBehavior} />
+        <Table.Cell content="None" key="1-3" accessibility={gridCellBehavior} />
+        <Table.Cell content="30000000000000 years" key="1-4" accessibility={gridCellBehavior} />
       </Table.Row>
     </Table>
   )
