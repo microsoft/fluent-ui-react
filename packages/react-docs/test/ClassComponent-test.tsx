cfafbe15d50e03464bded17f6deecbee99a55dfd
Levi Thomason <me@levithomason.com>
2020-02-25 09:19:38 -0800

    test: update test stub

--- packages/react-docs/test/ClassComponent-test.tsx
+++ packages/react-docs/test/ClassComponent-test.tsx
@@ -1,11 +1,19 @@
 import { getComponentInfo } from '@fluentui/react-docs'
 
-describe('getComp', () => {
-  it('creates a Provider component', () => {
-    const componentInfo = getComponentInfo('./../fixtures/ClassComponentPropsInline')
+describe('getComponentInfo', () => {
+  it('throws if path does not exist', () => {
+    throw new Error('TODO')
+  })
+
+  it('throws if path is not a TSX file', () => {
+    throw new Error('TODO')
+  })
+
+  it('recognizes class components with props interfaces', () => {
+    const componentInfo = getComponentInfo('./fixtures/ClassComponentPropsInterface')
 
     expect(componentInfo).toEqual({
-      displayName: 'ClassComponentPropsInline',
+      displayName: 'ClassComponentPropsInterface',
     })
   })
 })
